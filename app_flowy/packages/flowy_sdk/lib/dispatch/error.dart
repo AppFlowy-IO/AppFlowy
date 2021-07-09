5d94c63377fcbf402d81bc6daa634d8c1cd7baef
appflowy <annie@appflowy.io>
2021-07-09 08:44:52 +0800

    fix flutter analyze issue

--- app_flowy/packages/flowy_sdk/lib/dispatch/error.dart
+++ app_flowy/packages/flowy_sdk/lib/dispatch/error.dart
@@ -1,7 +1,40 @@
 import 'package:flowy_logger/flowy_logger.dart';
-import 'package:flowy_sdk/dispatch/flowy_error.dart';
 import 'package:flowy_sdk/protobuf/ffi_request.pb.dart';
 import 'package:flowy_sdk/protobuf/ffi_response.pb.dart';
+
+class FlowyError {
+  late FFIStatusCode _statusCode;
+  late String _error;
+
+  FFIStatusCode get statusCode {
+    return _statusCode;
+  }
+
+  String get error {
+    return _error;
+  }
+
+  bool get has_error {
+    return _statusCode != FFIStatusCode.Ok;
+  }
+
+  String toString() {
+    return "$_statusCode: $_error";
+  }
+
+  FlowyError({required FFIStatusCode statusCode, required String error}) {
+    _statusCode = statusCode;
+    _error = error;
+  }
+
+  factory FlowyError.from(FFIResponse resp) {
+    return FlowyError(statusCode: resp.code, error: resp.error);
+  }
+
+  factory FlowyError.fromError(String error) {
+    return FlowyError(statusCode: FFIStatusCode.Err, error: error);
+  }
+}
 
 class StackTraceError {
   Object error;
