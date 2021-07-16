adeee5abe02cb0e76f9f761a910911c835dfcea0
Jaylen Bian <minecoder@163.com>
2021-07-16 09:48:05 +0800

    [infra_ui] update example lib pubspec

--- app_flowy/packages/flowy_infra_ui/example/test/widget_test.dart
+++ app_flowy/packages/flowy_infra_ui/example/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:example/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const ExampleApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
