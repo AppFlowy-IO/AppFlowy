b257ca11d0a54586607465545afb38eda92efec6
Jaylen Bian <jabian@linkedin.com>
2021-07-17 10:05:18 +0800

    [infra_ui][keyboard] Change ios plugin to swift and impl ios-side impl

--- app_flowy/packages/flowy_infra_ui/ios/Classes/SwiftFlowyInfraUiPlugin.swift
+++ app_flowy/packages/flowy_infra_ui/ios/Classes/SwiftFlowyInfraUiPlugin.swift
@@ -2,13 +2,33 @@ import Flutter
 import UIKit
 
 public class SwiftFlowyInfraUiPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "flowy_infra_ui", binaryMessenger: registrar.messenger())
-    let instance = SwiftFlowyInfraUiPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
-  }
+    enum Constant {
+        static let infraUIMethodChannelName = "flowy_infra_ui_method"
+        static let infraUIKeyboardEventChannelName = "flowy_infra_ui_event/keyboard"
+    }
+
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let instance = SwiftFlowyInfraUiPlugin()
+
+        let methodChannel = FlutterMethodChannel(
+            name: Constant.infraUIMethodChannelName,
+            binaryMessenger: registrar.messenger())
+        registrar.addMethodCallDelegate(instance, channel: methodChannel)
+
+        let keyboardEventChannel = FlutterEventChannel(
+            name: Constant.infraUIKeyboardEventChannelName,
+            binaryMessenger: registrar.messenger())
+        keyboardEventChannel.setStreamHandler(KeyboardEventHandler())
+    }
+
+    // MARK: - Method Channel
+
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        default:
+            assertionFailure("Unsupported method \(call.method)")
+        }
+    }
+
 }
