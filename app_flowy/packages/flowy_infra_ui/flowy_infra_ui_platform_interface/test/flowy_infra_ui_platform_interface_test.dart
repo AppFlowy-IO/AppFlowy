777b3a68ede63a1f6cccd6545a4704af5db5388f
Jaylen Bian <jabian@linkedin.com>
2021-07-21 11:23:41 +0800

    [infra_ui] Fix warning issues raised by analyzer

--- app_flowy/packages/flowy_infra_ui/flowy_infra_ui_platform_interface/test/flowy_infra_ui_platform_interface_test.dart
+++ app_flowy/packages/flowy_infra_ui/flowy_infra_ui_platform_interface/test/flowy_infra_ui_platform_interface_test.dart
@@ -1,5 +1,1 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flowy_infra_ui_platform_interface/flowy_infra_ui_platform_interface.dart';
-
 void main() {}
--- app_flowy/packages/flowy_infra_ui/test/flowy_infra_ui_test.dart
+++ app_flowy/packages/flowy_infra_ui/test/flowy_infra_ui_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flowy_infra_ui/flowy_infra_ui.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('flowy_infra_ui');
@@ -15,9 +14,5 @@ void main() {
 
   tearDown(() {
     channel.setMockMethodCallHandler(null);
-  });
-
-  test('getPlatformVersion', () async {
-    expect(await FlowyInfraUi.platformVersion, '42');
   });
 }
