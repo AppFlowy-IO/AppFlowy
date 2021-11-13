dcbd0022d96122257a8a4d75ee7f287720827f44
appflowy <annie@appflowy.io>
2021-11-13 12:36:39 +0800

    [flutter]: update avatar config

--- app_flowy/macos/Flutter/GeneratedPluginRegistrant.swift
+++ app_flowy/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,7 +10,6 @@ import flowy_infra_ui
 import flowy_sdk
 import package_info_plus_macos
 import path_provider_macos
-import sqflite
 import url_launcher_macos
 import window_size
 
@@ -20,7 +19,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlowySdkPlugin.register(with: registry.registrar(forPlugin: "FlowySdkPlugin"))
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   WindowSizePlugin.register(with: registry.registrar(forPlugin: "WindowSizePlugin"))
 }
