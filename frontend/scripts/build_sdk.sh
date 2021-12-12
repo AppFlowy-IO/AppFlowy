94dc76d1a9ad4f524438b3eb9d67e3014f2b14da
Mike <mike@wallacehub.com>
2021-12-11 23:38:49 -0500

    Fix build_sdk.sh Fixed a typo (aarch64 -> x86) Since I was here : Capitalized the OS names

--- frontend/scripts/build_sdk.sh
+++ frontend/scripts/build_sdk.sh
@@ -16,15 +16,14 @@ rustup show
 
 # TODO: Automatically exec the script base on the current system
 
-# for macos
+# for macOS
 cargo make --profile development-mac flowy-sdk-dev
 
-# for window
+# for Windows
 #cargo make --profile development-windows flowy-sdk-dev
 
-
-# for linux aarch64
+# for Linux x86
 #cargo make --profile development-linux-x86 flowy-sdk-dev
 
-# for linux aarch64
+# for Linux aarch64
 #cargo make --profile development-linux-aarch64 flowy-sdk-dev
