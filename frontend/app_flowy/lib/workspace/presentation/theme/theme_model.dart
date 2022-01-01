4711dc69f67f8e1247c9f9aeab9a536078573e36
MikeWallaceDev <mike@wallacehub.com>
2022-01-01 13:15:45 -0500

    Just changed the order to put the variable above the getter

--- frontend/app_flowy/lib/workspace/presentation/theme/theme_model.dart
+++ frontend/app_flowy/lib/workspace/presentation/theme/theme_model.dart
@@ -2,8 +2,9 @@ import 'package:flowy_infra/theme.dart';
 import 'package:flutter/material.dart';
 
 class ThemeModel extends ChangeNotifier {
-  ThemeType get theme => _theme;
   ThemeType _theme = ThemeType.light;
+
+  ThemeType get theme => _theme;
 
   set theme(ThemeType value) {
     _theme = value;
