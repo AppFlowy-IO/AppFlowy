57e2289cca501326838f5f6472ac24ad93fb39f3
MikeWallaceDev <mike@wallacehub.com>
2022-01-02 12:17:36 -0500

    Added a check so that notifyListeners() isn't called for nothing

--- frontend/app_flowy/lib/workspace/presentation/theme/theme_model.dart
+++ frontend/app_flowy/lib/workspace/presentation/theme/theme_model.dart
@@ -1,17 +1,25 @@
+import 'package:equatable/equatable.dart';
 import 'package:flowy_infra/theme.dart';
 import 'package:flutter/material.dart';
 
-class ThemeModel extends ChangeNotifier {
+class ThemeModel extends ChangeNotifier with EquatableMixin {
   ThemeType _theme = ThemeType.light;
+
+  @override
+  List<Object> get props {
+    return [_theme];
+  }
 
   ThemeType get theme => _theme;
 
   set theme(ThemeType value) {
-    _theme = value;
-    notifyListeners();
+    if (_theme != value) {
+      _theme = value;
+      notifyListeners();
+    }
   }
 
   void swapTheme() {
-    theme = (theme == ThemeType.light ? ThemeType.dark : ThemeType.light);
+    theme = (theme == ThemeType.light ? ThemeType.dark : ThemeType.light);    
   }
 }
