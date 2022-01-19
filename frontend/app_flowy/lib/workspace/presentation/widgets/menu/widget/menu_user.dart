03e58a90d2101095e6420385163d85a4307c8832
Harinandan <harinandan.alukkal@gmail.com>
2022-01-19 10:27:35 +0530

    Cleanup imports

--- frontend/app_flowy/lib/workspace/presentation/widgets/menu/widget/menu_user.dart
+++ frontend/app_flowy/lib/workspace/presentation/widgets/menu/widget/menu_user.dart
@@ -2,15 +2,11 @@ import 'package:app_flowy/startup/startup.dart';
 import 'package:app_flowy/workspace/application/menu/menu_user_bloc.dart';
 import 'package:app_flowy/workspace/presentation/settings/settings_dialog.dart';
 import 'package:flowy_infra/size.dart';
-import 'package:flowy_infra/theme.dart';
+import 'package:flowy_infra_ui/style_widget/text.dart';
 import 'package:flowy_infra_ui/widget/spacing.dart';
 import 'package:flowy_sdk/protobuf/flowy-user-data-model/protobuf.dart' show UserProfile;
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flowy_infra_ui/style_widget/text.dart';
-import 'package:easy_localization/easy_localization.dart';
-import 'package:app_flowy/workspace/presentation/theme/theme_model.dart';
-import 'package:app_flowy/generated/locale_keys.g.dart';
 
 class MenuUser extends StatelessWidget {
   final UserProfile user;
