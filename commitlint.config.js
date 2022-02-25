87cc5422751949bc858d135910b6c8f1079eaac5
MikeWallaceDev <mike@wallacehub.com>
2022-02-24 20:22:02 -0500

    refactor: change order of rules
    rules are now in order of a commit message and separated into sections.

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,20 +1,24 @@
 // module.exports = {extends: ['@commitlint/config-conventional']}
 module.exports = {
     rules: {
-        'type-enum': [2, 'always', ['chore', 'ci', 'docs', 'feat', 'fix', 'refactor', 'style', 'test']],
-        'body-leading-blank': [1, 'always'],
-        'body-max-line-length': [2, 'always', 100],
-        'footer-leading-blank': [1, 'always'],
-        'footer-max-line-length': [2, 'always', 100],
         'header-max-length': [2, 'always', 100],
+
+        'type-enum': [2, 'always', ['chore', 'ci', 'docs', 'feat', 'fix', 'refactor', 'style', 'test']],
+        'type-empty': [2, 'never'],
+        'type-case': [2, 'always', 'lower-case'],
+
         'subject-case': [
             2,
             'never',
             ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
         ],
         'subject-empty': [2, 'never'],
-        'type-empty': [2, 'never'],
-        'type-case': [2, 'always', 'lower-case'],
-        'body-case': [2, 'never', []]
+
+        'body-leading-blank': [1, 'always'],
+        'body-max-line-length': [2, 'always', 100],
+        'body-case': [2, 'never', []],
+
+        'footer-leading-blank': [1, 'always'],
+        'footer-max-line-length': [2, 'always', 100]
     },
 };
