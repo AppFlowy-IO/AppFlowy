147e948b350e6f89b7bd7831618f1be1184b7c4f
appflowy <annie@appflowy.io>
2022-04-02 23:54:48 +0800

    chore: add grid setting

---
+++ frontend/app_flowy/lib/workspace/presentation/plugins/grid/src/widgets/header/grid_setting.dart
@@ -0,0 +1,35 @@
+enum GridSetting {
+  filter,
+  sortBy,
+  properties,
+}
+
+extension _GridSettingExtension on GridSetting {
+  String iconName() {
+    switch (this) {
+      case GridSetting.filter:
+        // TODO: Handle this case.
+        break;
+      case GridSetting.sortBy:
+        // TODO: Handle this case.
+        break;
+      case GridSetting.properties:
+        // TODO: Handle this case.
+        break;
+    }
+  }
+
+  String title() {
+    switch (this) {
+      case GridSetting.filter:
+        // TODO: Handle this case.
+        break;
+      case GridSetting.sortBy:
+        // TODO: Handle this case.
+        break;
+      case GridSetting.properties:
+        // TODO: Handle this case.
+        break;
+    }
+  }
+}
