16a167c19c63d8234b45f60f9df4624f081fa2ef
appflowy <annie@appflowy.io>
2022-04-11 21:14:45 +0800

    chore: update default grid

--- frontend/rust-lib/flowy-grid/src/util.rs
+++ frontend/rust-lib/flowy-grid/src/util.rs
@@ -3,37 +3,25 @@ use flowy_grid_data_model::entities::{BuildGridContext, FieldType};
 use flowy_sync::client_grid::GridBuilder;
 
 pub fn make_default_grid() -> BuildGridContext {
+    // text
     let text_field = FieldBuilder::new(RichTextTypeOptionBuilder::default())
         .name("Name")
         .visibility(true)
         .build();
 
     // single select
-    let single_select = SingleSelectTypeOptionBuilder::default()
-        .option(SelectOption::new("Done"))
-        .option(SelectOption::new("Unknown"))
-        .option(SelectOption::new("Progress"));
-    let single_select_field = FieldBuilder::new(single_select).name("Status").visibility(true).build();
+    let single_select = SingleSelectTypeOptionBuilder::default();
+    let single_select_field = FieldBuilder::new(single_select).name("Type").visibility(true).build();
 
-    //multiple select
-    let multi_select = MultiSelectTypeOptionBuilder::default()
-        .option(SelectOption::new("A"))
-        .option(SelectOption::new("B"))
-        .option(SelectOption::new("C"));
-    let multi_select_field = FieldBuilder::new(multi_select)
-        .name("Alphabet")
-        .visibility(true)
-        .build();
-
+    // checkbox
     let checkbox_field = FieldBuilder::from_field_type(&FieldType::Checkbox)
-        .name("isReady")
+        .name("Done")
         .visibility(true)
         .build();
 
     GridBuilder::default()
         .add_field(text_field)
         .add_field(single_select_field)
-        .add_field(multi_select_field)
         .add_field(checkbox_field)
         .add_empty_row()
         .add_empty_row()
