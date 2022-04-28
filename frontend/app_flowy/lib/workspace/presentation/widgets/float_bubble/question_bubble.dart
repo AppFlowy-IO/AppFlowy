6e52b470a7cbbe26b2c77eddfef4cb425f25e814
appflowy <annie@appflowy.io>
2022-04-28 20:59:35 +0800

    chore: fix wanrings

--- frontend/app_flowy/lib/workspace/presentation/widgets/float_bubble/question_bubble.dart
+++ frontend/app_flowy/lib/workspace/presentation/widgets/float_bubble/question_bubble.dart
@@ -7,7 +7,6 @@ import 'package:flowy_infra_ui/flowy_infra_ui.dart';
 import 'package:flowy_infra_ui/style_widget/button.dart';
 import 'package:flowy_infra_ui/style_widget/text.dart';
 import 'package:flowy_infra_ui/widget/spacing.dart';
-import 'package:flowy_sdk/log.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
