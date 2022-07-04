e85606ac21175fcda9808c263fdfb3860330a629
appflowy <annie@appflowy.io>
2022-07-04 15:53:21 +0800

    chore: fix unit test

--- shared-lib/flowy-folder-data-model/src/revision/trash_rev.rs
+++ shared-lib/flowy-folder-data-model/src/revision/trash_rev.rs
@@ -33,7 +33,35 @@ impl<'de> serde::Deserialize<'de> for TrashTypeRevision {
             type Value = TrashTypeRevision;
 
             fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-                formatter.write_str("struct Insert")
+                formatter.write_str("u8")
+            }
+
+            fn visit_i8<E>(self, v: i8) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                self.visit_u8(v as u8)
+            }
+
+            fn visit_i16<E>(self, v: i16) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                self.visit_u8(v as u8)
+            }
+
+            fn visit_i32<E>(self, v: i32) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                self.visit_u8(v as u8)
+            }
+
+            fn visit_i64<E>(self, v: i64) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                self.visit_u8(v as u8)
             }
 
             fn visit_u8<E>(self, v: u8) -> Result<Self::Value, E>
@@ -48,6 +76,27 @@ impl<'de> serde::Deserialize<'de> for TrashTypeRevision {
                 };
 
                 Ok(ty)
+            }
+
+            fn visit_u16<E>(self, v: u16) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                self.visit_u8(v as u8)
+            }
+
+            fn visit_u32<E>(self, v: u32) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                self.visit_u8(v as u8)
+            }
+
+            fn visit_u64<E>(self, v: u64) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                self.visit_u8(v as u8)
             }
 
             fn visit_str<E>(self, s: &str) -> Result<Self::Value, E>
