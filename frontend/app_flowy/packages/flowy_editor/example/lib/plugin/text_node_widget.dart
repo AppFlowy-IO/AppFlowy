4b7c997083912c519fe6e4b941df6f11f720164b
Vincent Chan <okcdz@diverse.space>
2022-07-18 15:49:39 +0800

    feat: text delta to text span

--- frontend/app_flowy/packages/flowy_editor/example/lib/plugin/text_node_widget.dart
+++ frontend/app_flowy/packages/flowy_editor/example/lib/plugin/text_node_widget.dart
@@ -1,3 +1,5 @@
+import 'package:flowy_editor/document/text_delta.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flowy_editor/flowy_editor.dart';
 import 'package:flutter/services.dart';
@@ -10,7 +12,7 @@ class TextNodeBuilder extends NodeWidgetBuilder {
     required super.editorState,
   }) : super.create() {
     nodeValidator = ((node) {
-      return node.type == 'text' && node.attributes.containsKey('content');
+      return node.type == 'text';
     });
   }
 
@@ -28,6 +30,55 @@ extension on Attributes {
       color: this['color'] != null ? Colors.red : Colors.black,
       fontSize: this['font-size'] != null ? 30 : 15,
     );
+  }
+}
+
+TextSpan _textInsertToTextSpan(TextInsert textInsert) {
+  FontWeight? fontWeight;
+  FontStyle? fontStyle;
+  TextDecoration? decoration;
+  GestureRecognizer? gestureRecognizer;
+  Color? color;
+  final attributes = textInsert.attributes;
+  if (attributes?['bold'] == true) {
+    fontWeight = FontWeight.bold;
+  }
+  if (attributes?['italic'] == true) {
+    fontStyle = FontStyle.italic;
+  }
+  if (attributes?["underline"] == true) {
+    decoration = TextDecoration.underline;
+  }
+  if (attributes?["href"] is String) {
+    color = const Color.fromARGB(255, 55, 120, 245);
+    decoration = TextDecoration.underline;
+    gestureRecognizer = TapGestureRecognizer()
+      ..onTap = () {
+        // TODO: open the link
+      };
+  }
+  return TextSpan(
+      text: textInsert.content,
+      style: TextStyle(
+        fontWeight: fontWeight,
+        fontStyle: fontStyle,
+        decoration: decoration,
+        color: color,
+      ),
+      recognizer: gestureRecognizer);
+}
+
+extension on TextNode {
+  List<TextSpan> toTextSpans() {
+    final result = <TextSpan>[];
+
+    for (final op in delta.operations) {
+      if (op is TextInsert) {
+        result.add(_textInsertToTextSpan(op));
+      }
+    }
+
+    return result;
   }
 }
 
@@ -49,8 +100,7 @@ class __TextNodeWidgetState extends State<_TextNodeWidget>
     implements TextInputClient {
   Node get node => widget.node;
   EditorState get editorState => widget.editorState;
-  String get content => node.attributes['content'] as String;
-  TextEditingValue get textEditingValue => TextEditingValue(text: content);
+  TextEditingValue get textEditingValue => const TextEditingValue();
 
   TextInputConnection? _textInputConnection;
 
@@ -60,13 +110,13 @@ class __TextNodeWidgetState extends State<_TextNodeWidget>
       value: node,
       builder: (_, __) => Consumer<Node>(
         builder: ((context, value, child) {
+          final textNode = value as TextNode;
           return Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SelectableText.rich(
                 TextSpan(
-                  text: content,
-                  style: node.attributes.toTextStyle(),
+                  children: textNode.toTextSpans(),
                 ),
                 onTap: () {
                   _textInputConnection?.close();
