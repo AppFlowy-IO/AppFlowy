9ceced464817b2df1a1df77c67cd52857001da68
Vincent Chan <okcdz@diverse.space>
2022-08-01 16:14:56 +0800

    feat: parse html

---
+++ frontend/app_flowy/packages/flowy_editor/lib/infra/html_converter.dart
@@ -0,0 +1,29 @@
+import 'dart:collection';
+
+import 'package:flowy_editor/document/node.dart';
+import 'package:flowy_editor/document/text_delta.dart';
+import 'package:html/parser.dart' show parse;
+import 'package:html/dom.dart' as html;
+
+class HTMLConverter {
+  final html.Document _document;
+
+  HTMLConverter(String htmlString) : _document = parse(htmlString);
+
+  List<Node> toNodes() {
+    final result = <Node>[];
+    final delta = Delta();
+
+    final bodyChildren = _document.body?.children ?? [];
+    for (final child in bodyChildren) {
+      delta.insert(child.text);
+    }
+
+    if (delta.operations.isNotEmpty) {
+      result.add(TextNode(
+          type: "text", children: LinkedList(), attributes: {}, delta: delta));
+    }
+
+    return result;
+  }
+}
--- frontend/app_flowy/packages/flowy_editor/lib/service/internal_key_event_handlers/copy_paste_handler.dart
+++ frontend/app_flowy/packages/flowy_editor/lib/service/internal_key_event_handlers/copy_paste_handler.dart
@@ -1,18 +1,53 @@
+import 'package:flowy_editor/flowy_editor.dart';
 import 'package:flowy_editor/service/keyboard_service.dart';
+import 'package:flowy_editor/infra/html_converter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:rich_clipboard/rich_clipboard.dart';
+
+_handleCopy() async {
+  debugPrint('copy');
+}
+
+_pasteHTML(EditorState editorState, String html) {
+  final converter = HTMLConverter(html);
+  final nodes = converter.toNodes();
+  final selection = editorState.cursorSelection;
+  if (selection == null) {
+    return;
+  }
+
+  final tb = TransactionBuilder(editorState);
+  for (final node in nodes) {
+    tb.insertNode(selection.end.path, node);
+  }
+  tb.commit();
+}
+
+_handlePaste(EditorState editorState) async {
+  final data = await RichClipboard.getData();
+  if (data.html != null) {
+    _pasteHTML(editorState, data.html!);
+    return;
+  }
+  debugPrint('paste ${data.text ?? ''}');
+}
+
+_handleCut() {
+  debugPrint('cut');
+}
 
 FlowyKeyEventHandler copyPasteKeysHandler = (editorState, event) {
   if (event.isMetaPressed && event.logicalKey == LogicalKeyboardKey.keyC) {
-    debugPrint("copy");
+    _handleCopy();
     return KeyEventResult.handled;
   }
   if (event.isMetaPressed && event.logicalKey == LogicalKeyboardKey.keyV) {
-    debugPrint("paste");
+    _handlePaste(editorState);
     return KeyEventResult.handled;
   }
   if (event.isMetaPressed && event.logicalKey == LogicalKeyboardKey.keyX) {
-    debugPrint("cut");
+    _handleCut();
     return KeyEventResult.handled;
   }
   return KeyEventResult.ignored;
