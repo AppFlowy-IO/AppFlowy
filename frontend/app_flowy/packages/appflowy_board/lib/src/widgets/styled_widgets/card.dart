0abd62829ce539fdb58048794ed621dbf3fe7c32
appflowy <annie@appflowy.io>
2022-08-08 19:13:03 +0800

    chore: update pubspec path

--- frontend/app_flowy/packages/appflowy_board/lib/src/widgets/styled_widgets/card.dart
+++ frontend/app_flowy/packages/appflowy_board/lib/src/widgets/styled_widgets/card.dart
@@ -4,13 +4,15 @@ class AppFlowyColumnItemCard extends StatefulWidget {
   final Widget? child;
   final Color backgroundColor;
   final double cornerRadius;
+  final EdgeInsets margin;
   final BoxConstraints boxConstraints;
 
   const AppFlowyColumnItemCard({
     this.child,
-    this.backgroundColor = Colors.white,
     this.cornerRadius = 0.0,
-    this.boxConstraints = const BoxConstraints.tightFor(height: 60),
+    this.margin = const EdgeInsets.all(4),
+    this.backgroundColor = Colors.white,
+    this.boxConstraints = const BoxConstraints(minHeight: 40),
     Key? key,
   }) : super(key: key);
 
@@ -22,10 +24,10 @@ class _AppFlowyColumnItemCardState extends State<AppFlowyColumnItemCard> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(4.0),
+      padding: const EdgeInsets.all(4),
       child: Container(
-        constraints: widget.boxConstraints,
         clipBehavior: Clip.hardEdge,
+        constraints: widget.boxConstraints,
         decoration: BoxDecoration(
           color: widget.backgroundColor,
           borderRadius: BorderRadius.circular(widget.cornerRadius),
