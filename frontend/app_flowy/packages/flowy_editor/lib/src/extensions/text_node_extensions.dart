79591791c1bdb17eb36b3813b9fa6b7712bf7e23
Lucas.Xu <lucas.xu@appflowy.io>
2022-08-10 14:05:31 +0800

    chore: move all code into src/

---
+++ frontend/app_flowy/packages/flowy_editor/lib/src/extensions/text_node_extensions.dart
@@ -0,0 +1,88 @@
+import 'package:flowy_editor/src/document/node.dart';
+import 'package:flowy_editor/src/document/path.dart';
+import 'package:flowy_editor/src/document/position.dart';
+import 'package:flowy_editor/src/document/selection.dart';
+import 'package:flowy_editor/src/document/text_delta.dart';
+import 'package:flowy_editor/src/render/rich_text/rich_text_style.dart';
+
+extension TextNodeExtension on TextNode {
+  bool allSatisfyBoldInSelection(Selection selection) =>
+      allSatisfyInSelection(StyleKey.bold, selection);
+
+  bool allSatisfyItalicInSelection(Selection selection) =>
+      allSatisfyInSelection(StyleKey.italic, selection);
+
+  bool allSatisfyUnderlineInSelection(Selection selection) =>
+      allSatisfyInSelection(StyleKey.underline, selection);
+
+  bool allSatisfyStrikethroughInSelection(Selection selection) =>
+      allSatisfyInSelection(StyleKey.strikethrough, selection);
+
+  bool allSatisfyInSelection(String styleKey, Selection selection) {
+    final ops = delta.operations.whereType<TextInsert>();
+    var start = 0;
+    for (final op in ops) {
+      if (start >= selection.end.offset) {
+        break;
+      }
+      final length = op.length;
+      if (start < selection.end.offset &&
+          start + length > selection.start.offset) {
+        if (op.attributes == null ||
+            !op.attributes!.containsKey(styleKey) ||
+            op.attributes![styleKey] == false) {
+          return false;
+        }
+      }
+      start += length;
+    }
+    return true;
+  }
+}
+
+extension TextNodesExtension on List<TextNode> {
+  bool allSatisfyBoldInSelection(Selection selection) =>
+      allSatisfyInSelection(StyleKey.bold, selection);
+
+  bool allSatisfyItalicInSelection(Selection selection) =>
+      allSatisfyInSelection(StyleKey.italic, selection);
+
+  bool allSatisfyUnderlineInSelection(Selection selection) =>
+      allSatisfyInSelection(StyleKey.underline, selection);
+
+  bool allSatisfyStrikethroughInSelection(Selection selection) =>
+      allSatisfyInSelection(StyleKey.strikethrough, selection);
+
+  bool allSatisfyInSelection(String styleKey, Selection selection) {
+    if (isEmpty) {
+      return false;
+    }
+    if (length == 1) {
+      return first.allSatisfyInSelection(styleKey, selection);
+    } else {
+      for (var i = 0; i < length; i++) {
+        final node = this[i];
+        final Selection newSelection;
+        if (i == 0 && pathEquals(node.path, selection.start.path)) {
+          newSelection = selection.copyWith(
+            end: Position(path: node.path, offset: node.toRawString().length),
+          );
+        } else if (i == length - 1 &&
+            pathEquals(node.path, selection.end.path)) {
+          newSelection = selection.copyWith(
+            start: Position(path: node.path, offset: 0),
+          );
+        } else {
+          newSelection = Selection(
+            start: Position(path: node.path, offset: 0),
+            end: Position(path: node.path, offset: node.toRawString().length),
+          );
+        }
+        if (!node.allSatisfyInSelection(styleKey, newSelection)) {
+          return false;
+        }
+      }
+      return true;
+    }
+  }
+}
--- frontend/app_flowy/packages/flowy_editor/test/delta_test.dart
+++ frontend/app_flowy/packages/flowy_editor/test/delta_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flowy_editor/document/text_delta.dart';
+import 'package:flowy_editor/src/document/text_delta.dart';
 
 void main() {
   group('compose', () {
