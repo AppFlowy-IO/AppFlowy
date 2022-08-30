071ff06d47bc2b3ce18cb2d6d94e97a0c6c7a167
Lucas.Xu <lucas.xu@appflowy.io>
2022-08-30 17:42:28 +0800

    feat: / supports inserting Quote #936

--- frontend/app_flowy/packages/appflowy_editor/test/render/selection_menu/selection_menu_widget_test.dart
+++ frontend/app_flowy/packages/appflowy_editor/test/render/selection_menu/selection_menu_widget_test.dart
@@ -38,17 +38,17 @@ void main() async {
     await editor.pressLogicKey(LogicalKeyboardKey.keyE);
     expect(
       find.byType(SelectionMenuItemWidget, skipOffstage: false),
-      findsNWidgets(2),
+      findsNWidgets(3),
     );
     await editor.pressLogicKey(LogicalKeyboardKey.backspace);
     expect(
       find.byType(SelectionMenuItemWidget, skipOffstage: false),
-      findsNWidgets(3),
+      findsNWidgets(4),
     );
     await editor.pressLogicKey(LogicalKeyboardKey.keyE);
     expect(
       find.byType(SelectionMenuItemWidget, skipOffstage: false),
-      findsNWidgets(2),
+      findsNWidgets(3),
     );
     await editor.pressLogicKey(LogicalKeyboardKey.keyX);
     expect(
@@ -73,7 +73,7 @@ void main() async {
     await editor.pressLogicKey(LogicalKeyboardKey.keyE);
     expect(
       find.byType(SelectionMenuItemWidget, skipOffstage: false),
-      findsNWidgets(2),
+      findsNWidgets(3),
     );
     await editor.pressLogicKey(LogicalKeyboardKey.escape);
     expect(
@@ -89,7 +89,7 @@ void main() async {
     await editor.pressLogicKey(LogicalKeyboardKey.keyE);
     expect(
       find.byType(SelectionMenuItemWidget, skipOffstage: false),
-      findsNWidgets(2),
+      findsNWidgets(3),
     );
     await editor.pressLogicKey(LogicalKeyboardKey.backspace);
     await editor.pressLogicKey(LogicalKeyboardKey.backspace);
