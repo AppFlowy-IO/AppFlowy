21e90ffac0d82b1e33d48e6b5cdad72ddaa35d0d
Vincent Chan <okcdz@diverse.space>
2022-09-05 13:49:57 +0800

    fix: lint of board_setting

--- frontend/app_flowy/lib/plugins/board/presentation/toolbar/board_setting.dart
+++ frontend/app_flowy/lib/plugins/board/presentation/toolbar/board_setting.dart
@@ -2,7 +2,6 @@ import 'package:app_flowy/generated/locale_keys.g.dart';
 import 'package:app_flowy/plugins/board/application/toolbar/board_setting_bloc.dart';
 import 'package:app_flowy/plugins/grid/application/field/field_controller.dart';
 import 'package:app_flowy/plugins/grid/presentation/layout/sizes.dart';
-import 'package:app_flowy/plugins/grid/presentation/widgets/toolbar/grid_group.dart';
 import 'package:app_flowy/plugins/grid/presentation/widgets/toolbar/grid_property.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flowy_infra/image.dart';
