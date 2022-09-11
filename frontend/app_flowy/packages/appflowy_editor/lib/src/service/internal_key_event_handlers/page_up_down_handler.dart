7b06b4c427d3276c2994230d46533536ebaece72
chiragkr04 <chiragkumar6921@gmail.com>
2022-09-11 10:56:18 +0530

    fix: splited shortcut event functions

--- frontend/app_flowy/packages/appflowy_editor/lib/src/service/internal_key_event_handlers/page_up_down_handler.dart
+++ frontend/app_flowy/packages/appflowy_editor/lib/src/service/internal_key_event_handlers/page_up_down_handler.dart
@@ -2,21 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:appflowy_editor/src/service/shortcut_event/shortcut_event_handler.dart';
 
-ShortcutEventHandler pageUpDownHandler = (editorState, event) {
-  if (event.logicalKey == LogicalKeyboardKey.pageUp) {
-    final scrollHeight = editorState.service.scrollService?.onePageHeight;
-    final scrollService = editorState.service.scrollService;
-    if (scrollHeight != null && scrollService != null) {
-      scrollService.scrollTo(scrollService.dy - scrollHeight);
-    }
-    return KeyEventResult.handled;
-  } else if (event.logicalKey == LogicalKeyboardKey.pageDown) {
-    final scrollHeight = editorState.service.scrollService?.onePageHeight;
-    final scrollService = editorState.service.scrollService;
-    if (scrollHeight != null && scrollService != null) {
-      scrollService.scrollTo(scrollService.dy + scrollHeight);
-    }
-    return KeyEventResult.handled;
+ShortcutEventHandler pageUpHandler = (editorState, _) {
+  final scrollHeight = editorState.service.scrollService?.onePageHeight;
+  final scrollService = editorState.service.scrollService;
+  if (scrollHeight != null && scrollService != null) {
+    scrollService.scrollTo(scrollService.dy - scrollHeight);
   }
-  return KeyEventResult.ignored;
+  return KeyEventResult.handled;
+};
+
+ShortcutEventHandler pageDownHandler = (editorState, _) {
+  final scrollHeight = editorState.service.scrollService?.onePageHeight;
+  final scrollService = editorState.service.scrollService;
+  if (scrollHeight != null && scrollService != null) {
+    scrollService.scrollTo(scrollService.dy + scrollHeight);
+  }
+  return KeyEventResult.handled;
 };
