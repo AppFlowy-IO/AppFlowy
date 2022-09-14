41b85510bff2d4dcd8d8426721ba83e6b8e13c56
Vincent Chan <okcdz@diverse.space>
2022-09-14 12:10:59 +0800

    fix: lint issues

--- frontend/app_flowy/lib/plugins/grid/presentation/widgets/header/grid_header.dart
+++ frontend/app_flowy/lib/plugins/grid/presentation/widgets/header/grid_header.dart
@@ -8,7 +8,6 @@ import 'package:appflowy_popover/popover.dart';
 import 'package:flowy_infra/image.dart';
 import 'package:flowy_infra/theme.dart';
 import 'package:flowy_infra_ui/flowy_infra_ui.dart';
-import 'package:flowy_infra_ui/flowy_infra_ui_web.dart';
 import 'package:flowy_infra_ui/style_widget/button.dart';
 import 'package:flowy_infra_ui/style_widget/text.dart';
 import 'package:flowy_sdk/protobuf/flowy-grid/field_entities.pb.dart';
--- frontend/app_flowy/lib/plugins/grid/presentation/widgets/header/type_option/select_option.dart
+++ frontend/app_flowy/lib/plugins/grid/presentation/widgets/header/type_option/select_option.dart
@@ -3,7 +3,6 @@ import 'package:appflowy_popover/popover.dart';
 import 'package:flowy_infra/image.dart';
 import 'package:flowy_infra/theme.dart';
 import 'package:flowy_infra_ui/flowy_infra_ui.dart';
-import 'package:flowy_infra_ui/flowy_infra_ui_web.dart';
 import 'package:flowy_infra_ui/style_widget/button.dart';
 import 'package:flowy_infra_ui/style_widget/text.dart';
 import 'package:flowy_infra_ui/widget/spacing.dart';
