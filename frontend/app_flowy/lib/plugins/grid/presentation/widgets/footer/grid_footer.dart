a6a607472c779ed2ece8b071d632ecb376351458
appflowy <annie@appflowy.io>
2022-10-01 17:18:02 +0800

    fix: compile error

--- frontend/app_flowy/lib/plugins/grid/presentation/widgets/footer/grid_footer.dart
+++ frontend/app_flowy/lib/plugins/grid/presentation/widgets/footer/grid_footer.dart
@@ -1,4 +1,6 @@
+import 'package:app_flowy/generated/locale_keys.g.dart';
 import 'package:app_flowy/plugins/grid/application/grid_bloc.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flowy_infra/image.dart';
 import 'package:flowy_infra/theme.dart';
 import 'package:flowy_infra_ui/style_widget/button.dart';
@@ -13,7 +15,7 @@ class GridAddRowButton extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = context.watch<AppTheme>();
     return FlowyButton(
-      text: const FlowyText.medium(LocaleKeys.grid_row_newRow.tr(), fontSize: 12),
+      text: FlowyText.medium(LocaleKeys.grid_row_newRow.tr(), fontSize: 12),
       hoverColor: theme.shader6,
       onTap: () => context.read<GridBloc>().add(const GridEvent.createRow()),
       leftIcon: svgWidget("home/add", color: theme.iconColor),
