eaf396630cb71649af81fdba4b8c30443c8adda8
Enzo Lizama <lizama.enzo@gmail.com>
2022-10-05 23:06:48 -0500

    chore: url launcher test

--- frontend/app_flowy/packages/appflowy_editor/test/extensions/url_launcher_extension_test.dart
+++ frontend/app_flowy/packages/appflowy_editor/test/extensions/url_launcher_extension_test.dart
@@ -2,18 +2,6 @@ import 'package:appflowy_editor/src/extensions/url_launcher_extension.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  // test('safeLaunchUrl with scheme', () async {
-  //   const href = 'https://github.com/AppFlowy-IO';
-  //   final result = await safeLaunchUrl(href);
-  //   expect(result, true);
-  // });
-
-  // test('safeLaunchUrl without scheme', () async {
-  //   const href = 'github.com/AppFlowy-IO';
-  //   final result = await safeLaunchUrl(href);
-  //   expect(result, true);
-  // });
-
   test('safeLaunchUrl without scheme', () async {
     const href = null;
     final result = await safeLaunchUrl(href);
