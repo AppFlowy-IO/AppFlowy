0d223de66e6e36a3e2fcdfd472017e3b66fe474f
appflowy <annie@appflowy.io>
2022-10-17 13:42:05 +0800

    chore: handle select option type option data transform in one place

--- frontend/rust-lib/flowy-grid/src/services/field/type_options/selection_type_option/mod.rs
+++ frontend/rust-lib/flowy-grid/src/services/field/type_options/selection_type_option/mod.rs
@@ -1,6 +1,7 @@
 mod multi_select_type_option;
 mod select_type_option;
 mod single_select_type_option;
+mod type_option_transform;
 
 pub use multi_select_type_option::*;
 pub use select_type_option::*;
