35cd72a3f17ed0f4f590b1e5655a3c870b0b6c24
Sean Riley Hawkins <rileyhawk249@gmail.com>
2022-10-21 12:47:11 +0200

    fix: removed unused import

--- frontend/app_flowy/packages/appflowy_editor/test/service/scroll_service_test.dart
+++ frontend/app_flowy/packages/appflowy_editor/test/service/scroll_service_test.dart
@@ -1,4 +1,3 @@
-import 'package:appflowy_editor/appflowy_editor.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import '../infra/test_editor.dart';
