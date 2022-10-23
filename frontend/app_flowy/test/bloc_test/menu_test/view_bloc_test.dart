0d8be870313ba49da66740381a4292a27b90f597
Nathan.fooo <86001920+appflowy@users.noreply.github.com>
2022-10-23 17:52:17 +0800

    chore: add view bloc test (#1343)
    Co-authored-by: nathan <nathan@appflowy.io>

---
+++ frontend/app_flowy/test/bloc_test/menu_test/view_bloc_test.dart
@@ -0,0 +1,67 @@
+import 'package:app_flowy/plugins/doc/document.dart';
+import 'package:app_flowy/workspace/application/app/app_bloc.dart';
+import 'package:app_flowy/workspace/application/view/view_bloc.dart';
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../util.dart';
+
+void main() {
+  late AppFlowyUnitTest test;
+  setUpAll(() async {
+    test = await AppFlowyUnitTest.ensureInitialized();
+  });
+
+  group('$ViewBloc', () {
+    late AppBloc appBloc;
+
+    setUpAll(() async {
+      final app = await test.createTestApp();
+      appBloc = AppBloc(app: app)..add(const AppEvent.initial());
+      appBloc.add(AppEvent.createView(
+        "Test document",
+        DocumentPluginBuilder(),
+      ));
+      await blocResponseFuture();
+    });
+
+    blocTest<ViewBloc, ViewState>(
+      "rename view",
+      build: () => ViewBloc(view: appBloc.state.views.first)
+        ..add(const ViewEvent.initial()),
+      act: (bloc) {
+        bloc.add(const ViewEvent.rename('Hello world'));
+      },
+      wait: blocResponseDuration(),
+      verify: (bloc) {
+        assert(bloc.state.view.name == "Hello world");
+      },
+    );
+
+    blocTest<ViewBloc, ViewState>(
+      "duplicate view",
+      build: () => ViewBloc(view: appBloc.state.views.first)
+        ..add(const ViewEvent.initial()),
+      act: (bloc) {
+        bloc.add(const ViewEvent.duplicate());
+      },
+      wait: blocResponseDuration(),
+      verify: (bloc) {
+        assert(appBloc.state.views.length == 2);
+      },
+    );
+
+    blocTest<ViewBloc, ViewState>(
+      "delete view",
+      build: () => ViewBloc(view: appBloc.state.views.first)
+        ..add(const ViewEvent.initial()),
+      act: (bloc) {
+        bloc.add(const ViewEvent.delete());
+      },
+      wait: blocResponseDuration(),
+      verify: (bloc) {
+        assert(appBloc.state.views.length == 1);
+      },
+    );
+  });
+}
