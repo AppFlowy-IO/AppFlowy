38e093a0d5fad5837a1c8a480b8c8ecae1251f28
Sean Riley Hawkins <rileyhawk249@gmail.com>
2022-11-11 15:05:04 +0200

    feat: pick local image files

--- frontend/app_flowy/packages/appflowy_editor/lib/src/render/image/image_node_builder.dart
+++ frontend/app_flowy/packages/appflowy_editor/lib/src/render/image/image_node_builder.dart
@@ -2,6 +2,12 @@ import 'package:appflowy_editor/src/core/document/node.dart';
 import 'package:appflowy_editor/src/infra/clipboard.dart';
 import 'package:appflowy_editor/src/service/render_plugin_service.dart';
 import 'package:flutter/material.dart';
+<<<<<<< HEAD
+=======
+import 'package:rich_clipboard/rich_clipboard.dart';
+import 'package:appflowy_editor/src/render/image/local_image_node_widget.dart';
+import 'package:appflowy_editor/src/render/image/network_image_node_widget.dart';
+>>>>>>> c066f53cd (feat: pick local image files)
 
 import 'image_node_widget.dart';
 
@@ -9,11 +15,14 @@ class ImageNodeBuilder extends NodeWidgetBuilder<Node> {
   @override
   Widget build(NodeWidgetContext<Node> context) {
     final src = context.node.attributes['image_src'];
+    final type = context.node.attributes['type'];
     final align = context.node.attributes['align'];
+    var widget;
     double? width;
     if (context.node.attributes.containsKey('width')) {
       width = context.node.attributes['width'].toDouble();
     }
+<<<<<<< HEAD
     return ImageNodeWidget(
       key: context.node.key,
       node: context.node,
@@ -43,6 +52,74 @@ class ImageNodeBuilder extends NodeWidgetBuilder<Node> {
         context.editorState.apply(transaction);
       },
     );
+=======
+    if (type == 'network') {
+      widget = NetworkImageNode(
+        key: context.node.key,
+        node: context.node,
+        src: src,
+        type: type,
+        width: width,
+        alignment: _textToAlignment(align),
+        onCopy: () {
+          RichClipboard.setData(RichClipboardData(text: src));
+        },
+        onDelete: () {
+          final transaction = context.editorState.transaction
+            ..deleteNode(context.node);
+          context.editorState.apply(transaction);
+        },
+        onAlign: (alignment) {
+          final transaction = context.editorState.transaction
+            ..updateNode(context.node, {
+              'align': _alignmentToText(alignment),
+            });
+          context.editorState.apply(transaction);
+        },
+        onResize: (width) {
+          final transaction = context.editorState.transaction
+            ..updateNode(context.node, {
+              'width': width,
+            });
+          context.editorState.apply(transaction);
+        },
+      );
+    } else if (type == 'file') {
+      widget = LocalImageNode(
+        key: context.node.key,
+        node: context.node,
+        src: src,
+        type: type,
+        width: width,
+        alignment: _textToAlignment(align),
+        onCopy: () {
+          RichClipboard.setData(RichClipboardData(text: src));
+        },
+        onDelete: () {
+          final transaction = context.editorState.transaction
+            ..deleteNode(context.node);
+          context.editorState.apply(transaction);
+        },
+        onAlign: (alignment) {
+          final transaction = context.editorState.transaction
+            ..updateNode(context.node, {
+              'align': _alignmentToText(alignment),
+            });
+          context.editorState.apply(transaction);
+        },
+        onResize: (width) {
+          final transaction = context.editorState.transaction
+            ..updateNode(context.node, {
+              'width': width,
+            });
+          context.editorState.apply(transaction);
+        },
+      );
+    } else {
+      print('type cannnot be found?');
+    }
+    return widget;
+>>>>>>> c066f53cd (feat: pick local image files)
   }
 
   @override
