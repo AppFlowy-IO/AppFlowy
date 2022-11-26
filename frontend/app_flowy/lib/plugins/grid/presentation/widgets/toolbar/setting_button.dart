0d82bb00a8b28dcd24148630d1a8fdbf5460e246
nathan <nathan@appflowy.io>
2022-11-26 15:17:50 +0800

    chore: update setting icon in grid

--- frontend/app_flowy/lib/plugins/grid/presentation/widgets/toolbar/setting_button.dart
+++ frontend/app_flowy/lib/plugins/grid/presentation/widgets/toolbar/setting_button.dart
@@ -1,9 +1,12 @@
+import 'package:app_flowy/generated/locale_keys.g.dart';
 import 'package:app_flowy/plugins/grid/application/grid_bloc.dart';
 import 'package:app_flowy/plugins/grid/application/setting/setting_bloc.dart';
 import 'package:appflowy_popover/appflowy_popover.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flowy_infra/color_extension.dart';
 import 'package:flowy_infra/image.dart';
 import 'package:flowy_infra_ui/flowy_infra_ui.dart';
+import 'package:flowy_infra_ui/style_widget/button.dart';
 import 'package:flowy_infra_ui/style_widget/icon_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -47,14 +50,15 @@ class _SettingButtonState extends State<SettingButton> {
           offset: const Offset(0, 10),
           margin: const EdgeInsets.all(6),
           triggerActions: PopoverTriggerFlags.none,
-          child: FlowyIconButton(
-            height: 26,
+          child: FlowyTextButton(
+            LocaleKeys.settings_title.tr(),
+            fontSize: 14,
+            fillColor: Colors.transparent,
             hoverColor: AFThemeExtension.of(context).lightGreyHover,
-            onPressed: () => popoverController.show(),
-            icon: svgWidget(
-              "grid/setting/setting",
-              color: Theme.of(context).colorScheme.onSurface,
-            ).padding(horizontal: 4, vertical: 4),
+            padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 6),
+            onPressed: () {
+              popoverController.show();
+            },
           ),
           popupBuilder: (BuildContext context) {
             return _GridSettingListPopover(settingContext: settingContext);
