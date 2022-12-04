a2161216d81624b742e6c46709c86c81c21f3f86
ENsu <ENsu@users.noreply.github.com>
2022-12-04 16:32:37 +0800

    feat: can transform options from single & multi select (#1493)
    * feat: can transform options from single & multi select
    
    * test: add single-multi switch test

--- frontend/rust-lib/flowy-grid/src/services/field/type_options/selection_type_option/single_select_type_option.rs
+++ frontend/rust-lib/flowy-grid/src/services/field/type_options/selection_type_option/single_select_type_option.rs
@@ -121,6 +121,27 @@ mod tests {
     }
 
     #[test]
+    fn single_select_transform_with_multiselect_option_test() {
+        let mut multiselect_type_option_builder = MultiSelectTypeOptionBuilder::default();
+
+        let google = SelectOptionPB::new("Google");
+        multiselect_type_option_builder = multiselect_type_option_builder.add_option(google);
+
+        let facebook = SelectOptionPB::new("Facebook");
+        multiselect_type_option_builder = multiselect_type_option_builder.add_option(facebook);
+
+        let multiselect_type_option_data = multiselect_type_option_builder.serializer().json_str();
+
+        let mut single_select = SingleSelectTypeOptionBuilder::default();
+        single_select.transform(&FieldType::MultiSelect, multiselect_type_option_data.clone());
+        debug_assert_eq!(single_select.0.options.len(), 2);
+
+        // Already contain the yes/no option. It doesn't need to insert new options
+        single_select.transform(&FieldType::MultiSelect, multiselect_type_option_data);
+        debug_assert_eq!(single_select.0.options.len(), 2);
+    }
+
+    #[test]
     fn single_select_insert_multi_option_test() {
         let google = SelectOptionPB::new("Google");
         let facebook = SelectOptionPB::new("Facebook");
