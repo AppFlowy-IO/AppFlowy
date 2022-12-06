29952bc7fdf0bc067385087a68277a6b1c72f72a
Lucas.Xu <lucas.xu@appflowy.io>
2022-12-06 17:24:10 +0800

    fix: add missing markdown converter for code block and divider

--- frontend/app_flowy/packages/appflowy_editor/lib/src/plugins/markdown/decoder/document_markdown_decoder.dart
+++ frontend/app_flowy/packages/appflowy_editor/lib/src/plugins/markdown/decoder/document_markdown_decoder.dart
@@ -80,6 +80,8 @@ class DocumentMarkdownDecoder extends Converter<String, Document> {
           BuiltInAttributeKey.subtype: BuiltInAttributeKey.quote,
         },
       );
+    } else if (RegExp(r'^-*').stringMatch(text) == text) {
+      return Node(type: 'divider');
     }
 
     if (text.isNotEmpty) {
