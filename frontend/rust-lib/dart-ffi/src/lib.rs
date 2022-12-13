f5d183c50cb14d4ba83b49163cdca74076c8f15c
nathan <nathan@appflowy.io>
2022-12-13 16:40:44 +0800

    chore: reinitialize flowy sdk when calling init_sdk

--- frontend/rust-lib/dart-ffi/src/lib.rs
+++ frontend/rust-lib/dart-ffi/src/lib.rs
@@ -8,17 +8,17 @@ use crate::{
     c::{extend_front_four_bytes_into_bytes, forget_rust},
     model::{FFIRequest, FFIResponse},
 };
-use crossbeam_utils::atomic::AtomicCell;
 use flowy_sdk::get_client_server_configuration;
 use flowy_sdk::*;
 use lib_dispatch::prelude::ToBytes;
 use lib_dispatch::prelude::*;
-use once_cell::sync::OnceCell;
 use std::{ffi::CStr, os::raw::c_char};
+use lazy_static::lazy_static;
+use parking_lot::RwLock;
 
-// static FLOWY_SDK: OnceCell<FlowySDK> = OnceCell::new();
-
-static mut FLOWY_SDK: Option<FlowySDK> = Option::None;
+lazy_static! {
+    static ref FLOWY_SDK: RwLock<Option<FlowySDK>> = RwLock::new(None);
+}
 
 #[no_mangle]
 pub extern "C" fn init_sdk(path: *mut c_char) -> i64 {
@@ -27,10 +27,7 @@ pub extern "C" fn init_sdk(path: *mut c_char) -> i64 {
 
     let server_config = get_client_server_configuration().unwrap();
     let config = FlowySDKConfig::new(path, "appflowy", server_config).log_filter("info");
-    // FLOWY_SDK.get_or_init(|| FlowySDK::new(config));
-    unsafe {
-        FLOWY_SDK = Option::Some(FlowySDK::new(config));
-    }
+    *FLOWY_SDK.write() = Some(FlowySDK::new(config));
 
     0
 }
@@ -45,19 +42,17 @@ pub extern "C" fn async_event(port: i64, input: *const u8, len: usize) {
         port
     );
 
-    unsafe {
-        let dispatcher = match &FLOWY_SDK {
-            None => {
-                log::error!("sdk not init yet.");
-                return;
-            }
-            Some(e) => e.event_dispatcher.clone(),
-        };
-        let _ = AFPluginDispatcher::async_send_with_callback(dispatcher, request, move |resp: AFPluginEventResponse| {
-            log::trace!("[FFI]: Post data to dart through {} port", port);
-            Box::pin(post_to_flutter(resp, port))
-        });
-    }
+    let dispatcher = match FLOWY_SDK.read().as_ref() {
+        None => {
+            log::error!("sdk not init yet.");
+            return;
+        }
+        Some(e) => e.event_dispatcher.clone(),
+    };
+    let _ = AFPluginDispatcher::async_send_with_callback(dispatcher, request, move |resp: AFPluginEventResponse| {
+        log::trace!("[FFI]: Post data to dart through {} port", port);
+        Box::pin(post_to_flutter(resp, port))
+    });
 }
 
 #[no_mangle]
@@ -65,21 +60,19 @@ pub extern "C" fn sync_event(input: *const u8, len: usize) -> *const u8 {
     let request: AFPluginRequest = FFIRequest::from_u8_pointer(input, len).into();
     log::trace!("[FFI]: {} Sync Event: {:?}", &request.id, &request.event,);
 
-    unsafe {
-        let dispatcher = match &FLOWY_SDK {
-            None => {
-                log::error!("sdk not init yet.");
-                return forget_rust(Vec::default());
-            }
-            Some(e) => e.event_dispatcher.clone(),
-        };
-        let _response = AFPluginDispatcher::sync_send(dispatcher, request);
-    
-        // FFIResponse {  }
-        let response_bytes = vec![];
-        let result = extend_front_four_bytes_into_bytes(&response_bytes);
-        forget_rust(result)
-    }
+    let dispatcher = match FLOWY_SDK.read().as_ref() {
+        None => {
+            log::error!("sdk not init yet.");
+            return forget_rust(Vec::default());
+        }
+        Some(e) => e.event_dispatcher.clone(),
+    };
+    let _response = AFPluginDispatcher::sync_send(dispatcher, request);
+
+    // FFIResponse {  }
+    let response_bytes = vec![];
+    let result = extend_front_four_bytes_into_bytes(&response_bytes);
+    forget_rust(result)
 }
 
 #[no_mangle]
