786b6c47af2fe928da83d59d500015728b91abbf
nathan <nathan@appflowy.io>
2023-01-16 21:54:55 +0800

    chore: track files

---
+++ frontend/appflowy_tauri/src/protobuf/index.ts
@@ -0,0 +1,6 @@
+export * from './crates/flowy-user';
+export * from './crates/flowy-document';
+export * from './crates/flowy-grid';
+export * from './crates/flowy-folder';
+export * from './crates/flowy-net';
+export * from './crates/flowy-error';
