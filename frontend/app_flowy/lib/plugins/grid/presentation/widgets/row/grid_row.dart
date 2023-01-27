243a781b6cf905f44c1a803a803f31ba624e0e81
Richard Shiue <71320345+richardshiue@users.noreply.github.com>
2023-01-27 10:35:17 +0800

    chore: improve grid focus and hover event handling (#1735)
    * chore: improve primary cell accessory behavior
    
    * fix: focus border disappearing
    
    * chore: port to GridCellState
    
    * chore: fix typo
    
    * chore: connect popover controller
    
    * chore: final

--- frontend/app_flowy/lib/plugins/grid/presentation/widgets/row/grid_row.dart
+++ frontend/app_flowy/lib/plugins/grid/presentation/widgets/row/grid_row.dart
@@ -225,8 +225,8 @@ class RowContent extends StatelessWidget {
 
         return CellContainer(
           width: cellId.fieldInfo.width.toDouble(),
-          rowStateNotifier:
-              Provider.of<RegionStateNotifier>(context, listen: false),
+          isPrimary: cellId.fieldInfo.isPrimary,
+          cellContainerNotifier: CellContainerNotifier(child),
           accessoryBuilder: (buildContext) {
             final builder = child.accessoryBuilder;
             List<GridCellAccessoryBuilder> accessories = [];
