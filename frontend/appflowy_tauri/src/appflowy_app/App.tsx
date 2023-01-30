2948d7a38cb334c243e7f98cddd8fe0b10a83279
ascarbek <ascarbek@gmail.com>
2023-01-30 01:02:36 -0800

    chore: move test calls to separate component

--- frontend/appflowy_tauri/src/appflowy_app/App.tsx
+++ frontend/appflowy_tauri/src/appflowy_app/App.tsx
@@ -1,44 +1,5 @@
-import './App.css';
-import { UserEventSignIn, SignInPayloadPB } from '../services/backend/events/flowy-user/index';
-import { nanoid } from 'nanoid';
-import { UserNotificationListener } from './components/user/application/notifications';
-
-function App() {
-  async function sendSignInEvent() {
-    let make_payload = () =>
-      SignInPayloadPB.fromObject({
-        email: nanoid(4) + '@gmail.com',
-        password: 'A!@123abc',
-        name: 'abc',
-      });
-
-    let listener = await new UserNotificationListener({
-      onUserSignIn: (userProfile) => {
-        console.log(userProfile);
-      },
-      onProfileUpdate(userProfile) {
-        console.log(userProfile);
-        // stop listening the changes
-        listener.stop();
-      },
-    });
-
-    listener.start();
-
-    await UserEventSignIn(make_payload());
-  }
-
-  return (
-    <div className='text-white bg-gray-500 h-screen flex flex-col justify-center items-center gap-4'>
-      <h1 className='text-3xl'>Welcome to AppFlowy!</h1>
-
-      <div>
-        <button className='bg-gray-700 p-4 rounded-md' type='button' onClick={() => sendSignInEvent()}>
-          Test Sign In Event
-        </button>
-      </div>
-    </div>
-  );
-}
+const App = () => {
+  return <div className='text-white bg-gray-500 h-screen flex flex-col justify-center items-center gap-4'>Body</div>;
+};
 
 export default App;
