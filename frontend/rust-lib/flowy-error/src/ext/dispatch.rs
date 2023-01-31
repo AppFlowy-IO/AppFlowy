c36cb9d196721394665d299d9e31f37868c327f4
nathan <nathan@appflowy.io>
2023-01-31 20:43:40 +0800

    ci: config rust log

--- frontend/rust-lib/flowy-error/src/ext/dispatch.rs
+++ frontend/rust-lib/flowy-error/src/ext/dispatch.rs
@@ -2,11 +2,11 @@ use crate::FlowyError;
 use bytes::Bytes;
 use lib_dispatch::prelude::{AFPluginEventResponse, ResponseBuilder};
 use std::convert::TryInto;
+use std::io::Seek;
 impl lib_dispatch::Error for FlowyError {
     fn as_response(&self) -> AFPluginEventResponse {
+        String::from_utf8_lossy()
         let bytes: Bytes = self.clone().try_into().unwrap();
-
-        println!("Serialize FlowyError: {:?} to event response", self);
         ResponseBuilder::Err().data(bytes).build()
     }
 }
