a366ee23ef3cb3c8e33d9721432c1fc05a152f9c
nathan <nathan@appflowy.io>
2023-02-07 19:53:13 +0800

    chore: remove appflowy-board

--- frontend/app_flowy/packages/appflowy_board/lib/appflowy_board.dart
+++
@@ -1,7 +0,0 @@
-/// AppFlowyBoard library
-library appflowy_board;
-
-export 'src/widgets/board_group/group_data.dart';
-export 'src/widgets/board_data.dart';
-export 'src/widgets/styled_widgets/widgets.dart';
-export 'src/widgets/board.dart';
