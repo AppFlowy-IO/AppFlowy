fd9a24b3a289c32f6af117b191586f5a9cf31206
Mayur Mahajan <mayur23smahajan@gmail.com>
2023-02-08 16:13:02 +0530

    refactor: handler to use transactions

--- frontend/app_flowy/packages/appflowy_editor/lib/src/service/internal_key_event_handlers/checkbox_event_handler.dart
+++ frontend/app_flowy/packages/appflowy_editor/lib/src/service/internal_key_event_handlers/checkbox_event_handler.dart
@@ -1,22 +1,49 @@
 import 'package:appflowy_editor/src/service/shortcut_event/shortcut_event_handler.dart';
 import 'package:appflowy_editor/src/core/document/attributes.dart';
 import 'package:appflowy_editor/src/core/document/node.dart';
+import 'package:appflowy_editor/appflowy_editor.dart';
 import 'package:flutter/material.dart';
 
 ShortcutEventHandler toggleCheckbox = (editorState, event) {
   final selection = editorState.service.selectionService.currentSelection.value;
   final nodes = editorState.service.selectionService.currentSelectedNodes;
-  final textNodes = nodes.whereType<TextNode>().toList(growable: false);
-  if (selection == null || textNodes.isEmpty) {
+  final checkboxTextNodes = nodes
+      .where(
+        (element) =>
+            element is TextNode &&
+            element.subtype == BuiltInAttributeKey.checkbox,
+      )
+      .toList(growable: false);
+
+  if (selection == null || checkboxTextNodes.isEmpty) {
     return KeyEventResult.ignored;
   }
-  for (Node node in textNodes) {
-    if (node.attributes.containsKey('checkbox') &&
-        node.attributes.containsValue('checkbox')) {
-      bool currentStatus = !(node.attributes['checkbox']);
-      Attributes checkboxAttribute = {'checkbox': currentStatus};
-      node.updateAttributes(checkboxAttribute);
+
+  //If any one of the checkboxes is unchecked then make all checkboxes checked
+
+  bool isAllCheckboxesChecked = true;
+  final transaction = editorState.transaction;
+  for (final node in checkboxTextNodes) {
+    if (node.attributes[BuiltInAttributeKey.checkbox] == false) {
+      isAllCheckboxesChecked = false;
+      transaction.updateNode(node, {
+        BuiltInAttributeKey.checkbox:
+            !node.attributes[BuiltInAttributeKey.checkbox]
+      });
     }
   }
+  editorState.apply(transaction);
+
+  //if all the checkboxes are checked, then make all of the checkboxes unchecked
+  final transaction2 = editorState.transaction;
+  if (isAllCheckboxesChecked) {
+    for (final node in checkboxTextNodes) {
+      transaction2.updateNode(node, {
+        BuiltInAttributeKey.checkbox:
+            !node.attributes[BuiltInAttributeKey.checkbox]
+      });
+    }
+  }
+  editorState.apply(transaction2);
   return KeyEventResult.handled;
 };
