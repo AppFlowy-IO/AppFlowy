ebca625858e562237fd44e8330733dcf9a9701ef
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2023-02-10 11:27:20 -0500

    fix: isDesktop doc comment

--- frontend/app_flowy/lib/core/helpers/target_platform.dart
+++ frontend/app_flowy/lib/core/helpers/target_platform.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/foundation.dart' show TargetPlatform;
 
-/// Convenience function to check if the app is running on a desktop computer.
-///
-/// Easily check if on desktop by checking `defaultTargetPlatform.isDesktop`.
 extension TargetPlatformHelper on TargetPlatform {
+  /// Convenience function to check if the app is running on a desktop computer.
+  ///
+  /// Easily check if on desktop by checking `defaultTargetPlatform.isDesktop`.
   bool get isDesktop =>
       this == TargetPlatform.linux ||
       this == TargetPlatform.macOS ||
