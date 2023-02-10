3b55372f178c4da2c579b7b963cfdfba972ab8cb
nathan <nathan@appflowy.io>
2023-02-10 10:48:54 +0800

    ci: add tauri ci

--- frontend/rust-lib/flowy-database/src/services/retry.rs
+++ frontend/rust-lib/flowy-database/src/services/retry.rs
@@ -2,11 +2,10 @@ use flowy_client_sync::client_database::GridBlockRevisionPad;
 use flowy_error::FlowyError;
 use grid_model::RowRevision;
 use lib_infra::retry::Action;
-
-use parking_lot::RwLock;
 use std::future::Future;
 use std::pin::Pin;
 use std::sync::Arc;
+use tokio::sync::RwLock;
 
 pub struct GetRowDataRetryAction {
     pub row_id: String,
@@ -23,8 +22,8 @@ impl Action for GetRowDataRetryAction {
         let row_id = self.row_id.clone();
         Box::pin(async move {
             match pad.try_read() {
-                None => Ok(None),
-                Some(read_guard) => Ok(read_guard.get_row_rev(&row_id)),
+                Err(_) => Ok(None),
+                Ok(read_guard) => Ok(read_guard.get_row_rev(&row_id)),
             }
         })
     }
