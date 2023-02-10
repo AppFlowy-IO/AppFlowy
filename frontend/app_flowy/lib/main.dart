26760dcd3448ab9e92ae478a5b379aa58f87c332
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2023-02-10 11:09:26 -0500

    refactor: move window logic to separate class
    This is much cleaner and better encapsulated. Sets up an area for
    further window management in the future.
    
    Does not try to initialize on mobile or web, which would crash.

--- frontend/app_flowy/lib/main.dart
+++ frontend/app_flowy/lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:hotkey_manager/hotkey_manager.dart';
-import 'package:window_manager/window_manager.dart';
 
 import 'startup/launch_configuration.dart';
 import 'startup/startup.dart';
 import 'user/presentation/splash_screen.dart';
+import 'window/window.dart';
 
 class FlowyApp implements EntryPoint {
   @override
@@ -21,19 +21,8 @@ Future<void> main() async {
 
   await EasyLocalization.ensureInitialized();
   await hotKeyManager.unregisterAll();
-  await windowManager.ensureInitialized();
 
-  await setWindowOptions();
+  await AppWindow.initialize();
 
   await FlowyRunner.run(FlowyApp());
-}
-
-Future<void> setWindowOptions() async {
-  WindowOptions windowOptions = const WindowOptions(
-    minimumSize: Size(600, 400),
-  );
-  return windowManager.waitUntilReadyToShow(windowOptions, () async {
-    await windowManager.show();
-    await windowManager.focus();
-  });
 }
