5005050683259ba81921cd284444cf3786e9a2a9
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2023-02-13 12:28:33 -0500

    feat: catch platform errors
    Adds handling for platform errors that are not caught by the
    Flutter framework. Doing so will log the error properly and
    prevent the app from crashing.

--- frontend/app_flowy/lib/main.dart
+++ frontend/app_flowy/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:appflowy_backend/log.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hotkey_manager/hotkey_manager.dart';
 
@@ -18,6 +20,13 @@ class FlowyApp implements EntryPoint {
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Handle platform errors not caught by Flutter.
+  // Reduces the likelihood of the app crashing, and logs the error.
+  PlatformDispatcher.instance.onError = (error, stack) {
+    Log.error('Uncaught platform error', error, stack);
+    return true;
+  };
 
   await EasyLocalization.ensureInitialized();
   await hotKeyManager.unregisterAll();
