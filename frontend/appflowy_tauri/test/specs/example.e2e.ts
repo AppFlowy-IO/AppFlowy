68f9e066ced764e6d8f0f99f61f7847db0aab84e
nathan <nathan@appflowy.io>
2023-02-16 19:21:13 +0800

    chore: config jest windows

---
+++ frontend/appflowy_tauri/test/specs/example.e2e.ts
@@ -0,0 +1,14 @@
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await browser.url(`https://the-internet.herokuapp.com/login`)
+
+        await $('#username').setValue('tomsmith')
+        await $('#password').setValue('SuperSecretPassword!')
+        await $('button[type="submit"]').click()
+
+        await expect($('#flash')).toBeExisting()
+        await expect($('#flash')).toHaveTextContaining(
+            'You logged into a secure area!')
+    })
+})
+
