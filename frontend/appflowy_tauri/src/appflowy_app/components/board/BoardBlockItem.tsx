327685a61ccf924275a066ee4de62c867e357aa4
ascarbek <ascarbek@gmail.com>
2023-02-21 19:38:48 +0600

    chore: boardblockitem details btn

--- frontend/appflowy_tauri/src/appflowy_app/components/board/BoardBlockItem.tsx
+++ frontend/appflowy_tauri/src/appflowy_app/components/board/BoardBlockItem.tsx
@@ -1,4 +1,5 @@
 import { IDatabaseColumn, IDatabaseRow } from '../../stores/reducers/database/slice';
+import { Details2Svg } from '../_shared/svg/Details2Svg';
 
 export const BoardBlockItem = ({
   groupingFieldId,
@@ -10,7 +11,10 @@ export const BoardBlockItem = ({
   row: IDatabaseRow;
 }) => {
   return (
-    <div className={'rounded-lg border border-shade-6 bg-white px-3 py-2'}>
+    <div className={'relative rounded-lg border border-shade-6 bg-white px-3 py-2'}>
+      <button className={'absolute right-4 top-2.5 h-5 w-5 rounded hover:bg-surface-2'}>
+        <Details2Svg></Details2Svg>
+      </button>
       <div className={'flex flex-col gap-2'}>
         {columns
           .filter((column) => column.fieldId !== groupingFieldId)
