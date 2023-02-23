0ef0234e2557195d84b45953efb0e387a0a04df4
Lucas.Xu <lucas.xu@appflowy.io>
2023-02-23 15:42:05 +0800

    test: add test for normalize and getTextInSelection function

--- frontend/app_flowy/packages/appflowy_editor/test/extensions/node_extension_test.dart
+++ frontend/app_flowy/packages/appflowy_editor/test/extensions/node_extension_test.dart
@@ -2,12 +2,13 @@ import 'dart:collection';
 
 import 'package:appflowy_editor/appflowy_editor.dart';
 import 'package:flutter_test/flutter_test.dart';
+import '../infra/test_editor.dart';
 import 'package:mockito/mockito.dart';
 
 class MockNode extends Mock implements Node {}
 
 void main() {
-  group('NodeExtensions::', () {
+  group('node_extension.dart', () {
     final selection = Selection(
       start: Position(path: [0]),
       end: Position(path: [1]),
@@ -42,6 +43,37 @@ void main() {
       );
       final result = node.inSelection(selection);
       expect(result, false);
+    });
+
+    testWidgets('insert a new checkbox after an exsiting checkbox',
+        (tester) async {
+      const text = 'Welcome to Appflowy 😁';
+      final editor = tester.editor
+        ..insertTextNode(
+          text,
+        )
+        ..insertTextNode(
+          text,
+        )
+        ..insertTextNode(
+          text,
+        );
+      await editor.startTesting();
+      final selection = Selection(
+        start: Position(path: [2], offset: 5),
+        end: Position(path: [0], offset: 5),
+      );
+      await editor.updateSelection(selection);
+      final nodes =
+          editor.editorState.service.selectionService.currentSelectedNodes;
+      expect(
+        nodes.map((e) => e.path).toList().toString(),
+        '[[2], [1], [0]]',
+      );
+      expect(
+        nodes.normalized.map((e) => e.path).toList().toString(),
+        '[[0], [1], [2]]',
+      );
     });
   });
 }
