351d813a5ef59440c643f5936ba6e25dd21d5d5f
ascarbek <ascarbek@gmail.com>
2023-02-24 20:03:08 +0600

    chore: new folder use backend service

--- frontend/appflowy_tauri/src/appflowy_app/components/layout/NavigationPanel/NewFolderButton.hooks.ts
+++ frontend/appflowy_tauri/src/appflowy_app/components/layout/NavigationPanel/NewFolderButton.hooks.ts
@@ -1,28 +1,18 @@
 import { useAppDispatch, useAppSelector } from '../../../stores/store';
 import { foldersActions } from '../../../stores/reducers/folders/slice';
-import { nanoid } from 'nanoid';
-import { CreateAppPayloadPB, FolderEventCreateApp } from '../../../../services/backend/events/flowy-folder';
-import { workspaceSlice } from '../../../stores/reducers/workspace/slice';
+import { WorkspaceBackendService } from '../../../stores/effects/folder/workspace/backend_service';
 
 export const useNewFolder = () => {
   const appDispatch = useAppDispatch();
   const workspace = useAppSelector((state) => state.workspace);
+  const workspaceBackendService = new WorkspaceBackendService(workspace.id || '');
 
   const onNewFolder = async () => {
-    const createAppResult = await FolderEventCreateApp(
-      CreateAppPayloadPB.fromObject({
-        workspace_id: workspace.id,
-        name: 'New Folder 1',
-        desc: '',
-      })
-    );
-    if (createAppResult.ok) {
-      const pb = createAppResult.val;
+    const newApp = await workspaceBackendService.createApp({
+      name: 'New Folder 1',
+    });
 
-      appDispatch(foldersActions.addFolder({ id: pb.id, title: pb.name }));
-    } else {
-      throw new Error('create app error');
-    }
+    appDispatch(foldersActions.addFolder({ id: newApp.id, title: newApp.name }));
   };
 
   return {
