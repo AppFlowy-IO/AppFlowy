8a3a7c9171089fe6d27cd582fe75851387f6de34
Muhammad Rizwan <47111784+rizwan3395@users.noreply.github.com>
2023-02-28 22:04:24 +0500

    fix: cover plugin implementation finalized

--- frontend/appflowy_flutter/lib/plugins/document/document_page.dart
+++ frontend/appflowy_flutter/lib/plugins/document/document_page.dart
@@ -148,7 +148,7 @@ class _AppFlowyEditorPageState extends State<_AppFlowyEditorPage> {
         // Cover
         kCoverType: CoverNodeWidgetBuilder(),
         // Smart Edit,
-        kSmartEditType: SmartEditInputBuilder(),
+        kSmartEditType: SmartEditInputBuilder()
 
       },
       shortcutEvents: [
