8e22ef223007a3d74fba03d64919c9d0b7f58245
Nathan.fooo <86001920+appflowy@users.noreply.github.com>
2023-03-05 16:13:06 +0800

    chore: observer app change (#1922)

--- frontend/appflowy_tauri/src/appflowy_app/components/layout/NavigationPanel/NavigationPanel.hooks.ts
+++ frontend/appflowy_tauri/src/appflowy_app/components/layout/NavigationPanel/NavigationPanel.hooks.ts
@@ -56,13 +56,10 @@ export const useNavigationPanelHooks = function () {
 
   return {
     width,
-
     folders,
     pages,
-
     navigate,
     onPageClick,
-
     onCollapseNavigationClick,
     onFixNavigationClick,
     navigationPanelFixed,
