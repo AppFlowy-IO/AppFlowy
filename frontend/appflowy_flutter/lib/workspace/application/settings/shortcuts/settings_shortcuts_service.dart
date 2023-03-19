e820652ea61f545910fafb35845d8c52fbbc19fd
Mayur Mahajan <mayur23smahajan@gmail.com>
2023-03-19 11:58:13 +0530

    feat: service for storing shortcuts

---
+++ frontend/appflowy_flutter/lib/workspace/application/settings/shortcuts/settings_shortcuts_service.dart
@@ -0,0 +1,66 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:appflowy/startup/tasks/prelude.dart';
+import 'package:appflowy/workspace/application/settings/shortcuts/shortcuts_model.dart';
+import 'package:appflowy_editor/appflowy_editor.dart';
+
+class SettingsShortcutService {
+  late File file;
+
+  SettingsShortcutService({File? passedFile}) {
+    if (passedFile == null) {
+      initializeService();
+    } else {
+      file = passedFile;
+    }
+  }
+
+  Future<void> initializeService() async {
+    Directory flowyDir = await appFlowyDocumentDirectory();
+    file = await File('${flowyDir.path}/shortcuts/shorcuts.json')
+        .create(recursive: true);
+  }
+
+  Future<void> saveShortcuts(List<ShortcutEvent> currentShortcuts) async {
+    file = await file.writeAsString(jsonEncode(currentShortcuts.toJson()),
+        flush: true);
+  }
+
+  Future<List<ShortcutEvent>> loadShortcuts() async {
+    String shortcutsInJson = '';
+    shortcutsInJson = await file.readAsString();
+
+    if (shortcutsInJson.isEmpty) {
+      return builtInShortcutEvents;
+    } else {
+      return loadUpdatedShortcuts(shortcutsInJson);
+    }
+  }
+
+  List<ShortcutEvent> loadUpdatedShortcuts(String savedJson) {
+    final shortcuts = Shortcuts.fromJson(jsonDecode(savedJson));
+    for (final shortcut in shortcuts.shortcuts) {
+      ShortcutEvent? shortcutEvent = builtInShortcutEvents.firstWhereOrNull(
+          (sEvent) => (sEvent.key == shortcut.key &&
+              sEvent.command != shortcut.command));
+      if (shortcutEvent != null) {
+        shortcutEvent.updateCommand(command: shortcut.command);
+      }
+    }
+    return builtInShortcutEvents;
+  }
+}
+
+extension on ShortcutEvent {
+  Map<String, dynamic> toJson() => {
+        "key": key,
+        "command": command,
+      };
+}
+
+extension on List<ShortcutEvent> {
+  Map<String, dynamic> toJson() => {
+        "shortcuts": List<dynamic>.from(map((sEvent) => sEvent.toJson())),
+      };
+}
---
+++ frontend/appflowy_flutter/lib/workspace/application/settings/shortcuts/shortcuts_model.dart
@@ -0,0 +1,39 @@
+import 'package:appflowy_editor/appflowy_editor.dart';
+
+class Shortcuts {
+  Shortcuts({
+    required this.shortcuts,
+  });
+
+  List<Shortcut> shortcuts;
+
+  factory Shortcuts.fromJson(Map<String, dynamic> json) => Shortcuts(
+        shortcuts: List<Shortcut>.from(
+            json["shortcuts"].map((x) => Shortcut.fromJson(x))),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "shortcuts": List<dynamic>.from(shortcuts.map((x) => x.toJson())),
+      };
+}
+
+class Shortcut {
+  final String key;
+  final String command;
+
+  const Shortcut({
+    required this.key,
+    required this.command,
+  });
+
+  factory Shortcut.fromJson(Map<String, dynamic> json) =>
+      Shortcut(key: json["key"], command: json["command"]);
+
+  factory Shortcut.fromShortcutEvent(ShortcutEvent sEvent) =>
+      Shortcut(key: sEvent.key, command: sEvent.command);
+
+  Map<String, dynamic> toJson() => {
+        "key": key,
+        "command": command,
+      };
+}
