8471bc299d7fda83e9e0a0024724b1a19d1aea7e
qinluhe <108015703+qinluhe@users.noreply.github.com>
2023-03-22 10:46:01 +0800

    feat: block list virtualized scroll (#2023)
    * feat: block list virtualized scroll
    
    * feat: block selection
    
    * refactor: block editor
    
    * fix: block selection scroll
    
    * fix: ts error

--- frontend/appflowy_tauri/src/appflowy_app/components/block/ListBlock/BulletedListBlock.tsx
+++ frontend/appflowy_tauri/src/appflowy_app/components/block/ListBlock/BulletedListBlock.tsx
@@ -1,13 +1,13 @@
 import { Circle } from '@mui/icons-material';
 
-import BlockComponent from '../BlockList/BlockComponent';
-import { TreeNodeInterface } from '$app/interfaces/index';
+import BlockComponent from '../BlockComponent';
+import { TreeNode } from '@/appflowy_app/block_editor/view/tree_node';
 
-export default function BulletedListBlock({ title, node }: { title: JSX.Element; node: TreeNodeInterface }) {
+export default function BulletedListBlock({ title, node }: { title: JSX.Element; node: TreeNode }) {
   return (
     <div className='bulleted-list-block relative'>
       <div className='relative flex'>
-        <div className={`relative mb-2 min-w-[24px] leading-5`}>
+        <div className={`relative flex h-[calc(1.5em_+_3px_+_3px)] min-w-[24px] select-none items-center`}>
           <Circle sx={{ width: 8, height: 8 }} />
         </div>
         {title}
