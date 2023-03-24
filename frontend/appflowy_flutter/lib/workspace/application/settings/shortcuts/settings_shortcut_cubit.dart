c20e028bbc6fe5b6b3686f79ea2bed020ab39293
Mayur Mahajan <mayur23smahajan@gmail.com>
2023-03-24 14:10:21 +0530

    feat: modify cubit and test it

--- frontend/appflowy_flutter/lib/workspace/application/settings/shortcuts/settings_shortcut_cubit.dart
+++ frontend/appflowy_flutter/lib/workspace/application/settings/shortcuts/settings_shortcut_cubit.dart
@@ -1,25 +1,40 @@
+import 'package:appflowy/workspace/application/settings/shortcuts/settings_shortcuts_service.dart';
 import 'package:appflowy_editor/appflowy_editor.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class ShortcutsCubit extends Cubit<ShortcutsState> {
-  ShortcutsCubit() : super(const ShortcutsState());
+enum ShortcutsStatus { initial, updating, success, failure }
 
-  void fetchShortcuts() {
-    emit(state.copyWith(
-        status: ShortcutsStatus.success, shortcuts: builtInShortcutEvents));
+class ShortcutsCubit extends Cubit<ShortcutsState> {
+  ShortcutsCubit(this.service) : super(const ShortcutsState());
+
+  final SettingsShortcutService service;
+
+  Future<void> fetchShortcuts() async {
+    emit(state.copyWith(status: ShortcutsStatus.updating));
+    try {
+      List<ShortcutEvent> newShortcuts = await service.loadShortcuts();
+      emit(state.copyWith(
+          status: ShortcutsStatus.success, shortcuts: newShortcuts));
+    } catch (e) {
+      //could also show an error
+      debugPrint("could not load ${e.toString()}");
+      emit(state.copyWith(status: ShortcutsStatus.failure));
+    }
   }
 
-  void updateShortcut(
-      {required ShortcutEvent shortcutEvent, required String command}) {
+  Future<void> updateShortcuts() async {
     emit(state.copyWith(status: ShortcutsStatus.updating));
-    shortcutEvent.updateCommand(command: command);
-    emit(state.copyWith(
-        status: ShortcutsStatus.success, shortcuts: builtInShortcutEvents));
+
+    try {
+      service.saveShortcuts(state.shortcuts);
+      emit(state.copyWith(status: ShortcutsStatus.success));
+    } catch (_) {
+      emit(state.copyWith(status: ShortcutsStatus.failure));
+    }
   }
 }
-
-enum ShortcutsStatus { initial, updating, success, failure }
 
 class ShortcutsState extends Equatable {
   final List<ShortcutEvent> shortcuts;
