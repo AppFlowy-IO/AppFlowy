e4afc6473a6fee6701719b0100c00ccf24c6f6a4
squidrye <mihirs167@gmail.com>
2023-03-28 01:20:25 +0530

    test: added integration tests to validate insert below and replace in smart-edit highlights text

---
+++ frontend/appflowy_flutter/integration_test/open_ai_smart_menu_test.dart
@@ -0,0 +1,129 @@
+import 'package:appflowy_editor/appflowy_editor.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+import 'util/base.dart';
+import 'util/mock/mock_file_picker.dart';
+import 'util/mock/mock_openai_repository.dart';
+import 'util/util.dart';
+import 'package:flowy_infra_ui/flowy_infra_ui.dart';
+import 'package:appflowy_editor/src/render/toolbar/toolbar_widget.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('trying to reach setup dummy keys for open ai', () {
+    const location = 'test_flowy';
+    setUp(() async {
+      await TestFolder.cleanTestLocation(location);
+      await TestFolder.setTestLocation(location);
+    });
+
+    tearDown(() async {
+      await TestFolder.cleanTestLocation(location);
+    });
+    tearDownAll(() async {
+      await TestFolder.cleanTestLocation(null);
+    });
+
+    testWidgets('testing selection on open-ai smart menu replace', (tester) async {
+
+      final appFlowyEditor = await setUpOpenAITesting(tester);
+      final editorState = appFlowyEditor.editorState;
+      const dummyText = 'Some text to test open ai';
+      editorState.document.insert([0], [TextNode(delta: Delta()..insert(dummyText))]);
+      await tester.pumpAndSettle();
+
+      editorState.service.selectionService.updateSelection(Selection(
+        start: Position(path: [0], offset: 0),
+        end: Position(path: [0], offset: dummyText.length),
+      ));
+      await tester.pumpAndSettle(const Duration(milliseconds: 500));
+      await tester.pumpAndSettle();
+      expect(find.byType(ToolbarWidget), findsAtLeastNWidgets(1));
+
+      await tester.tap(find.byTooltip('AI Assistants'));
+      await tester.pumpAndSettle();
+
+      await tester.tap(find.text('Summarize'));
+      await tester.pumpAndSettle();
+
+      await tester.tap(find.byType(FlowyRichTextButton, skipOffstage: false).first);
+      await tester.pumpAndSettle();
+
+      expect(
+        editorState.service.selectionService.currentSelection.value,
+        Selection(
+          start: Position(path: [0], offset: 0),
+          end: Position(path: [0], offset: 338),
+        ),
+      );
+    });
+    testWidgets('testing selection on open-ai smart menu insert', (tester) async {
+
+      final appFlowyEditor = await setUpOpenAITesting(tester);
+      final editorState = appFlowyEditor.editorState;
+      const dummyText = 'Some text to test open ai';
+      editorState.document.insert([0], [TextNode(delta: Delta()..insert(dummyText))]);
+      await tester.pumpAndSettle();
+
+      editorState.service.selectionService.updateSelection(Selection(
+        start: Position(path: [0], offset: 0),
+        end: Position(path: [0], offset: dummyText.length),
+      ));
+      await tester.pumpAndSettle(const Duration(milliseconds: 500));
+      await tester.pumpAndSettle();
+      expect(find.byType(ToolbarWidget), findsAtLeastNWidgets(1));
+
+      await tester.tap(find.byTooltip('AI Assistants'));
+      await tester.pumpAndSettle();
+
+      await tester.tap(find.text('Summarize'));
+      await tester.pumpAndSettle();
+
+      await tester.tap(find.byType(FlowyRichTextButton, skipOffstage: false).at(1));
+      await tester.pumpAndSettle();
+
+      expect(
+        editorState.service.selectionService.currentSelection.value,
+        Selection(
+          start: Position(path: [1], offset: 0),
+          end: Position(path: [2], offset: 0),
+        ),
+      );
+    });
+  });
+}
+
+Future<AppFlowyEditor> setUpOpenAITesting(WidgetTester tester)async{
+  const folderName = 'test_flowy';
+  const dummyKey = 'dummyKey';
+  await TestFolder.cleanTestLocation(folderName);
+  await TestFolder.setTestLocation(folderName);
+
+  await tester.initializeAppFlowy();
+
+  await mockGetDirectoryPath(folderName);
+
+  await tester.tapOpenFolderButton();
+
+  await tester.wait(1000);
+  await tester.expectToSeeWelcomePage();
+
+  await mockOpenAIRepository();
+
+  await tester.openSettings();
+  await tester.openSettingsPage(SettingsPage.user);
+  await tester.enterDummyApiKey(dummyKey);
+  await tester.pumpAndSettle();
+
+  await simulateKeyDownEvent(LogicalKeyboardKey.escape);
+  await tester.pumpAndSettle();
+
+
+  final Finder editor = find.byType(AppFlowyEditor);
+  await tester.tap(editor);
+  await tester.pumpAndSettle();
+  return (tester.state(editor).widget as AppFlowyEditor);
+}
