7b61ed228f77322a515b150cc05bf3a29b2acbc3
Lucas.Xu <lucas.xu@appflowy.io>
2023-03-28 16:30:26 +0800

    fix: bold tests failed (#2122)

--- frontend/appflowy_flutter/packages/appflowy_editor/test/service/internal_key_event_handlers/markdown_syntax_to_styled_text_test.dart
+++ frontend/appflowy_flutter/packages/appflowy_editor/test/service/internal_key_event_handlers/markdown_syntax_to_styled_text_test.dart
@@ -17,7 +17,7 @@ void main() async {
       }) async {
         for (var i = 0; i < repeat; i++) {
           await editor.pressLogicKey(
-            LogicalKeyboardKey.backquote,
+            key: LogicalKeyboardKey.backquote,
           );
         }
       }
@@ -99,7 +99,7 @@ void main() async {
       }) async {
         for (var i = 0; i < repeat; i++) {
           await editor.pressLogicKey(
-            LogicalKeyboardKey.backquote,
+            key: LogicalKeyboardKey.backquote,
           );
         }
       }
@@ -157,10 +157,7 @@ void main() async {
         int repeat = 1,
       }) async {
         for (var i = 0; i < repeat; i++) {
-          await editor.pressLogicKey(
-            LogicalKeyboardKey.tilde,
-            isShiftPressed: true,
-          );
+          await editor.pressLogicKey(character: '~');
         }
       }
 
@@ -264,10 +261,7 @@ void main() async {
       int repeat = 1,
     }) async {
       for (var i = 0; i < repeat; i++) {
-        await editor.pressLogicKey(
-          LogicalKeyboardKey.digit8,
-          isShiftPressed: true,
-        );
+        await editor.pressLogicKey(character: '*');
       }
     }
 
@@ -358,10 +352,7 @@ void main() async {
       int repeat = 1,
     }) async {
       for (var i = 0; i < repeat; i++) {
-        await editor.pressLogicKey(
-          LogicalKeyboardKey.underscore,
-          isShiftPressed: true,
-        );
+        await editor.pressLogicKey(character: '_');
       }
     }
 
@@ -378,8 +369,13 @@ void main() async {
 
       await insertUnderscore(editor);
 
-      final allBold = textNode.allSatisfyBoldInSelection(Selection.single(
-          path: [0], startOffset: 0, endOffset: textNode.toPlainText().length));
+      final allBold = textNode.allSatisfyBoldInSelection(
+        Selection.single(
+          path: [0],
+          startOffset: 0,
+          endOffset: textNode.toPlainText().length,
+        ),
+      );
 
       expect(allBold, true);
       expect(textNode.toPlainText(), 'AppFlowy');
