80946faf099f2c7e9a9ee561e5f2527506f5dd74
Alex Wallen <wallenstephen@outlook.com>
2023-04-06 16:19:51 -1000

    chore: trigger CI (remove later)

--- frontend/appflowy_flutter/integration_test/board_test.dart
+++ frontend/appflowy_flutter/integration_test/board_test.dart
@@ -3,6 +3,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'util/util.dart';
 
+// Triggering CI
+
 /// Integration tests for an empty board. The [TestWorkspaceService] will load
 /// a workspace from an empty board `assets/test/workspaces/board.zip` for all
 /// tests.
