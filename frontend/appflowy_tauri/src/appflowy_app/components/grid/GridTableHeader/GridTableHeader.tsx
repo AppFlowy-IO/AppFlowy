a33fc3dd6078631a72674480f2024768296a8b3d
Mike Abebe <mikiastilahun@gmail.com>
2023-04-09 14:34:32 +0300

    feat/implement field options edit and change field option type functionality

--- frontend/appflowy_tauri/src/appflowy_app/components/grid/GridTableHeader/GridTableHeader.tsx
+++ frontend/appflowy_tauri/src/appflowy_app/components/grid/GridTableHeader/GridTableHeader.tsx
@@ -1,43 +1,21 @@
+import { DatabaseController } from '@/appflowy_app/stores/effects/database/database_controller';
+
 import AddSvg from '../../_shared/svg/AddSvg';
 import { useGridTableHeaderHooks } from './GridTableHeader.hooks';
-import { TextTypeSvg } from '../../_shared/svg/TextTypeSvg';
-import { NumberTypeSvg } from '../../_shared/svg/NumberTypeSvg';
-import { DateTypeSvg } from '../../_shared/svg/DateTypeSvg';
-import { SingleSelectTypeSvg } from '../../_shared/svg/SingleSelectTypeSvg';
-import { MultiSelectTypeSvg } from '../../_shared/svg/MultiSelectTypeSvg';
-import { ChecklistTypeSvg } from '../../_shared/svg/ChecklistTypeSvg';
-import { UrlTypeSvg } from '../../_shared/svg/UrlTypeSvg';
-import { FieldType } from '@/services/backend/models/flowy-database/field_entities';
-import { DatabaseController } from '@/appflowy_app/stores/effects/database/database_controller';
+
+import { GridTableHeaderItem } from './GridTableHeaderItem';
 import { useTranslation } from 'react-i18next';
 
 export const GridTableHeader = ({ controller }: { controller: DatabaseController }) => {
   const { fields, onAddField } = useGridTableHeaderHooks(controller);
-
   const { t } = useTranslation('');
 
   return (
     <>
       <thead>
         <tr>
-          {fields.map((field) => {
-            return (
-              <th key={field.fieldId} className='m-0 border border-l-0 border-shade-6 p-0'>
-                <div className={'flex cursor-pointer items-center px-4 py-2 hover:bg-main-secondary'}>
-                  <i className={'mr-2 h-5 w-5 text-shade-3'}>
-                    {field.fieldType === FieldType.RichText && <TextTypeSvg></TextTypeSvg>}
-                    {field.fieldType === FieldType.Number && <NumberTypeSvg></NumberTypeSvg>}
-                    {field.fieldType === FieldType.DateTime && <DateTypeSvg></DateTypeSvg>}
-                    {field.fieldType === FieldType.SingleSelect && <SingleSelectTypeSvg></SingleSelectTypeSvg>}
-                    {field.fieldType === FieldType.MultiSelect && <MultiSelectTypeSvg></MultiSelectTypeSvg>}
-                    {field.fieldType === FieldType.Checklist && <ChecklistTypeSvg></ChecklistTypeSvg>}
-                    {field.fieldType === FieldType.Checkbox && <ChecklistTypeSvg></ChecklistTypeSvg>}
-                    {field.fieldType === FieldType.URL && <UrlTypeSvg></UrlTypeSvg>}
-                  </i>
-                  <span>{field.name}</span>
-                </div>
-              </th>
-            );
+          {fields.map((field, i) => {
+            return <GridTableHeaderItem field={field} controller={controller} key={i} />;
           })}
 
           <th className='m-0 w-40 border border-r-0 border-shade-6 p-0'>
