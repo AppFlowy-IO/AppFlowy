9b1ab1ae02a851647d63b7f923dbe9241f85f7e3
Gunjan Lunkad <glunkad007@gmail.com>
2023-04-12 14:07:33 +0530

    feat: underline node file created

---
+++ frontend/appflowy_flutter/lib/plugins/document/presentation/plugins/parsers/underline_node_parser.dart
@@ -0,0 +1,13 @@
+import 'package:appflowy_editor/appflowy_editor.dart';
+
+class UnderlineNodeParser extends NodeParser {
+	const UnderlineNodeParser();
+
+	@override 
+	String get id = 'underline';
+
+	@override 
+	String transform(Node node){
+		return '<u>${node.attributes['underline']}</u>'
+	}
+}
