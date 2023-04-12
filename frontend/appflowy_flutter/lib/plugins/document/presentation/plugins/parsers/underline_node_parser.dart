a11bbef882a90194168291ad019b8f32684af841
Gunjan Lunkad <glunkad007@gmail.com>
2023-04-12 23:04:09 +0530

    feat: some changes

--- frontend/appflowy_flutter/lib/plugins/document/presentation/plugins/parsers/underline_node_parser.dart
+++ frontend/appflowy_flutter/lib/plugins/document/presentation/plugins/parsers/underline_node_parser.dart
@@ -1,13 +1,12 @@
 import 'package:appflowy_editor/appflowy_editor.dart';
 
 class UnderlineNodeParser extends NodeParser {
-	const UnderlineNodeParser();
+const UnderlineNodeParser();
+@override 
+String get id = 'underline';
 
-	@override 
-	String get id = 'underline';
-
-	@override 
-	String transform(Node node){
-		return '<u>${node.attributes['underline']}</u>'
-	}
+@override 
+String transform(Node node){
+return '<u>${node.attributes['underline']}</u>';
+}
 }
