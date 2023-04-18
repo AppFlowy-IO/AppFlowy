576eb4870ec1f1b825b7724383484e6d56c48624
qinluhe <qinluhe.twodog@gmail.com>
2023-04-18 12:58:42 +0800

    fix: node can not rerender when the node data change

--- frontend/appflowy_tauri/src/appflowy_app/components/document/Node/index.tsx
+++ frontend/appflowy_tauri/src/appflowy_app/components/document/Node/index.tsx
@@ -9,26 +9,22 @@ import { NodeContext } from '../_shared/SubscribeNode.hooks';
 function NodeComponent({ id, ...props }: { id: string } & React.HTMLAttributes<HTMLDivElement>) {
   const { node, childIds, isSelected, ref } = useNode(id);
 
-  console.log('=====', id);
-  const renderBlock = useCallback((_props: { node: Node; childIds?: string[] }) => {
-    switch (_props.node.type) {
+  const renderBlock = useCallback(() => {
+    switch (node.type) {
       case 'text': {
         return <TextBlock node={node} childIds={childIds} />;
       }
       default:
         break;
     }
-  }, []);
+  }, [node, childIds]);
 
   if (!node) return null;
 
   return (
     <NodeContext.Provider value={node}>
       <div {...props} ref={ref} data-block-id={node.id} className={`relative my-[2px] px-[2px] ${props.className}`}>
-        {renderBlock({
-          node,
-          childIds,
-        })}
+        {renderBlock()}
         <div className='block-overlay' />
         {isSelected ? <div className='pointer-events-none absolute inset-0 z-[-1] rounded-[4px] bg-[#E0F8FF]' /> : null}
       </div>
