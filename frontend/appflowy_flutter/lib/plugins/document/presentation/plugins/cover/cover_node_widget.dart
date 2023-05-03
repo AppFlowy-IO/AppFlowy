c672a2a8ba0205f5cd5ab226eafd0b7cfee2312a
Lucas.Xu <lucas.xu@appflowy.io>
2023-05-03 16:18:05 +0800

    fix: hover color for change icon button (#2447)

--- frontend/appflowy_flutter/lib/plugins/document/presentation/plugins/cover/cover_node_widget.dart
+++ frontend/appflowy_flutter/lib/plugins/document/presentation/plugins/cover/cover_node_widget.dart
@@ -412,9 +412,9 @@ class _CoverImageState extends State<_CoverImage> {
                   setOverlayButtonsHidden(true);
                 },
                 hoverColor: Theme.of(context).colorScheme.surface,
-                textColor: Theme.of(context).colorScheme.onSurface,
+                textColor: Theme.of(context).colorScheme.tertiary,
                 fillColor:
-                    Theme.of(context).colorScheme.surface.withOpacity(0.8),
+                    Theme.of(context).colorScheme.surface.withOpacity(0.5),
                 width: 120,
                 height: 28,
                 title: LocaleKeys.document_plugins_cover_changeCover.tr(),
@@ -435,13 +435,13 @@ class _CoverImageState extends State<_CoverImage> {
             maintainState: true,
             visible: !isOverlayButtonsHidden,
             child: FlowyIconButton(
-              fillColor: Theme.of(context).colorScheme.surface.withOpacity(0.8),
               hoverColor: Theme.of(context).colorScheme.surface,
+              fillColor: Theme.of(context).colorScheme.surface.withOpacity(0.5),
               iconPadding: const EdgeInsets.all(5),
               width: 28,
               icon: svgWidget(
                 'editor/delete',
-                color: Theme.of(context).colorScheme.onSurface,
+                color: Theme.of(context).colorScheme.tertiary,
               ),
               onPressed: () {
                 widget.onCoverChanged(CoverSelectionType.initial, null);
