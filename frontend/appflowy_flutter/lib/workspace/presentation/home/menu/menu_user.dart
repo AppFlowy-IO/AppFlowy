16067bba4161b9d59a833a1525c77d8a2329626a
Daniyar Nurmukhamet <nurmukhametdaniyar@gmail.com>
2023-05-15 07:37:26 +0600

    feature: added generating color in the defined HSL ranges

--- frontend/appflowy_flutter/lib/workspace/presentation/home/menu/menu_user.dart
+++ frontend/appflowy_flutter/lib/workspace/presentation/home/menu/menu_user.dart
@@ -84,15 +84,61 @@ class MenuUser extends StatelessWidget {
   }
 
   Color _generateRandomNameColor(String name) {
-    int hashCode = name.hashCode;
-    Random random = Random(hashCode);
-    Color color = Color(random.nextInt(0xffffffff));
-    double luminance = color.computeLuminance();
-    // Checking if the color is too light and returning the darker version
-    if (luminance > 0.5) {
-      return color.withOpacity(0.8);
+    final hash = name.hashCode;
+    final h = _normalizeHash(
+      hash,
+      0,
+      360,
+    );
+    final s = _normalizeHash(
+      hash,
+      50,
+      75,
+    );
+    final l = _normalizeHash(
+      hash,
+      25,
+      60,
+    );
+    final color = _hslToColor(
+      [h, s, l],
+    );
+    return color;
+  }
+
+  Color _hslToColor(List<int> hsl) {
+    final h = hsl[0] / 360.0;
+    final s = hsl[1] / 100.0;
+    final l = hsl[2] / 100.0;
+    late final double r, g, b;
+
+    if (s == 0) {
+      r = g = b = l;
+    } else {
+      final q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+      final p = 2 * l - q;
+      r = _hueToRGB(p, q, h + 1 / 3);
+      g = _hueToRGB(p, q, h);
+      b = _hueToRGB(p, q, h - 1 / 3);
     }
-    return color.withOpacity(1.0);
+
+    final red = (r * 255).round();
+    final green = (g * 255).round();
+    final blue = (b * 255).round();
+    return Color.fromARGB(255, red, green, blue);
+  }
+
+  double _hueToRGB(double p, double q, double t) {
+    if (t < 0) t += 1;
+    if (t > 1) t -= 1;
+    if (t < 1 / 6) return p + (q - p) * 6 * t;
+    if (t < 1 / 2) return q;
+    if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+    return p;
+  }
+
+  int _normalizeHash(int hash, int min, int max) {
+    return (hash % (max - min)) + min;
   }
 
   Widget _renderUserName(BuildContext context) {
