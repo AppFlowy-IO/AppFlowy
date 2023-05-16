876dbeed6bdac8fb2014c8df1b05905590e813cf
nathan <nathan@appflowy.io>
2023-05-16 13:13:25 +0800

    refactor: rm flowy-database and flowy-folder

--- frontend/rust-lib/flowy-database/src/services/field/mod.rs
+++
@@ -1,9 +0,0 @@
-mod field_builder;
-mod field_operation;
-mod type_option_builder;
-pub(crate) mod type_options;
-
-pub use field_builder::*;
-pub use field_operation::*;
-pub use type_option_builder::*;
-pub use type_options::*;
--- frontend/rust-lib/flowy-database/tests/database/sort_test/mod.rs
+++
@@ -1,4 +0,0 @@
-mod checkbox_and_text_test;
-mod multi_sort_test;
-mod script;
-mod single_sort_test;
