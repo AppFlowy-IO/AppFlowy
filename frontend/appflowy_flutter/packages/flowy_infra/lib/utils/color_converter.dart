a9baa2e3b27465a758dba54c6434997c48f600aa
Stephen (Alex) Wallen <wallenstephen@outlook.com>
2023-05-31 16:03:57 -1000

    feat: provide fallback color if plugin becomes out of date (transparent)

---
+++ frontend/appflowy_flutter/packages/flowy_infra/lib/utils/color_converter.dart
@@ -0,0 +1,19 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+class ColorConverter implements JsonConverter<Color, String> {
+  const ColorConverter();
+
+  static const Color fallback = Colors.transparent;
+
+  @override
+  Color fromJson(String radixString) {
+    final int? color = int.tryParse(radixString);
+    return color == null ? fallback : Color(color);
+  }
+
+  @override
+  String toJson(Color color) => color.value.toRadixString(16);
+}
