b719e485b8484227e22263b261928ed4677cb77b
Stephen (Alex) Wallen <wallenstephen@outlook.com>
2023-06-02 16:29:38 -1000

    fix: path not correct in generate.sh

--- frontend/scripts/code_generation/generate.sh
+++ frontend/scripts/code_generation/generate.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
+# Store the current working directory
+original_dir=$(pwd)
+
+# Change the current working directory to the script's location
+cd "$(dirname "$0")"
+
 # Call the script in the 'freezed' folder
 echo "Generating files using build_runner"
-./freezed/generate_freezed.sh "$@"
+cd freezed
+./generate_freezed.sh "$@"
+
+# Return to the main script directory
+cd ..
 
 # Call the script in the 'language_files' folder
 echo "Generating files using easy_localization"
-./language_files/generate_language_files.sh "$@"
+cd language_files
+./generate_language_files.sh "$@"
+
+# Return to the original directory
+cd "$original_dir"
