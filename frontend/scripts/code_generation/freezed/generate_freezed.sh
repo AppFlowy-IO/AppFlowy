ed76458377e879e329110b4b4324d8bbb3288d04
Stephen (Alex) Wallen <wallenstephen@outlook.com>
2023-06-03 11:40:04 -1000

    chore: clean build_runner before executing

--- frontend/scripts/code_generation/freezed/generate_freezed.sh
+++ frontend/scripts/code_generation/freezed/generate_freezed.sh
@@ -8,7 +8,7 @@ cd ../../../appflowy_flutter
 
 # Navigate to the appflowy_flutter directory and generate files
 echo "Generating files for appflowy_flutter"
-flutter clean > /dev/null 2>&1 && flutter packages pub get > /dev/null 2>&1 && flutter pub run build_runner build --delete-conflicting-outputs
+flutter clean > /dev/null 2>&1 && flutter packages pub get > /dev/null 2>&1 && flutter pub run build_runner clean && flutter pub run build_runner build --delete-conflicting-outputs
 echo "Done generating files for appflowy_flutter"
 
 # Generate files for packages
@@ -22,7 +22,7 @@ for d in */ ; do
     if [ -f "pubspec.yaml" ]; then
         echo "Generating freezed files in $d..."
         echo "Please wait while we clean the project and fetch the dependencies."
-        flutter clean > /dev/null 2>&1 && flutter packages pub get > /dev/null 2>&1 && flutter pub run build_runner build --delete-conflicting-outputs
+        flutter clean > /dev/null 2>&1 && flutter packages pub get > /dev/null 2>&1 && flutter pub run build_runner clean && flutter pub run build_runner build --delete-conflicting-outputs
         echo "Done running build command in $d"
     else
         echo "No pubspec.yaml found in $d, it can't be a Dart project. Skipping."
