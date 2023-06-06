6d41e58086d7ba4320a71944d538d06afa67d372
nathan <nathan@appflowy.io>
2023-06-06 15:46:45 +0800

    ci: fix s fmt

--- frontend/rust-lib/flowy-sqlite/src/schema.rs
+++ frontend/rust-lib/flowy-sqlite/src/schema.rs
@@ -22,7 +22,4 @@ diesel::table! {
     }
 }
 
-diesel::allow_tables_to_appear_in_same_query!(
-    collab_snapshot,
-    user_table,
-);
+diesel::allow_tables_to_appear_in_same_query!(collab_snapshot, user_table,);
