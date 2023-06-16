b093814c40071cde612cbddb3dfc1eb041c1bfaa
qinluhe <qinluhe.twodog@gmail.com>
2023-06-16 19:37:23 +0800

    fix: optimizate selectOptionByUpDown

--- frontend/appflowy_tauri/src/appflowy_app/utils/document/menu.ts
+++ frontend/appflowy_tauri/src/appflowy_app/utils/document/menu.ts
@@ -1,20 +1,8 @@
 export function selectOptionByUpDown(isUp: boolean, selected: string | null, options: string[]) {
   const index = options.findIndex((option) => option === selected);
+  const length = options.length;
 
-  let nextIndex = -1;
-
-  if (isUp) {
-    nextIndex = index - 1;
-  } else {
-    nextIndex = index + 1;
-  }
-
-  if (nextIndex < 0) {
-    nextIndex = options.length - 1;
-  } else if (nextIndex >= options.length) {
-    nextIndex = 0;
-  }
-
+  const nextIndex = isUp ? (index - 1 + length) % length : (index + 1) % length;
   const nextOption = options[nextIndex];
 
   return nextOption;
