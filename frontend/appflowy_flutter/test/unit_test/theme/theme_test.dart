d2f792fc5b3c31939a724c767268080c75e0d2a6
Stephen (Alex) Wallen <wallenstephen@outlook.com>
2023-06-20 10:32:10 -1000

    fix: theme tests

--- frontend/appflowy_flutter/test/unit_test/theme/theme_test.dart
+++ frontend/appflowy_flutter/test/unit_test/theme/theme_test.dart
@@ -1,28 +1,80 @@
+import 'package:flowy_infra/colorscheme/colorscheme.dart';
+import 'package:flowy_infra/plugins/service/location_service.dart';
+import 'package:flowy_infra/plugins/service/models/flowy_dynamic_plugin.dart';
+import 'package:flowy_infra/plugins/service/plugin_service.dart';
 import 'package:flowy_infra/theme.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+class MockPluginService implements FlowyPluginService {
+  @override
+  Future<void> addPlugin(FlowyDynamicPlugin plugin) =>
+      throw UnimplementedError();
+
+  @override
+  Future<FlowyDynamicPlugin?> lookup({required String name}) =>
+      throw UnimplementedError();
+
+  @override
+  Future<DynamicPluginLibrary> get plugins async => const Iterable.empty();
+
+  @override
+  void setLocation(PluginLocationService locationService) =>
+      throw UnimplementedError();
+}
+
 void main() {
-  TestWidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
+  group('AppTheme', () {
+    test('fallback theme', () {
+      const theme = AppTheme.fallback;
 
-  group('AppTheme tests', () {
-    test(
-      'AppTheme.fromName succeeds when builtin theme is requested',
-      () async {
-        expect(
-          () => AppTheme.fromName(BuiltInTheme.defaultTheme),
-          returnsNormally,
-        );
-      },
-    );
+      expect(theme.builtIn, true);
+      expect(theme.themeName, BuiltInTheme.defaultTheme);
+      expect(theme.lightTheme, isA<FlowyColorScheme>());
+      expect(theme.darkTheme, isA<FlowyColorScheme>());
+    });
 
-    test(
-      'AppTheme.fromName fails when a garbage theme name is provided',
-      () async {
+    test('built-in themes', () {
+      final themes = AppTheme.builtins;
+
+      expect(themes, isNotEmpty);
+      for (final theme in themes) {
+        expect(theme.builtIn, true);
         expect(
-          () => AppTheme.fromName('garbage'),
-          throwsException,
+          theme.themeName,
+          anyOf([
+            BuiltInTheme.defaultTheme,
+            BuiltInTheme.dandelion,
+            BuiltInTheme.lavender,
+          ]),
         );
-      },
-    );
+        expect(theme.lightTheme, isA<FlowyColorScheme>());
+        expect(theme.darkTheme, isA<FlowyColorScheme>());
+      }
+    });
+
+    test('fromName returns existing theme', () async {
+      final theme = await AppTheme.fromName(
+        BuiltInTheme.defaultTheme,
+        pluginService: MockPluginService(),
+      );
+
+      expect(theme, isNotNull);
+      expect(theme.builtIn, true);
+      expect(theme.themeName, BuiltInTheme.defaultTheme);
+      expect(theme.lightTheme, isA<FlowyColorScheme>());
+      expect(theme.darkTheme, isA<FlowyColorScheme>());
+    });
+
+    test('fromName throws error for non-existent theme', () async {
+      expect(
+        () async => await AppTheme.fromName(
+          'bogus',
+          pluginService: MockPluginService(),
+        ),
+        throwsArgumentError,
+      );
+    });
   });
 }
