411f341855144d1c1a453417f3af237c5b09f3a1
Stephen (Alex) Wallen <wallenstephen@outlook.com>
2023-06-29 11:48:34 -1000

    refactor: appearance for settings appearance view

--- frontend/appflowy_flutter/lib/workspace/presentation/settings/widgets/settings_appearance_view.dart
+++ frontend/appflowy_flutter/lib/workspace/presentation/settings/widgets/settings_appearance_view.dart
@@ -1,10 +1,13 @@
 import 'package:appflowy/generated/locale_keys.g.dart';
 import 'package:appflowy/workspace/application/appearance.dart';
 import 'package:appflowy/workspace/presentation/settings/widgets/theme_upload/theme_upload_view.dart';
+import 'package:appflowy/workspace/presentation/settings/widgets/theme_upload/upload_new_theme_widget.dart';
+import 'package:appflowy/workspace/presentation/widgets/dialogs.dart';
 import 'package:appflowy_popover/appflowy_popover.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flowy_infra/image.dart';
 import 'package:flowy_infra/plugins/bloc/dynamic_plugin_bloc.dart';
+import 'package:flowy_infra/plugins/bloc/dynamic_plugin_event.dart';
 import 'package:flowy_infra/plugins/bloc/dynamic_plugin_state.dart';
 import 'package:flowy_infra/plugins/service/models/flowy_dynamic_plugin.dart';
 import 'package:flowy_infra/theme.dart';
@@ -29,10 +32,9 @@ class SettingsAppearanceView extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 BrightnessSetting(currentThemeMode: state.themeMode),
-                const ThemeUploadWidget(),
-                const SizedBox(height: ThemePreviewGrid.mainAxisSpacing),
-                ThemePreviewBuilder(
-                  currentTheme: state.appTheme,
+                ColorSchemeSetting(
+                  currentTheme: state.appTheme.themeName,
+                  bloc: context.read<DynamicPluginBloc>(),
                 ),
               ],
             );
@@ -43,121 +45,162 @@ class SettingsAppearanceView extends StatelessWidget {
   }
 }
 
-class ThemePreviewBuilder extends StatelessWidget {
-  const ThemePreviewBuilder({
+class ColorSchemeSetting extends StatelessWidget {
+  const ColorSchemeSetting({
     super.key,
     required this.currentTheme,
-  });
-
-  final AppTheme currentTheme;
-
-  Widget get uninitialized => const SizedBox.shrink();
-  Widget get processing => const SizedBox.shrink();
-  Widget get compilationFailure => const SizedBox.shrink();
-  Widget get deletionFailure => const SizedBox.shrink();
-  Widget get deletionSuccess => const SizedBox.shrink();
-  Widget get compilationSuccess => const SizedBox.shrink();
-  Widget ready(Iterable<FlowyDynamicPlugin> plugins) {
-    return Column(
-      children: [
-        Row(
-          children: [
-            FlowyText.medium(
-              LocaleKeys.settings_appearance_builtInsLabel.tr(),
-              textAlign: TextAlign.left,
-            ),
-          ],
-        ),
-        const SizedBox(height: ThemePreviewGrid.mainAxisSpacing),
-        ThemePreviewGrid(
-          currentTheme: currentTheme.themeName,
-          themes: AppTheme.builtins,
-        ),
-        const SizedBox(height: ThemePreviewGrid.mainAxisSpacing),
-        Row(
-          children: [
-            FlowyText.medium(
-              LocaleKeys.settings_appearance_pluginsLabel.tr(),
-              textAlign: TextAlign.left,
-            ),
-          ],
-        ),
-        const SizedBox(height: ThemePreviewGrid.mainAxisSpacing),
-        ThemePreviewGrid(
-          currentTheme: currentTheme.themeName,
-          themes: plugins.map((plugin) => plugin.theme).whereType<AppTheme>(),
-        ),
-        const SizedBox(height: ThemePreviewGrid.mainAxisSpacing),
-      ],
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        BlocBuilder<DynamicPluginBloc, DynamicPluginState>(
-          buildWhen: (previous, current) => current is Ready,
-          builder: (context, state) {
-            return state.when(
-              uninitialized: () => uninitialized,
-              processing: () => processing,
-              compilationFailure: (message) => compilationFailure,
-              deletionFailure: (message) => deletionFailure,
-              deletionSuccess: () => deletionSuccess,
-              compilationSuccess: () => compilationSuccess,
-              ready: (plugins) => ready(plugins),
-            );
-          },
-        ),
-      ],
-    );
-  }
-}
-
-class ThemePreviewGrid extends StatefulWidget {
-  const ThemePreviewGrid({
-    super.key,
-    required this.currentTheme,
-    required this.themes,
+    required this.bloc,
   });
 
   final String currentTheme;
-  final Iterable<AppTheme> themes;
-  static const double crossAxisSpacing = 16;
-  static const double mainAxisSpacing = 16;
-
-  @override
-  State<ThemePreviewGrid> createState() => _ThemePreviewGridState();
-}
-
-class _ThemePreviewGridState extends State<ThemePreviewGrid> {
-  int get crossAxisCount {
-    final factor = FormFactor.fromWidth(MediaQuery.of(context).size.width);
-    return switch (factor) {
-      FormFactor.mobile => 1,
-      FormFactor.tablet => 2,
-      FormFactor.desktop => 3,
-    };
-  }
+  final DynamicPluginBloc bloc;
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      shrinkWrap: true,
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: crossAxisCount,
-        crossAxisSpacing: ThemePreviewGrid.crossAxisSpacing,
-        mainAxisSpacing: ThemePreviewGrid.mainAxisSpacing,
+    return Row(
+      children: [
+        Expanded(
+          child: FlowyText.medium(
+            LocaleKeys.settings_appearance_theme.tr(),
+            overflow: TextOverflow.ellipsis,
+          ),
+        ),
+        ThemeUploadOverlayButton(bloc: bloc),
+        const SizedBox(width: 4),
+        ThemeSelectionPopover(currentTheme: currentTheme, bloc: bloc),
+      ],
+    );
+  }
+}
+
+class ThemeUploadOverlayButton extends StatelessWidget {
+  const ThemeUploadOverlayButton({super.key, required this.bloc});
+
+  final DynamicPluginBloc bloc;
+
+  @override
+  Widget build(BuildContext context) {
+    return FlowyIconButton(
+      width: 24,
+      icon: const FlowySvg(name: 'folder'),
+      iconColorOnHover: Theme.of(context).colorScheme.onPrimary,
+      onPressed: () => Dialogs.show(
+        context,
+        child: BlocProvider<DynamicPluginBloc>.value(
+          value: bloc,
+          child: const FlowyDialog(
+            constraints: BoxConstraints(maxHeight: 300),
+            child: ThemeUploadWidget(),
+          ),
+        ),
+      ).then((value) {
+        if (value == null) return;
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: FlowyText.medium(
+              color: Theme.of(context).colorScheme.onPrimary,
+              LocaleKeys.settings_appearance_themeUpload_uploadSuccess.tr(),
+            ),
+          ),
+        );
+      }),
+    );
+  }
+}
+
+class ThemeSelectionPopover extends StatelessWidget {
+  const ThemeSelectionPopover({
+    super.key,
+    required this.currentTheme,
+    required this.bloc,
+  });
+
+  final String currentTheme;
+  final DynamicPluginBloc bloc;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppFlowyPopover(
+      direction: PopoverDirection.bottomWithRightAligned,
+      child: FlowyTextButton(
+        currentTheme,
+        fontColor: Theme.of(context).colorScheme.onBackground,
+        fillColor: Colors.transparent,
+        onPressed: () {},
       ),
-      itemCount: widget.themes.length,
-      itemBuilder: (context, index) {
-        final theme = widget.themes.elementAt(index);
-        return ThemePreview(
-          theme: theme,
-          isCurrentTheme: theme.themeName == widget.currentTheme,
+      popupBuilder: (BuildContext context) {
+        return IntrinsicWidth(
+          child: BlocBuilder<DynamicPluginBloc, DynamicPluginState>(
+            bloc: bloc..add(DynamicPluginEvent.load()),
+            buildWhen: (previous, current) => current is Ready,
+            builder: (context, state) {
+              return state.when(
+                uninitialized: () => const SizedBox.shrink(),
+                processing: () => const SizedBox.shrink(),
+                compilationFailure: (message) => const SizedBox.shrink(),
+                deletionFailure: (message) => const SizedBox.shrink(),
+                deletionSuccess: () => const SizedBox.shrink(),
+                compilationSuccess: () => const SizedBox.shrink(),
+                ready: (plugins) => Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    ...AppTheme.builtins
+                        .map(
+                          (theme) => _themeItemButton(context, theme.themeName),
+                        )
+                        .toList(),
+                    if (plugins.isNotEmpty) ...[
+                      const Divider(),
+                      ...plugins
+                          .map((plugin) => plugin.theme)
+                          .whereType<AppTheme>()
+                          .map(
+                            (theme) => _themeItemButton(
+                                context, theme.themeName, false),
+                          )
+                          .toList()
+                    ],
+                  ],
+                ),
+              );
+            },
+          ),
         );
       },
+    );
+  }
+
+  Widget _themeItemButton(
+    BuildContext context,
+    String theme, [
+    bool isBuiltin = true,
+  ]) {
+    return SizedBox(
+      height: 32,
+      child: Row(
+        children: [
+          Expanded(
+            child: FlowyButton(
+              text: FlowyText.medium(theme),
+              rightIcon: currentTheme == theme
+                  ? const FlowySvg(name: 'grid/checkmark')
+                  : null,
+              onTap: () {
+                if (currentTheme != theme) {
+                  context.read<AppearanceSettingsCubit>().setTheme(theme);
+                }
+              },
+            ),
+          ),
+          if (!isBuiltin)
+            FlowyIconButton(
+              icon: const FlowySvg(name: 'home/close'),
+              width: 20,
+              onPressed: () =>
+                  bloc.add(DynamicPluginEvent.removePlugin(name: theme)),
+            )
+        ],
+      ),
     );
   }
 }
