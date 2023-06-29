5ec62fd9b34a66733ec70b762522b8ea141316f0
Stephen (Alex) Wallen <wallenstephen@outlook.com>
2023-06-28 18:38:23 -1000

    feat: add fontFamily to document appearance cubit

--- frontend/appflowy_flutter/lib/plugins/document/presentation/more/cubit/document_appearance_cubit.dart
+++ frontend/appflowy_flutter/lib/plugins/document/presentation/more/cubit/document_appearance_cubit.dart
@@ -2,25 +2,33 @@ import 'package:bloc/bloc.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 const String _kDocumentAppearanceFontSize = 'kDocumentAppearanceFontSize';
+const String _kDocumentAppearanceFontFamily = 'kDocumentAppearanceFontFamily';
 
 class DocumentAppearance {
   const DocumentAppearance({
     required this.fontSize,
+    required this.fontFamily,
   });
 
   final double fontSize;
+  final String fontFamily;
   // Will be supported...
   // final String fontName;
 
-  DocumentAppearance copyWith({double? fontSize}) {
+  DocumentAppearance copyWith({
+    double? fontSize,
+    String? fontFamily,
+  }) {
     return DocumentAppearance(
       fontSize: fontSize ?? this.fontSize,
+      fontFamily: fontFamily ?? this.fontFamily,
     );
   }
 }
 
 class DocumentAppearanceCubit extends Cubit<DocumentAppearance> {
-  DocumentAppearanceCubit() : super(const DocumentAppearance(fontSize: 16.0));
+  DocumentAppearanceCubit()
+      : super(const DocumentAppearance(fontSize: 16.0, fontFamily: 'Poppins'));
 
   void fetch() async {
     final prefs = await SharedPreferences.getInstance();
@@ -48,6 +56,16 @@ class DocumentAppearanceCubit extends Cubit<DocumentAppearance> {
     emit(
       state.copyWith(
         fontSize: fontSize,
+      ),
+    );
+  }
+
+  void syncFontFamily(String fontFamily) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString(_kDocumentAppearanceFontFamily, fontFamily);
+    emit(
+      state.copyWith(
+        fontFamily: fontFamily,
       ),
     );
   }
