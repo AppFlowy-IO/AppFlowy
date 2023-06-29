6fe5cc7ac8251611f1f7d21430965c88358f5791
Stephen (Alex) Wallen <wallenstephen@outlook.com>
2023-06-28 20:19:48 -1000

    refactor: levenshtein

--- frontend/appflowy_flutter/lib/workspace/presentation/settings/widgets/levenshtein.dart
+++ frontend/appflowy_flutter/lib/workspace/presentation/settings/widgets/levenshtein.dart
@@ -1,38 +1,25 @@
 import 'dart:math';
 
-/// Levenshtein algorithm implementation based on:
-/// http://en.wikipedia.org/wiki/Levenshtein_distance#Iterative_with_two_matrix_rows
-int levenshtein(
-  String s,
-  String t, {
-  bool caseSensitive = true,
-}) {
+int levenshtein(String s, String t, {bool caseSensitive = true}) {
   if (!caseSensitive) {
     s = s.toLowerCase();
     t = t.toLowerCase();
   }
+
   if (s == t) return 0;
-  if (s.isEmpty) return t.length;
-  if (t.isEmpty) return s.length;
 
-  final List<int> v0 = List<int>.filled(t.length + 1, 0);
-  final List<int> v1 = List<int>.filled(t.length + 1, 0);
+  final v0 = List<int>.generate(t.length + 1, (i) => i);
+  final v1 = List<int>.filled(t.length + 1, 0);
 
-  for (int i = 0; i < t.length + 1; i < i++) {
-    v0[i] = i;
-  }
-
-  for (int i = 0; i < s.length; i++) {
+  for (var i = 0; i < s.length; i++) {
     v1[0] = i + 1;
 
-    for (int j = 0; j < t.length; j++) {
-      final int cost = (s[i] == t[j]) ? 0 : 1;
+    for (var j = 0; j < t.length; j++) {
+      final cost = (s[i] == t[j]) ? 0 : 1;
       v1[j + 1] = min(v1[j] + 1, min(v0[j + 1] + 1, v0[j] + cost));
     }
 
-    for (int j = 0; j < t.length + 1; j++) {
-      v0[j] = v1[j];
-    }
+    v0.setAll(0, v1);
   }
 
   return v1[t.length];
