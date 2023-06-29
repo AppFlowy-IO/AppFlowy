da580b16a7054fbc7d1544a63c099cd6f588c7e6
Stephen (Alex) Wallen <wallenstephen@outlook.com>
2023-06-28 18:53:23 -1000

    fix: use fontFamily in document appearance cubit

--- frontend/appflowy_flutter/lib/plugins/document/presentation/more/cubit/document_appearance_cubit.dart
+++ frontend/appflowy_flutter/lib/plugins/document/presentation/more/cubit/document_appearance_cubit.dart
@@ -1,8 +1,6 @@
+import 'package:appflowy/core/config/kv_keys.dart';
 import 'package:bloc/bloc.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
-const String _kDocumentAppearanceFontSize = 'kDocumentAppearanceFontSize';
-const String _kDocumentAppearanceFontFamily = 'kDocumentAppearanceFontFamily';
 
 class DocumentAppearance {
   const DocumentAppearance({
@@ -32,7 +30,10 @@ class DocumentAppearanceCubit extends Cubit<DocumentAppearance> {
 
   void fetch() async {
     final prefs = await SharedPreferences.getInstance();
-    final fontSize = prefs.getDouble(_kDocumentAppearanceFontSize) ?? 16.0;
+    final fontSize =
+        prefs.getDouble(KVKeys.kDocumentAppearanceFontSize) ?? 16.0;
+    final fontFamily =
+        prefs.getString(KVKeys.kDocumentAppearanceFontFamily) ?? 'Poppins';
 
     if (isClosed) {
       return;
@@ -41,13 +42,14 @@ class DocumentAppearanceCubit extends Cubit<DocumentAppearance> {
     emit(
       state.copyWith(
         fontSize: fontSize,
+        fontFamily: fontFamily,
       ),
     );
   }
 
   void syncFontSize(double fontSize) async {
     final prefs = await SharedPreferences.getInstance();
-    prefs.setDouble(_kDocumentAppearanceFontSize, fontSize);
+    prefs.setDouble(KVKeys.kDocumentAppearanceFontSize, fontSize);
 
     if (isClosed) {
       return;
@@ -62,7 +64,12 @@ class DocumentAppearanceCubit extends Cubit<DocumentAppearance> {
 
   void syncFontFamily(String fontFamily) async {
     final prefs = await SharedPreferences.getInstance();
-    prefs.setString(_kDocumentAppearanceFontFamily, fontFamily);
+    prefs.setString(KVKeys.kDocumentAppearanceFontFamily, fontFamily);
+
+    if (isClosed) {
+      return;
+    }
+
     emit(
       state.copyWith(
         fontFamily: fontFamily,
