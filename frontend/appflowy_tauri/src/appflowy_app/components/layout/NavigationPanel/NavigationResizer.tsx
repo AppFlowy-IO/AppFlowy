8ef48a527d3cb8af7d1c0f0e711f04bbcea3c9f0
ascarbek <ascarbek@gmail.com>
2023-07-05 23:53:50 +0600

    chore: nav panel resize fix

--- frontend/appflowy_tauri/src/appflowy_app/components/layout/NavigationPanel/NavigationResizer.tsx
+++ frontend/appflowy_tauri/src/appflowy_app/components/layout/NavigationPanel/NavigationResizer.tsx
@@ -6,21 +6,21 @@ import { navigationWidthActions } from '$app_reducers/navigation-width/slice';
 export const NavigationResizer = ({ minWidth }: { minWidth: number }) => {
   const width = useAppSelector((state) => state.navigationWidth);
   const appDispatch = useAppDispatch();
-  const { onMouseDown, movementX } = useResizer();
+  const { onMouseDown, newSizeX } = useResizer();
 
   useEffect(() => {
-    if (width + movementX < minWidth) {
+    if (newSizeX < minWidth) {
       appDispatch(navigationWidthActions.changeWidth(minWidth));
     } else {
-      appDispatch(navigationWidthActions.changeWidth(width + movementX));
+      appDispatch(navigationWidthActions.changeWidth(newSizeX));
     }
-  }, [movementX]);
+  }, [newSizeX]);
 
   return (
     <button
       className={'fixed z-10 h-full w-[15px] cursor-ew-resize'}
       style={{ left: `${width - 8}px`, userSelect: 'none' }}
-      onMouseDown={onMouseDown}
+      onMouseDown={(e) => onMouseDown(e, width)}
     ></button>
   );
 };
