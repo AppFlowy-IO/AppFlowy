4d01a87bcc818bf64f0d3bee57d37399837d69b2
squidrye <mihirs167@gmail.com>
2023-07-06 14:36:52 +0000

    feat: added service handlers for favorites

---
+++ frontend/appflowy_flutter/lib/workspace/application/favorite/favorite_service.dart
@@ -0,0 +1,15 @@
+import 'package:appflowy_backend/dispatch/dispatch.dart';
+import 'package:appflowy_backend/protobuf/flowy-error/errors.pb.dart';
+import 'package:appflowy_backend/protobuf/flowy-folder2/protobuf.dart';
+import 'package:dartz/dartz.dart';
+
+class FavoriteService {
+  Future<Either<RepeatedFavoritesPB, FlowyError>> readFavorites() {
+    return FolderEventReadFavorites().send();
+  }
+
+  Future<Either<Unit, FlowyError>> toggleFavorite(String viewId) {
+    final id = RepeatedViewIdPB.create()..items.add(viewId);
+    return FolderEventToggleFavorite(id).send();
+  }
+}
