4223d8c27de502d3c9c5523262a6edab2f54eb0d
squidrye <mihirs167@gmail.com>
2023-07-06 14:55:42 +0000

    feat: added struct for favorites

--- frontend/rust-lib/flowy-folder2/src/entities/mod.rs
+++ frontend/rust-lib/flowy-folder2/src/entities/mod.rs
@@ -1,9 +1,11 @@
+pub mod favorite;
 mod import;
 mod parser;
 pub mod trash;
 pub mod view;
 pub mod workspace;
 
+pub use favorite::*;
 pub use import::*;
 pub use trash::*;
 pub use view::*;
--- frontend/rust-lib/flowy-folder2/src/entities/parser/mod.rs
+++ frontend/rust-lib/flowy-folder2/src/entities/parser/mod.rs
@@ -1,5 +1,6 @@
 // pub mod app;
 pub mod empty_str;
+pub mod favourite;
 pub mod trash;
 pub mod view;
 pub mod workspace;
