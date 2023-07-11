ed90e8e54c63b90b6caa6dd64339eab040544340
Mukund-Tandon <mukundtandon1211@gmail.com>
2023-07-11 21:01:28 +0530

    feat: added card in import pannel to support import from notion

--- frontend/appflowy_flutter/lib/workspace/presentation/home/menu/app/header/import/import_panel.dart
+++ frontend/appflowy_flutter/lib/workspace/presentation/home/menu/app/header/import/import_panel.dart
@@ -10,6 +10,7 @@ import 'package:appflowy/workspace/presentation/home/menu/app/header/import/impo
 import 'package:appflowy/workspace/presentation/home/menu/app/header/import/import_type.dart';
 import 'package:appflowy/workspace/presentation/home/menu/app/header/import/importer/notion_importer.dart';
 import 'package:appflowy_backend/protobuf/flowy-folder2/protobuf.dart';
+import 'package:appflowy_popover/appflowy_popover.dart';
 import 'package:flowy_infra/file_picker/file_picker_service.dart';
 import 'package:appflowy_editor/appflowy_editor.dart';
 import 'package:flowy_infra_ui/flowy_infra_ui.dart';
@@ -54,7 +55,7 @@ Future<void> showImportPanel(
 }
 
 class ImportPanel extends StatelessWidget {
-  const ImportPanel({
+  ImportPanel({
     super.key,
     required this.parentViewId,
     required this.importCallback,
@@ -62,26 +63,10 @@ class ImportPanel extends StatelessWidget {
 
   final String parentViewId;
   final ImportCallback importCallback;
-
+  final PopoverController popoverController = PopoverController();
   @override
   Widget build(BuildContext context) {
-    final importCards = _buildImportCards(context);
-    final width = MediaQuery.of(context).size.width * 0.7;
-    final height = width * 0.5;
-    return FlowyContainer(
-      Theme.of(context).colorScheme.surface,
-      height: height,
-      width: width,
-      child: GridView.count(
-        childAspectRatio: 1 / .2,
-        crossAxisCount: 2,
-        children: importCards,
-      ),
-    );
-  }
-
-  List<Widget> _buildImportCards(BuildContext context) {
-    final importCards = ImportType.values
+    List<Widget> importCards = ImportType.values
         .where((element) => element.enableOnRelease)
         .map(
           (e) => Card(
@@ -105,21 +90,122 @@ class ImportPanel extends StatelessWidget {
         .toList();
     importCards.add(
       Card(
-        child: ImportFromNotionWidget(
-          callback: (type, path) async {
-            if (path != null) {
-              final notionImporter = NotionImporter(
-                parentViewId: parentViewId,
-              );
-              await notionImporter.importFromNotion(type, path);
-            }
+        child: AppFlowyPopover(
+          popupBuilder: (BuildContext context) {
+            return Container(
+              color: Theme.of(context).colorScheme.surface,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: ImportFromNotionType.values
+                    .map(
+                      (e) => Card(
+                        child: FlowyButton(
+                          leftIconSize: const Size.square(20),
+                          text: FlowyText.medium(
+                            e.toString(),
+                            fontSize: 15,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          onTap: () async {
+                            popoverController.close();
+                            await FlowyOverlay.show(
+                              context: context,
+                              builder: (context) =>
+                                  _uploadFileToImportFromOverlay(context, e),
+                            );
+                          },
+                        ),
+                      ),
+                    )
+                    .toList(),
+              ),
+            );
           },
+          controller: popoverController,
+          constraints: BoxConstraints.loose(const Size(200, 200)),
+          direction: PopoverDirection.bottomWithCenterAligned,
+          margin: EdgeInsets.zero,
+          triggerActions: PopoverTriggerFlags.none,
+          child: FlowyButton(
+            leftIcon: Icon(Icons.abc_outlined),
+            leftIconSize: const Size.square(20),
+            text: const FlowyText.medium(
+              'Import from Notion',
+              fontSize: 15,
+              overflow: TextOverflow.ellipsis,
+            ),
+            onTap: () async {
+              popoverController.show();
+            },
+          ),
+        ),
+      ),
+    );
+    final width = MediaQuery.of(context).size.width * 0.7;
+    final height = width * 0.5;
+    return FlowyContainer(
+      Theme.of(context).colorScheme.surface,
+      height: height,
+      width: width,
+      child: GridView.count(
+        childAspectRatio: 1 / .2,
+        crossAxisCount: 2,
+        children: importCards,
+      ),
+    );
+  }
+
+  Widget _uploadFileToImportFromOverlay(
+      BuildContext context, ImportFromNotionType importFromNotionType) {
+    return FlowyDialog(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      title: FlowyText.semibold(
+        'Import Notion ${importFromNotionType.toString()}',
+        fontSize: 20,
+        color: Theme.of(context).colorScheme.tertiary,
+      ),
+      constraints: BoxConstraints.loose(const Size(300, 200)),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 10.0,
+          horizontal: 20.0,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text('1. Go to the page you want to export'),
+            const Text('2. Click on the three dots on the top right corner'),
+            const Text('3. Click on export'),
+            const Text('4. Click on Markdown & CSV'),
+            const Text('5. Click on export'),
+            const Text('6. Select the file you just downloaded'),
+            const SizedBox(height: 20),
+            Center(
+              child: FlowyButton(
+                text: const FlowyText.medium(
+                  'Upload zip file',
+                  fontSize: 15,
+                  overflow: TextOverflow.ellipsis,
+                  textAlign: TextAlign.center,
+                ),
+                onTap: () async {
+                  await _importFromNotion(parentViewId, importFromNotionType);
+                  if (context.mounted) {
+                    FlowyOverlay.pop(context);
+                  }
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
     return importCards;
   }
 
+  Future<void> _importFromNotion(
+      String parentViewId, ImportFromNotionType importFromNotionType) async {}
   Future<void> _importFile(String parentViewId, ImportType importType) async {
     final result = await getIt<FilePickerService>().pickFiles(
       type: FileType.custom,
@@ -136,6 +222,7 @@ class ImportPanel extends StatelessWidget {
         continue;
       }
       final data = await File(path).readAsString();
+      print(data);
       final name = p.basenameWithoutExtension(path);
 
       switch (importType) {
