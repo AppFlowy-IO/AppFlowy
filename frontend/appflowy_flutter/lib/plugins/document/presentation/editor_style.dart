0f577761a04e3ab67765a1688bd08f879b2bd1d4
Mukund-Tandon <mukundtandon1211@gmail.com>
2023-07-14 19:45:55 +0530

    fix: support to import images from notion page

--- frontend/appflowy_flutter/lib/plugins/document/presentation/editor_style.dart
+++ frontend/appflowy_flutter/lib/plugins/document/presentation/editor_style.dart
@@ -89,13 +89,13 @@ class EditorStyleCustomizer {
         ),
         italic: baseTextStyle(fontFamily).copyWith(
           fontStyle: FontStyle.italic,
-          ),
+        ),
         underline: baseTextStyle(fontFamily).copyWith(
           decoration: TextDecoration.underline,
-          ),
+        ),
         strikethrough: baseTextStyle(fontFamily).copyWith(
           decoration: TextDecoration.lineThrough,
-          ),
+        ),
         href: baseTextStyle(fontFamily).copyWith(
           color: theme.colorScheme.primary,
           decoration: TextDecoration.underline,
