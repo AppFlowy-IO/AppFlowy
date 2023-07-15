56d58af9b2837d1c8e9dd9c8dac66d410834f69c
Mukund-Tandon <mukundtandon1211@gmail.com>
2023-07-15 10:29:16 +0530

    fixed: name of page being imported and a error in importing

--- frontend/appflowy_flutter/lib/workspace/presentation/home/menu/app/header/import/import_panel.dart
+++ frontend/appflowy_flutter/lib/workspace/presentation/home/menu/app/header/import/import_panel.dart
@@ -291,7 +291,7 @@ class ImportPanel extends StatelessWidget {
     }
 
     final File zipfile = File(result.files[0].path!);
-    final name = p.basenameWithoutExtension(zipfile.path);
+    
     final bytes = await zipfile.readAsBytes();
     final unzipped = ZipDecoder().decodeBytes(bytes);
     dynamic markdownFile;
@@ -308,6 +308,7 @@ class ImportPanel extends StatelessWidget {
     if (markdownFile == null) {
       return;
     }
+    final name = p.basenameWithoutExtension(markdownFile.name);
     //This for will help us store image assets of our page
     final List<ArchiveFile> images = [];
     for (final file in unzipped) {
@@ -332,7 +333,7 @@ class ImportPanel extends StatelessWidget {
     );
     if (data != null) {
       await ImportBackendService.importData(
-        bytes,
+        data,
         name,
         parentViewId,
         ImportTypePB.HistoryDocument,
