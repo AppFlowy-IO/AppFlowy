e143e75e4a65bcc4e845616a1fb8fb5235c043cf
Mukund-Tandon <mukundtandon1211@gmail.com>
2023-07-20 14:07:35 +0530

    fix: fixed overflow error

--- frontend/appflowy_flutter/lib/workspace/presentation/home/menu/app/header/import/import_from_notion_widget.dart
+++ frontend/appflowy_flutter/lib/workspace/presentation/home/menu/app/header/import/import_from_notion_widget.dart
@@ -127,7 +127,6 @@ class NotionImportTips extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(type.tooltips),
-            const SizedBox(height: 20),
             Center(
               child: FlowyButton(
                 text: FlowyText.medium(
