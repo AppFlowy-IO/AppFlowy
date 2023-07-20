b1378b4545404f03c22884583940397be0ec4eb9
Mayur Mahajan <47064215+MayurSMahajan@users.noreply.github.com>
2023-07-20 13:41:00 +0530

    feat: customize command shortcuts (#2848)

--- frontend/appflowy_flutter/lib/workspace/presentation/settings/settings_dialog.dart
+++ frontend/appflowy_flutter/lib/workspace/presentation/settings/settings_dialog.dart
@@ -2,6 +2,7 @@ import 'package:appflowy/startup/startup.dart';
 import 'package:appflowy/generated/locale_keys.g.dart';
 import 'package:appflowy/workspace/presentation/settings/widgets/setting_supabase_view.dart';
 import 'package:appflowy/workspace/presentation/settings/widgets/settings_appearance_view.dart';
+import 'package:appflowy/workspace/presentation/settings/widgets/settings_customize_shortcuts_view.dart';
 import 'package:appflowy/workspace/presentation/settings/widgets/settings_file_system_view.dart';
 import 'package:appflowy/workspace/presentation/settings/widgets/settings_language_view.dart';
 import 'package:appflowy/workspace/presentation/settings/widgets/settings_user_view.dart';
@@ -88,6 +89,8 @@ class SettingsDialog extends StatelessWidget {
         return SettingsUserView(user);
       case SettingsPage.supabaseSetting:
         return const SupabaseSettingView();
+      case SettingsPage.shortcuts:
+        return const SettingsCustomizeShortcutsWrapper();
       default:
         return Container();
     }
