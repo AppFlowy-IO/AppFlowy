13b046e5a505b9eb062c0c58ec066f433115c2b7
nathan <nathan@appflowy.io>
2023-07-27 12:17:47 +0800

    chore: fix conflict

--- frontend/rust-lib/flowy-server/src/supabase/storage_impls/restful_api/user.rs
+++ frontend/rust-lib/flowy-server/src/supabase/storage_impls/restful_api/user.rs
@@ -1,28 +1,25 @@
 use anyhow::Error;
-use hyper::http::StatusCode;
+
 use std::sync::Arc;
 
-use postgrest::Postgrest;
-use reqwest::Response;
-
-use flowy_error::{internal_error, ErrorCode, FlowyError};
 use flowy_user_deps::cloud::*;
 use flowy_user_deps::entities::*;
 use lib_infra::box_any::BoxAny;
 use lib_infra::future::FutureResult;
 
 use crate::supabase::entities::GetUserProfileParams;
-use crate::supabase::storage_impls::restful_api::util::InsertParamsBuilder;
+use crate::supabase::storage_impls::restful_api::util::{ExtendedResponse, InsertParamsBuilder};
+use crate::supabase::storage_impls::restful_api::PostgresWrapper;
 use crate::supabase::storage_impls::USER_EMAIL;
 use crate::supabase::storage_impls::USER_TABLE;
 use crate::supabase::storage_impls::USER_UUID;
 
 pub struct RESTfulSupabaseUserAuthServiceImpl {
-  postgrest: Arc<Postgrest>,
+  postgrest: Arc<PostgresWrapper>,
 }
 
 impl RESTfulSupabaseUserAuthServiceImpl {
-  pub fn new(postgrest: Arc<Postgrest>) -> Self {
+  pub fn new(postgrest: Arc<PostgresWrapper>) -> Self {
     Self { postgrest }
   }
 }
@@ -32,25 +29,19 @@ impl UserService for RESTfulSupabaseUserAuthServiceImpl {
     let postgrest = self.postgrest.clone();
     FutureResult::new(async move {
       let params = third_party_params_from_box_any(params)?;
-      let params_uuid = params.uuid;
-      let params_uuid_str = params_uuid.to_string();
-
-      let response = postgrest
+      let is_new_user = postgrest
         .from(USER_TABLE)
         .select("uid")
-        .eq("uuid", params_uuid_str)
+        .eq("uuid", params.uuid.to_string())
         .execute()
-        .await
-        .map_err(|e| FlowyError::new(ErrorCode::HttpError, e))?;
+        .await?
+        .get_value::<Vec<i64>>()
+        .await?
+        .is_empty();
 
-      let uids: Vec<i64> = from_response(response).await?;
-      println!("uids: {:?}", uids);
-
-      if uids.len() > 1 {
-        return Err(anyhow::anyhow!("expected 0 or 1 records, but got {}", uids.len()));
-      }
-
-      if uids.len() == 0 {
+      // Insert the user if it's a new user. After the user is inserted, we can query the user profile
+      // and workspaces. The profile and workspaces are created by the database trigger.
+      if is_new_user {
         let insert_params = InsertParamsBuilder::new()
           .insert(USER_UUID, params.uuid.to_string())
           .insert(USER_EMAIL, params.email)
@@ -59,21 +50,15 @@ impl UserService for RESTfulSupabaseUserAuthServiceImpl {
           .from(USER_TABLE)
           .insert(insert_params)
           .execute()
-          .await
-          .map_err(internal_error)?;
-        if response.status() != StatusCode::OK {
-          return Err(anyhow::anyhow!(
-              "user creation error. expected status code 200, but got {}, body: {}",
-              response.status(),
-              response.text().await.unwrap_or_default()
-          ));
-        }
+          .await?
+          .error_for_status()?;
+        tracing::debug!("Create user response: {:?}", response.text().await);
       }
 
-      println!("user uuid: {}", params_uuid);
-
+      // Query the user profile and workspaces
+      tracing::debug!("user uuid: {}", params.uuid);
       let user_profile =
-        get_user_profile(postgrest.clone(), GetUserProfileParams::Uuid(params_uuid))?;
+        get_user_profile(postgrest.clone(), GetUserProfileParams::Uuid(params.uuid))?;
       let user_workspaces = get_user_workspaces(postgrest, user_profile.id)?;
       let latest_workspace = user_workspaces
         .iter()
@@ -85,14 +70,8 @@ impl UserService for RESTfulSupabaseUserAuthServiceImpl {
         name: user_profile.name,
         latest_workspace: latest_workspace.unwrap(),
         user_workspaces,
-        is_new: uids.len() == 0,
-        email: {
-          if user_profile.email.len() == 0 {
-            None
-          } else {
-            Some(user_profile.email)
-          }
-        },
+        is_new: is_new_user,
+        email: Some(user_profile.email),
         token: None,
       })
     })
@@ -147,34 +126,15 @@ impl UserService for RESTfulSupabaseUserAuthServiceImpl {
 }
 
 fn get_user_profile(
-  postgrest: Arc<Postgrest>,
-  params: GetUserProfileParams,
-) -> Result<UserProfile, FlowyError> {
+  _postgrest: Arc<PostgresWrapper>,
+  _params: GetUserProfileParams,
+) -> Result<UserProfile, Error> {
   todo!()
 }
 
 fn get_user_workspaces(
-  postgrest: Arc<Postgrest>,
-  uid: i64,
-) -> Result<Vec<UserWorkspace>, FlowyError> {
+  _postgrest: Arc<PostgresWrapper>,
+  _uid: i64,
+) -> Result<Vec<UserWorkspace>, Error> {
   todo!()
-}
-
-async fn from_response<T>(response: Response) -> Result<T, FlowyError>
-where
-  T: serde::de::DeserializeOwned,
-{
-  if response.status() != StatusCode::OK {
-    return Err(FlowyError::new(
-      ErrorCode::HttpError,
-      format!(
-        "expected status code 200, but got {}, body: {}",
-        response.status(),
-        response.text().await.unwrap_or_default()
-      ),
-    ));
-  }
-  let text = response.text().await.map_err(internal_error)?;
-
-  serde_json::from_str(&text).map_err(|e| FlowyError::new(ErrorCode::HttpError, e))
 }
