e215204bd1a5184d73b0919144ab7610cbcffe89
Mathias Mogensen <mathiasrieckm@gmail.com>
2023-08-03 16:42:37 +0200

    test: fix tabs tests

--- frontend/appflowy_flutter/integration_test/tabs_test.dart
+++ frontend/appflowy_flutter/integration_test/tabs_test.dart
@@ -1,22 +1,26 @@
+import 'dart:io';
+
+import 'package:appflowy/workspace/presentation/home/tabs/flowy_tab.dart';
 import 'package:appflowy/workspace/presentation/home/tabs/tabs_manager.dart';
 import 'package:appflowy_backend/protobuf/flowy-folder2/view.pb.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
 import 'util/base.dart';
 import 'util/common_operations.dart';
 import 'util/expectation.dart';
+import 'util/keyboard.dart';
 
-const _documentName = 'Document';
-const _calendarName = 'Calendar';
+const _documentName = 'First Doc';
+const _documentTwoName = 'Second Doc';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   group('Tabs', () {
-    testWidgets('Open AppFlowy and open/navigate/close multiple tabs',
-        (tester) async {
+    testWidgets('Open AppFlowy and open/navigate/close tabs', (tester) async {
       await tester.initializeAppFlowy();
       await tester.tapGoButton();
 
@@ -29,66 +33,55 @@ void main() {
       );
 
       await tester.createNewPageWithName(
-        name: _calendarName,
-        layout: ViewLayoutPB.Calendar,
-      );
-
-      await tester.createNewPageWithName(
         name: _documentName,
         layout: ViewLayoutPB.Document,
       );
 
-      // Navigate current view to "Read me" document again
-      await tester.tapButtonWithName(gettingStarted);
+      await tester.createNewPageWithName(
+        name: _documentTwoName,
+        layout: ViewLayoutPB.Document,
+      );
 
       /// Open second menu item in a new tab
-      await tester.openAppInNewTab(_calendarName, ViewLayoutPB.Calendar);
+      await tester.openAppInNewTab(gettingStarted, ViewLayoutPB.Document);
 
       /// Open third menu item in a new tab
       await tester.openAppInNewTab(_documentName, ViewLayoutPB.Document);
 
       expect(
         find.descendant(
-          of: find.byType(TabsManager),
-          matching: find.byType(TabBar),
+          of: find.byType(TabBar),
+          matching: find.byType(FlowyTab),
         ),
-        findsOneWidget,
+        findsNWidgets(3),
       );
 
-      // expect(
-      //   find.descendant(
-      //     of: find.byType(TabBar),
-      //     matching: find.byType(FlowyTab),
-      //   ),
-      //   findsNWidgets(3),
-      // );
+      /// Navigate to the second tab
+      await tester.tap(
+        find.descendant(
+          of: find.byType(FlowyTab),
+          matching: find.text(gettingStarted),
+        ),
+      );
 
-      // /// Navigate to the first tab
-      // await tester.tap(
-      //   find.descendant(
-      //     of: find.byType(FlowyTab),
-      //     matching: find.text(gettingStarted),
-      //   ),
-      // );
+      /// Close tab by shortcut
+      await FlowyTestKeyboard.simulateKeyDownEvent(
+        [
+          Platform.isMacOS
+              ? LogicalKeyboardKey.meta
+              : LogicalKeyboardKey.control,
+          LogicalKeyboardKey.keyW,
+        ],
+        tester: tester,
+      );
 
-      // /// Close tab by shortcut
-      // await FlowyTestKeyboard.simulateKeyDownEvent(
-      //   [
-      //     Platform.isMacOS
-      //         ? LogicalKeyboardKey.meta
-      //         : LogicalKeyboardKey.control,
-      //     LogicalKeyboardKey.keyW,
-      //   ],
-      //   tester: tester,
-      // );
-
-      // expect(
-      //   find.descendant(
-      //     of: find.byType(TabBar),
-      //     matching: find.byType(FlowyTab),
-      //   ),
-      //   findsNWidgets(2),
-      // );
+      expect(
+        find.descendant(
+          of: find.byType(TabBar),
+          matching: find.byType(FlowyTab),
+        ),
+        findsNWidgets(2),
+      );
     });
   });
 }
