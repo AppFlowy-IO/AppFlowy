95a938657b7de56cd5978258d86d11b6e83c8b49
Lucas.Xu <lucas.xu@appflowy.io>
2023-08-07 16:54:50 +0700

    feat: support windows supabase deeplink callback (#3133)

--- frontend/appflowy_flutter/lib/startup/tasks/supabase_task.dart
+++ frontend/appflowy_flutter/lib/startup/tasks/supabase_task.dart
@@ -1,14 +1,28 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:appflowy/env/env.dart';
 import 'package:appflowy/workspace/application/settings/application_data_storage.dart';
 import 'package:flutter/foundation.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:url_protocol/url_protocol.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:path/path.dart' as p;
-
 import '../startup.dart';
+
+// ONLY supports in macOS and Windows now.
+//
+// If you need to update the schema, please update the following files:
+// - appflowy_flutter/macos/Runner/Info.plist (macOS)
+// - the callback url in Supabase dashboard
+const appflowyDeepLinkSchema = 'appflowy-flutter';
+const supabaseLoginCallback = '$appflowyDeepLinkSchema://login-callback';
 
 bool isSupabaseInitialized = false;
 const hiveBoxName = 'appflowy_supabase_authentication';
+
+// Used to store the session of the supabase in case of the user switch the different folder.
+Supabase? supabase;
 
 class InitSupabaseTask extends LaunchTask {
   @override
@@ -20,7 +34,15 @@ class InitSupabaseTask extends LaunchTask {
     if (isSupabaseInitialized) {
       return;
     }
-    await Supabase.initialize(
+
+    // register deep link for Windows
+    if (Platform.isWindows) {
+      registerProtocolHandler(appflowyDeepLinkSchema);
+    }
+
+    supabase?.dispose();
+    supabase = null;
+    supabase = await Supabase.initialize(
       url: Env.supabaseUrl,
       anonKey: Env.supabaseAnonKey,
       debug: kDebugMode,
