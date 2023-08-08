2b929ad5bfe12689cc6ed7da05823bf5f9eda8af
Aman Negi <akuro787898@gmail.com>
2023-08-08 17:58:55 +0530

    fix: update integration test

--- frontend/appflowy_flutter/integration_test/document/document_template_test.dart
+++ frontend/appflowy_flutter/integration_test/document/document_template_test.dart
@@ -50,8 +50,10 @@ void main() {
       await tester.tapGoButton();
 
       await tester.createNewPageWithName(
-        ViewLayoutPB.Document,
-        'template',
+       name: 'template',
+        layout: ViewLayoutPB.Document,
+
+        
       );
 
       const zipFileName = 'template.zip';
@@ -81,13 +83,14 @@ Future<void> insertReferenceDatabase(
   final id = uuid();
   final name = '${layout.name}_$id';
   await tester.createNewPageWithName(
-    layout,
-    name,
+    name: name,
+    layout: layout,
   );
+  
   // create a new document
   await tester.createNewPageWithName(
-    ViewLayoutPB.Document,
-    'template',
+    name: 'template',
+    layout: ViewLayoutPB.Document,
   );
   // tap the first line of the document
   await tester.editor.tapLineOfEditorAt(0);
