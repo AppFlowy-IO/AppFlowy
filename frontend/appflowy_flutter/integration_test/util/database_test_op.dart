0fe8c3920a34a8f71f3a1fe25331f665f9d7b3b2
Richard Shiue <71320345+richardshiue@users.noreply.github.com>
2023-09-12 17:44:23 +0800

    fix: flutter analyzer errors

--- frontend/appflowy_flutter/integration_test/util/database_test_op.dart
+++ frontend/appflowy_flutter/integration_test/util/database_test_op.dart
@@ -63,7 +63,6 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flowy_infra_ui/flowy_infra_ui.dart';
 import 'package:flowy_infra_ui/style_widget/text_input.dart';
 import 'package:flowy_infra_ui/widget/buttons/primary_button.dart';
-import 'package:flowy_svg/flowy_svg.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
