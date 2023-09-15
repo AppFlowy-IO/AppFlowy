ad2ecd0758c93a5e85583809086cb3fb7f51c5fe
Lucas.Xu <lucas.xu@appflowy.io>
2023-09-15 15:47:54 +0800

    chore: update translations

--- frontend/appflowy_flutter/lib/workspace/presentation/settings/widgets/settings_appearance/create_file_setting.dart
+++ frontend/appflowy_flutter/lib/workspace/presentation/settings/widgets/settings_appearance/create_file_setting.dart
@@ -1,5 +1,7 @@
+import 'package:appflowy/generated/locale_keys.g.dart';
 import 'package:appflowy/workspace/application/settings/prelude.dart';
 import 'package:appflowy/workspace/presentation/settings/widgets/settings_appearance/theme_setting_entry_template.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -15,7 +17,8 @@ class CreateFileSettings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ThemeSettingEntryTemplateWidget(
-      label: 'Show rename dialog when creating a new file',
+      label:
+          LocaleKeys.settings_appearance_showNamingDialogWhenCreatingPage.tr(),
       trailing: [
         BlocProvider.value(
           value: cubit,
