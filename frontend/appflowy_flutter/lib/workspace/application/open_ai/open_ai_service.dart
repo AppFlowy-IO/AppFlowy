43b11b6b6a3ed208589aef4db7de1344b4add9c7
nathan <nathan@appflowy.io>
2023-09-18 18:14:49 +0800

    chore: rename

--- frontend/appflowy_flutter/lib/workspace/application/open_ai/open_ai_service.dart
+++ frontend/appflowy_flutter/lib/workspace/application/open_ai/open_ai_service.dart
@@ -1,6 +1,6 @@
 import 'package:appflowy_backend/dispatch/dispatch.dart';
 import 'package:appflowy_backend/protobuf/flowy-error/errors.pb.dart';
-import 'package:appflowy_backend/protobuf/flowy-plugins/protobuf.dart';
+import 'package:appflowy_backend/protobuf/flowy-ai/protobuf.dart';
 import 'package:dartz/dartz.dart';
 
 class OpenAIService {
