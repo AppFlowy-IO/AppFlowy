6db96e593cfed04ca2d60ceff6e4cb7c684cbb85
Yijing Huang <hyj891204@gmail.com>
2023-09-25 21:00:20 -0500

    fix: fix Exception'Looking up a deactivated widget's ancestor'

--- frontend/appflowy_flutter/lib/workspace/presentation/home/menu/sidebar/sidebar_user.dart
+++ frontend/appflowy_flutter/lib/workspace/presentation/home/menu/sidebar/sidebar_user.dart
@@ -66,7 +66,7 @@ class SidebarUser extends StatelessWidget {
             context: context,
             builder: (dialogContext) {
               return BlocProvider<DocumentAppearanceCubit>.value(
-                value: BlocProvider.of<DocumentAppearanceCubit>(context),
+                value: BlocProvider.of<DocumentAppearanceCubit>(dialogContext),
                 child: SettingsDialog(
                   userProfile,
                   didLogout: () async {
