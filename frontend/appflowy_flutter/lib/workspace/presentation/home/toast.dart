01c3fec5aa5ce9e909ed1a64a461bfac69de6a3f
Aman Negi <37607224+AmanNegi@users.noreply.github.com>
2023-10-02 12:44:40 +0530

    feat: improve snackbar UI style (#3562)

--- frontend/appflowy_flutter/lib/workspace/presentation/home/toast.dart
+++ frontend/appflowy_flutter/lib/workspace/presentation/home/toast.dart
@@ -1,4 +1,6 @@
+import 'package:appflowy/generated/locale_keys.g.dart';
 import 'package:appflowy/startup/startup.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flowy_infra/size.dart';
 import 'package:flowy_infra_ui/style_widget/text.dart';
 import 'package:flutter/material.dart';
@@ -40,9 +42,22 @@ void showMessageToast(String message) {
   );
 }
 
-void showSnackBarMessage(BuildContext context, String message) {
+void showSnackBarMessage(
+  BuildContext context,
+  String message, {
+  bool showCancel = false,
+}) {
   ScaffoldMessenger.of(context).showSnackBar(
     SnackBar(
+      action: !showCancel
+          ? null
+          : SnackBarAction(
+              label: LocaleKeys.button_Cancel.tr(),
+              textColor: Theme.of(context).colorScheme.onSurface,
+              onPressed: () {
+                ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              },
+            ),
       content: FlowyText(
         message,
         color: Theme.of(context).colorScheme.onSurface,
