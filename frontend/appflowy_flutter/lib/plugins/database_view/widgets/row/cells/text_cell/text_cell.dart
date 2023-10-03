3d4d7f4fbd57c36f25b3ae18123c492a016ec37b
Jaskeerat Singh <jaskeerat10000@gmail.com>
2023-10-03 21:42:30 +0530

    fix: Missing white space b/w primary field text & row's icon

--- frontend/appflowy_flutter/lib/plugins/database_view/widgets/row/cells/text_cell/text_cell.dart
+++ frontend/appflowy_flutter/lib/plugins/database_view/widgets/row/cells/text_cell/text_cell.dart
@@ -24,7 +24,7 @@ class GridTextCellStyle extends GridCellStyle {
     this.autofocus = false,
     this.showEmoji = true,
     this.emojiFontSize = 16,
-    this.emojiHPadding = 0,
+    this.emojiHPadding = 8,
     this.useRoundedBorder = false,
   });
 }
