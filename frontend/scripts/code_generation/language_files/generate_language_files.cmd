65cc02395c1425a9deb87d9d64e59615a2381d2b
Sidhant Mishra <69967446+rishi457@users.noreply.github.com>
2023-10-03 09:17:02 +0530

    Update generate_language_files.cmd
    In this script, we've made several improvements to enhance its functionality and flexibility. Here's a breakdown of the changes:
    
    1. **Command-line Arguments**: We've added the ability to specify source and destination directories as command-line arguments when running the script. This means you can customize the script's behavior without modifying the script itself.
    
    2. **Default Directories**: To provide sensible defaults, we've defined default source and destination directories within the script. These defaults will be used if you don't specify custom directories as command-line arguments.
    
    3. **Dependency Checks**: Before proceeding with the script's main tasks, we check if the required dependencies, namely Flutter and Dart, are available in the system's PATH. This ensures that the script doesn't run into issues due to missing dependencies.
    
    4. **Processing Command-line Arguments**: We process the command-line arguments using `if` statements. If you provide custom directories as arguments when running the script, those arguments will override the default directories defined in the script.
    
    5. **Directory Formatting**: To ensure that the provided directories end with a backslash, we add a backslash to the directory paths if they are missing it. This ensures consistency in directory paths and prevents potential issues during directory operations.
    
    6. **Copying Files**: We use the `xcopy` command to efficiently copy the contents of the source directory to the destination directory. This step is performed after validating the directories.
    
    7. **Running Localization Tasks**: The script continues to execute Flutter and Dart commands for localization tasks as originally intended, specifying the source directory for AppFlowy Localizations and generating language files for AppFlowy.
    
    8. **Logging**: We've retained informative log messages to keep you informed about each step of the script's execution.
    
    9. **Return to Original Directory**: Finally, the script returns to the original working directory, ensuring that it doesn't leave you in a different directory after completion.
    
    By incorporating these changes, the script becomes more versatile, user-friendly, and robust. You can easily adapt it to your specific project requirements by providing custom source and destination directories as needed.

--- frontend/scripts/code_generation/language_files/generate_language_files.cmd
+++ frontend/scripts/code_generation/language_files/generate_language_files.cmd
@@ -1,6 +1,5 @@
 @echo off
-
-echo 'Generating language files'
+setlocal enabledelayedexpansion
 
 REM Store the current working directory
 set "original_dir=%CD%"
@@ -8,22 +7,45 @@ set "original_dir=%CD%"
 REM Change the current working directory to the script's location
 cd /d "%~dp0"
 
-cd ..\..\..\appflowy_flutter
+REM Define default source and destination directories
+set "resources_dir=..\resources\translations"
+set "flutter_project_dir=..\..\..\appflowy_flutter"
+set "assets_dir=%flutter_project_dir%\assets\translations"
 
-REM copy the resources/translations folder to
-REM   the appflowy_flutter/assets/translation directory
-echo Copying resources/translations to appflowy_flutter/assets/translations
-xcopy /E /Y /I ..\resources\translations assets\translations
+REM Check if required dependencies are available
+where flutter > nul 2>&1 || (
+  echo Error: Flutter not found in PATH.
+  exit /b 1
+)
 
-call flutter packages pub get
+where dart > nul 2>&1 || (
+  echo Error: Dart not found in PATH.
+  exit /b 1
+)
+
+REM Process command-line arguments for source and destination directories
+if "%~1" neq "" set "resources_dir=%~1"
+if "%~2" neq "" set "assets_dir=%~2"
+
+REM Ensure that source and destination directories end with a backslash
+if "%resources_dir:~-1%" neq "\" set "resources_dir=%resources_dir%\"
+if "%assets_dir:~-1%" neq "\" set "assets_dir=%assets_dir%\"
+
+flutter packages pub get
+
+REM Copy the resources/translations folder to the appflowy_flutter/assets/translations directory
+echo Copying %resources_dir% to %assets_dir%
+xcopy /E /Y /I "%resources_dir%" "%assets_dir%"
 
 echo Specifying source directory for AppFlowy Localizations.
-call dart run easy_localization:generate -S assets/translations/
+dart run easy_localization:generate -S %assets_dir%
 
 echo Generating language files for AppFlowy.
-call dart run easy_localization:generate -f keys -o locale_keys.g.dart -S assets/translations/ -s en.json
+dart run easy_localization:generate -f keys -o %flutter_project_dir%\locale_keys.g.dart -S %assets_dir% -s en.json
 
 echo Done generating language files.
 
 REM Return to the original directory
 cd /d "%original_dir%"
+
+endlocal
