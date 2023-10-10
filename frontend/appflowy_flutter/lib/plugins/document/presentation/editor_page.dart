3e32695404b3ed228595ad535fc18c2842312985
Lucas.Xu <lucas.xu@appflowy.io>
2023-10-10 11:29:33 +0800

    fix: enter on Toggle list moves heading down without contents

--- frontend/appflowy_flutter/lib/plugins/document/presentation/editor_page.dart
+++ frontend/appflowy_flutter/lib/plugins/document/presentation/editor_page.dart
@@ -151,6 +151,9 @@ class _AppFlowyEditorPageState extends State<AppFlowyEditorPage> {
     effectiveScrollController = widget.scrollController ?? ScrollController();
 
     // keep the previous font style when typing new text.
+    supportSlashMenuNodeWhiteList.addAll([
+      ToggleListBlockKeys.type,
+    ]);
     AppFlowyRichTextKeys.supportSliced.add(AppFlowyRichTextKeys.fontFamily);
   }
 
