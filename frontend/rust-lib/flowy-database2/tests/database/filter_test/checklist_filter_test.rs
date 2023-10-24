6c3d7d2079cdc3596551dbd756bfa78118436fc4
Richard Shiue <71320345+richardshiue@users.noreply.github.com>
2023-10-24 10:15:28 +0800

    feat: show checklist items inline in row page (#3737)
    * feat: show checklist items inline in row page
    
    * fix: tauri build

--- frontend/rust-lib/flowy-database2/tests/database/filter_test/checklist_filter_test.rs
+++ frontend/rust-lib/flowy-database2/tests/database/filter_test/checklist_filter_test.rs
@@ -1,4 +1,5 @@
-use flowy_database2::entities::ChecklistFilterConditionPB;
+use flowy_database2::entities::{ChecklistFilterConditionPB, FieldType};
+use flowy_database2::services::field::checklist_type_option::ChecklistCellData;
 
 use crate::database::filter_test::script::FilterScript::*;
 use crate::database::filter_test::script::{DatabaseFilterTest, FilterRowChanged};
@@ -8,10 +9,12 @@ async fn grid_filter_checklist_is_incomplete_test() {
   let mut test = DatabaseFilterTest::new().await;
   let expected = 6;
   let row_count = test.row_details.len();
+  let option_ids = get_checklist_cell_options(&test).await;
+
   let scripts = vec![
     UpdateChecklistCell {
       row_id: test.row_details[0].row.id.clone(),
-      f: Box::new(|options| options.into_iter().map(|option| option.id).collect()),
+      selected_option_ids: option_ids,
     },
     CreateChecklistFilter {
       condition: ChecklistFilterConditionPB::IsIncomplete,
@@ -30,10 +33,11 @@ async fn grid_filter_checklist_is_complete_test() {
   let mut test = DatabaseFilterTest::new().await;
   let expected = 1;
   let row_count = test.row_details.len();
+  let option_ids = get_checklist_cell_options(&test).await;
   let scripts = vec![
     UpdateChecklistCell {
       row_id: test.row_details[0].row.id.clone(),
-      f: Box::new(|options| options.into_iter().map(|option| option.id).collect()),
+      selected_option_ids: option_ids,
     },
     CreateChecklistFilter {
       condition: ChecklistFilterConditionPB::IsComplete,
@@ -45,4 +49,21 @@ async fn grid_filter_checklist_is_complete_test() {
     AssertNumberOfVisibleRows { expected },
   ];
   test.run_scripts(scripts).await;
+}
+
+async fn get_checklist_cell_options(test: &DatabaseFilterTest) -> Vec<String> {
+  let field = test.get_first_field(FieldType::Checklist);
+  let row_cell = test
+    .editor
+    .get_cell(&field.id, &test.row_details[0].row.id)
+    .await;
+  row_cell
+    .map_or_else(
+      || ChecklistCellData::default(),
+      |cell| ChecklistCellData::from(&cell),
+    )
+    .options
+    .into_iter()
+    .map(|option| option.id)
+    .collect()
 }
