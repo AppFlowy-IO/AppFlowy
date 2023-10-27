ac14f7a94a666934f26e7b36f150794a72b3d720
Lucas.Xu <lucas.xu@appflowy.io>
2023-10-27 11:32:46 +0800

    feat: enable feedback when tapping icons

--- frontend/appflowy_flutter/lib/mobile/presentation/base/app_bar_actions.dart
+++ frontend/appflowy_flutter/lib/mobile/presentation/base/app_bar_actions.dart
@@ -1,30 +1,18 @@
-import 'package:appflowy/generated/flowy_svgs.g.dart';
 import 'package:flutter/material.dart';
 
 class AppBarBackButton extends StatelessWidget {
   const AppBarBackButton({
     super.key,
-    this.extent = 16.0,
     required this.onTap,
   });
-
-  // used to extend the hit area of the back button
-  final double extent;
 
   final VoidCallback onTap;
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      enableFeedback: true,
+    return AppBarButton(
       onTap: onTap,
-      child: Padding(
-        padding: EdgeInsets.all(extent),
-        child: const FlowySvg(
-          FlowySvgs.back_m,
-          size: Size.square(12.0),
-        ),
-      ),
+      child: const Icon(Icons.arrow_back_ios_new),
     );
   }
 }
@@ -32,26 +20,46 @@ class AppBarBackButton extends StatelessWidget {
 class AppBarMoreButton extends StatelessWidget {
   const AppBarMoreButton({
     super.key,
-    this.extent = 16.0,
     required this.onTap,
   });
-
-  // used to extend the hit area of the more button
-  final double extent;
 
   final void Function(BuildContext context) onTap;
 
   @override
   Widget build(BuildContext context) {
+    return AppBarButton(
+      onTap: () => onTap(context),
+      child: const Icon(
+        // replace with flowy icon
+        Icons.more_horiz_sharp,
+      ),
+    );
+  }
+}
+
+class AppBarButton extends StatelessWidget {
+  const AppBarButton({
+    super.key,
+    this.extent = 16.0,
+    required this.onTap,
+    required this.child,
+  });
+
+  // used to extend the hit area of the more button
+  final double extent;
+
+  final VoidCallback onTap;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
     return InkWell(
       enableFeedback: true,
-      onTap: () => onTap(context),
+      borderRadius: BorderRadius.circular(28),
+      onTap: onTap,
       child: Padding(
         padding: EdgeInsets.all(extent),
-        child: const FlowySvg(
-          FlowySvgs.three_dots_s,
-          size: Size.square(20.0),
-        ),
+        child: child,
       ),
     );
   }
