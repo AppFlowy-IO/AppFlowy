368a44c7f89e2f009048740a005aa2a551b03deb
Richard Shiue <71320345+richardshiue@users.noreply.github.com>
2023-11-08 14:03:02 +0800

    chore: implement hidden group popups

--- frontend/appflowy_flutter/lib/plugins/database_view/board/presentation/widgets/board_hidden_groups.dart
+++ frontend/appflowy_flutter/lib/plugins/database_view/board/presentation/widgets/board_hidden_groups.dart
@@ -1,8 +1,23 @@
 import 'package:appflowy/generated/flowy_svgs.g.dart';
+import 'package:appflowy/generated/locale_keys.g.dart';
+import 'package:appflowy/plugins/database_view/application/cell/cell_service.dart';
 import 'package:appflowy/plugins/database_view/application/database_controller.dart';
+import 'package:appflowy/plugins/database_view/application/field/field_info.dart';
+import 'package:appflowy/plugins/database_view/application/row/row_cache.dart';
+import 'package:appflowy/plugins/database_view/application/row/row_controller.dart';
 import 'package:appflowy/plugins/database_view/board/application/board_bloc.dart';
 import 'package:appflowy/plugins/database_view/board/application/hidden_groups_bloc.dart';
+import 'package:appflowy/plugins/database_view/grid/presentation/layout/sizes.dart';
+import 'package:appflowy/plugins/database_view/widgets/card/card_cell_builder.dart';
+import 'package:appflowy/plugins/database_view/widgets/card/cells/card_cell.dart';
+import 'package:appflowy/plugins/database_view/widgets/row/cell_builder.dart';
+import 'package:appflowy/plugins/database_view/widgets/row/cells/text_cell/text_cell_bloc.dart';
+import 'package:appflowy/plugins/database_view/widgets/row/row_detail.dart';
 import 'package:appflowy_backend/protobuf/flowy-database2/group.pb.dart';
+import 'package:appflowy_backend/protobuf/flowy-database2/row_entities.pb.dart';
+import 'package:appflowy_popover/appflowy_popover.dart';
+import 'package:collection/collection.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flowy_infra_ui/flowy_infra_ui.dart';
 import 'package:flowy_infra_ui/style_widget/hover.dart';
 import 'package:flowy_infra_ui/widget/flowy_tooltip.dart';
@@ -119,10 +134,19 @@ class HiddenGroupCard extends StatefulWidget {
 }
 
 class _HiddenGroupCardState extends State<HiddenGroupCard> {
+  late final PopoverController _popoverController;
+
+  @override
+  void initState() {
+    super.initState();
+    _popoverController = PopoverController();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 26),
+    final button = GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: () => _popoverController.show(),
       child: FlowyHover(
         resetHoverOnRebuild: false,
         builder: (context, isHovering) {
@@ -132,11 +156,11 @@ class _HiddenGroupCardState extends State<HiddenGroupCard> {
               padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 4),
               child: Row(
                 children: [
-                  // Opacity(
-                  //   opacity: isHover ? 1 : 0,
-                  //   child: const HiddenGroupCardActions(),
-                  // ),
-                  // const HSpace(4),
+                  Opacity(
+                    opacity: isHovering ? 1 : 0,
+                    child: const HiddenGroupCardActions(),
+                  ),
+                  const HSpace(4),
                   FlowyText.medium(
                     widget.group.groupName,
                     overflow: TextOverflow.ellipsis,
@@ -173,6 +197,45 @@ class _HiddenGroupCardState extends State<HiddenGroupCard> {
         },
       ),
     );
+
+    final databaseController = context.read<BoardBloc>().databaseController;
+    final primaryField = databaseController.fieldController.fieldInfos
+        .firstWhereOrNull((element) => element.isPrimary)!;
+    return Padding(
+      padding: const EdgeInsets.only(left: 26),
+      child: _wrapPopover(
+        button,
+        databaseController,
+        widget.group.rows,
+        primaryField,
+        widget.group.groupName,
+      ),
+    );
+  }
+
+  Widget _wrapPopover(
+    Widget child,
+    DatabaseController databaseController,
+    List<RowMetaPB> items,
+    FieldInfo primaryField,
+    String groupName,
+  ) {
+    return AppFlowyPopover(
+      controller: _popoverController,
+      direction: PopoverDirection.bottomWithCenterAligned,
+      triggerActions: PopoverTriggerFlags.none,
+      constraints: const BoxConstraints(maxWidth: 234),
+      popupBuilder: (popoverContext) {
+        return HiddenGroupPopupItemList(
+          name: groupName,
+          viewId: databaseController.viewId,
+          primaryField: primaryField,
+          groupItems: items,
+          rowCache: databaseController.rowCache,
+        );
+      },
+      child: child,
+    );
   }
 }
 
@@ -186,6 +249,142 @@ class HiddenGroupCardActions extends StatelessWidget {
       child: FlowySvg(
         FlowySvgs.drag_element_s,
         color: Theme.of(context).hintColor,
+      ),
+    );
+  }
+}
+
+class HiddenGroupPopover extends StatelessWidget {
+  const HiddenGroupPopover({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(width: 234, height: 100);
+  }
+}
+
+class HiddenGroupPopupItemList extends StatelessWidget {
+  final String name;
+  final String viewId;
+  final FieldInfo primaryField;
+  final RowCache rowCache;
+  final List<RowMetaPB> groupItems;
+  const HiddenGroupPopupItemList({
+    required this.viewId,
+    required this.primaryField,
+    required this.groupItems,
+    required this.rowCache,
+    required this.name,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final cells = <Widget>[
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 4),
+        child: FlowyText.medium(
+          name,
+          fontSize: 10,
+          color: Theme.of(context).hintColor,
+          overflow: TextOverflow.ellipsis,
+        ),
+      ),
+      ...groupItems.map(
+        (item) {
+          final rowController = RowController(
+            rowMeta: item,
+            viewId: viewId,
+            rowCache: rowCache,
+          );
+          final renderHook = RowCardRenderHook<String>();
+          renderHook.addTextCellHook((cellData, _, __) {
+            return BlocBuilder<TextCellBloc, TextCellState>(
+              builder: (context, state) {
+                final text = cellData.isEmpty
+                    ? LocaleKeys.grid_row_titlePlaceholder.tr()
+                    : cellData;
+
+                if (text.isEmpty) {
+                  return const SizedBox.shrink();
+                }
+
+                return Align(
+                  alignment: Alignment.centerLeft,
+                  child: FlowyText.medium(
+                    text,
+                    textAlign: TextAlign.left,
+                    fontSize: 11,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                );
+              },
+            );
+          });
+
+          return HiddenGroupPopupItem(
+            cellContext: rowCache.loadCells(item)[primaryField.id]!,
+            primaryField: primaryField,
+            rowController: rowController,
+            cellBuilder: CardCellBuilder<String>(rowController.cellCache),
+            renderHook: renderHook,
+            onPressed: () {
+              FlowyOverlay.show(
+                context: context,
+                builder: (BuildContext context) {
+                  return RowDetailPage(
+                    cellBuilder:
+                        GridCellBuilder(cellCache: rowController.cellCache),
+                    rowController: rowController,
+                  );
+                },
+              );
+              PopoverContainer.of(context).close();
+            },
+          );
+        },
+      )
+    ];
+
+    return ListView.separated(
+      itemBuilder: (context, index) => cells[index],
+      itemCount: cells.length,
+      separatorBuilder: (context, index) =>
+          VSpace(GridSize.typeOptionSeparatorHeight),
+      shrinkWrap: true,
+    );
+  }
+}
+
+class HiddenGroupPopupItem extends StatelessWidget {
+  final DatabaseCellContext cellContext;
+  final FieldInfo primaryField;
+  final RowController rowController;
+  final CardCellBuilder cellBuilder;
+  final RowCardRenderHook<String> renderHook;
+  final VoidCallback onPressed;
+  const HiddenGroupPopupItem({
+    super.key,
+    required this.cellContext,
+    required this.onPressed,
+    required this.cellBuilder,
+    required this.rowController,
+    required this.primaryField,
+    required this.renderHook,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 26,
+      child: FlowyButton(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+        text: cellBuilder.buildCell(
+          cellContext: cellContext,
+          renderHook: renderHook,
+        ),
+        onTap: onPressed,
       ),
     );
   }
