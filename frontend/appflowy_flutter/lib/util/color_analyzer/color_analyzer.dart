d230eaa8dd580d9d43cb9a5dad33159433ab7231
Rituraj Tripsthy <rituraj.tripathy@gmail.com>
2023-11-11 01:22:40 +0530

    nameing convention channges

--- frontend/appflowy_flutter/lib/util/color_analyzer/color_analyzer.dart
+++ frontend/appflowy_flutter/lib/util/color_analyzer/color_analyzer.dart
@@ -2,25 +2,25 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 
 class ColorAnalyzer {
-  static const double RGB_SCALE = 255.0;
-  static const double LUMINANCE_THRESHOLD = 0.03928;
-  static const double ADJUSTMENT_FACTOR = 12.92;
-  static const double SCALE_NORM = 1.055;
-  static const List<double> LUMINANCE_COEFFICIENTS = [0.2126, 0.7152, 0.0722];
+  static const double rgbScale = 255.0;
+  static const double luminanceThreshold = 0.03928;
+  static const double adjustmentFactor = 12.92;
+  static const double scaleNorm = 1.055;
+  static const List<double> luminanceCoefficients = [0.2126, 0.7152, 0.0722];
 
   double calculateLuminance(Color color) {
-    List<double> rgb =
-        [color.red, color.green, color.blue].map((c) => c / RGB_SCALE).toList();
+    final List<double> rgb =
+        [color.red, color.green, color.blue].map((c) => c / rgbScale).toList();
 
     for (int i = 0; i < 3; i++) {
-      rgb[i] = rgb[i] <= LUMINANCE_THRESHOLD
-          ? rgb[i] / ADJUSTMENT_FACTOR
-          : pow((rgb[i] + (SCALE_NORM - 1)) / SCALE_NORM, 2.4).toDouble();
+      rgb[i] = rgb[i] <= luminanceThreshold
+          ? rgb[i] / adjustmentFactor
+          : pow((rgb[i] + (scaleNorm - 1)) / scaleNorm, 2.4).toDouble();
     }
 
-    return rgb[0] * LUMINANCE_COEFFICIENTS[0] +
-        rgb[1] * LUMINANCE_COEFFICIENTS[1] +
-        rgb[2] * LUMINANCE_COEFFICIENTS[2];
+    return rgb[0] * luminanceCoefficients[0] +
+        rgb[1] * luminanceCoefficients[1] +
+        rgb[2] * luminanceCoefficients[2];
   }
 
   double calculateContrast(Color color1, Color color2) {
@@ -38,7 +38,8 @@ class ColorAnalyzer {
 
   Color getAppropriateTextColor(
       Color backgroundColor, Color primaryTextColor, Color altTextColor) {
-    double contrastRatio = calculateContrast(backgroundColor, primaryTextColor);
+    final double contrastRatio =
+        calculateContrast(backgroundColor, primaryTextColor);
 
     return contrastRatio < 1.36 ? altTextColor : primaryTextColor;
   }
