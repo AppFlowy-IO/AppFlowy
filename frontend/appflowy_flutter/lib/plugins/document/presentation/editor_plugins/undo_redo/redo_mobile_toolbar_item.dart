3708a5b86ade2509330a7d7a8cf46baaf3a4194c
Lucas.Xu <lucas.xu@appflowy.io>
2023-11-13 18:08:39 +0800

    feat: add alignment and indent/outdent toolbar item (#3927)
    * chore: rename mobile list toolbar item
    
    * feat: add alignment and indent/outdent toolbar item
    
    * feat: adjust link menu on mobile platform

--- frontend/appflowy_flutter/lib/plugins/document/presentation/editor_plugins/undo_redo/redo_mobile_toolbar_item.dart
+++ frontend/appflowy_flutter/lib/plugins/document/presentation/editor_plugins/undo_redo/redo_mobile_toolbar_item.dart
@@ -2,7 +2,7 @@ import 'package:appflowy/generated/flowy_svgs.g.dart';
 import 'package:appflowy_editor/appflowy_editor.dart';
 
 final redoMobileToolbarItem = MobileToolbarItem.action(
-  itemIcon: const FlowySvg(FlowySvgs.m_redo_m),
+  itemIconBuilder: (_, __) => const FlowySvg(FlowySvgs.m_redo_m),
   actionHandler: (editorState, selection) async {
     editorState.undoManager.redo();
   },
