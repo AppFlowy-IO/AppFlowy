197906708dc5a2acdb212ff7271a443622445aa9
Lucas.Xu <lucas.xu@appflowy.io>
2023-11-14 21:09:16 +0800

    fix: ci tests

--- frontend/scripts/code_generation/freezed/generate_freezed.sh
+++ frontend/scripts/code_generation/freezed/generate_freezed.sh
@@ -23,31 +23,31 @@ if [ "$no_pub_get" = false ]; then
   flutter packages pub get >/dev/null 2>&1
 fi
 
-dart run build_runner clean && dart run build_runner build -d
+dart run build_runner build -d
 echo "Done generating files for appflowy_flutter"
 
-# echo "Generating files for packages"
-# cd packages
-# for d in */; do
-#   # Navigate into the subdirectory
-#   cd "$d"
+echo "Generating files for packages"
+cd packages
+for d in */; do
+  # Navigate into the subdirectory
+  cd "$d"
 
-#   # Check if the subdirectory contains a pubspec.yaml file
-#   if [ -f "pubspec.yaml" ]; then
-#     echo "Generating freezed files in $d..."
-#     echo "Please wait while we clean the project and fetch the dependencies."
-#     if [ "$no_pub_get" = false ]; then
-#       flutter packages pub get >/dev/null 2>&1
-#     fi
-#     dart run build_runner clean && dart run build_runner build -d
-#     echo "Done running build command in $d"
-#   else
-#     echo "No pubspec.yaml found in $d, it can\'t be a Dart project. Skipping."
-#   fi
+  # Check if the subdirectory contains a pubspec.yaml file
+  if [ -f "pubspec.yaml" ]; then
+    echo "Generating freezed files in $d..."
+    echo "Please wait while we clean the project and fetch the dependencies."
+    if [ "$no_pub_get" = false ]; then
+      flutter packages pub get >/dev/null 2>&1
+    fi
+    dart run build_runner build -d
+    echo "Done running build command in $d"
+  else
+    echo "No pubspec.yaml found in $d, it can\'t be a Dart project. Skipping."
+  fi
 
-#   # Navigate back to the packages directory
-#   cd ..
-# done
+  # Navigate back to the packages directory
+  cd ..
+done
 
 # Return to the original directory
 cd "$original_dir"
