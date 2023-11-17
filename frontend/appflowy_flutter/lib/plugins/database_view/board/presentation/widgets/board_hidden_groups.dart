729b8571b588f1dedb39e2d736db4c97e332f00d
Richard Shiue <71320345+richardshiue@users.noreply.github.com>
2023-11-17 09:27:10 +0800

    chore: fix ungroupedsection being shown (#3953)
    * chore: fix ungroupedsection being shown
    
    * chore: board ui adjustment

--- frontend/appflowy_flutter/lib/plugins/database_view/board/presentation/widgets/board_hidden_groups.dart
+++ frontend/appflowy_flutter/lib/plugins/database_view/board/presentation/widgets/board_hidden_groups.dart
@@ -53,7 +53,7 @@ class HiddenGroupsColumn extends StatelessWidget {
                   ),
                 )
               : SizedBox(
-                  width: 260,
+                  width: 234,
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
@@ -62,7 +62,7 @@ class HiddenGroupsColumn extends StatelessWidget {
                         child: Padding(
                           padding: EdgeInsets.only(
                             left: 40 + margin.left,
-                            right: margin.right,
+                            right: margin.right + 4,
                           ),
                           child: Row(
                             children: [
@@ -99,6 +99,7 @@ class HiddenGroupsColumn extends StatelessWidget {
       message: isCollapsed
           ? LocaleKeys.board_hiddenGroupSection_expandTooltip.tr()
           : LocaleKeys.board_hiddenGroupSection_collapseTooltip.tr(),
+      preferBelow: false,
       child: FlowyIconButton(
         width: 20,
         height: 20,
@@ -252,7 +253,7 @@ class HiddenGroupButtonContent extends StatelessWidget {
                   }
 
                   return SizedBox(
-                    height: 30,
+                    height: 32,
                     child: Padding(
                       padding: const EdgeInsets.symmetric(
                         horizontal: 4,
