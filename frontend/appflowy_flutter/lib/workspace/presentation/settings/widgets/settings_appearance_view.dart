39821853172aac2bf8a1d8a77c2cd59e3b8f0729
Mohammad Zolfaghari <zidom72@gmail.com>
2023-11-17 22:27:39 +0330

    style: fix flutter analyze

--- frontend/appflowy_flutter/lib/workspace/presentation/settings/widgets/settings_appearance_view.dart
+++ frontend/appflowy_flutter/lib/workspace/presentation/settings/widgets/settings_appearance_view.dart
@@ -39,7 +39,7 @@ class SettingsAppearanceView extends StatelessWidget {
                 TextDirectionSetting(
                   currentTextDirection: state.textDirection,
                 ),
-                EnableRTLToolbarItemsSetting(),
+                const EnableRTLToolbarItemsSetting(),
                 const Divider(),
                 DateFormatSetting(
                   currentFormat: state.dateFormat,
