84c6c3815d1a9b49b988d9af574ba853a5445643
Rituraj Tripsthy <rituraj.tripathy@gmail.com>
2023-11-19 11:50:57 +0530

    fixed test writing

--- frontend/appflowy_flutter/test/unit_test/color_analyzer_test.dart
+++ frontend/appflowy_flutter/test/unit_test/color_analyzer_test.dart
@@ -8,38 +8,32 @@ void main() {
       const Color backgroundColor = Color(0xff00bcf0);
       const Color primaryTextColor = Color(0xffBBC3CD);
       const Color altTextColor = Color(0xff131720);
-      const Color expectedColor = altTextColor;
 
       final Color result = ColorAnalyzer.getAppropriateTextColor(
           backgroundColor, primaryTextColor, altTextColor);
 
-      expect(result, expectedColor);
+      expect(result, altTextColor);
     });
 
     test('getAppropriateTextColor returns expected color', () {
       const Color backgroundColor = Color(0xff00bcf0);
       const Color primaryTextColor = Color(0xffBBC3CD);
       const Color altTextColor = Color(0xff131720);
-      const Color expectedColor = altTextColor;
 
       final Color result = ColorAnalyzer.getAppropriateTextColor(
           backgroundColor, primaryTextColor, altTextColor);
 
-      expect(result, expectedColor);
+      expect(result, altTextColor);
     });
 
     test('getAppropriateTextColor returns expected color', () {
-      // Define test colors
-      final Color backgroundColor =
-          Color(0xffbcf0ff); // Replace with actual color
-      final Color primaryTextColor = Color(0xff333333); // Example color
-      final Color altTextColor = Color(0xff333333); // Example alternative color
-      final Color expectedColor = altTextColor; // Replace with expected result
-
+      final Color backgroundColor = Color(0xffbcf0ff);
+      final Color primaryTextColor = Color(0xff333333);
+      final Color altTextColor = Color(0xff333333);
       final Color result = ColorAnalyzer.getAppropriateTextColor(
           backgroundColor, primaryTextColor, altTextColor);
 
-      expect(result, expectedColor);
+      expect(result, altTextColor);
     });
   });
 }
