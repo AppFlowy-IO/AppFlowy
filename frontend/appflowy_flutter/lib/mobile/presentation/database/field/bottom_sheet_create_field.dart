bd1492ac4a083a224c3bffcfe693a413acb370ff
Richard Shiue <71320345+richardshiue@users.noreply.github.com>
2023-12-02 21:35:12 +0800

    chore: use new field editor

--- frontend/appflowy_flutter/lib/mobile/presentation/database/field/bottom_sheet_create_field.dart
+++ frontend/appflowy_flutter/lib/mobile/presentation/database/field/bottom_sheet_create_field.dart
@@ -1,7 +1,10 @@
 import 'package:appflowy/mobile/presentation/bottom_sheet/bottom_sheet.dart';
 import 'package:appflowy/mobile/presentation/database/card/card_detail/mobile_create_field_screen.dart';
+import 'package:appflowy/mobile/presentation/database/card/card_detail/mobile_edit_field_screen.dart';
 import 'package:appflowy/mobile/presentation/database/card/card_detail/widgets/_field_options.dart';
 import 'package:appflowy/mobile/presentation/database/card/card_detail/widgets/_field_options_eidtor.dart';
+import 'package:appflowy/plugins/database_view/application/field/field_info.dart';
+import 'package:appflowy/plugins/database_view/application/field/field_service.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -38,4 +41,42 @@ void showCreateFieldBottomSheet(BuildContext context, String viewId) {
       );
     },
   );
+}
+
+void showEditFieldScreen(
+  BuildContext context,
+  String viewId,
+  FieldInfo field,
+) async {
+  final optionValues = await context.push<FieldOptionValues>(
+    MobileEditPropertyScreen.routeName,
+    extra: {
+      MobileEditPropertyScreen.argViewId: viewId,
+      MobileEditPropertyScreen.argField: field.field,
+      MobileEditPropertyScreen.argIsPrimary: field.isPrimary,
+    },
+  );
+  if (optionValues != null) {
+    final service = FieldBackendService(
+      viewId: viewId,
+      fieldId: field.id,
+    );
+
+    if (optionValues.name != field.name) {
+      await service.updateField(name: optionValues.name);
+    }
+
+    if (optionValues.type != field.fieldType) {
+      await service.updateFieldType(fieldType: optionValues.type);
+    }
+
+    final data = optionValues.toTypeOptionBuffer();
+    if (data != null) {
+      await FieldBackendService.updateFieldTypeOption(
+        viewId: viewId,
+        fieldId: field.id,
+        typeOptionData: data,
+      );
+    }
+  }
 }
