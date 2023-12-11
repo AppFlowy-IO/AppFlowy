55ecadfdb5256ee94bc742609961a5c0fa0124d9
Hassam Sulehria <sulehria.hassam@gmail.com>
2023-12-11 18:47:00 -0500

    test: Fixed testcase for emoji_shortcut_test (#2038)

--- frontend/appflowy_flutter/integration_test/emoji_shortcut_test.dart
+++ frontend/appflowy_flutter/integration_test/emoji_shortcut_test.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:appflowy/workspace/presentation/settings/widgets/emoji_picker/emoji_picker.dart';
 import 'package:appflowy_editor/appflowy_editor.dart';
+import 'package:appflowy_editor/src/editor/editor_component/service/editor.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
@@ -10,13 +11,13 @@ import 'util/util.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-
   // May be better to move this to an existing test but unsure what it fits with
   group('Keyboard shortcuts related to emojis', () {
     testWidgets('cmd/ctrl+alt+e shortcut opens the emoji picker',
         (tester) async {
       await tester.initializeAppFlowy();
-
+      await tester.tapGoButton();
+      
       final Finder editor = find.byType(AppFlowyEditor);
       await tester.tap(editor);
       await tester.pumpAndSettle();
