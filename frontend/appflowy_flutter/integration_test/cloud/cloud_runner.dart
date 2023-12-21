61552752cf9cdfdc0933c3505030664aebdd2774
nathan <nathan@appflowy.io>
2023-12-22 02:23:14 +0800

    chore: add to runner

--- frontend/appflowy_flutter/integration_test/cloud/cloud_runner.dart
+++ frontend/appflowy_flutter/integration_test/cloud/cloud_runner.dart
@@ -2,6 +2,7 @@ import 'empty_test.dart' as empty_test;
 import 'appflowy_cloud_auth_test.dart' as appflowy_cloud_auth_test;
 import 'document_sync_test.dart' as document_sync_test;
 import 'user_setting_sync_test.dart' as user_sync_test;
+import 'anon_user_continue_test.dart' as anon_user_continue_test;
 
 Future<void> main() async {
   empty_test.main();
@@ -11,4 +12,6 @@ Future<void> main() async {
   document_sync_test.main();
 
   user_sync_test.main();
+
+  anon_user_continue_test.main();
 }
