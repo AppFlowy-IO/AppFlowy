eae8dbac5ed59985ea07dc439f664af72afdcd25
nathan <nathan@appflowy.io>
2023-12-22 04:54:12 +0800

    test: fix

---
+++ frontend/appflowy_flutter/integration_test/cloud/migrate_anon_user_to_af_cloud_test.dart
@@ -0,0 +1,66 @@
+// ignore_for_file: unused_import
+
+import 'dart:io';
+
+import 'package:appflowy/env/cloud_env.dart';
+import 'package:appflowy/generated/locale_keys.g.dart';
+import 'package:appflowy/startup/startup.dart';
+import 'package:appflowy/user/application/auth/af_cloud_mock_auth_service.dart';
+import 'package:appflowy/user/application/auth/auth_service.dart';
+import 'package:appflowy/workspace/application/settings/prelude.dart';
+import 'package:appflowy/workspace/presentation/settings/widgets/setting_appflowy_cloud.dart';
+import 'package:appflowy/workspace/presentation/settings/widgets/settings_user_view.dart';
+import 'package:appflowy_backend/protobuf/flowy-folder2/view.pb.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flowy_infra/uuid.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:path/path.dart' as p;
+import 'package:integration_test/integration_test.dart';
+import '../util/dir.dart';
+import '../util/mock/mock_file_picker.dart';
+import '../util/util.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('appflowy cloud', () {
+    testWidgets('anon user and then sign in', (tester) async {
+      await tester.initializeAppFlowy(
+        cloudType: AuthenticatorType.appflowyCloud,
+      );
+
+      tester.expectToSeeText(LocaleKeys.signIn_loginStartWithAnonymous.tr());
+      await tester.tapGoButton();
+      tester.expectToSeeHomePage();
+
+      // reanme the name of the anon user
+      await tester.openSettings();
+      await tester.openSettingsPage(SettingsPage.user);
+      final userNameFinder = find.descendant(
+        of: find.byType(SettingsUserView),
+        matching: find.byType(UserNameInput),
+      );
+      await tester.enterText(userNameFinder, 'local_user');
+      await tester.pumpAndSettle(const Duration(seconds: 1));
+
+      // sign up with Google
+      await tester.tapGoogleLoginInButton();
+
+      // sign out
+      await tester.openSettings();
+      await tester.openSettingsPage(SettingsPage.user);
+      await tester.logout();
+
+      // tap the continue as anonymous button
+      await tester
+          .tapButton(find.text(LocaleKeys.signIn_continueAnonymousUser.tr()));
+      tester.expectToSeeHomePage();
+
+      // assert the name of the anon user is local_user
+      await tester.openSettings();
+      await tester.openSettingsPage(SettingsPage.user);
+      final userNameInput = tester.widget(userNameFinder) as UserNameInput;
+      expect(userNameInput.name, 'local_user');
+    });
+  });
+}
