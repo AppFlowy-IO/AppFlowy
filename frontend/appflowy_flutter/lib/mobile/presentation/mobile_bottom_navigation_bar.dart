0b3c904984fe85c7ff9e9cd0a05f8056a359a20e
Lucas.Xu <lucas.xu@appflowy.io>
2023-12-28 12:37:10 +0800

    feat: add block menu (#4222)
    * feat: add block menu
    
    * fix: android add block issue

--- frontend/appflowy_flutter/lib/mobile/presentation/mobile_bottom_navigation_bar.dart
+++ frontend/appflowy_flutter/lib/mobile/presentation/mobile_bottom_navigation_bar.dart
@@ -19,7 +19,6 @@ class MobileBottomNavigationBar extends StatelessWidget {
     final style = Theme.of(context);
 
     return Scaffold(
-      backgroundColor: Colors.red,
       body: navigationShell,
       bottomNavigationBar: BottomNavigationBar(
         showSelectedLabels: false,
