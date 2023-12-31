430b7921769dc7b016874dcf0505aa65805f377d
nathan <nathan@appflowy.io>
2023-12-31 08:25:36 +0800

    chore: fix clippy

--- frontend/rust-lib/flowy-sqlite/src/sqlite_impl/errors.rs
+++ frontend/rust-lib/flowy-sqlite/src/sqlite_impl/errors.rs
@@ -1,15 +1,13 @@
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
   #[error("Migration error: {0}")]
-  MigrationError(#[from] diesel_migrations::MigrationError),
+  Migration(#[from] diesel_migrations::MigrationError),
   #[error("r2d2 error: {0}")]
-  R2D2Error(#[from] r2d2::Error),
+  R2D2(#[from] r2d2::Error),
   #[error("diesel error: {0}")]
-  DieselError(#[from] diesel::result::Error),
+  Diesel(#[from] diesel::result::Error),
   #[error("diesel connect error: {0}")]
-  ConnectionError(#[from] diesel::ConnectionError),
-  #[error("io error: {0}")]
-  IoError(#[from] std::io::Error),
+  Connect(#[from] diesel::ConnectionError),
   #[error("internal error: {0}")]
   Internal(#[from] anyhow::Error),
 }
