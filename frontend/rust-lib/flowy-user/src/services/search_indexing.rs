1af92b16251399124644c9a99ae8926626cbb875
Jiraffe7 <twajxjiraffe@gmail.com>
2024-01-02 12:12:19 -0600

    feat: add document indexing to user manager

--- frontend/rust-lib/flowy-user/src/services/search_indexing.rs
+++ frontend/rust-lib/flowy-user/src/services/search_indexing.rs
@@ -1,5 +1,7 @@
 use flowy_error::FlowyResult;
-use flowy_sqlite::search::{add_view, delete_view, update_view, View};
+use flowy_sqlite::search::{
+  add, delete_document, delete_view, update_document, update_view, SearchData,
+};
 
 use crate::manager::UserManager;
 
@@ -7,21 +9,15 @@ impl UserManager {
   /// Adds the view to the search index with `doc_id` and `name` for the user with `uid`.
   pub fn add_view_index(&self, uid: i64, doc_id: &str, name: &str) -> FlowyResult<()> {
     let mut conn = self.db_connection(uid)?;
-    let view = View {
-      id: doc_id.to_owned(),
-      name: name.to_owned(),
-    };
-    add_view(&mut conn, &view)?;
+    let view = SearchData::new_view(doc_id, name);
+    add(&mut conn, &view)?;
     Ok(())
   }
 
   /// Updates the view to the search index with `doc_id` and `name` for the user with `uid`.
   pub fn update_view_index(&self, uid: i64, doc_id: &str, name: &str) -> FlowyResult<()> {
     let mut conn = self.db_connection(uid)?;
-    let view = View {
-      id: doc_id.to_owned(),
-      name: name.to_owned(),
-    };
+    let view = SearchData::new_view(doc_id, name);
     update_view(&mut conn, &view)?;
     Ok(())
   }
@@ -31,6 +27,43 @@ impl UserManager {
     let mut conn = self.db_connection(uid)?;
     for id in ids {
       delete_view(&mut conn, id)?;
+    }
+    Ok(())
+  }
+
+  /// Adds the document to the search index with `view_id`, `page_id` and `text` for the user with `uid`.
+  pub fn add_document_index(
+    &self,
+    uid: i64,
+    view_id: &str,
+    page_id: &str,
+    text: &str,
+  ) -> FlowyResult<()> {
+    let mut conn = self.db_connection(uid)?;
+    let doc = SearchData::new_document(view_id, page_id, text);
+    add(&mut conn, &doc)?;
+    Ok(())
+  }
+
+  /// Updates the document to the search index with `view_id`, `page_id` and `text` for the user with `uid`.
+  pub fn update_document_index(
+    &self,
+    uid: i64,
+    view_id: &str,
+    page_id: &str,
+    text: &str,
+  ) -> FlowyResult<()> {
+    let mut conn = self.db_connection(uid)?;
+    let doc = SearchData::new_document(view_id, page_id, text);
+    update_document(&mut conn, &doc)?;
+    Ok(())
+  }
+
+  /// Deletes the views from the search index in `view_ids` for the user with `uid`.
+  pub fn delete_document_index(&self, uid: i64, page_ids: &[String]) -> FlowyResult<()> {
+    let mut conn = self.db_connection(uid)?;
+    for id in page_ids {
+      delete_document(&mut conn, id)?;
     }
     Ok(())
   }
