59aba93d92231be5ddda63eef2e56bf8023282c7
nathan <nathan@appflowy.io>
2024-01-05 23:50:03 +0800

    chore: fix test

--- frontend/rust-lib/flowy-wasm/wasm-demo/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import wasm from 'vite-plugin-wasm';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), wasm()],
-})
