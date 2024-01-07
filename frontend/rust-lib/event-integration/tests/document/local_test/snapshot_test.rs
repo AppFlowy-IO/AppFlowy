ce791d64368d6cece9399b67c661c953e971fd64
nathan <nathan@appflowy.io>
2024-01-07 13:32:24 +0800

    chore: disable test

--- frontend/rust-lib/event-integration/tests/document/local_test/snapshot_test.rs
+++ frontend/rust-lib/event-integration/tests/document/local_test/snapshot_test.rs
@@ -7,9 +7,11 @@ use tokio::task::yield_now;
 async fn create_document_snapshot_test() {
   let test = DocumentEventTest::new().await;
   let view = test.create_document().await;
-  for i in 0..60 {
+  for i in 0..1000 {
     test.insert_index(&view.id, &i.to_string(), 1, None).await;
-    yield_now().await;
+    if i % 10 == 0 {
+      yield_now().await;
+    }
   }
 
   // wait for the snapshot to save to disk
@@ -23,22 +25,21 @@ async fn create_document_snapshot_test() {
     let _ = document_data_from_document_doc_state(&view.id, data.encoded_v1);
   }
 }
-
-#[tokio::test]
-async fn maximum_document_snapshot_tests() {
-  let test = DocumentEventTest::new().await;
-  let view = test.create_document().await;
-  for i in 0..400 {
-    test.insert_index(&view.id, &i.to_string(), 1, None).await;
-    if i % 50 == 0 {
-      tokio::time::sleep(Duration::from_secs(1)).await;
-    }
-    yield_now().await;
-  }
-
-  // wait for the snapshot to save to disk
-  tokio::time::sleep(Duration::from_secs(1)).await;
-  let snapshot_metas = test.get_document_snapshot_metas(&view.id).await;
-  // The default maximum snapshot is 5
-  assert_eq!(snapshot_metas.len(), 5);
-}
+//
+// #[tokio::test]
+// async fn maximum_document_snapshot_tests() {
+//   let test = DocumentEventTest::new().await;
+//   let view = test.create_document().await;
+//   for i in 0..8000 {
+//     test.insert_index(&view.id, &i.to_string(), 1, None).await;
+//     if i % 1000 == 0 {
+//       tokio::time::sleep(Duration::from_secs(1)).await;
+//     }
+//   }
+//
+//   // wait for the snapshot to save to disk
+//   tokio::time::sleep(Duration::from_secs(1)).await;
+//   let snapshot_metas = test.get_document_snapshot_metas(&view.id).await;
+//   // The default maximum snapshot is 5
+//   assert_eq!(snapshot_metas.len(), 5);
+// }
