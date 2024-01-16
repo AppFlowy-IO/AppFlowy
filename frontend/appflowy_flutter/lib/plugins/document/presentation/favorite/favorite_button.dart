a012302f6d7c5a0ffcde08e100d7e68160c891b5
AnsahMohammad <mohdansah10@gmail.com>
2024-01-16 22:53:59 +0530

    chore: arranged imports in alphabetical order

--- frontend/appflowy_flutter/lib/plugins/document/presentation/favorite/favorite_button.dart
+++ frontend/appflowy_flutter/lib/plugins/document/presentation/favorite/favorite_button.dart
@@ -1,12 +1,12 @@
 import 'package:appflowy/generated/flowy_svgs.g.dart';
 import 'package:appflowy/generated/locale_keys.g.dart';
+import 'package:appflowy_backend/protobuf/flowy-folder/view.pb.dart';
+import 'package:appflowy/workspace/application/favorite/favorite_service.dart';
+import 'package:appflowy/plugins/document/presentation/favorite/favorite_cubit.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flowy_infra_ui/style_widget/hover.dart';
 import 'package:flowy_infra_ui/widget/flowy_tooltip.dart';
 import 'package:flutter/material.dart';
-import 'package:appflowy_backend/protobuf/flowy-folder/view.pb.dart';
-import 'package:appflowy/workspace/application/favorite/favorite_service.dart';
-import 'package:appflowy/plugins/document/presentation/favorite/favorite_cubit.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class DocumentFavoriteButton extends StatelessWidget {
