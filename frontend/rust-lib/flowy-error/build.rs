1541aad40279ad9b7e83f7d27c380e6eb7550ce0
nathan <nathan@appflowy.io>
2024-01-29 15:13:42 +0800

    fix: compile

--- frontend/rust-lib/flowy-error/build.rs
+++ frontend/rust-lib/flowy-error/build.rs
@@ -6,12 +6,12 @@ fn main() {
   #[cfg(feature = "dart")]
   flowy_codegen::protobuf_file::dart_gen2("flowy-error", &proto_crates);
 
-  // #[cfg(feature = "tauri_ts")]
-  // flowy_codegen::protobuf_file::ts_gen2(
-  //   "flowy-error",
-  //   &proto_crates,
-  //   flowy_codegen::Project::Tauri,
-  // );
+  #[cfg(feature = "tauri_ts")]
+  flowy_codegen::protobuf_file::ts_gen2(
+    "flowy-error",
+    &proto_crates,
+    flowy_codegen::Project::Tauri,
+  );
 
   #[cfg(feature = "web_ts")]
   flowy_codegen::protobuf_file::ts_gen2(
