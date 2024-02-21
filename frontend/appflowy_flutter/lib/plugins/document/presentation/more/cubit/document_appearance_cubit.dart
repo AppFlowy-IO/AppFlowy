0c06c58fa3d285b7c38f277c296da456113e7115
Lucas.Xu <lucas.xu@appflowy.io>
2024-02-21 15:42:36 +0800

    feat: support scaling text on mobile

--- frontend/appflowy_flutter/lib/plugins/document/presentation/more/cubit/document_appearance_cubit.dart
+++ frontend/appflowy_flutter/lib/plugins/document/presentation/more/cubit/document_appearance_cubit.dart
@@ -78,13 +78,15 @@ class DocumentAppearanceCubit extends Cubit<DocumentAppearance> {
         ? Color(int.parse(selectionColorString))
         : null;
 
+    final textScaleFactor = prefs.getDouble(KVKeys.textScaleFactor) ?? 1.0;
+
     if (isClosed) {
       return;
     }
 
     emit(
       state.copyWith(
-        fontSize: fontSize,
+        fontSize: fontSize * textScaleFactor,
         fontFamily: fontFamily,
         cursorColor: cursorColor,
         selectionColor: selectionColor,
--- frontend/appflowy_flutter/lib/plugins/document/presentation/more/font_size_slider.dart
+++ frontend/appflowy_flutter/lib/plugins/document/presentation/more/font_size_slider.dart
@@ -1,51 +1,71 @@
-import 'package:flutter/material.dart';
-
-import 'package:appflowy/plugins/document/presentation/more/cubit/document_appearance_cubit.dart';
 import 'package:flowy_infra_ui/style_widget/text.dart';
 import 'package:flowy_infra_ui/widget/spacing.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter/material.dart';
 
-class FontSizeStepper extends StatelessWidget {
-  const FontSizeStepper({super.key});
+class FontSizeStepper extends StatefulWidget {
+  const FontSizeStepper({
+    super.key,
+    required this.minimumValue,
+    required this.maximumValue,
+    required this.value,
+    required this.divisions,
+    required this.onChanged,
+  });
+
+  final double minimumValue;
+  final double maximumValue;
+  final double value;
+  final ValueChanged<double> onChanged;
+  final int divisions;
+
+  @override
+  State<FontSizeStepper> createState() => _FontSizeStepperState();
+}
+
+class _FontSizeStepperState extends State<FontSizeStepper> {
+  late double _value = widget.value.clamp(
+    widget.minimumValue,
+    widget.maximumValue,
+  );
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<DocumentAppearanceCubit, DocumentAppearance>(
-      builder: (context, state) {
-        return Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: Row(
-            children: [
-              const FlowyText('A', fontSize: 14),
-              const HSpace(6),
-              Expanded(
-                child: SliderTheme(
-                  data: Theme.of(context).sliderTheme.copyWith(
-                        showValueIndicator: ShowValueIndicator.never,
-                        thumbShape: const RoundSliderThumbShape(
-                          enabledThumbRadius: 8,
-                        ),
-                        overlayShape: const RoundSliderOverlayShape(
-                          overlayRadius: 16,
-                        ),
-                      ),
-                  child: Slider(
-                    value: state.fontSize,
-                    min: 10,
-                    max: 24,
-                    divisions: 8,
-                    onChanged: (fontSize) => context
-                        .read<DocumentAppearanceCubit>()
-                        .syncFontSize(fontSize),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Row(
+        children: [
+          const FlowyText('A', fontSize: 14),
+          const HSpace(6),
+          Expanded(
+            child: SliderTheme(
+              data: Theme.of(context).sliderTheme.copyWith(
+                    showValueIndicator: ShowValueIndicator.never,
+                    thumbShape: const RoundSliderThumbShape(
+                      enabledThumbRadius: 8,
+                    ),
+                    overlayShape: const RoundSliderOverlayShape(
+                      overlayRadius: 16,
+                    ),
                   ),
-                ),
+              child: Slider(
+                value: _value,
+                min: widget.minimumValue,
+                max: widget.maximumValue,
+                divisions: widget.divisions,
+                onChanged: (value) {
+                  setState(() {
+                    _value = value;
+                  });
+
+                  widget.onChanged(value);
+                },
               ),
-              const HSpace(6),
-              const FlowyText('A', fontSize: 20),
-            ],
+            ),
           ),
-        );
-      },
+          const HSpace(6),
+          const FlowyText('A', fontSize: 20),
+        ],
+      ),
     );
   }
 }
