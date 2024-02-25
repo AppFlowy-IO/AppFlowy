93af6e69a1232195b0ee89221724a2df1b5b5e76
Lucas.Xu <lucas.xu@appflowy.io>
2024-02-25 21:59:23 +0700

    feat: convert link preview block to url text (#4734)

--- frontend/appflowy_flutter/lib/plugins/document/presentation/editor_plugins/link_preview/custom_link_preview.dart
+++ frontend/appflowy_flutter/lib/plugins/document/presentation/editor_plugins/link_preview/custom_link_preview.dart
@@ -1,17 +1,28 @@
+import 'package:appflowy/generated/flowy_svgs.g.dart';
+import 'package:appflowy/generated/locale_keys.g.dart';
+import 'package:appflowy/mobile/presentation/widgets/widgets.dart';
+import 'package:appflowy/plugins/document/presentation/editor_plugins/actions/mobile_block_action_buttons.dart';
+import 'package:appflowy/plugins/document/presentation/editor_plugins/link_preview/shared.dart';
 import 'package:appflowy/shared/appflowy_network_image.dart';
+import 'package:appflowy_editor/appflowy_editor.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flowy_infra_ui/flowy_infra_ui.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
 class CustomLinkPreviewWidget extends StatelessWidget {
   const CustomLinkPreviewWidget({
     super.key,
+    required this.node,
     required this.url,
     this.title,
     this.description,
     this.imageUrl,
   });
 
+  final Node node;
   final String? title;
   final String? description;
   final String? imageUrl;
@@ -19,20 +30,29 @@ class CustomLinkPreviewWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () => launchUrlString(url),
-      child: Container(
-        clipBehavior: Clip.hardEdge,
-        decoration: BoxDecoration(
-          border: Border.all(
-            color: Theme.of(context).colorScheme.onSurface,
-          ),
-          borderRadius: BorderRadius.circular(
-            6.0,
-          ),
+    final documentFontSize = context
+            .read<EditorState>()
+            .editorStyle
+            .textStyleConfiguration
+            .text
+            .fontSize ??
+        16.0;
+    final (fontSize, width) = PlatformExtension.isDesktopOrWeb
+        ? (documentFontSize, 180.0)
+        : (documentFontSize - 2, 120.0);
+    final Widget child = Container(
+      clipBehavior: Clip.hardEdge,
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Theme.of(context).colorScheme.onSurface,
         ),
+        borderRadius: BorderRadius.circular(
+          6.0,
+        ),
+      ),
+      child: IntrinsicHeight(
         child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             if (imageUrl != null)
               ClipRRect(
@@ -42,8 +62,7 @@ class CustomLinkPreviewWidget extends StatelessWidget {
                 ),
                 child: FlowyNetworkImage(
                   url: imageUrl!,
-                  width: 180,
-                  height: 120,
+                  width: width,
                 ),
               ),
             Expanded(
@@ -55,12 +74,15 @@ class CustomLinkPreviewWidget extends StatelessWidget {
                   children: [
                     if (title != null)
                       Padding(
-                        padding: const EdgeInsets.only(bottom: 4.0),
+                        padding: const EdgeInsets.only(
+                          bottom: 4.0,
+                          right: 10.0,
+                        ),
                         child: FlowyText.medium(
                           title!,
                           maxLines: 2,
                           overflow: TextOverflow.ellipsis,
-                          fontSize: 16.0,
+                          fontSize: fontSize,
                         ),
                       ),
                     if (description != null)
@@ -70,6 +92,7 @@ class CustomLinkPreviewWidget extends StatelessWidget {
                           description!,
                           maxLines: 2,
                           overflow: TextOverflow.ellipsis,
+                          fontSize: fontSize - 4,
                         ),
                       ),
                     FlowyText(
@@ -77,6 +100,7 @@ class CustomLinkPreviewWidget extends StatelessWidget {
                       overflow: TextOverflow.ellipsis,
                       maxLines: 2,
                       color: Theme.of(context).hintColor,
+                      fontSize: fontSize - 4,
                     ),
                   ],
                 ),
@@ -86,5 +110,40 @@ class CustomLinkPreviewWidget extends StatelessWidget {
         ),
       ),
     );
+
+    if (PlatformExtension.isDesktopOrWeb) {
+      return InkWell(
+        onTap: () => launchUrlString(url),
+        child: child,
+      );
+    }
+
+    return MobileBlockActionButtons(
+      node: node,
+      editorState: context.read<EditorState>(),
+      extendActionWidgets: _buildExtendActionWidgets(context),
+      child: child,
+    );
+  }
+
+  // only used on mobile platform
+  List<Widget> _buildExtendActionWidgets(BuildContext context) {
+    return [
+      FlowyOptionTile.text(
+        showTopBorder: false,
+        text: LocaleKeys.document_plugins_urlPreview_convertToLink.tr(),
+        leftIcon: const FlowySvg(
+          FlowySvgs.m_aa_link_s,
+          size: Size.square(20),
+        ),
+        onTap: () {
+          context.pop();
+          convertUrlPreviewNodeToLink(
+            context.read<EditorState>(),
+            node,
+          );
+        },
+      ),
+    ];
   }
 }
