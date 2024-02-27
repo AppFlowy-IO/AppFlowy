55847ae56e8b50baf2f05ccbf72791d5a1ec532d
Lucas.Xu <lucas.xu@appflowy.io>
2024-02-27 22:39:36 +0800

    feat: update add block menu colors

--- frontend/appflowy_flutter/lib/plugins/document/presentation/editor_plugins/mobile_toolbar_v3/add_block_toolbar_item.dart
+++ frontend/appflowy_flutter/lib/plugins/document/presentation/editor_plugins/mobile_toolbar_v3/add_block_toolbar_item.dart
@@ -83,7 +83,7 @@ Future<bool?> showAddBlockMenu(
 }
 
 class _AddBlockMenu extends StatelessWidget {
-  _AddBlockMenu({
+  const _AddBlockMenu({
     required this.selection,
     required this.editorState,
   });
@@ -91,151 +91,10 @@ class _AddBlockMenu extends StatelessWidget {
   final Selection selection;
   final EditorState editorState;
 
-  late final List<TypeOptionMenuItemValue<String>> typeOptionMenuItemValue = [
-    // heading 1 - 3
-    TypeOptionMenuItemValue(
-      value: HeadingBlockKeys.type,
-      backgroundColor: const Color(0xFFBAC9FF),
-      text: LocaleKeys.editor_heading1.tr(),
-      icon: FlowySvgs.m_add_block_h1_s,
-      onTap: (_, __) => _insertBlock(headingNode(level: 1)),
-    ),
-    TypeOptionMenuItemValue(
-      value: HeadingBlockKeys.type,
-      backgroundColor: const Color(0xFFBAC9FF),
-      text: LocaleKeys.editor_heading2.tr(),
-      icon: FlowySvgs.m_add_block_h2_s,
-      onTap: (_, __) => _insertBlock(headingNode(level: 2)),
-    ),
-    TypeOptionMenuItemValue(
-      value: HeadingBlockKeys.type,
-      backgroundColor: const Color(0xFFBAC9FF),
-      text: LocaleKeys.editor_heading3.tr(),
-      icon: FlowySvgs.m_add_block_h3_s,
-      onTap: (_, __) => _insertBlock(headingNode(level: 3)),
-    ),
-
-    // paragraph
-    TypeOptionMenuItemValue(
-      value: ParagraphBlockKeys.type,
-      backgroundColor: const Color(0xFFBAC9FF),
-      text: LocaleKeys.editor_text.tr(),
-      icon: FlowySvgs.m_add_block_paragraph_s,
-      onTap: (_, __) => _insertBlock(paragraphNode()),
-    ),
-
-    // checkbox
-    TypeOptionMenuItemValue(
-      value: TodoListBlockKeys.type,
-      backgroundColor: const Color(0xFF98F4CD),
-      text: LocaleKeys.editor_checkbox.tr(),
-      icon: FlowySvgs.m_add_block_checkbox_s,
-      onTap: (_, __) => _insertBlock(todoListNode(checked: false)),
-    ),
-
-    // quote
-    TypeOptionMenuItemValue(
-      value: QuoteBlockKeys.type,
-      backgroundColor: const Color(0xFFFDEDA7),
-      text: LocaleKeys.editor_quote.tr(),
-      icon: FlowySvgs.m_add_block_quote_s,
-      onTap: (_, __) => _insertBlock(quoteNode()),
-    ),
-
-    // bulleted list, numbered list, toggle list
-    TypeOptionMenuItemValue(
-      value: BulletedListBlockKeys.type,
-      backgroundColor: const Color(0xFFFFB9EF),
-      text: LocaleKeys.editor_bulletedListShortForm.tr(),
-      icon: FlowySvgs.m_add_block_bulleted_list_s,
-      onTap: (_, __) => _insertBlock(bulletedListNode()),
-    ),
-    TypeOptionMenuItemValue(
-      value: NumberedListBlockKeys.type,
-      backgroundColor: const Color(0xFFFFB9EF),
-      text: LocaleKeys.editor_numberedListShortForm.tr(),
-      icon: FlowySvgs.m_add_block_numbered_list_s,
-      onTap: (_, __) => _insertBlock(numberedListNode()),
-    ),
-    TypeOptionMenuItemValue(
-      value: ToggleListBlockKeys.type,
-      backgroundColor: const Color(0xFFFFB9EF),
-      text: LocaleKeys.editor_toggleListShortForm.tr(),
-      icon: FlowySvgs.m_add_block_toggle_s,
-      onTap: (_, __) => _insertBlock(toggleListBlockNode()),
-    ),
-
-    // image
-    TypeOptionMenuItemValue(
-      value: DividerBlockKeys.type,
-      backgroundColor: const Color(0xFF98F4CD),
-      text: LocaleKeys.editor_image.tr(),
-      icon: FlowySvgs.m_add_block_image_s,
-      onTap: (_, __) async {
-        AppGlobals.rootNavKey.currentContext?.pop(true);
-        Future.delayed(const Duration(milliseconds: 400), () async {
-          final imagePlaceholderKey = GlobalKey<ImagePlaceholderState>();
-          await editorState.insertEmptyImageBlock(imagePlaceholderKey);
-        });
-      },
-    ),
-
-    // date
-    TypeOptionMenuItemValue(
-      value: ParagraphBlockKeys.type,
-      backgroundColor: const Color(0xFF91EAF5),
-      text: LocaleKeys.editor_date.tr(),
-      icon: FlowySvgs.m_add_block_date_s,
-      onTap: (_, __) => _insertBlock(dateMentionNode()),
-    ),
-
-    // divider
-    TypeOptionMenuItemValue(
-      value: DividerBlockKeys.type,
-      backgroundColor: const Color(0xFF98F4CD),
-      text: LocaleKeys.editor_divider.tr(),
-      icon: FlowySvgs.m_add_block_divider_s,
-      onTap: (_, __) {
-        AppGlobals.rootNavKey.currentContext?.pop(true);
-        Future.delayed(const Duration(milliseconds: 100), () {
-          editorState.insertDivider(selection);
-        });
-      },
-    ),
-
-    // callout, code, math equation
-    TypeOptionMenuItemValue(
-      value: CalloutBlockKeys.type,
-      backgroundColor: const Color(0xFFCABDFF),
-      text: LocaleKeys.document_plugins_callout.tr(),
-      icon: FlowySvgs.m_add_block_callout_s,
-      onTap: (_, __) => _insertBlock(calloutNode()),
-    ),
-    TypeOptionMenuItemValue(
-      value: CodeBlockKeys.type,
-      backgroundColor: const Color(0xFFCABDFF),
-      text: LocaleKeys.editor_codeBlockShortForm.tr(),
-      icon: FlowySvgs.m_add_block_code_s,
-      onTap: (_, __) => _insertBlock(codeBlockNode()),
-    ),
-    TypeOptionMenuItemValue(
-      value: MathEquationBlockKeys.type,
-      backgroundColor: const Color(0xFFCABDFF),
-      text: LocaleKeys.editor_mathEquationShortForm.tr(),
-      icon: FlowySvgs.m_add_block_formula_s,
-      onTap: (_, __) {
-        AppGlobals.rootNavKey.currentContext?.pop(true);
-        Future.delayed(const Duration(milliseconds: 100), () {
-          editorState.insertMathEquation(selection);
-        });
-      },
-    ),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return TypeOptionMenu<String>(
-      values: typeOptionMenuItemValue,
+      values: buildTypeOptionMenuItemValues(context),
       scaleFactor: context.scale,
     );
   }
@@ -248,6 +107,188 @@ class _AddBlockMenu extends StatelessWidget {
         node,
       );
     });
+  }
+
+  List<TypeOptionMenuItemValue<String>> buildTypeOptionMenuItemValues(
+    BuildContext context,
+  ) {
+    final colorMap = _colorMap(context);
+    return [
+      // heading 1 - 3
+      TypeOptionMenuItemValue(
+        value: HeadingBlockKeys.type,
+        backgroundColor: colorMap[HeadingBlockKeys.type]!,
+        text: LocaleKeys.editor_heading1.tr(),
+        icon: FlowySvgs.m_add_block_h1_s,
+        onTap: (_, __) => _insertBlock(headingNode(level: 1)),
+      ),
+      TypeOptionMenuItemValue(
+        value: HeadingBlockKeys.type,
+        backgroundColor: colorMap[HeadingBlockKeys.type]!,
+        text: LocaleKeys.editor_heading2.tr(),
+        icon: FlowySvgs.m_add_block_h2_s,
+        onTap: (_, __) => _insertBlock(headingNode(level: 2)),
+      ),
+      TypeOptionMenuItemValue(
+        value: HeadingBlockKeys.type,
+        backgroundColor: colorMap[HeadingBlockKeys.type]!,
+        text: LocaleKeys.editor_heading3.tr(),
+        icon: FlowySvgs.m_add_block_h3_s,
+        onTap: (_, __) => _insertBlock(headingNode(level: 3)),
+      ),
+
+      // paragraph
+      TypeOptionMenuItemValue(
+        value: ParagraphBlockKeys.type,
+        backgroundColor: colorMap[ParagraphBlockKeys.type]!,
+        text: LocaleKeys.editor_text.tr(),
+        icon: FlowySvgs.m_add_block_paragraph_s,
+        onTap: (_, __) => _insertBlock(paragraphNode()),
+      ),
+
+      // checkbox
+      TypeOptionMenuItemValue(
+        value: TodoListBlockKeys.type,
+        backgroundColor: colorMap[TodoListBlockKeys.type]!,
+        text: LocaleKeys.editor_checkbox.tr(),
+        icon: FlowySvgs.m_add_block_checkbox_s,
+        onTap: (_, __) => _insertBlock(todoListNode(checked: false)),
+      ),
+
+      // quote
+      TypeOptionMenuItemValue(
+        value: QuoteBlockKeys.type,
+        backgroundColor: colorMap[QuoteBlockKeys.type]!,
+        text: LocaleKeys.editor_quote.tr(),
+        icon: FlowySvgs.m_add_block_quote_s,
+        onTap: (_, __) => _insertBlock(quoteNode()),
+      ),
+
+      // bulleted list, numbered list, toggle list
+      TypeOptionMenuItemValue(
+        value: BulletedListBlockKeys.type,
+        backgroundColor: colorMap[BulletedListBlockKeys.type]!,
+        text: LocaleKeys.editor_bulletedListShortForm.tr(),
+        icon: FlowySvgs.m_add_block_bulleted_list_s,
+        onTap: (_, __) => _insertBlock(bulletedListNode()),
+      ),
+      TypeOptionMenuItemValue(
+        value: NumberedListBlockKeys.type,
+        backgroundColor: colorMap[NumberedListBlockKeys.type]!,
+        text: LocaleKeys.editor_numberedListShortForm.tr(),
+        icon: FlowySvgs.m_add_block_numbered_list_s,
+        onTap: (_, __) => _insertBlock(numberedListNode()),
+      ),
+      TypeOptionMenuItemValue(
+        value: ToggleListBlockKeys.type,
+        backgroundColor: colorMap[ToggleListBlockKeys.type]!,
+        text: LocaleKeys.editor_toggleListShortForm.tr(),
+        icon: FlowySvgs.m_add_block_toggle_s,
+        onTap: (_, __) => _insertBlock(toggleListBlockNode()),
+      ),
+
+      // image
+      TypeOptionMenuItemValue(
+        value: ImageBlockKeys.type,
+        backgroundColor: colorMap[ImageBlockKeys.type]!,
+        text: LocaleKeys.editor_image.tr(),
+        icon: FlowySvgs.m_add_block_image_s,
+        onTap: (_, __) async {
+          AppGlobals.rootNavKey.currentContext?.pop(true);
+          Future.delayed(const Duration(milliseconds: 400), () async {
+            final imagePlaceholderKey = GlobalKey<ImagePlaceholderState>();
+            await editorState.insertEmptyImageBlock(imagePlaceholderKey);
+          });
+        },
+      ),
+
+      // date
+      TypeOptionMenuItemValue(
+        value: ParagraphBlockKeys.type,
+        backgroundColor: colorMap['date']!,
+        text: LocaleKeys.editor_date.tr(),
+        icon: FlowySvgs.m_add_block_date_s,
+        onTap: (_, __) => _insertBlock(dateMentionNode()),
+      ),
+
+      // divider
+      TypeOptionMenuItemValue(
+        value: DividerBlockKeys.type,
+        backgroundColor: colorMap[DividerBlockKeys.type]!,
+        text: LocaleKeys.editor_divider.tr(),
+        icon: FlowySvgs.m_add_block_divider_s,
+        onTap: (_, __) {
+          AppGlobals.rootNavKey.currentContext?.pop(true);
+          Future.delayed(const Duration(milliseconds: 100), () {
+            editorState.insertDivider(selection);
+          });
+        },
+      ),
+
+      // callout, code, math equation
+      TypeOptionMenuItemValue(
+        value: CalloutBlockKeys.type,
+        backgroundColor: colorMap[CalloutBlockKeys.type]!,
+        text: LocaleKeys.document_plugins_callout.tr(),
+        icon: FlowySvgs.m_add_block_callout_s,
+        onTap: (_, __) => _insertBlock(calloutNode()),
+      ),
+      TypeOptionMenuItemValue(
+        value: CodeBlockKeys.type,
+        backgroundColor: colorMap[CodeBlockKeys.type]!,
+        text: LocaleKeys.editor_codeBlockShortForm.tr(),
+        icon: FlowySvgs.m_add_block_code_s,
+        onTap: (_, __) => _insertBlock(codeBlockNode()),
+      ),
+      TypeOptionMenuItemValue(
+        value: MathEquationBlockKeys.type,
+        backgroundColor: colorMap[MathEquationBlockKeys.type]!,
+        text: LocaleKeys.editor_mathEquationShortForm.tr(),
+        icon: FlowySvgs.m_add_block_formula_s,
+        onTap: (_, __) {
+          AppGlobals.rootNavKey.currentContext?.pop(true);
+          Future.delayed(const Duration(milliseconds: 100), () {
+            editorState.insertMathEquation(selection);
+          });
+        },
+      ),
+    ];
+  }
+
+  Map<String, Color> _colorMap(BuildContext context) {
+    final isDarkMode = Theme.of(context).brightness == Brightness.dark;
+    if (isDarkMode) {
+      return {
+        HeadingBlockKeys.type: const Color(0xFF5465A1),
+        ParagraphBlockKeys.type: const Color(0xFF5465A1),
+        TodoListBlockKeys.type: const Color(0xFF4BB299),
+        QuoteBlockKeys.type: const Color(0xFFBAAC74),
+        BulletedListBlockKeys.type: const Color(0xFFA35F94),
+        NumberedListBlockKeys.type: const Color(0xFFA35F94),
+        ToggleListBlockKeys.type: const Color(0xFFA35F94),
+        ImageBlockKeys.type: const Color(0xFFBAAC74),
+        'date': const Color(0xFF40AAB8),
+        DividerBlockKeys.type: const Color(0xFF4BB299),
+        CalloutBlockKeys.type: const Color(0xFF66599B),
+        CodeBlockKeys.type: const Color(0xFF66599B),
+        MathEquationBlockKeys.type: const Color(0xFF66599B),
+      };
+    }
+    return {
+      HeadingBlockKeys.type: const Color(0xFFBECCFF),
+      ParagraphBlockKeys.type: const Color(0xFFBECCFF),
+      TodoListBlockKeys.type: const Color(0xFF98F4CD),
+      QuoteBlockKeys.type: const Color(0xFFFDEDA7),
+      BulletedListBlockKeys.type: const Color(0xFFFFB9EF),
+      NumberedListBlockKeys.type: const Color(0xFFFFB9EF),
+      ToggleListBlockKeys.type: const Color(0xFFFFB9EF),
+      ImageBlockKeys.type: const Color(0xFFFDEDA7),
+      'date': const Color(0xFF91EAF5),
+      DividerBlockKeys.type: const Color(0xFF98F4CD),
+      CalloutBlockKeys.type: const Color(0xFFCABDFF),
+      CodeBlockKeys.type: const Color(0xFFCABDFF),
+      MathEquationBlockKeys.type: const Color(0xFFCABDFF),
+    };
   }
 }
 
