f4369b726c8cbbc1fa278392577cbf374a5288ff
Lucas.Xu <lucas.xu@appflowy.io>
2024-03-04 10:19:44 +0800

    feat: support clearing caches

--- frontend/appflowy_flutter/lib/shared/custom_image_cache_manager.dart
+++ frontend/appflowy_flutter/lib/shared/custom_image_cache_manager.dart
@@ -1,6 +1,9 @@
+import 'package:appflowy/shared/appflowy_cache_manager.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 
-class CustomImageCacheManager extends CacheManager with ImageCacheManager {
+class CustomImageCacheManager extends CacheManager
+    with ImageCacheManager
+    implements ICache {
   CustomImageCacheManager._() : super(Config(key));
 
   factory CustomImageCacheManager() => _instance;
@@ -8,4 +11,16 @@ class CustomImageCacheManager extends CacheManager with ImageCacheManager {
   static final CustomImageCacheManager _instance = CustomImageCacheManager._();
 
   static const key = 'appflowy_image_cache';
+
+  @override
+  Future<int> cacheSize() async {
+    // https://github.com/Baseflow/flutter_cache_manager/issues/239#issuecomment-719475429
+    // this package does not provide a way to get the cache size
+    return 0;
+  }
+
+  @override
+  Future<void> clearAll() async {
+    await emptyCache();
+  }
 }
