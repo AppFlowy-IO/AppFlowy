1f562c3a4df76a5d904b7b110a5db39e3f16191f
Mathias Mogensen <mathias@appflowy.io>
2024-03-05 21:16:33 +0100

    fix: image url check in for embed link

--- frontend/appflowy_flutter/lib/plugins/document/presentation/editor_plugins/image/embed_image_url_widget.dart
+++ frontend/appflowy_flutter/lib/plugins/document/presentation/editor_plugins/image/embed_image_url_widget.dart
@@ -1,7 +1,15 @@
+import 'package:flutter/material.dart';
+
 import 'package:appflowy/generated/locale_keys.g.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flowy_infra_ui/flowy_infra_ui.dart';
-import 'package:flutter/material.dart';
+
+/// This pattern allows for both HTTP and HTTPS Scheme
+/// It allows for query parameters
+/// It only allows the following image extensions: .png, .jpg, .gif, .webm
+///
+const String _imgUrlPattern =
+    r'(https?:\/\/)([^\s(["<,>/]*)(\/)[^\s[",><]*(.png|.jpg|.gif|.webm)(\?[^\s[",><]*)?';
 
 class EmbedImageUrlWidget extends StatefulWidget {
   const EmbedImageUrlWidget({
@@ -16,6 +24,7 @@ class EmbedImageUrlWidget extends StatefulWidget {
 }
 
 class _EmbedImageUrlWidgetState extends State<EmbedImageUrlWidget> {
+  bool isUrlValid = true;
   String inputText = '';
 
   @override
@@ -25,8 +34,15 @@ class _EmbedImageUrlWidgetState extends State<EmbedImageUrlWidget> {
         FlowyTextField(
           hintText: LocaleKeys.document_imageBlock_embedLink_placeholder.tr(),
           onChanged: (value) => inputText = value,
-          onEditingComplete: () => widget.onSubmit(inputText),
+          onEditingComplete: submit,
         ),
+        if (!isUrlValid) ...[
+          const VSpace(8),
+          FlowyText(
+            LocaleKeys.document_plugins_cover_invalidImageUrl.tr(),
+            color: Theme.of(context).colorScheme.error,
+          ),
+        ],
         const VSpace(8),
         SizedBox(
           width: 160,
@@ -37,10 +53,23 @@ class _EmbedImageUrlWidgetState extends State<EmbedImageUrlWidget> {
               LocaleKeys.document_imageBlock_embedLink_label.tr(),
               textAlign: TextAlign.center,
             ),
-            onTap: () => widget.onSubmit(inputText),
+            onTap: submit,
           ),
         ),
       ],
     );
+  }
+
+  void submit() {
+    if (checkUrlValidity(inputText)) {
+      return widget.onSubmit(inputText);
+    }
+
+    setState(() => isUrlValid = false);
+  }
+
+  bool checkUrlValidity(String url) {
+    final regex = RegExp(_imgUrlPattern);
+    return regex.hasMatch(url);
   }
 }
