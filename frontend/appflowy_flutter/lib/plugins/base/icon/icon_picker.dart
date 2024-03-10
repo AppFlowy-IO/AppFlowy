a3f72a80bb91d268543c11f44e4115d5c5e20420
Mathias Mogensen <mathias@appflowy.io>
2024-03-10 16:16:58 +0100

    fix: set icon type when removing icon

--- frontend/appflowy_flutter/lib/plugins/base/icon/icon_picker.dart
+++ frontend/appflowy_flutter/lib/plugins/base/icon/icon_picker.dart
@@ -1,11 +1,26 @@
+import 'package:flutter/material.dart';
+
 import 'package:appflowy/generated/flowy_svgs.g.dart';
 import 'package:appflowy/generated/locale_keys.g.dart';
 import 'package:appflowy/plugins/base/emoji/emoji_picker.dart';
+import 'package:appflowy_backend/protobuf/flowy-folder/icon.pbenum.dart';
 import 'package:appflowy_editor/appflowy_editor.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flowy_infra_ui/flowy_infra_ui.dart';
 import 'package:flowy_infra_ui/style_widget/hover.dart';
-import 'package:flutter/material.dart';
+
+extension ToProto on FlowyIconType {
+  ViewIconTypePB toProto() {
+    switch (this) {
+      case FlowyIconType.emoji:
+        return ViewIconTypePB.Emoji;
+      case FlowyIconType.icon:
+        return ViewIconTypePB.Icon;
+      case FlowyIconType.custom:
+        return ViewIconTypePB.Url;
+    }
+  }
+}
 
 enum FlowyIconType {
   emoji,
