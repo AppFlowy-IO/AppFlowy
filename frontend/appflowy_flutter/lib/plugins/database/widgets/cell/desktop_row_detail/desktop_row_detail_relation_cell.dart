5345d5e0cdf3828e467f61f52e6b53bcc639aa2f
Richard Shiue <71320345+richardshiue@users.noreply.github.com>
2024-03-12 19:19:29 +0800

    chore: empty in row detail

--- frontend/appflowy_flutter/lib/plugins/database/widgets/cell/desktop_row_detail/desktop_row_detail_relation_cell.dart
+++ frontend/appflowy_flutter/lib/plugins/database/widgets/cell/desktop_row_detail/desktop_row_detail_relation_cell.dart
@@ -2,6 +2,7 @@ import 'package:appflowy/generated/locale_keys.g.dart';
 import 'package:appflowy/plugins/database/widgets/row/cells/cell_container.dart';
 import 'package:appflowy/plugins/database/widgets/cell_editor/relation_cell_editor.dart';
 import 'package:appflowy/plugins/database/application/cell/bloc/relation_cell_bloc.dart';
+import 'package:appflowy_backend/protobuf/flowy-database2/protobuf.dart';
 import 'package:appflowy_popover/appflowy_popover.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flowy_infra_ui/flowy_infra_ui.dart';
@@ -36,22 +37,35 @@ class DesktopRowDetailRelationCellSkin extends IEditableRelationCellSkin {
       child: Container(
         alignment: AlignmentDirectional.centerStart,
         padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 6),
-        child: Wrap(
-          runSpacing: 4.0,
-          spacing: 4.0,
-          children: state.rows.map(
-            (row) {
-              final isEmpty = row.name.isEmpty;
-              return FlowyText.medium(
-                isEmpty ? LocaleKeys.grid_row_titlePlaceholder.tr() : row.name,
-                color: isEmpty ? Theme.of(context).hintColor : null,
-                decoration: TextDecoration.underline,
-                overflow: TextOverflow.ellipsis,
-              );
-            },
-          ).toList(),
-        ),
+        child: state.rows.isEmpty
+            ? _buildPlaceholder(context)
+            : _buildRows(context, state.rows),
       ),
+    );
+  }
+
+  Widget _buildPlaceholder(BuildContext context) {
+    return FlowyText(
+      LocaleKeys.grid_row_textPlaceholder.tr(),
+      color: Theme.of(context).hintColor,
+    );
+  }
+
+  Widget _buildRows(BuildContext context, List<RelatedRowDataPB> rows) {
+    return Wrap(
+      runSpacing: 4.0,
+      spacing: 4.0,
+      children: rows.map(
+        (row) {
+          final isEmpty = row.name.isEmpty;
+          return FlowyText.medium(
+            isEmpty ? LocaleKeys.grid_row_titlePlaceholder.tr() : row.name,
+            color: isEmpty ? Theme.of(context).hintColor : null,
+            decoration: TextDecoration.underline,
+            overflow: TextOverflow.ellipsis,
+          );
+        },
+      ).toList(),
     );
   }
 }
