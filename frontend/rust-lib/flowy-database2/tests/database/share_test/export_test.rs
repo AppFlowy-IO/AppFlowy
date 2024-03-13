0f48310e227531ce3c0fd221db0659c873a330ef
nathan <nathan@appflowy.io>
2024-03-13 13:30:29 +0800

    chore: fix test

--- frontend/rust-lib/flowy-database2/tests/database/share_test/export_test.rs
+++ frontend/rust-lib/flowy-database2/tests/database/share_test/export_test.rs
@@ -1,8 +1,8 @@
-use chrono::Duration;
 use flowy_database2::entities::FieldType;
 use flowy_database2::services::cell::stringify_cell_data;
 use flowy_database2::services::field::CHECK;
 use flowy_database2::services::share::csv::CSVFormat;
+use std::time::Duration;
 use tokio::time::sleep;
 
 use crate::database::database_editor::DatabaseEditorTest;
