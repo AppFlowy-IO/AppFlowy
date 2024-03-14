a09cfd0056f27000b838ce1139aa6511d47314b8
Jayaprakash <jayaprakash07.dev@gmail.com>
2024-03-15 01:53:02 +0530

    fix: resolves merge conflicts with base branch

--- frontend/appflowy_flutter/integration_test/desktop/document/document_with_overview_block_test.dart
+++ frontend/appflowy_flutter/integration_test/desktop/document/document_with_overview_block_test.dart
@@ -9,7 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
-import '../util/util.dart';
+import '../../shared/util.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
