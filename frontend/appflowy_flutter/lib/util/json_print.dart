4fd0b31ce4022f108990025444cf48f07690dd10
Mathias Mogensen <mathias@appflowy.io>
2024-03-19 12:37:27 +0100

    chore: remove unused files

--- frontend/appflowy_flutter/lib/util/json_print.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:convert';
-
-import 'package:appflowy_backend/log.dart';
-
-const JsonEncoder _encoder = JsonEncoder.withIndent('  ');
-void prettyPrintJson(Object? object) {
-  Log.trace(_encoder.convert(object));
-}
