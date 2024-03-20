3507b57da112bb1342d5b2a8a4188aa84080d09c
Lucas.Xu <lucas.xu@appflowy.io>
2024-03-20 23:15:30 +0800

    feat: support workspace on mobile

--- frontend/appflowy_flutter/lib/mobile/presentation/home/mobile_folders.dart
+++ frontend/appflowy_flutter/lib/mobile/presentation/home/mobile_folders.dart
@@ -1,9 +1,12 @@
+import 'package:appflowy/generated/locale_keys.g.dart';
 import 'package:appflowy/mobile/application/mobile_router.dart';
 import 'package:appflowy/mobile/presentation/home/section_folder/mobile_home_section_folder.dart';
+import 'package:appflowy/shared/feature_flags.dart';
 import 'package:appflowy/workspace/application/favorite/favorite_bloc.dart';
 import 'package:appflowy/workspace/application/menu/sidebar_sections_bloc.dart';
 import 'package:appflowy_backend/protobuf/flowy-folder/protobuf.dart';
-import 'package:appflowy_backend/protobuf/flowy-user/user_profile.pb.dart';
+import 'package:appflowy_backend/protobuf/flowy-user/protobuf.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flowy_infra_ui/flowy_infra_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -49,19 +52,30 @@ class MobileFolders extends StatelessWidget {
           }
         },
         builder: (context, state) {
+          final isCollaborativeWorkspace =
+              user.authenticator != AuthenticatorPB.Local &&
+                  FeatureFlag.collaborativeWorkspace.isOn;
           return SlidableAutoCloseBehavior(
             child: Column(
               children: [
-                // TODO: disable the public folder in local mode.
-                MobileSectionFolder(
-                  title: 'Public',
-                  views: state.section.publicViews,
-                ),
-                const VSpace(8.0),
-                MobileSectionFolder(
-                  title: 'Private',
-                  views: state.section.privateViews,
-                ),
+                ...isCollaborativeWorkspace
+                    ? [
+                        MobileSectionFolder(
+                          title: LocaleKeys.sideBar_public.tr(),
+                          views: state.section.publicViews,
+                        ),
+                        const VSpace(8.0),
+                        MobileSectionFolder(
+                          title: LocaleKeys.sideBar_private.tr(),
+                          views: state.section.privateViews,
+                        ),
+                      ]
+                    : [
+                        MobileSectionFolder(
+                          title: LocaleKeys.sideBar_personal.tr(),
+                          views: state.section.publicViews,
+                        ),
+                      ],
                 const VSpace(8.0),
               ],
             ),
