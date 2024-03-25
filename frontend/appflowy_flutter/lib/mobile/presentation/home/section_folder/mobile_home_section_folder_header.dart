87a3468ed1d4b7b3a8d3225bcbaf282656706146
Lucas.Xu <lucas.xu@appflowy.io>
2024-03-25 17:37:00 +0800

    fix: remove sidebar_root_view_bloc

--- frontend/appflowy_flutter/lib/mobile/presentation/home/section_folder/mobile_home_section_folder_header.dart
+++ frontend/appflowy_flutter/lib/mobile/presentation/home/section_folder/mobile_home_section_folder_header.dart
@@ -7,6 +7,9 @@ import 'package:flowy_infra_ui/flowy_infra_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+@visibleForTesting
+const Key mobileCreateNewPageButtonKey = Key('mobileCreateNewPageButtonKey');
+
 class MobileSectionFolderHeader extends StatefulWidget {
   const MobileSectionFolderHeader({
     super.key,
@@ -60,6 +63,7 @@ class _MobileSectionFolderHeaderState extends State<MobileSectionFolderHeader> {
           ),
         ),
         FlowyIconButton(
+          key: mobileCreateNewPageButtonKey,
           hoverColor: Theme.of(context).colorScheme.secondaryContainer,
           iconPadding: const EdgeInsets.all(2),
           height: iconSize,
