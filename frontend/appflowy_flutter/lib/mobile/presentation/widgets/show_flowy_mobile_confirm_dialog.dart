266a2a53ab4b43ef868bedb8f68b5d4fc161e79e
Lucas.Xu <lucas.xu@appflowy.io>
2024-05-06 11:27:55 +0800

    feat: request permission again if the user denied the photo permission (#5251)
    * fix: clear the email field after sending email
    
    * fix: ask permission before picking image
    
    * feat: improve photo permission UI design
    
    * chore: update translations
    
    * fix: android photo permission
    
    * chore: update translations
    
    * fix: awareness meta data decode error

--- frontend/appflowy_flutter/lib/mobile/presentation/widgets/show_flowy_mobile_confirm_dialog.dart
+++ frontend/appflowy_flutter/lib/mobile/presentation/widgets/show_flowy_mobile_confirm_dialog.dart
@@ -1,53 +1,86 @@
 import 'package:appflowy/generated/locale_keys.g.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flowy_infra_ui/flowy_infra_ui.dart';
 import 'package:flutter/material.dart';
 
-///show the dialog to confirm one single action
-///[onActionButtonPressed] and [onCancelButtonPressed] end with close the dialog
+enum ConfirmDialogActionAlignment {
+  // The action buttons are aligned vertically
+  // ---------------------
+  // |  Action Button    |
+  // |  Cancel Button    |
+  vertical,
+  // The action buttons are aligned horizontally
+  // ---------------------
+  // |  Action Button    |  Cancel Button    |
+  horizontal,
+}
+
+/// show the dialog to confirm one single action
+/// [onActionButtonPressed] and [onCancelButtonPressed] end with close the dialog
 Future<T?> showFlowyMobileConfirmDialog<T>(
   BuildContext context, {
   Widget? title,
   Widget? content,
+  ConfirmDialogActionAlignment actionAlignment =
+      ConfirmDialogActionAlignment.horizontal,
   required String actionButtonTitle,
+  required VoidCallback? onActionButtonPressed,
   Color? actionButtonColor,
   String? cancelButtonTitle,
-  required void Function()? onActionButtonPressed,
-  void Function()? onCancelButtonPressed,
+  Color? cancelButtonColor,
+  VoidCallback? onCancelButtonPressed,
 }) async {
   return showDialog(
     context: context,
     builder: (dialogContext) {
       final foregroundColor = Theme.of(context).colorScheme.onSurface;
+      final actionButton = TextButton(
+        child: FlowyText(
+          actionButtonTitle,
+          color: actionButtonColor ?? foregroundColor,
+        ),
+        onPressed: () {
+          onActionButtonPressed?.call();
+          // we cannot use dialogContext.pop() here because this is no GoRouter in dialogContext. Use Navigator instead to close the dialog.
+          Navigator.of(dialogContext).pop();
+        },
+      );
+      final cancelButton = TextButton(
+        child: FlowyText(
+          cancelButtonTitle ?? LocaleKeys.button_cancel.tr(),
+          color: cancelButtonColor ?? foregroundColor,
+        ),
+        onPressed: () {
+          onCancelButtonPressed?.call();
+          Navigator.of(dialogContext).pop();
+        },
+      );
+
+      final actions = switch (actionAlignment) {
+        ConfirmDialogActionAlignment.horizontal => [
+            actionButton,
+            cancelButton,
+          ],
+        ConfirmDialogActionAlignment.vertical => [
+            Column(
+              children: [
+                actionButton,
+                const Divider(height: 1, color: Colors.grey),
+                cancelButton,
+              ],
+            ),
+          ],
+      };
+
       return AlertDialog.adaptive(
         title: title,
         content: content,
-        actions: [
-          TextButton(
-            child: Text(
-              actionButtonTitle,
-              style: TextStyle(
-                color: actionButtonColor ?? foregroundColor,
-              ),
-            ),
-            onPressed: () {
-              onActionButtonPressed?.call();
-              // we cannot use dialogContext.pop() here because this is no GoRouter in dialogContext. Use Navigator instead to close the dialog.
-              Navigator.of(dialogContext).pop();
-            },
-          ),
-          TextButton(
-            child: Text(
-              cancelButtonTitle ?? LocaleKeys.button_cancel.tr(),
-              style: TextStyle(
-                color: foregroundColor,
-              ),
-            ),
-            onPressed: () {
-              onCancelButtonPressed?.call();
-              Navigator.of(dialogContext).pop();
-            },
-          ),
-        ],
+        contentPadding: const EdgeInsets.symmetric(
+          horizontal: 24.0,
+          vertical: 4.0,
+        ),
+        actionsAlignment: MainAxisAlignment.center,
+        actions: actions,
       );
     },
   );
--- frontend/appflowy_flutter/lib/plugins/document/presentation/editor_plugins/page_style/_page_style_cover_image.dart
+++ frontend/appflowy_flutter/lib/plugins/document/presentation/editor_plugins/page_style/_page_style_cover_image.dart
@@ -1,21 +1,28 @@
+import 'dart:async';
+
 import 'package:appflowy/generated/flowy_svgs.g.dart';
 import 'package:appflowy/generated/locale_keys.g.dart';
 import 'package:appflowy/mobile/application/page_style/document_page_style_bloc.dart';
 import 'package:appflowy/mobile/presentation/bottom_sheet/bottom_sheet.dart';
+import 'package:appflowy/mobile/presentation/widgets/show_flowy_mobile_confirm_dialog.dart';
 import 'package:appflowy/plugins/document/presentation/editor_plugins/image/image_util.dart';
 import 'package:appflowy/plugins/document/presentation/editor_plugins/image/unsplash_image_widget.dart';
 import 'package:appflowy/plugins/document/presentation/editor_plugins/page_style/_page_cover_bottom_sheet.dart';
 import 'package:appflowy/plugins/document/presentation/editor_plugins/page_style/_page_style_util.dart';
 import 'package:appflowy/shared/feedback_gesture_detector.dart';
+import 'package:appflowy/startup/tasks/device_info_task.dart';
 import 'package:appflowy/user/application/user_service.dart';
+import 'package:appflowy_backend/log.dart';
 import 'package:appflowy_backend/protobuf/flowy-user/protobuf.dart';
 import 'package:appflowy_result/appflowy_result.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flowy_infra_ui/flowy_infra_ui.dart';
 import 'package:flowy_infra_ui/style_widget/snap_bar.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class PageStyleCoverImage extends StatelessWidget {
   PageStyleCoverImage({
@@ -114,9 +121,22 @@ class PageStyleCoverImage extends StatelessWidget {
   }
 
   Future<void> _pickImage(BuildContext context) async {
-    final result = await _imagePicker.pickImage(
-      source: ImageSource.gallery,
-    );
+    final photoPermission = await _checkPhotoPermission(context);
+    if (!photoPermission) {
+      Log.error('Has no permission to access the photo library');
+      return;
+    }
+
+    XFile? result;
+    try {
+      result = await _imagePicker.pickImage(
+        source: ImageSource.gallery,
+      );
+    } catch (e) {
+      Log.error('Error while picking image: $e');
+      return;
+    }
+
     final path = result?.path;
     if (path != null && context.mounted) {
       final String? result;
@@ -203,6 +223,54 @@ class PageStyleCoverImage extends StatelessWidget {
         );
       },
     );
+  }
+
+  Future<bool> _checkPhotoPermission(BuildContext context) async {
+    // check the permission first
+    final status = await Permission.photos.status;
+    // if the permission is permanently denied, we should open the app settings
+    if (status.isPermanentlyDenied && context.mounted) {
+      unawaited(
+        showFlowyMobileConfirmDialog(
+          context,
+          title: FlowyText.semibold(
+            LocaleKeys.pageStyle_photoPermissionTitle.tr(),
+            maxLines: 3,
+            textAlign: TextAlign.center,
+          ),
+          content: FlowyText(
+            LocaleKeys.pageStyle_photoPermissionDescription.tr(),
+            maxLines: 5,
+            textAlign: TextAlign.center,
+            fontSize: 12.0,
+          ),
+          actionAlignment: ConfirmDialogActionAlignment.vertical,
+          actionButtonTitle: LocaleKeys.pageStyle_openSettings.tr(),
+          actionButtonColor: Colors.blue,
+          cancelButtonTitle: LocaleKeys.pageStyle_doNotAllow.tr(),
+          cancelButtonColor: Colors.blue,
+          onActionButtonPressed: () {
+            openAppSettings();
+          },
+        ),
+      );
+
+      return false;
+    } else if (status.isDenied) {
+      // https://github.com/Baseflow/flutter-permission-handler/issues/1262#issuecomment-2006340937
+      Permission permission = Permission.photos;
+      if (defaultTargetPlatform == TargetPlatform.android &&
+          ApplicationInfo.androidSDKVersion <= 32) {
+        permission = Permission.storage;
+      }
+      // if the permission is denied, we should request the permission
+      final newStatus = await permission.request();
+      if (newStatus.isDenied) {
+        return false;
+      }
+    }
+
+    return true;
   }
 }
 
