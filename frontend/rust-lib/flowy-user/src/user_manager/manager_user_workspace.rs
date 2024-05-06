5e4f34ff2dcefc400bac50431407b5a8c0f714e3
nathan <nathan@appflowy.io>
2024-05-06 16:25:17 +0800

    chore: drop old user awarenss when open workspace

--- frontend/rust-lib/flowy-user/src/user_manager/manager_user_workspace.rs
+++ frontend/rust-lib/flowy-user/src/user_manager/manager_user_workspace.rs
@@ -166,6 +166,13 @@ impl UserManager {
       .authenticate_user
       .set_user_workspace(user_workspace.clone())?;
 
+    if let Err(err) = self.try_initial_user_awareness(&self.get_session()?).await {
+      error!(
+        "Failed to initialize user awareness when opening workspace: {:?}",
+        err
+      );
+    }
+
     let uid = self.user_id()?;
     if let Err(err) = self
       .user_status_callback
