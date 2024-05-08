ed1a2ea1d082d9f8df270ae18db31edab299a242
Richard Shiue <71320345+richardshiue@users.noreply.github.com>
2024-05-09 00:24:44 +0800

    fix: always build

--- frontend/appflowy_flutter/lib/mobile/presentation/database/board/mobile_board_page.dart
+++ frontend/appflowy_flutter/lib/mobile/presentation/database/board/mobile_board_page.dart
@@ -47,7 +47,6 @@ class _MobileBoardPageState extends State<MobileBoardPage> {
         databaseController: widget.databaseController,
       )..add(const BoardEvent.initial()),
       child: BlocBuilder<BoardBloc, BoardState>(
-        buildWhen: (p, c) => !p.isReady && c.isReady,
         builder: (context, state) => state.maybeMap(
           loading: (_) => const Center(
             child: CircularProgressIndicator.adaptive(),
