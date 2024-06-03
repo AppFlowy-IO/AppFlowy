ab99a518ff76ec598291312cf05d4e951f799cae
Lucas.Xu <lucas.xu@appflowy.io>
2024-06-03 15:43:47 +0800

    fix: header icon size

--- frontend/appflowy_flutter/lib/workspace/presentation/home/menu/sidebar/folder/_folder_header.dart
+++ frontend/appflowy_flutter/lib/workspace/presentation/home/menu/sidebar/folder/_folder_header.dart
@@ -1,4 +1,5 @@
 import 'package:appflowy/generated/flowy_svgs.g.dart';
+import 'package:appflowy/workspace/presentation/home/home_sizes.dart';
 import 'package:flowy_infra_ui/flowy_infra_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -36,36 +37,41 @@ class _FolderHeaderState extends State<FolderHeader> {
 
   @override
   Widget build(BuildContext context) {
-    return MouseRegion(
-      onEnter: (_) => isHovered.value = true,
-      onExit: (_) => isHovered.value = false,
-      child: FlowyButton(
-        onTap: widget.onPressed,
-        margin: const EdgeInsets.symmetric(horizontal: 6.0),
-        rightIcon: ValueListenableBuilder(
-          valueListenable: isHovered,
-          builder: (context, onHover, child) =>
-              Opacity(opacity: onHover ? 1 : 0, child: child),
-          child: FlowyIconButton(
-            tooltipText: widget.addButtonTooltip,
-            icon: const FlowySvg(FlowySvgs.view_item_add_s),
-            onPressed: widget.onAdded,
+    return SizedBox(
+      height: HomeSizes.workspaceSectionHeight,
+      child: MouseRegion(
+        onEnter: (_) => isHovered.value = true,
+        onExit: (_) => isHovered.value = false,
+        child: FlowyButton(
+          onTap: widget.onPressed,
+          margin: const EdgeInsets.only(left: 6.0, right: 4.0),
+          rightIcon: ValueListenableBuilder(
+            valueListenable: isHovered,
+            builder: (context, onHover, child) =>
+                Opacity(opacity: onHover ? 1 : 0, child: child),
+            child: FlowyIconButton(
+              width: 24,
+              iconPadding: const EdgeInsets.all(4.0),
+              tooltipText: widget.addButtonTooltip,
+              icon: const FlowySvg(FlowySvgs.view_item_add_s),
+              onPressed: widget.onAdded,
+            ),
           ),
-        ),
-        iconPadding: 10.0,
-        text: Row(
-          children: [
-            FlowyText(
-              widget.title,
-              lineHeight: 1.15,
-            ),
-            const HSpace(4.0),
-            FlowySvg(
-              widget.isExpanded
-                  ? FlowySvgs.workspace_drop_down_menu_show_s
-                  : FlowySvgs.workspace_drop_down_menu_hide_s,
-            ),
-          ],
+          iconPadding: 10.0,
+          text: Row(
+            children: [
+              FlowyText(
+                widget.title,
+                lineHeight: 1.15,
+              ),
+              const HSpace(4.0),
+              FlowySvg(
+                widget.isExpanded
+                    ? FlowySvgs.workspace_drop_down_menu_show_s
+                    : FlowySvgs.workspace_drop_down_menu_hide_s,
+              ),
+            ],
+          ),
         ),
       ),
     );
