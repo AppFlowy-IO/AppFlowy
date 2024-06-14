8e83470729c49394f0a95f6edd118f07ebdd41e5
nathan <nathan@appflowy.io>
2024-06-14 11:33:54 +0800

    chore: fix clippy

--- frontend/rust-lib/flowy-sqlite/src/schema.rs
+++ frontend/rust-lib/flowy-sqlite/src/schema.rs
@@ -80,11 +80,11 @@ diesel::table! {
 }
 
 diesel::allow_tables_to_appear_in_same_query!(
-    chat_message_table,
-    chat_table,
-    collab_snapshot,
-    user_data_migration_records,
-    user_table,
-    user_workspace_table,
-    workspace_members_table,
+  chat_message_table,
+  chat_table,
+  collab_snapshot,
+  user_data_migration_records,
+  user_table,
+  user_workspace_table,
+  workspace_members_table,
 );
