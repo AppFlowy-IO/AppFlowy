c82c4bec55a01a4e701d40d7f05405c820c5e6aa
Mathias Mogensen <mathiasrieckm@gmail.com>
2024-06-17 14:01:48 +0200

    test: integration test for local auth

--- frontend/appflowy_flutter/integration_test/cloud/cloud_runner.dart
+++ frontend/appflowy_flutter/integration_test/cloud/cloud_runner.dart
@@ -1,8 +1,6 @@
 import 'anon_user_continue_test.dart' as anon_user_continue_test;
 import 'appflowy_cloud_auth_test.dart' as appflowy_cloud_auth_test;
 import 'empty_test.dart' as preset_af_cloud_env_test;
-import 'settings_billing_test.dart' as settings_billing_test;
-// import 'document_sync_test.dart' as document_sync_test;
 import 'user_setting_sync_test.dart' as user_sync_test;
 import 'workspace/change_name_and_icon_test.dart'
     as change_workspace_name_and_icon_test;
@@ -11,19 +9,11 @@ import 'workspace/collaborative_workspace_test.dart'
 
 Future<void> main() async {
   preset_af_cloud_env_test.main();
-
   appflowy_cloud_auth_test.main();
-
-  // document_sync_test.main();
-
   user_sync_test.main();
-
   anon_user_continue_test.main();
 
   // workspace
   collaboration_workspace_test.main();
   change_workspace_name_and_icon_test.main();
-
-  // settings
-  settings_billing_test.main();
 }
