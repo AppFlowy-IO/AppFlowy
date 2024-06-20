c3caa319e24bf117150ec9ed02cda2d36721b9fe
Lucas.Xu <lucas.xu@appflowy.io>
2024-06-20 21:19:39 +0800

    fix: duplicated spaces could be created when migrating (#5586)
    * fix: duplicated spaces could be created when migrating
    
    * chore: bump version 0.6.1
    
    * fix: use fixed uuid to generate space id
    
    * fix: rust ci
    
    * chore: remove libmpv dependency

--- frontend/appflowy_flutter/lib/mobile/presentation/mobile_bottom_navigation_bar.dart
+++ frontend/appflowy_flutter/lib/mobile/presentation/mobile_bottom_navigation_bar.dart
@@ -1,17 +1,14 @@
 import 'dart:ui';
 
 import 'package:appflowy/generated/flowy_svgs.g.dart';
-import 'package:appflowy/generated/locale_keys.g.dart';
-import 'package:appflowy/mobile/application/mobile_router.dart';
-import 'package:appflowy/mobile/presentation/bottom_sheet/bottom_sheet.dart';
 import 'package:appflowy/util/theme_extension.dart';
-import 'package:appflowy/workspace/application/workspace/workspace_service.dart';
-import 'package:appflowy_backend/dispatch/dispatch.dart';
-import 'package:appflowy_backend/log.dart';
 import 'package:appflowy_backend/protobuf/flowy-folder/view.pb.dart';
-import 'package:easy_localization/easy_localization.dart';
+import 'package:appflowy_editor/appflowy_editor.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+
+final PropertyValueNotifier<ViewLayoutPB?> createNewPageNotifier =
+    PropertyValueNotifier(null);
 
 const _homeLabel = 'home';
 const _addLabel = 'add';
@@ -93,7 +90,7 @@ class MobileBottomNavigationBar extends StatelessWidget {
   void _onTap(BuildContext context, int bottomBarIndex) {
     if (_items[bottomBarIndex].label == _addLabel) {
       // show an add dialog
-      _showCreatePageBottomSheet(context);
+      createNewPageNotifier.value = ViewLayoutPB.Document;
       return;
     }
     // When navigating to a new branch, it's recommended to use the goBranch
@@ -106,42 +103,6 @@ class MobileBottomNavigationBar extends StatelessWidget {
       // already active. This example demonstrates how to support this behavior,
       // using the initialLocation parameter of goBranch.
       initialLocation: bottomBarIndex == navigationShell.currentIndex,
-    );
-  }
-
-  void _showCreatePageBottomSheet(BuildContext context) {
-    showMobileBottomSheet(
-      context,
-      showHeader: true,
-      title: LocaleKeys.sideBar_addAPage.tr(),
-      showDragHandle: true,
-      showCloseButton: true,
-      useRootNavigator: true,
-      builder: (sheetContext) {
-        return AddNewPageWidgetBottomSheet(
-          view: ViewPB(),
-          onAction: (layout) async {
-            Navigator.of(sheetContext).pop();
-            final currentWorkspaceId =
-                await FolderEventReadCurrentWorkspace().send();
-            await currentWorkspaceId.fold((s) async {
-              final workspaceService = WorkspaceService(workspaceId: s.id);
-              final result = await workspaceService.createView(
-                name: LocaleKeys.menuAppHeader_defaultNewPageName.tr(),
-                viewSection: ViewSectionPB.Private,
-                layout: layout,
-              );
-              result.fold((s) {
-                context.pushView(s);
-              }, (e) {
-                Log.error('Failed to create new page: $e');
-              });
-            }, (e) {
-              Log.error('Failed to read current workspace: $e');
-            });
-          },
-        );
-      },
     );
   }
 }
