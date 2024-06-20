1de4920706a5ae669c24ccd9c02be08e395b2f0e
Lucas.Xu <lucas.xu@appflowy.io>
2024-06-20 20:54:19 +0800

    chore: enable reopen the app after closing it

--- frontend/appflowy_flutter/macos/Runner/AppDelegate.swift
+++ frontend/appflowy_flutter/macos/Runner/AppDelegate.swift
@@ -6,4 +6,14 @@ class AppDelegate: FlutterAppDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
     return false
   }
+
+  override func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+    if !flag {
+        for window in sender.windows {
+            window.makeKeyAndOrderFront(self)
+        }
+    }
+
+    return true
+  }
 }
