51938341ace487ad8873c71963373c5947e54e72
Mathias Mogensen <42929161+Xazin@users.noreply.github.com>
2024-07-01 00:07:25 +0200

    chore: disable cloud search

--- frontend/rust-lib/flowy-core/src/deps_resolve/search_deps.rs
+++ frontend/rust-lib/flowy-core/src/deps_resolve/search_deps.rs
@@ -1,5 +1,4 @@
 use flowy_folder::manager::FolderManager;
-use flowy_search::document::handler::DocumentSearchHandler;
 use flowy_search::folder::handler::FolderSearchHandler;
 use flowy_search::folder::indexer::FolderIndexManagerImpl;
 use flowy_search::services::manager::SearchManager;
@@ -10,11 +9,11 @@ pub struct SearchDepsResolver();
 impl SearchDepsResolver {
   pub async fn resolve(
     folder_indexer: Arc<FolderIndexManagerImpl>,
-    cloud_service: Arc<dyn SearchCloudService>,
-    folder_manager: Arc<FolderManager>,
+    _cloud_service: Arc<dyn SearchCloudService>,
+    _folder_manager: Arc<FolderManager>,
   ) -> Arc<SearchManager> {
     let folder_handler = Arc::new(FolderSearchHandler::new(folder_indexer));
-    let document_handler = Arc::new(DocumentSearchHandler::new(cloud_service, folder_manager));
-    Arc::new(SearchManager::new(vec![folder_handler, document_handler]))
+    // let document_handler = Arc::new(DocumentSearchHandler::new(cloud_service, folder_manager));
+    Arc::new(SearchManager::new(vec![folder_handler]))
   }
 }
