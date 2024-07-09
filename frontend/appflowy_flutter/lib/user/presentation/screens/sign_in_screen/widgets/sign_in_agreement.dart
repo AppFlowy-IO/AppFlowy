bb275ede1cf919848aba853a30007e2256c863bf
qinluhe <qinluhe.twodog@gmail.com>
2024-07-09 16:20:16 +0800

    fix: modified sign in on flutter

---
+++ frontend/appflowy_flutter/lib/user/presentation/screens/sign_in_screen/widgets/sign_in_agreement.dart
@@ -0,0 +1,37 @@
+import 'package:appflowy/generated/locale_keys.g.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flowy_infra_ui/style_widget/text.dart';
+import 'package:flutter/material.dart';
+
+class SignInAgreement extends StatelessWidget {
+  const SignInAgreement({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      textAlign: TextAlign.center,
+      text: TextSpan(
+        children: [
+          TextSpan(
+            text: '${LocaleKeys.web_signInAgreement.tr()} ',
+            style: const TextStyle(color: Colors.grey, fontSize: 12),
+          ),
+          TextSpan(
+            text: '${LocaleKeys.web_termOfUse.tr()} ',
+            style: const TextStyle(color: Colors.blue, fontSize: 12),
+          ),
+          TextSpan(
+            text: '${LocaleKeys.web_and.tr()} ',
+            style: const TextStyle(color: Colors.grey, fontSize: 12),
+          ),
+          TextSpan(
+            text: LocaleKeys.web_privacyPolicy.tr(),
+            style: const TextStyle(color: Colors.blue, fontSize: 12),
+          ),
+        ],
+      ),
+    );
+  }
+}
