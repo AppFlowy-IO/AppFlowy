c043c472f28623f61fe95b2066a491a1668fe2c7
qinluhe <qinluhe.twodog@gmail.com>
2024-07-10 13:19:13 +0800

    fix: add link to login

--- frontend/appflowy_flutter/lib/user/presentation/screens/sign_in_screen/widgets/sign_in_agreement.dart
+++ frontend/appflowy_flutter/lib/user/presentation/screens/sign_in_screen/widgets/sign_in_agreement.dart
@@ -1,6 +1,9 @@
 import 'package:appflowy/generated/locale_keys.g.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+
+import 'package:appflowy/core/helpers/url_launcher.dart';
 
 class SignInAgreement extends StatelessWidget {
   const SignInAgreement({
@@ -20,15 +23,21 @@ class SignInAgreement extends StatelessWidget {
           TextSpan(
             text: '${LocaleKeys.web_termOfUse.tr()} ',
             style: const TextStyle(color: Colors.blue, fontSize: 12),
+            mouseCursor: SystemMouseCursors.click,
+            recognizer: TapGestureRecognizer()
+              ..onTap = () => afLaunchUrlString('https://appflowy.io/terms'),
           ),
           TextSpan(
             text: '${LocaleKeys.web_and.tr()} ',
             style: const TextStyle(color: Colors.grey, fontSize: 12),
           ),
           TextSpan(
-            text: LocaleKeys.web_privacyPolicy.tr(),
-            style: const TextStyle(color: Colors.blue, fontSize: 12),
-          ),
+              text: LocaleKeys.web_privacyPolicy.tr(),
+              style: const TextStyle(color: Colors.blue, fontSize: 12),
+              mouseCursor: SystemMouseCursors.click,
+              recognizer: TapGestureRecognizer()
+                ..onTap =
+                    () => afLaunchUrlString('https://appflowy.io/privacy')),
         ],
       ),
     );
