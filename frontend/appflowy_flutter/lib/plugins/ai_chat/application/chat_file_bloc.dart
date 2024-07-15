c6ad57f11d04ba58b147c7e106e147c732062827
Nathan.fooo <86001920+appflowy@users.noreply.github.com>
2024-07-15 22:45:53 +0800

    chore: show plugin state (#5740)
    * chore: show plugin state
    
    * chore: flutter analyzer
    
    * chore: update

--- frontend/appflowy_flutter/lib/plugins/ai_chat/application/chat_file_bloc.dart
+++ frontend/appflowy_flutter/lib/plugins/ai_chat/application/chat_file_bloc.dart
@@ -1,3 +1,4 @@
+import 'package:appflowy/workspace/application/settings/ai/local_llm_listener.dart';
 import 'package:appflowy_backend/dispatch/dispatch.dart';
 import 'package:appflowy_backend/protobuf/flowy-chat/entities.pb.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -9,18 +10,53 @@ class ChatFileBloc extends Bloc<ChatFileEvent, ChatFileState> {
   ChatFileBloc({
     required String chatId,
     dynamic message,
-  }) : super(ChatFileState.initial(message)) {
+  })  : listener = LocalLLMListener(),
+        super(ChatFileState.initial(message)) {
+    listener.start(
+      stateCallback: (pluginState) {
+        if (!isClosed) {
+          add(ChatFileEvent.updateLocalAIState(pluginState));
+        }
+      },
+    );
+
     on<ChatFileEvent>(
       (event, emit) async {
         await event.when(
-          initial: () async {},
+          initial: () async {
+            final result = await ChatEventGetPluginState().send();
+            result.fold(
+              (pluginState) {
+                if (!isClosed) {
+                  add(ChatFileEvent.updateLocalAIState(pluginState));
+                }
+              },
+              (err) {},
+            );
+          },
           newFile: (String filePath) {
             final payload = ChatFilePB(filePath: filePath, chatId: chatId);
             ChatEventChatWithFile(payload).send();
           },
+          updateLocalAIState: (PluginStatePB pluginState) {
+            emit(
+              state.copyWith(
+                supportChatWithFile:
+                    pluginState.state == RunningStatePB.Running,
+              ),
+            );
+          },
         );
       },
     );
+  }
+
+  final LocalLLMListener listener;
+
+  @override
+  Future<void> close() async {
+    await listener.stop();
+    return super.close();
   }
 }
 
@@ -28,12 +64,15 @@ class ChatFileBloc extends Bloc<ChatFileEvent, ChatFileState> {
 class ChatFileEvent with _$ChatFileEvent {
   const factory ChatFileEvent.initial() = Initial;
   const factory ChatFileEvent.newFile(String filePath) = _NewFile;
+  const factory ChatFileEvent.updateLocalAIState(PluginStatePB pluginState) =
+      _UpdateLocalAIState;
 }
 
 @freezed
 class ChatFileState with _$ChatFileState {
   const factory ChatFileState({
     required String text,
+    @Default(false) bool supportChatWithFile,
   }) = _ChatFileState;
 
   factory ChatFileState.initial(dynamic text) {
--- frontend/appflowy_flutter/lib/workspace/application/settings/ai/plugin_state_bloc.dart
+++ frontend/appflowy_flutter/lib/workspace/application/settings/ai/plugin_state_bloc.dart
@@ -1,24 +1,67 @@
 import 'dart:async';
 
+import 'package:appflowy/workspace/application/settings/ai/local_llm_listener.dart';
+import 'package:appflowy_backend/dispatch/dispatch.dart';
+import 'package:appflowy_backend/log.dart';
 import 'package:appflowy_backend/protobuf/flowy-chat/entities.pb.dart';
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 part 'plugin_state_bloc.freezed.dart';
 
-class PluginStateBloc extends Bloc<PluginStateEvent, PluginState> {
+class PluginStateBloc extends Bloc<PluginStateEvent, PluginStateState> {
   PluginStateBloc()
-      : super(
-          const PluginState(runningState: RunningStatePB.Connecting),
-        ) {
+      : listener = LocalLLMListener(),
+        super(const PluginStateState(action: PluginStateAction.init())) {
+    listener.start(
+      stateCallback: (pluginState) {
+        if (!isClosed) {
+          add(PluginStateEvent.updateState(pluginState));
+        }
+      },
+    );
+
     on<PluginStateEvent>(_handleEvent);
+  }
+
+  final LocalLLMListener listener;
+
+  @override
+  Future<void> close() async {
+    await listener.stop();
+    return super.close();
   }
 
   Future<void> _handleEvent(
     PluginStateEvent event,
-    Emitter<PluginState> emit,
+    Emitter<PluginStateState> emit,
   ) async {
     await event.when(
-      started: () async {},
+      started: () async {
+        final result = await ChatEventGetPluginState().send();
+        result.fold(
+          (pluginState) {
+            if (!isClosed) {
+              add(PluginStateEvent.updateState(pluginState));
+            }
+          },
+          (err) => Log.error(err.toString()),
+        );
+      },
+      updateState: (PluginStatePB pluginState) {
+        switch (pluginState.state) {
+          case RunningStatePB.Running:
+            emit(const PluginStateState(action: PluginStateAction.ready()));
+            break;
+          default:
+            emit(
+              state.copyWith(action: const PluginStateAction.reloadRequired()),
+            );
+            break;
+        }
+      },
+      restartLocalAI: () {
+        ChatEventRestartLocalAI().send();
+      },
     );
   }
 }
@@ -26,11 +69,20 @@ class PluginStateBloc extends Bloc<PluginStateEvent, PluginState> {
 @freezed
 class PluginStateEvent with _$PluginStateEvent {
   const factory PluginStateEvent.started() = _Started;
+  const factory PluginStateEvent.updateState(PluginStatePB pluginState) =
+      _UpdatePluginState;
+  const factory PluginStateEvent.restartLocalAI() = _RestartLocalAI;
 }
 
 @freezed
-class PluginState with _$PluginState {
-  const factory PluginState({
-    required RunningStatePB runningState,
-  }) = _PluginState;
+class PluginStateState with _$PluginStateState {
+  const factory PluginStateState({required PluginStateAction action}) =
+      _PluginStateState;
+}
+
+@freezed
+class PluginStateAction with _$PluginStateAction {
+  const factory PluginStateAction.init() = _Init;
+  const factory PluginStateAction.ready() = _Ready;
+  const factory PluginStateAction.reloadRequired() = _ReloadRequired;
 }
