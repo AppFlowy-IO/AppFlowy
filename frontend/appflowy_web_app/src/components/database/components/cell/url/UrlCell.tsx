78e51965ac623905fa3676cf06ab4c450b76bc77
Kilu <lu@appflowy.io>
2024-07-16 18:46:54 +0800

    fix: add button to url cell

--- frontend/appflowy_web_app/src/components/database/components/cell/url/UrlCell.tsx
+++ frontend/appflowy_web_app/src/components/database/components/cell/url/UrlCell.tsx
@@ -1,13 +1,20 @@
 import { useReadOnly } from '@/application/database-yjs';
 import { CellProps, UrlCell as UrlCellType } from '@/application/database-yjs/cell.type';
+import { notify } from '@/components/_shared/notify';
+import { copyTextToClipboard } from '@/utils/copy';
 import { openUrl, processUrl } from '@/utils/url';
+import { IconButton, Tooltip } from '@mui/material';
 import React, { useMemo } from 'react';
+import { ReactComponent as LinkSvg } from '@/assets/link.svg';
+import { ReactComponent as CopySvg } from '@/assets/copy.svg';
+import { useTranslation } from 'react-i18next';
 
 export function UrlCell({ cell, style, placeholder }: CellProps<UrlCellType>) {
   const readOnly = useReadOnly();
 
   const isUrl = useMemo(() => (cell ? processUrl(cell.data) : false), [cell]);
 
+  const [showActions, setShowActions] = React.useState(false);
   const className = useMemo(() => {
     const classList = ['select-text', 'w-fit', 'flex', 'w-full', 'items-center'];
 
@@ -20,6 +27,8 @@ export function UrlCell({ cell, style, placeholder }: CellProps<UrlCellType>) {
     return classList.join(' ');
   }, [isUrl]);
 
+  const { t } = useTranslation();
+
   if (!cell?.data)
     return placeholder ? (
       <div style={style} className={'text-text-placeholder'}>
@@ -30,6 +39,8 @@ export function UrlCell({ cell, style, placeholder }: CellProps<UrlCellType>) {
   return (
     <div
       style={style}
+      onMouseEnter={() => setShowActions(true)}
+      onMouseLeave={() => setShowActions(false)}
       onClick={(e) => {
         if (!isUrl || !cell) return;
         if (readOnly) {
@@ -40,6 +51,37 @@ export function UrlCell({ cell, style, placeholder }: CellProps<UrlCellType>) {
       className={className}
     >
       {cell?.data}
+      {showActions && isUrl && (
+        <div className={'absolute right-0 flex items-center gap-1 px-2'}>
+          <Tooltip title={t('editor.openLink')} placement={'top'}>
+            <IconButton
+              sx={{
+                border: '1px solid var(--line-divider)',
+              }}
+              onClick={(e) => {
+                e.stopPropagation();
+                void openUrl(cell.data, '_blank');
+              }}
+            >
+              <LinkSvg />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title={t('button.copyLink')} placement={'top'}>
+            <IconButton
+              sx={{
+                border: '1px solid var(--line-divider)',
+              }}
+              onClick={async (e) => {
+                e.stopPropagation();
+                await copyTextToClipboard(cell.data);
+                notify.success(t('grid.url.copy'));
+              }}
+            >
+              <CopySvg />
+            </IconButton>
+          </Tooltip>
+        </div>
+      )}
     </div>
   );
 }
