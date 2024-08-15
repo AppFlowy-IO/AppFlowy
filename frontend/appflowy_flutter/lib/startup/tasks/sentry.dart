88cc0caab70428822c8b8282bfb944a764ef4edc
Lucas.Xu <lucas.xu@appflowy.io>
2024-08-15 10:00:27 +0800

    feat: integrate Sentry Flutter and enable it if SENTRY_DSN is not empty (#5959)
    * chore: add dart dependency validator
    
    * feat: integrate sentry flutter
    
    * chore: remove user info collection
    
    * fix: flutter analyze
    
    * fix: ios compile
    
    * chore: add log

---
+++ frontend/appflowy_flutter/lib/startup/tasks/sentry.dart
@@ -0,0 +1,31 @@
+import 'package:appflowy/env/env.dart';
+import 'package:appflowy_backend/log.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
+
+import '../startup.dart';
+
+class InitSentryTask extends LaunchTask {
+  const InitSentryTask();
+
+  @override
+  Future<void> initialize(LaunchContext context) async {
+    const dsn = Env.sentryDsn;
+    if (dsn.isEmpty) {
+      Log.info('Sentry DSN is not set, skipping initialization');
+      return;
+    }
+
+    Log.info('Initializing Sentry');
+
+    await SentryFlutter.init(
+      (options) {
+        options.dsn = dsn;
+        options.tracesSampleRate = 1.0;
+        options.profilesSampleRate = 1.0;
+      },
+    );
+  }
+
+  @override
+  Future<void> dispose() async {}
+}
