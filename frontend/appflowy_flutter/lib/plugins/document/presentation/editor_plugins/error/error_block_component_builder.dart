8e34951477c27ed6e043bc82a5f3ecb871934ed8
Lucas.Xu <lucas.xu@appflowy.io>
2024-09-04 17:11:50 +0800

    feat: auto-fix table block data when duplicating it (#6185)
    * fix: remove camera permission and update photo usage description
    
    * feat: optimize the error block style in document
    
    * feat: auto-fix table block when duplicating it

--- frontend/appflowy_flutter/lib/plugins/document/presentation/editor_plugins/error/error_block_component_builder.dart
+++ frontend/appflowy_flutter/lib/plugins/document/presentation/editor_plugins/error/error_block_component_builder.dart
@@ -1,10 +1,11 @@
 import 'dart:convert';
 
 import 'package:appflowy/generated/locale_keys.g.dart';
+import 'package:appflowy/mobile/presentation/base/animated_gesture.dart';
 import 'package:appflowy/plugins/document/presentation/editor_plugins/actions/mobile_block_action_buttons.dart';
 import 'package:appflowy/plugins/document/presentation/editor_plugins/copy_and_paste/clipboard_service.dart';
 import 'package:appflowy/startup/startup.dart';
-import 'package:appflowy/workspace/presentation/home/toast.dart';
+import 'package:appflowy/workspace/presentation/widgets/dialogs.dart';
 import 'package:appflowy_editor/appflowy_editor.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flowy_infra_ui/flowy_infra_ui.dart';
@@ -59,25 +60,15 @@ class _ErrorBlockComponentWidgetState extends State<ErrorBlockComponentWidget>
 
   @override
   Widget build(BuildContext context) {
-    Widget child = DecoratedBox(
+    Widget child = Container(
+      width: double.infinity,
       decoration: BoxDecoration(
         color: Theme.of(context).colorScheme.surfaceContainerHighest,
         borderRadius: BorderRadius.circular(4),
       ),
-      child: FlowyButton(
-        onTap: () async {
-          showSnackBarMessage(
-            context,
-            LocaleKeys.document_errorBlock_blockContentHasBeenCopied.tr(),
-          );
-          await getIt<ClipboardService>().setData(
-            ClipboardServiceData(plainText: jsonEncode(node.toJson())),
-          );
-        },
-        text: PlatformExtension.isDesktopOrWeb
-            ? _buildDesktopErrorBlock(context)
-            : _buildMobileErrorBlock(context),
-      ),
+      child: PlatformExtension.isDesktopOrWeb
+          ? _buildDesktopErrorBlock(context)
+          : _buildMobileErrorBlock(context),
     );
 
     child = Padding(
@@ -107,40 +98,61 @@ class _ErrorBlockComponentWidgetState extends State<ErrorBlockComponentWidget>
   Widget _buildDesktopErrorBlock(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 12),
-      child: Wrap(
-        crossAxisAlignment: WrapCrossAlignment.center,
+      child: Row(
         children: [
-          const HSpace(4),
+          const HSpace(12),
           FlowyText.regular(
-            LocaleKeys.document_errorBlock_theBlockIsNotSupported.tr(),
+            LocaleKeys.document_errorBlock_parseError.tr(args: [node.type]),
           ),
-          const HSpace(4),
-          FlowyText.regular(
-            '(${LocaleKeys.document_errorBlock_clickToCopyTheBlockContent.tr()})',
-            color: Theme.of(context).hintColor,
+          const Spacer(),
+          OutlinedRoundedButton(
+            text: LocaleKeys.document_errorBlock_copyBlockContent.tr(),
+            onTap: _copyBlockContent,
           ),
+          const HSpace(12),
         ],
       ),
     );
   }
 
   Widget _buildMobileErrorBlock(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 4),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          FlowyText.regular(
-            LocaleKeys.document_errorBlock_theBlockIsNotSupported.tr(),
-          ),
-          const VSpace(6),
-          FlowyText.regular(
-            '(${LocaleKeys.document_errorBlock_clickToCopyTheBlockContent.tr()})',
-            color: Theme.of(context).hintColor,
-            fontSize: 12.0,
-          ),
-        ],
+    return AnimatedGestureDetector(
+      onTapUp: _copyBlockContent,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 4),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 4.0, right: 24.0),
+              child: FlowyText.regular(
+                LocaleKeys.document_errorBlock_parseError.tr(args: [node.type]),
+                maxLines: 3,
+              ),
+            ),
+            const VSpace(6),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4),
+              child: FlowyText.regular(
+                '(${LocaleKeys.document_errorBlock_clickToCopyTheBlockContent.tr()})',
+                color: Theme.of(context).hintColor,
+                fontSize: 12.0,
+              ),
+            ),
+          ],
+        ),
       ),
+    );
+  }
+
+  void _copyBlockContent() {
+    showToastNotification(
+      context,
+      message: LocaleKeys.document_errorBlock_blockContentHasBeenCopied.tr(),
+    );
+
+    getIt<ClipboardService>().setData(
+      ClipboardServiceData(plainText: jsonEncode(node.toJson())),
     );
   }
 }
