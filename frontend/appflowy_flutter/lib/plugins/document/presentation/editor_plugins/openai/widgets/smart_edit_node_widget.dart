00203b11b5d0f22da623b2dcb3c4d24d33aeb438
Lucas.Xu <lucas.xu@appflowy.io>
2024-09-11 14:18:31 +0800

    feat: integrate smart_edit_bloc

--- frontend/appflowy_flutter/lib/plugins/document/presentation/editor_plugins/openai/widgets/smart_edit_node_widget.dart
+++ frontend/appflowy_flutter/lib/plugins/document/presentation/editor_plugins/openai/widgets/smart_edit_node_widget.dart
@@ -2,21 +2,15 @@ import 'dart:async';
 
 import 'package:appflowy/generated/locale_keys.g.dart';
 import 'package:appflowy/plugins/document/presentation/editor_plugins/openai/service/ai_client.dart';
-import 'package:appflowy/plugins/document/presentation/editor_plugins/openai/service/error.dart';
 import 'package:appflowy/plugins/document/presentation/editor_plugins/openai/widgets/smart_edit_action.dart';
-import 'package:appflowy/startup/startup.dart';
-import 'package:appflowy/user/application/ai_service.dart';
-import 'package:appflowy/workspace/presentation/home/toast.dart';
+import 'package:appflowy/plugins/document/presentation/editor_plugins/openai/widgets/smart_edit_bloc.dart';
 import 'package:appflowy/workspace/presentation/widgets/dialogs.dart';
 import 'package:appflowy_editor/appflowy_editor.dart';
 import 'package:appflowy_popover/appflowy_popover.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flowy_infra_ui/flowy_infra_ui.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:provider/provider.dart';
-
-import 'ai_limit_dialog.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SmartEditBlockKeys {
   const SmartEditBlockKeys._();
@@ -85,70 +79,74 @@ class SmartEditBlockComponentWidget extends BlockComponentStatefulWidget {
 class _SmartEditBlockComponentWidgetState
     extends State<SmartEditBlockComponentWidget> {
   final popoverController = PopoverController();
-  final key = GlobalKey(debugLabel: 'smart_edit_input');
 
   late final editorState = context.read<EditorState>();
+  late final action = SmartEditAction
+      .values[widget.node.attributes[SmartEditBlockKeys.action] as int];
 
   @override
   void initState() {
     super.initState();
 
-    // todo: don't use a popover to show the content of the smart edit.
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
       popoverController.show();
     });
   }
 
-  @override
-  void reassemble() {
-    super.reassemble();
+  // @override
+  // void reassemble() {
+  //   super.reassemble();
 
-    final transaction = editorState.transaction..deleteNode(widget.node);
-    editorState.apply(transaction);
-  }
+  //   final transaction = editorState.transaction..deleteNode(widget.node);
+  //   editorState.apply(transaction);
+  // }
 
   @override
   Widget build(BuildContext context) {
     final width = _getEditorWidth();
 
-    return AppFlowyPopover(
-      controller: popoverController,
-      direction: PopoverDirection.bottomWithLeftAligned,
-      triggerActions: PopoverTriggerFlags.none,
-      margin: EdgeInsets.zero,
-      offset: const Offset(40, 0), // align the editor block
-      constraints: BoxConstraints(maxWidth: width),
-      child: const SizedBox(
-        width: double.infinity,
+    return BlocProvider(
+      create: (context) => SmartEditBloc(
+        node: widget.node,
+        editorState: editorState,
+        aiRepository: context.read<AIRepository>(),
+        action: action,
+      )..add(const SmartEditEvent.started()),
+      child: AppFlowyPopover(
+        controller: popoverController,
+        direction: PopoverDirection.bottomWithLeftAligned,
+        triggerActions: PopoverTriggerFlags.none,
+        margin: EdgeInsets.zero,
+        offset: const Offset(40, 0), // align the editor block
+        constraints: BoxConstraints(maxWidth: width),
+        child: const SizedBox(
+          width: double.infinity,
+        ),
+        canClose: () async {
+          final completer = Completer<bool>();
+          final state = context.read<SmartEditBloc>().state;
+          if (state.result.isEmpty) {
+            completer.complete(true);
+          } else {
+            await showCancelAndConfirmDialog(
+              context: context,
+              title: LocaleKeys.document_plugins_discardResponse.tr(),
+              description: '',
+              confirmLabel: LocaleKeys.button_discard.tr(),
+              onConfirm: () => completer.complete(true),
+              onCancel: () => completer.complete(false),
+            );
+          }
+          return completer.future;
+        },
+        onClose: () {
+          final transaction = editorState.transaction..deleteNode(widget.node);
+          editorState.apply(transaction);
+        },
+        popupBuilder: (BuildContext popoverContext) {
+          return const SmartEditInputContent();
+        },
       ),
-      canClose: () async {
-        final completer = Completer<bool>();
-        final state = key.currentState as _SmartEditInputWidgetState;
-        if (state.result.isEmpty) {
-          completer.complete(true);
-        } else {
-          await showCancelAndConfirmDialog(
-            context: context,
-            title: LocaleKeys.document_plugins_discardResponse.tr(),
-            description: '',
-            confirmLabel: LocaleKeys.button_discard.tr(),
-            onConfirm: () => completer.complete(true),
-            onCancel: () => completer.complete(false),
-          );
-        }
-        return completer.future;
-      },
-      onClose: () {
-        final transaction = editorState.transaction..deleteNode(widget.node);
-        editorState.apply(transaction);
-      },
-      popupBuilder: (BuildContext popoverContext) {
-        return SmartEditInputWidget(
-          key: key,
-          node: widget.node,
-          editorState: editorState,
-        );
-      },
     );
   }
 
@@ -163,277 +161,92 @@ class _SmartEditBlockComponentWidgetState
   }
 }
 
-class SmartEditInputWidget extends StatefulWidget {
-  const SmartEditInputWidget({
-    required super.key,
-    required this.node,
-    required this.editorState,
-  });
-
-  final Node node;
-  final EditorState editorState;
-
-  @override
-  State<SmartEditInputWidget> createState() => _SmartEditInputWidgetState();
-}
-
-class _SmartEditInputWidgetState extends State<SmartEditInputWidget> {
-  final focusNode = FocusNode();
-  final client = http.Client();
-
-  SmartEditAction get action => SmartEditAction.from(
-        widget.node.attributes[SmartEditBlockKeys.action],
-      );
-  String get content => widget.node.attributes[SmartEditBlockKeys.content];
-  EditorState get editorState => widget.editorState;
-
-  bool loading = true;
-  String result = '';
-
-  @override
-  void initState() {
-    super.initState();
-
-    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-      editorState.service.keyboardService?.disable();
-      // editorState.selection = null;
-    });
-
-    focusNode.requestFocus();
-    _requestCompletions();
-  }
-
-  @override
-  void dispose() {
-    client.close();
-    focusNode.dispose();
-    super.dispose();
-  }
+class SmartEditInputContent extends StatelessWidget {
+  const SmartEditInputContent({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      color: Theme.of(context).colorScheme.surface,
-      child: Container(
-        margin: const EdgeInsets.all(10),
-        child: _buildSmartEditPanel(context),
-      ),
+    return BlocBuilder<SmartEditBloc, SmartEditState>(
+      builder: (context, state) {
+        return Card(
+          elevation: 5,
+          color: Theme.of(context).colorScheme.surface,
+          child: Container(
+            margin: const EdgeInsets.all(10),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                FlowyText.medium(
+                  state.action.name,
+                  fontSize: 14,
+                ),
+                const VSpace(10),
+                _buildResultWidget(context, state),
+                const VSpace(10),
+                _buildInputFooterWidget(context, state),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 
-  Widget _buildSmartEditPanel(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        FlowyText.medium(
-          action.name,
-          fontSize: 14,
+  Widget _buildResultWidget(BuildContext context, SmartEditState state) {
+    if (state.result.isEmpty || state.loading) {
+      return const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 4.0),
+        child: SizedBox.square(
+          dimension: 14,
+          child: CircularProgressIndicator(),
         ),
-        // _buildHeaderWidget(context),
-        const Space(0, 10),
-        _buildResultWidget(context),
-        const Space(0, 10),
-        _buildInputFooterWidget(context),
-      ],
-    );
-  }
-
-  Widget _buildResultWidget(BuildContext context) {
-    final loadingWidget = Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 4.0),
-      child: SizedBox.fromSize(
-        size: const Size.square(14),
-        child: const CircularProgressIndicator(),
-      ),
-    );
-    if (result.isEmpty || loading) {
-      return loadingWidget;
+      );
     }
+    // todo: replace it with appflowy_editor
     return Flexible(
-      child: Text(
-        result,
-      ),
+      child: Text(state.result),
     );
   }
 
-  Widget _buildInputFooterWidget(BuildContext context) {
+  Widget _buildInputFooterWidget(BuildContext context, SmartEditState state) {
     return Row(
       children: [
-        FlowyRichTextButton(
-          TextSpan(
-            children: [
-              TextSpan(
-                text: LocaleKeys.document_plugins_autoGeneratorRewrite.tr(),
-                style: Theme.of(context).textTheme.bodyMedium,
-              ),
-            ],
-          ),
-          onPressed: () => _requestCompletions(rewrite: true),
+        OutlinedButton(
+          child: Text(LocaleKeys.document_plugins_autoGeneratorRewrite.tr()),
+          onPressed: () =>
+              context.read<SmartEditBloc>().add(const SmartEditEvent.rewrite()),
         ),
-        const Space(10, 0),
-        FlowyRichTextButton(
-          TextSpan(
-            children: [
-              TextSpan(
-                text: LocaleKeys.button_replace.tr(),
-                style: Theme.of(context).textTheme.bodyMedium,
-              ),
-            ],
-          ),
-          onPressed: () async {
-            await _onReplace();
-            await _onExit();
-          },
+        const HSpace(10),
+        OutlinedButton(
+          child: Text(LocaleKeys.button_replace.tr()),
+          onPressed: () =>
+              context.read<SmartEditBloc>().add(const SmartEditEvent.replace()),
         ),
-        const Space(10, 0),
-        FlowyRichTextButton(
-          TextSpan(
-            children: [
-              TextSpan(
-                text: LocaleKeys.button_insertBelow.tr(),
-                style: Theme.of(context).textTheme.bodyMedium,
-              ),
-            ],
-          ),
-          onPressed: () async {
-            await _onInsertBelow();
-            await _onExit();
-          },
+        const HSpace(10),
+        OutlinedButton(
+          child: Text(LocaleKeys.button_insertBelow.tr()),
+          onPressed: () => context
+              .read<SmartEditBloc>()
+              .add(const SmartEditEvent.insertBelow()),
         ),
-        const Space(10, 0),
-        FlowyRichTextButton(
-          TextSpan(
-            children: [
-              TextSpan(
-                text: LocaleKeys.button_cancel.tr(),
-                style: Theme.of(context).textTheme.bodyMedium,
-              ),
-            ],
-          ),
-          onPressed: () async => _onExit(),
+        const HSpace(10),
+        OutlinedButton(
+          child: Text(LocaleKeys.button_cancel.tr()),
+          onPressed: () =>
+              context.read<SmartEditBloc>().add(const SmartEditEvent.cancel()),
         ),
         Expanded(
           child: Container(
             alignment: Alignment.centerRight,
-            child: FlowyText.regular(
+            child: Text(
               LocaleKeys.document_plugins_warning.tr(),
-              color: Theme.of(context).hintColor,
+              style: TextStyle(color: Theme.of(context).hintColor),
               overflow: TextOverflow.ellipsis,
             ),
           ),
         ),
       ],
-    );
-  }
-
-  Future<void> _onReplace() async {
-    final selection = editorState.selection?.normalized;
-    if (selection == null) {
-      return;
-    }
-    final nodes = editorState.getNodesInSelection(selection);
-    if (nodes.isEmpty || !nodes.every((element) => element.delta != null)) {
-      return;
-    }
-    final replaceTexts = result.split('\n')
-      ..removeWhere((element) => element.isEmpty);
-    final transaction = editorState.transaction;
-    transaction.replaceTexts(
-      nodes,
-      selection,
-      replaceTexts,
-    );
-    await editorState.apply(transaction);
-
-    int endOffset = replaceTexts.last.length;
-    if (replaceTexts.length == 1) {
-      endOffset += selection.start.offset;
-    }
-
-    editorState.selection = Selection(
-      start: selection.start,
-      end: Position(
-        path: [selection.start.path.first + replaceTexts.length - 1],
-        offset: endOffset,
-      ),
-    );
-  }
-
-  Future<void> _onInsertBelow() async {
-    final selection = editorState.selection?.normalized;
-    if (selection == null) {
-      return;
-    }
-    final insertedText = result.split('\n')
-      ..removeWhere((element) => element.isEmpty);
-    final transaction = editorState.transaction;
-    transaction.insertNodes(
-      selection.end.path.next,
-      insertedText.map(
-        (e) => paragraphNode(
-          text: e,
-        ),
-      ),
-    );
-    transaction.afterSelection = Selection(
-      start: Position(path: selection.end.path.next),
-      end: Position(
-        path: [selection.end.path.next.first + insertedText.length],
-      ),
-    );
-    await editorState.apply(transaction);
-  }
-
-  Future<void> _onExit() async {
-    final transaction = editorState.transaction..deleteNode(widget.node);
-    return editorState.apply(
-      transaction,
-      options: const ApplyOptions(
-        recordUndo: false,
-      ),
-    );
-  }
-
-  Future<void> _requestCompletions({bool rewrite = false}) async {
-    if (rewrite) {
-      setState(() {
-        loading = true;
-        result = "";
-      });
-    }
-    final aiResitory = await getIt.getAsync<AIRepository>();
-    await aiResitory.streamCompletion(
-      text: content,
-      completionType: completionTypeFromInt(action),
-      onStart: () async {
-        setState(() {
-          loading = false;
-        });
-      },
-      onProcess: (text) async {
-        setState(() {
-          result += text;
-        });
-      },
-      onEnd: () async {
-        setState(() {
-          result += '\n';
-        });
-      },
-      onError: (error) async {
-        if (error.isLimitExceeded) {
-          showAILimitDialog(context, error.message);
-        } else {
-          showSnackBarMessage(
-            context,
-            error.message,
-            showCancel: true,
-          );
-        }
-        await _onExit();
-      },
     );
   }
 }
