f44fb02e4ad733e107619e59d0105868b84ef156
Mathias Mogensen <42929161+Xazin@users.noreply.github.com>
2024-09-15 22:33:00 +0200

    fix: provider issue in row document (#6289)
    * fix: provider issue in row document
    
    * fix: use bloc consumer

--- frontend/appflowy_flutter/lib/plugins/document/presentation/editor_page.dart
+++ frontend/appflowy_flutter/lib/plugins/document/presentation/editor_page.dart
@@ -1,5 +1,8 @@
 import 'dart:ui' as ui;
 
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
 import 'package:appflowy/generated/locale_keys.g.dart';
 import 'package:appflowy/plugins/document/application/document_bloc.dart';
 import 'package:appflowy/plugins/document/presentation/editor_configuration.dart';
@@ -27,8 +30,6 @@ import 'package:collection/collection.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flowy_infra/theme_extension.dart';
 import 'package:flowy_infra_ui/flowy_infra_ui.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:universal_platform/universal_platform.dart';
 
