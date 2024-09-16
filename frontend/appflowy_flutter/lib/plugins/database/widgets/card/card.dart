6a8d9dae4710d45b593e468e7d25ef959d82e864
Mathias Mogensen <mathiasrieckm@gmail.com>
2024-09-16 14:04:20 +0200

    fix: analyzer

--- frontend/appflowy_flutter/lib/plugins/database/widgets/card/card.dart
+++ frontend/appflowy_flutter/lib/plugins/database/widgets/card/card.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'package:appflowy/generated/flowy_svgs.g.dart';
-import 'package:appflowy/generated/locale_keys.g.dart';
 import 'package:appflowy/mobile/presentation/database/card/card.dart';
 import 'package:appflowy/plugins/database/application/field/field_controller.dart';
 import 'package:appflowy/plugins/database/application/row/row_cache.dart';
@@ -12,8 +11,6 @@ import 'package:appflowy_backend/protobuf/flowy-database2/protobuf.dart';
 import 'package:appflowy_backend/protobuf/flowy-user/user_profile.pb.dart';
 import 'package:appflowy_popover/appflowy_popover.dart';
 import 'package:collection/collection.dart';
-import 'package:easy_localization/easy_localization.dart';
-import 'package:flowy_infra/theme_extension.dart';
 import 'package:flowy_infra_ui/flowy_infra_ui.dart';
 import 'package:flowy_infra_ui/style_widget/hover.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -217,7 +214,6 @@ class _CardContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final attachmentCount = rowMeta.attachmentCount.toInt();
     final child = Column(
       mainAxisSize: MainAxisSize.min,
       children: [
