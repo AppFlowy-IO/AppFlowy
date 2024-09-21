b0008241d99c7c24d448cf27d585cd2f1fb5335a
nathan <nathan@appflowy.io>
2024-09-21 16:04:03 +0800

    chore: fix test

--- frontend/appflowy_flutter/integration_test/cloud/anon_user_continue_test.dart
+++ frontend/appflowy_flutter/integration_test/cloud/anon_user_continue_test.dart
@@ -75,18 +75,6 @@ void main() {
 
       await tester.logout();
       await tester.pumpAndSettle();
-
-      // tap the continue as anonymous button
-      // await tester
-      //     .tapButton(find.text(LocaleKeys.signIn_loginStartWithAnonymous.tr()));
-      // await tester.expectToSeeHomePage();
-
-      // // New anon user name
-      // await tester.openSettings();
-      // await tester.openSettingsPage(SettingsPage.account);
-      // final userNameInput =
-      //     tester.widget(find.byType(AccountUserProfile)) as AccountUserProfile;
-      // expect(userNameInput.name, 'Me');
     });
   });
 }
