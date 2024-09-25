54f9fcfad48af2f30e5997d3a3497a0f25b2b10b
Lucas.Xu <lucas.xu@appflowy.io>
2024-09-25 16:57:14 +0800

    test: arrow key in title test

--- frontend/appflowy_flutter/integration_test/desktop/document/document_title_test.dart
+++ frontend/appflowy_flutter/integration_test/desktop/document/document_title_test.dart
@@ -1,6 +1,9 @@
+import 'package:appflowy_editor/appflowy_editor.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
+import 'package:universal_platform/universal_platform.dart';
 
 import '../../shared/util.dart';
 
@@ -8,6 +11,10 @@ void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   group('document title: ', () {
+    // 1. create a new document
+    // 2. edit title
+    // 3. press enter to create a new line
+    // 4. insert text
     testWidgets('create a new document and edit title', (tester) async {
       await tester.initializeAppFlowy();
       await tester.tapAnonymousSignInButton();
@@ -36,6 +43,40 @@ void main() {
 
       final firstLineText = find.text(firstLine, findRichText: true);
       expect(firstLineText, findsOneWidget);
+
+      // press cmd/ctrl+left to move the cursor to the start of the line
+      await tester.simulateKeyEvent(
+        LogicalKeyboardKey.arrowLeft,
+        isControlPressed: !UniversalPlatform.isMacOS,
+        isMetaPressed: UniversalPlatform.isMacOS,
+      );
+      await tester.pumpAndSettle();
+
+      // press arrow left to delete the first line
+      await tester.simulateKeyEvent(LogicalKeyboardKey.arrowLeft);
+      await tester.pumpAndSettle();
+
+      // check if the title is on focus
+      final titleOnFocus = tester.editor.findDocumentTitle(name);
+      final titleWidget = tester.widget<TextField>(titleOnFocus);
+      expect(titleWidget.focusNode?.hasFocus, isTrue);
+
+      // press the right arrow key to move the cursor to the first line
+      await tester.simulateKeyEvent(LogicalKeyboardKey.arrowRight);
+      await tester.pumpAndSettle();
+
+      // check if the title is not on focus
+      expect(titleWidget.focusNode?.hasFocus, isFalse);
+
+      final editorState = tester.editor.getCurrentEditorState();
+      expect(editorState.selection, Selection.collapsed(Position(path: [0])));
+
+      // press the backspace key to go to the title
+      await tester.simulateKeyEvent(LogicalKeyboardKey.backspace);
+      await tester.pumpAndSettle();
+
+      expect(editorState.selection, null);
+      expect(titleWidget.focusNode?.hasFocus, isTrue);
     });
   });
 }
