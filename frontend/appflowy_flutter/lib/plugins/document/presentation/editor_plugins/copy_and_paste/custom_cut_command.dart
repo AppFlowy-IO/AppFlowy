a0ee47b8097f97e4efd87e3da930266a756ccbc3
Lucas <lucas.xu@appflowy.io>
2024-09-30 16:34:41 +0800

    feat: support copy/cut command when the selection is collapsed (#6429)
    * feat: add delete line command
    
    * test: add delete line test
    
    * test: add delete line command test
    
    * feat: support copy command when selection is collasped

--- frontend/appflowy_flutter/lib/plugins/document/presentation/editor_plugins/copy_and_paste/custom_cut_command.dart
+++ frontend/appflowy_flutter/lib/plugins/document/presentation/editor_plugins/copy_and_paste/custom_cut_command.dart
@@ -18,7 +18,30 @@ final CommandShortcutEvent customCutCommand = CommandShortcutEvent(
 );
 
 CommandShortcutEventHandler _cutCommandHandler = (editorState) {
+  final selection = editorState.selection;
+  if (selection == null) {
+    return KeyEventResult.ignored;
+  }
+
   customCopyCommand.execute(editorState);
-  editorState.deleteSelectionIfNeeded();
+
+  if (!selection.isCollapsed) {
+    editorState.deleteSelectionIfNeeded();
+  } else {
+    final node = editorState.getNodeAtPath(selection.end.path);
+    if (node == null) {
+      return KeyEventResult.handled;
+    }
+    final transaction = editorState.transaction;
+    transaction.deleteNode(node);
+    final nextNode = node.next;
+    if (nextNode != null && nextNode.delta != null) {
+      transaction.afterSelection = Selection.collapsed(
+        Position(path: node.path, offset: nextNode.delta?.length ?? 0),
+      );
+    }
+    editorState.apply(transaction);
+  }
+
   return KeyEventResult.handled;
 };
