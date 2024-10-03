b690887f53d6d48b28d208c3f95c4a2f83f033b2
Lucas.Xu <lucas.xu@appflowy.io>
2024-10-03 21:42:07 +0800

    test: add ignore same icon update test

--- frontend/appflowy_flutter/integration_test/desktop/cloud/workspace/workspace_test_runner.dart
+++ frontend/appflowy_flutter/integration_test/desktop/cloud/workspace/workspace_test_runner.dart
@@ -3,6 +3,7 @@ import 'package:integration_test/integration_test.dart';
 import 'change_name_and_icon_test.dart' as change_name_and_icon_test;
 import 'collaborative_workspace_test.dart' as collaborative_workspace_test;
 import 'share_menu_test.dart' as share_menu_test;
+import 'workspace_icon_test.dart' as workspace_icon_test;
 import 'workspace_settings_test.dart' as workspace_settings_test;
 
 void startTesting() {
@@ -12,4 +13,5 @@ void startTesting() {
   share_menu_test.main();
   collaborative_workspace_test.main();
   change_name_and_icon_test.main();
+  workspace_icon_test.main();
 }
