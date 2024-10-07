f8ce501b537c02f66450e5ccc939bbbcc1d85932
Nathan.fooo <86001920+appflowy@users.noreply.github.com>
2024-10-07 13:31:47 +0800

    chore: fix anon user integration test (#6493)
    * chore: fix anon user integration test
    
    * chore: fix env
    
    * chore: appflowy cloud version
    
    * chore: appflowy cloud version

--- frontend/appflowy_flutter/integration_test/desktop/cloud/cloud_runner.dart
+++ frontend/appflowy_flutter/integration_test/desktop/cloud/cloud_runner.dart
@@ -3,9 +3,13 @@ import 'sidebar/sidebar_move_page_test.dart' as sidebar_move_page_test;
 import 'uncategorized/uncategorized_test_runner.dart'
     as uncategorized_test_runner;
 import 'workspace/workspace_test_runner.dart' as workspace_test_runner;
-import 'data_migration/data_migration_test_runner.dart' as data_migration_test_runner;
+import 'data_migration/data_migration_test_runner.dart'
+    as data_migration_test_runner;
+import 'set_env.dart' as preset_af_cloud_env_test;
 
 Future<void> main() async {
+  preset_af_cloud_env_test.main();
+
   data_migration_test_runner.main();
   // uncategorized
   uncategorized_test_runner.main();
@@ -18,5 +22,4 @@ Future<void> main() async {
 
   // sidebar
   sidebar_move_page_test.main();
-
 }
