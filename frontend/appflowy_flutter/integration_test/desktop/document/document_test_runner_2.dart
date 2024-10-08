a55e229cf538f1b82d0b610c88238d54a3ed0f05
Lucas.Xu <lucas.xu@appflowy.io>
2024-10-08 13:55:19 +0800

    chore: disable subpage feature

--- frontend/appflowy_flutter/integration_test/desktop/document/document_test_runner_2.dart
+++ frontend/appflowy_flutter/integration_test/desktop/document/document_test_runner_2.dart
@@ -1,7 +1,6 @@
 import 'package:integration_test/integration_test.dart';
 
 import 'document_app_lifecycle_test.dart' as document_app_lifecycle_test;
-import 'document_sub_page_test.dart' as document_sub_page_test;
 import 'document_title_test.dart' as document_title_test;
 
 void main() {
@@ -9,6 +8,7 @@ void main() {
 
   // Document integration tests
   document_title_test.main();
-  document_sub_page_test.main();
+  // Disable subPage test temporarily, enable it in version 0.7.2
+  // document_sub_page_test.main();
   document_app_lifecycle_test.main();
 }
