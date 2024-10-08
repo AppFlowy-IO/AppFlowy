9eaf518d1a17de961e13b1d69233a55c6e74ddb0
Lucas.Xu <lucas.xu@appflowy.io>
2024-10-08 18:02:11 +0800

    test: add ai limit test

--- frontend/appflowy_flutter/test/bloc_test/smart_edit_test/smart_editor_bloc_test.dart
+++ frontend/appflowy_flutter/test/bloc_test/smart_edit_test/smart_editor_bloc_test.dart
@@ -9,7 +9,7 @@ import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
-class MockAIRepository extends Mock implements AIRepository {
+class _MockAIRepository extends Mock implements AIRepository {
   @override
   Future<void> streamCompletion({
     required String text,
@@ -25,6 +25,26 @@ class MockAIRepository extends Mock implements AIRepository {
       await onProcess('UPDATED: ${lines[i]}\n\n');
     }
     await onEnd();
+  }
+}
+
+class _MockErrorRepository extends Mock implements AIRepository {
+  @override
+  Future<void> streamCompletion({
+    required String text,
+    required CompletionTypePB completionType,
+    required Future<void> Function() onStart,
+    required Future<void> Function(String text) onProcess,
+    required Future<void> Function() onEnd,
+    required void Function(AIError error) onError,
+  }) async {
+    await onStart();
+    onError(
+      const AIError(
+        message: 'Error',
+        code: AIErrorCode.aiResponseLimitExceeded,
+      ),
+    );
   }
 }
 
@@ -64,7 +84,7 @@ void main() {
         );
       },
       act: (bloc) {
-        bloc.add(SmartEditEvent.initial(Future.value(MockAIRepository())));
+        bloc.add(SmartEditEvent.initial(Future.value(_MockAIRepository())));
         bloc.add(const SmartEditEvent.rewrite());
       },
       expect: () => [
@@ -78,17 +98,56 @@ void main() {
         isA<SmartEditState>().having((s) => s.loading, 'loading', false),
       ],
     );
+
+    blocTest<SmartEditBloc, SmartEditState>(
+      'exceed the ai response limit',
+      build: () {
+        const text1 = '1. Select text to style using the toolbar menu.';
+        const text2 = '2. Discover more styling options in Aa.';
+        const text3 =
+            '3. AppFlowy empowers you to beautifully and effortlessly style your content.';
+        final document = Document(
+          root: pageNode(
+            children: [
+              paragraphNode(text: text1),
+              paragraphNode(text: text2),
+              paragraphNode(text: text3),
+            ],
+          ),
+        );
+        final editorState = EditorState(document: document);
+        editorState.selection = Selection(
+          start: Position(path: [0]),
+          end: Position(path: [2], offset: text3.length),
+        );
+
+        final node = smartEditNode(
+          action: SmartEditAction.makeItLonger,
+          content: [text1, text2, text3].join('\n'),
+        );
+        return SmartEditBloc(
+          node: node,
+          editorState: editorState,
+          action: SmartEditAction.makeItLonger,
+          enableLogging: false,
+        );
+      },
+      act: (bloc) {
+        bloc.add(SmartEditEvent.initial(Future.value(_MockErrorRepository())));
+        bloc.add(const SmartEditEvent.rewrite());
+      },
+      expect: () => [
+        isA<SmartEditState>()
+            .having((s) => s.loading, 'loading', true)
+            .having((s) => s.result, 'result', isEmpty),
+        isA<SmartEditState>()
+            .having((s) => s.requestError, 'requestError', isNotNull)
+            .having(
+              (s) => s.requestError?.code,
+              'requestError.code',
+              AIErrorCode.aiResponseLimitExceeded,
+            ),
+      ],
+    );
   });
 }
-
-
-// [
-//             _$SmartEditStateImpl:SmartEditState(loading: true, result: , action: SmartEditAction.makeItLonger),
-//             _$SmartEditStateImpl:SmartEditState(loading: false, result: UPDATED: 1. Select text to style using the toolbar menu.
-//           2. Discover more styling options in Aa.
-//           3. AppFlowy empowers you to beautifully and effortlessly style your content.
-
-//           , action: SmartEditAction.makeItLonger),
-//             _$SmartEditStateImpl:SmartEditState(loading: false, result:
-//           , action: SmartEditAction.makeItLonger)
-//           ]
