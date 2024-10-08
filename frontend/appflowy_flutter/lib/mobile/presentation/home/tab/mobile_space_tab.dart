8b249178a52a7b9874d5b408e16464c347b7cbb1
Lucas.Xu <lucas.xu@appflowy.io>
2024-10-08 10:22:02 +0800

    fix: cover title test on mobile

--- frontend/appflowy_flutter/lib/mobile/presentation/home/tab/mobile_space_tab.dart
+++ frontend/appflowy_flutter/lib/mobile/presentation/home/tab/mobile_space_tab.dart
@@ -1,4 +1,3 @@
-import 'package:appflowy/generated/locale_keys.g.dart';
 import 'package:appflowy/mobile/application/mobile_router.dart';
 import 'package:appflowy/mobile/presentation/home/favorite_folder/favorite_space.dart';
 import 'package:appflowy/mobile/presentation/home/home_space/home_space.dart';
@@ -15,7 +14,6 @@ import 'package:appflowy/workspace/application/view/view_ext.dart';
 import 'package:appflowy_backend/log.dart';
 import 'package:appflowy_backend/protobuf/flowy-folder/view.pb.dart';
 import 'package:appflowy_backend/protobuf/flowy-user/protobuf.dart';
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flowy_infra_ui/flowy_infra_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -196,7 +194,7 @@ class _MobileSpaceTabState extends State<MobileSpaceTab>
       // only support create document in section
       context.read<SidebarSectionsBloc>().add(
             SidebarSectionsEvent.createRootViewInSection(
-              name: LocaleKeys.menuAppHeader_defaultNewPageName.tr(),
+              name: layout.defaultName,
               index: 0,
               viewSection: FolderSpaceType.public.toViewSectionPB,
             ),
