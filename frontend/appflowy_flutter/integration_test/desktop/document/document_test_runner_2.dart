a04c1577707ac8e88bd1a38ab1ecc5e1afbddc26
Ahad Patel <69256193+Ahad-patel@users.noreply.github.com>
2024-10-14 18:24:03 +0530

    fix: date issue when enbling time feild (#6447)
    * fix: date issue when enbling time field
    
    * chore: add integration test for date or reminder field
    
    * chore: add test file to document_test_runner.dart
    
    * fix: flutter analyze
    
    ---------
    
    Co-authored-by: Lucas.Xu <lucas.xu@appflowy.io>

--- frontend/appflowy_flutter/integration_test/desktop/document/document_test_runner_2.dart
+++ frontend/appflowy_flutter/integration_test/desktop/document/document_test_runner_2.dart
@@ -4,6 +4,8 @@ import 'document_app_lifecycle_test.dart' as document_app_lifecycle_test;
 import 'document_deletion_test.dart' as document_deletion_test;
 import 'document_option_action_test.dart' as document_option_action_test;
 import 'document_title_test.dart' as document_title_test;
+import 'document_with_date_reminder_test.dart'
+    as document_with_date_reminder_test;
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
@@ -13,6 +15,7 @@ void main() {
   // Disable subPage test temporarily, enable it in version 0.7.2
   // document_sub_page_test.main();
   document_app_lifecycle_test.main();
+  document_with_date_reminder_test.main();
   document_deletion_test.main();
   document_option_action_test.main();
 }
