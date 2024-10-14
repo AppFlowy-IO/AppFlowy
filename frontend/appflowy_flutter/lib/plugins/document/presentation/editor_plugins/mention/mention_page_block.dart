f4466b22d8a40262fa992f9e0dff1bf055005f16
Lucas <lucas.xu@appflowy.io>
2024-10-14 10:08:07 +0800

    fix: integration test

--- frontend/appflowy_flutter/lib/plugins/document/presentation/editor_plugins/mention/mention_page_block.dart
+++ frontend/appflowy_flutter/lib/plugins/document/presentation/editor_plugins/mention/mention_page_block.dart
@@ -89,7 +89,7 @@ class _MentionPageBlockState extends State<MentionPageBlock> {
             return const SizedBox.shrink();
           }
 
-          if (state.isDeleted || view == null) {
+          if (view == null) {
             return _NoAccessMentionPageBlock(
               textStyle: widget.textStyle,
             );
