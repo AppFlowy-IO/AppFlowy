8c0a9c5ff33af79fe95cf754e24c32a28c5772c3
Lucas.Xu <lucas.xu@appflowy.io>
2024-10-14 15:54:22 +0800

    fix: flutter analyze

--- frontend/appflowy_flutter/integration_test/desktop/document/document_test_runner_2.dart
+++ frontend/appflowy_flutter/integration_test/desktop/document/document_test_runner_2.dart
@@ -1,12 +1,11 @@
 import 'package:integration_test/integration_test.dart';
 
 import 'document_app_lifecycle_test.dart' as document_app_lifecycle_test;
-import 'document_title_test.dart' as document_title_test;
-import 'document_with_date_reminder_test.dart'
-    as document_with_date_reminder_test;
 import 'document_deletion_test.dart' as document_deletion_test;
 import 'document_option_action_test.dart' as document_option_action_test;
 import 'document_title_test.dart' as document_title_test;
+import 'document_with_date_reminder_test.dart'
+    as document_with_date_reminder_test;
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
