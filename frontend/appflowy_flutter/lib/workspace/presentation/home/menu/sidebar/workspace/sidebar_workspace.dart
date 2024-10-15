7bc53d7bc6edfa664084bfd566e096dbcb26fcad
Lucas <lucas.xu@appflowy.io>
2024-10-15 15:12:59 +0800

    feat: support 'share link' and 'publish' on mobile (#6548)
    * feat: support publish on mobile
    
    * chore: replace icons
    
    * feat: support publish and copy link on mobile
    
    * feat: support copy link
    
    * feat: support copy share link
    
    * chore: update icons
    
    * fix: flutter analyze
    
    * fix: ignore opening same page on mobile
    
    * test: add copy link test on mobile

--- frontend/appflowy_flutter/lib/workspace/presentation/home/menu/sidebar/workspace/sidebar_workspace.dart
+++ frontend/appflowy_flutter/lib/workspace/presentation/home/menu/sidebar/workspace/sidebar_workspace.dart
@@ -15,7 +15,6 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flowy_infra_ui/flowy_infra_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:toastification/toastification.dart';
 
 class SidebarWorkspace extends StatefulWidget {
   const SidebarWorkspace({super.key, required this.userProfile});
--- frontend/appflowy_flutter/lib/workspace/presentation/settings/settings_dialog.dart
+++ frontend/appflowy_flutter/lib/workspace/presentation/settings/settings_dialog.dart
@@ -28,7 +28,6 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flowy_infra_ui/flowy_infra_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:toastification/toastification.dart';
 
 import 'widgets/setting_cloud.dart';
 
