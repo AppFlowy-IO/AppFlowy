1dcec1f5a589f5a9e94bf1522377e6c78eee47f8
Lucas.Xu <lucas.xu@appflowy.io>
2024-10-15 13:55:19 +0800

    fix: ignore opening same page on mobile

--- frontend/appflowy_flutter/lib/plugins/document/presentation/editor_plugins/mention/mention_page_block.dart
+++ frontend/appflowy_flutter/lib/plugins/document/presentation/editor_plugins/mention/mention_page_block.dart
@@ -116,8 +116,11 @@ class _MentionPageBlockState extends State<MentionPageBlock> {
   }
 
   Future<void> handleTap(ViewPB view) async {
-    if (UniversalPlatform.isMobile && mounted) {
-      await context.pushView(view);
+    if (UniversalPlatform.isMobile) {
+      final currentViewId = context.read<DocumentBloc>().documentId;
+      if (mounted && currentViewId != widget.pageId) {
+        await context.pushView(view);
+      }
     } else {
       getIt<TabsBloc>().add(
         TabsEvent.openPlugin(plugin: view.plugin(), view: view),
