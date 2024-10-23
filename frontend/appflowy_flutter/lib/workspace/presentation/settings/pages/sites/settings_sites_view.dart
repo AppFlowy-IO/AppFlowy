ff5a3244b8b6c510a3df42785d58ef5ba67459f9
Lucas.Xu <lucas.xu@appflowy.io>
2024-10-23 20:58:21 +0800

    feat: fetch namespace

--- frontend/appflowy_flutter/lib/workspace/presentation/settings/pages/sites/settings_sites_view.dart
+++ frontend/appflowy_flutter/lib/workspace/presentation/settings/pages/sites/settings_sites_view.dart
@@ -1,27 +1,40 @@
+import 'package:appflowy/workspace/presentation/settings/pages/sites/published_page_item.dart';
+import 'package:appflowy/workspace/presentation/settings/pages/sites/settings_sites_bloc.dart';
 import 'package:appflowy/workspace/presentation/settings/shared/settings_body.dart';
 import 'package:flowy_infra_ui/flowy_infra_ui.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class SettingsSitesPage extends StatefulWidget {
-  const SettingsSitesPage({
-    super.key,
-  });
+class SettingsSitesPage extends StatelessWidget {
+  const SettingsSitesPage({super.key});
 
   @override
-  State<SettingsSitesPage> createState() => _SettingsSitesPageState();
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => SettingsSitesBloc(),
+      child: const _SettingsSitesPageView(),
+    );
+  }
 }
 
-class _SettingsSitesPageState extends State<SettingsSitesPage> {
-  // late final Future<>
+class _SettingsSitesPageView extends StatelessWidget {
+  const _SettingsSitesPageView();
+
   @override
   Widget build(BuildContext context) {
     return SettingsBody(
       title: 'Sites',
       children: [
-        SeparatedColumn(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          separatorBuilder: () => const Divider(),
-          children: const [],
+        BlocBuilder<SettingsSitesBloc, SettingsSitesState>(
+          builder: (context, state) {
+            return SeparatedColumn(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              separatorBuilder: () => const Divider(),
+              children: state.publishedViews
+                  .map((view) => PublishedPageItem(publishInfoView: view))
+                  .toList(),
+            );
+          },
         ),
       ],
     );
