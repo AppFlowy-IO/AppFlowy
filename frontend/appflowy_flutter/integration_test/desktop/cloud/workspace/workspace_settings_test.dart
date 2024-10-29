1e35a500553868792a6770d0b41e1c09f6769d47
LucasXu0 <lucas.xu@appflowy.io>
2024-10-29 20:47:14 +0800

    test: add update namespace tests

--- frontend/appflowy_flutter/integration_test/desktop/cloud/workspace/workspace_settings_test.dart
+++ frontend/appflowy_flutter/integration_test/desktop/cloud/workspace/workspace_settings_test.dart
@@ -15,6 +15,11 @@ import 'package:appflowy/workspace/presentation/home/menu/sidebar/workspace/_sid
 import 'package:appflowy/workspace/presentation/home/menu/sidebar/workspace/_sidebar_workspace_menu.dart';
 import 'package:appflowy/workspace/presentation/home/menu/sidebar/workspace/sidebar_workspace.dart';
 import 'package:appflowy/workspace/presentation/settings/pages/settings_workspace_view.dart';
+import 'package:appflowy/workspace/presentation/settings/pages/sites/domain/domain_item.dart';
+import 'package:appflowy/workspace/presentation/settings/pages/sites/domain/domain_more_action.dart';
+import 'package:appflowy/workspace/presentation/settings/pages/sites/domain/domain_settings_dialog.dart';
+import 'package:appflowy/workspace/presentation/settings/pages/sites/domain/home_page_menu.dart';
+import 'package:appflowy/workspace/presentation/settings/pages/sites/published_page_item.dart';
 import 'package:appflowy/workspace/presentation/settings/shared/setting_list_tile.dart';
 import 'package:appflowy/workspace/presentation/settings/widgets/setting_appflowy_cloud.dart';
 import 'package:appflowy/workspace/presentation/widgets/user_avatar.dart';
@@ -27,6 +32,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:path/path.dart' as p;
 
+import '../../../shared/constants.dart';
 import '../../../shared/database_test_op.dart';
 import '../../../shared/dir.dart';
 import '../../../shared/emoji.dart';
@@ -92,5 +98,119 @@ void main() {
         );
       },
     );
+  });
+
+  group('sites settings:', () {
+    testWidgets('publish page and check in sites page', (tester) async {
+      await tester.initializeAppFlowy(
+        cloudType: AuthenticatorType.appflowyCloudSelfHost,
+      );
+      await tester.tapGoogleLoginInButton();
+      await tester.expectToSeeHomePageWithGetStartedPage();
+
+      const pageName = 'Document';
+
+      await tester.createNewPageInSpace(
+        spaceName: Constants.generalSpaceName,
+        layout: ViewLayoutPB.Document,
+        pageName: pageName,
+      );
+
+      // open the publish menu
+      await tester.openPublishMenu();
+
+      // publish the document
+      await tester.tapButton(find.text(LocaleKeys.shareAction_publish.tr()));
+
+      // click empty area to close the publish menu
+      await tester.tapAt(Offset.zero);
+
+      // check if the page is published in sites page
+      await tester.openSettings();
+      await tester.openSettingsPage(SettingsPage.sites);
+      // wait the backend return the sites data
+      await tester.wait(1000);
+
+      // check if the page is published in sites page
+      final pageItem = find.byWidgetPredicate(
+        (widget) =>
+            widget is PublishedPageItem &&
+            widget.publishInfoView.view.name == pageName,
+      );
+      expect(pageItem, findsOneWidget);
+
+      // set it to homepage
+      await tester.tapButton(
+        find.textContaining(
+          LocaleKeys.settings_sites_selectHomePage.tr(),
+        ),
+      );
+      await tester.tapButton(
+        find.descendant(
+          of: find.byType(SelectHomePageMenu),
+          matching: find.text(pageName),
+        ),
+      );
+      await tester.pumpAndSettle();
+
+      // check if the page is set to homepage
+      final homePageItem = find.descendant(
+        of: find.byType(DomainItem),
+        matching: find.text(pageName),
+      );
+      expect(homePageItem, findsOneWidget);
+    });
+
+    testWidgets('update namespace', (tester) async {
+      await tester.initializeAppFlowy(
+        cloudType: AuthenticatorType.appflowyCloudSelfHost,
+      );
+      await tester.tapGoogleLoginInButton();
+      await tester.expectToSeeHomePageWithGetStartedPage();
+
+      // check if the page is published in sites page
+      await tester.openSettings();
+      await tester.openSettingsPage(SettingsPage.sites);
+      // wait the backend return the sites data
+      await tester.wait(1000);
+
+      // update the domain
+      final domainMoreAction = find.byType(DomainMoreAction);
+      await tester.tap(domainMoreAction);
+
+      // click the update namespace button
+      final updateNamespaceButton = find.text(
+        LocaleKeys.settings_sites_updateNamespace.tr(),
+      );
+      await tester.tap(updateNamespaceButton);
+
+      // expect to see the dialog
+      await tester.updateNamespace('&&&???');
+
+      // expect to see the toast with error message
+      final errorToast = find.text(
+        LocaleKeys.settings_sites_error_namespaceContainsInvalidCharacters.tr(),
+      );
+      expect(errorToast, findsOneWidget);
+      await tester.pumpUntilNotFound(errorToast);
+
+      // short namespace
+      await tester.updateNamespace('a');
+
+      // expect to see the toast with error message
+      final errorToast2 = find.text(
+        LocaleKeys.settings_sites_error_namespaceTooShort.tr(),
+      );
+      expect(errorToast2, findsOneWidget);
+      await tester.pumpUntilNotFound(errorToast2);
+      // valid namespace
+      await tester.updateNamespace('AppFlowy');
+
+      // expect to see the toast with success message
+      final successToast = find.text(
+        LocaleKeys.settings_sites_success_namespaceUpdated.tr(),
+      );
+      expect(successToast, findsOneWidget);
+    });
   });
 }
