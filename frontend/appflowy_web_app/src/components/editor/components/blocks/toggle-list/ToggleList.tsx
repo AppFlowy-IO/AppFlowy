ef7b8189ed6932376dec7f9d4528ad0f8a76ed0c
Kilu <lu@appflowy.io>
2024-11-04 13:34:09 +0800

    feat: add login to no found page

--- frontend/appflowy_web_app/src/components/editor/components/blocks/toggle-list/ToggleList.tsx
+++ frontend/appflowy_web_app/src/components/editor/components/blocks/toggle-list/ToggleList.tsx
@@ -1,19 +1,23 @@
+import { getHeadingCssProperty } from '@/components/editor/components/blocks/heading';
 import React, { forwardRef, memo, useMemo } from 'react';
 import { EditorElementProps, ToggleListNode } from '@/components/editor/editor.type';
 
 export const ToggleList = memo(
   forwardRef<HTMLDivElement, EditorElementProps<ToggleListNode>>(({ node, children, ...attributes }, ref) => {
-    const { collapsed } = useMemo(() => node.data || {}, [node.data]);
-    const className = `${attributes.className ?? ''} flex w-full flex-col ${collapsed ? 'collapsed' : ''}`;
+    const { collapsed, level } = useMemo(() => node.data || {}, [node.data]);
+    const fontSizeCssProperty = getHeadingCssProperty(level || 0);
+    const className = `${attributes.className ?? ''} flex w-full flex-col ${collapsed ? 'collapsed' : ''} ${fontSizeCssProperty} level-${level}`;
 
     return (
       <>
-        <div {...attributes} ref={ref} className={className}>
+        <div {...attributes} ref={ref}
+             className={className}
+        >
           {children}
         </div>
       </>
     );
-  })
+  }),
 );
 
 export default ToggleList;
