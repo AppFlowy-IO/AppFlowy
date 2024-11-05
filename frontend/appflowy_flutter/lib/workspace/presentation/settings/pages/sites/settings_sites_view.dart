54096b391fe4c127b83d9d0b47f8c4cb24a9105a
Lucas <lucas.xu@appflowy.io>
2024-11-05 09:23:38 +0800

    fix: settings site issues (#6701)
    * fix: pages overflow when selecting homepage
    
    * fix: settings site issues
    
    * chore: try to fix windows ci
    
    * test: add tests
    
    * fix: shareblock state update
    
    * fix: pages overflow when selecting homepage
    
    * fix: view name doesn't update after publishing
    
    * chore: add translations
    
    * feat: close popup menu after unpublishing page
    
    * feat: align the published page name with header

--- frontend/appflowy_flutter/lib/workspace/presentation/settings/pages/sites/settings_sites_view.dart
+++ frontend/appflowy_flutter/lib/workspace/presentation/settings/pages/sites/settings_sites_view.dart
@@ -1,5 +1,6 @@
 import 'package:appflowy/generated/locale_keys.g.dart';
 import 'package:appflowy/workspace/application/user/user_workspace_bloc.dart';
+import 'package:appflowy/workspace/presentation/settings/pages/sites/constants.dart';
 import 'package:appflowy/workspace/presentation/settings/pages/sites/domain/domain_header.dart';
 import 'package:appflowy/workspace/presentation/settings/pages/sites/domain/domain_item.dart';
 import 'package:appflowy/workspace/presentation/settings/pages/sites/published_page/published_view_item.dart';
@@ -80,9 +81,18 @@ class _SettingsSitesPageView extends StatelessWidget {
               ),
               children: [
                 const DomainHeader(),
-                DomainItem(
-                  namespace: state.namespace,
-                  homepage: '',
+                ConstrainedBox(
+                  constraints: const BoxConstraints(minHeight: 36.0),
+                  child: Transform.translate(
+                    offset: const Offset(
+                      -SettingsPageSitesConstants.alignPadding,
+                      0,
+                    ),
+                    child: DomainItem(
+                      namespace: state.namespace,
+                      homepage: '',
+                    ),
+                  ),
                 ),
               ],
             );
@@ -134,7 +144,13 @@ class _SettingsSitesPageView extends StatelessWidget {
       } else {
         children.addAll(
           publishedViews.map(
-            (view) => PublishedViewItem(publishInfoView: view),
+            (view) => Transform.translate(
+              offset: const Offset(
+                -SettingsPageSitesConstants.alignPadding,
+                0,
+              ),
+              child: PublishedViewItem(publishInfoView: view),
+            ),
           ),
         );
       }
