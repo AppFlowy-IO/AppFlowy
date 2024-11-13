57933736eaa440afca77c33deaf56992cd0263ec
Lucas <lucas.xu@appflowy.io>
2024-11-13 17:16:48 +0800

    feat: support embed webp (#6775)
    * feat: support uploading webp
    
    * feat: support jpeg and bmp image format

--- frontend/appflowy_flutter/lib/shared/patterns/common_patterns.dart
+++ frontend/appflowy_flutter/lib/shared/patterns/common_patterns.dart
@@ -7,10 +7,10 @@ final hrefRegex = RegExp(_hrefPattern);
 
 /// This pattern allows for both HTTP and HTTPS Scheme
 /// It allows for query parameters
-/// It only allows the following image extensions: .png, .jpg, .gif, .webm
+/// It only allows the following image extensions: .png, .jpg, .jpeg, .gif, .webm, .webp, .bmp
 ///
 const _imgUrlPattern =
-    r'(https?:\/\/)([^\s(["<,>/]*)(\/)[^\s[",><]*(.png|.jpg|.gif|.webm)(\?[^\s[",><]*)?';
+    r'(https?:\/\/)([^\s(["<,>/]*)(\/)[^\s[",><]*(.png|.jpg|.jpeg|.gif|.webm|.webp|.bmp)(\?[^\s[",><]*)?';
 final imgUrlRegex = RegExp(_imgUrlPattern);
 
 /// This pattern allows for both HTTP and HTTPS Scheme
