f526df98c7a1f468b6dfc1d2306fcd627bbd8693
Kilu.He <108015703+qinluhe@users.noreply.github.com>
2024-11-17 14:58:28 +0800

    fix: add template button to publish page (#6809)

--- frontend/appflowy_web_app/src/components/publish/header/RightMenu.tsx
+++ frontend/appflowy_web_app/src/components/publish/header/RightMenu.tsx
@@ -1,19 +1,41 @@
 import MoreActions from '@/components/_shared/more-actions/MoreActions';
 import { openOrDownload } from '@/utils/open_schema';
-import { Divider, Tooltip } from '@mui/material';
-import React from 'react';
+import { Divider, IconButton, Tooltip } from '@mui/material';
+import React, { useCallback, useContext } from 'react';
 import { ReactComponent as Logo } from '@/assets/logo.svg';
 import { Duplicate } from '@/components/publish/header/duplicate';
 import { useTranslation } from 'react-i18next';
+import { PublishContext } from '@/application/publish';
+import { useCurrentUser } from '@/components/main/app.hooks';
+import  { ReactComponent as TemplateIcon } from '@/assets/template.svg';
 
 function RightMenu () {
   const { t } = useTranslation();
+  const viewMeta = useContext(PublishContext)?.viewMeta;
+  const viewId = viewMeta?.view_id;
+  const viewName = viewMeta?.name;
+
+  const handleTemplateClick = useCallback(() => {
+    const url = `${window.origin}${window.location.pathname}`;
+
+    window.open(`${window.origin}/as-template?viewUrl=${encodeURIComponent(url)}&viewName=${viewName || ''}&viewId=${viewId || ''}`, '_blank');
+  }, [viewId, viewName]);
+
+  const currentUser = useCurrentUser();
+
+  const isAppFlowyUser = currentUser?.email?.endsWith('@appflowy.io');
 
   return (
     <>
       <MoreActions />
       <Duplicate />
-
+      {isAppFlowyUser && (
+        <Tooltip title={t('template.asTemplate')}>
+          <IconButton onClick={handleTemplateClick} size={'small'}>
+            <TemplateIcon />
+          </IconButton>
+        </Tooltip>
+      )}
       <Divider
         orientation={'vertical'}
         className={'mx-2'}
