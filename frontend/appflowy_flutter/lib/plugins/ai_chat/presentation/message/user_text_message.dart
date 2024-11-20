fce18dfe2e28efeae7c13f445829e1862218c590
Richard Shiue <71320345+richardshiue@users.noreply.github.com>
2024-11-20 10:46:26 +0300

    chore: simplify chat user message bloc and widgets

--- frontend/appflowy_flutter/lib/plugins/ai_chat/presentation/message/user_text_message.dart
+++ frontend/appflowy_flutter/lib/plugins/ai_chat/presentation/message/user_text_message.dart
@@ -1,3 +1,6 @@
+import 'package:appflowy/plugins/ai_chat/application/chat_entity.dart';
+import 'package:appflowy/plugins/ai_chat/application/chat_message_service.dart';
+import 'package:appflowy/plugins/ai_chat/application/chat_message_stream.dart';
 import 'package:appflowy/plugins/ai_chat/application/chat_user_message_bloc.dart';
 import 'package:flowy_infra/theme_extension.dart';
 import 'package:flowy_infra_ui/flowy_infra_ui.dart';
@@ -5,32 +8,62 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_chat_core/flutter_chat_core.dart';
 
+import 'user_message_bubble.dart';
+
 class ChatUserMessageWidget extends StatelessWidget {
   const ChatUserMessageWidget({
     super.key,
     required this.user,
     required this.message,
+    required this.isCurrentUser,
   });
 
   final User user;
-  final dynamic message;
+  final TextMessage message;
+  final bool isCurrentUser;
 
   @override
   Widget build(BuildContext context) {
+    final stream = message.metadata?["$QuestionStream"];
+    final messageText = stream is QuestionStream ? stream.text : message.text;
+
     return BlocProvider(
-      create: (context) => ChatUserMessageBloc(message: message)
-        ..add(const ChatUserMessageEvent.initial()),
-      child: BlocBuilder<ChatUserMessageBloc, ChatUserMessageState>(
-        builder: (context, state) {
-          return Opacity(
-            opacity: state.messageState.isFinish ? 1.0 : 0.8,
-            child: TextMessageText(
-              text: state.text,
-            ),
-          );
-        },
+      create: (context) => ChatUserMessageBloc(
+        text: messageText,
+        questionStream: stream,
+      ),
+      child: ChatUserMessageBubble(
+        message: message,
+        isCurrentUser: isCurrentUser,
+        files: _getFiles(),
+        child: BlocBuilder<ChatUserMessageBloc, ChatUserMessageState>(
+          builder: (context, state) {
+            return Opacity(
+              opacity: state.messageState.isFinish ? 1.0 : 0.8,
+              child: TextMessageText(
+                text: state.text,
+              ),
+            );
+          },
+        ),
       ),
     );
+  }
+
+  List<ChatFile> _getFiles() {
+    if (message.metadata == null) {
+      return const [];
+    }
+
+    final refSourceMetadata =
+        message.metadata?[messageRefSourceJsonStringKey] as String?;
+    if (refSourceMetadata != null) {
+      return chatFilesFromMetadataString(refSourceMetadata);
+    }
+
+    final chatFileList =
+        message.metadata![messageChatFileListKey] as List<ChatFile>?;
+    return chatFileList ?? [];
   }
 }
 
