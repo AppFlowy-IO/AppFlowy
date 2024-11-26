f337acd8105fd445918666d149e333deed1310e3
Richard Shiue <71320345+richardshiue@users.noreply.github.com>
2024-11-26 13:58:57 +0800

    fix: view editor used during initialization

--- frontend/rust-lib/flowy-database2/src/services/database/database_editor.rs
+++ frontend/rust-lib/flowy-database2/src/services/database/database_editor.rs
@@ -1981,11 +1981,12 @@ impl DatabaseViewOperation for DatabaseViewOperationImpl {
   }
 
   async fn get_cells_for_field(&self, view_id: &str, field_id: &str) -> Vec<RowCell> {
-    let editor = self.editor_by_view_id.read().await.get(view_id).cloned();
-    match editor {
-      None => vec![],
-      Some(editor) => editor.v_get_cells_for_field(field_id).await,
-    }
+    self
+      .database
+      .read()
+      .await
+      .get_cells_for_field(view_id, field_id)
+      .await
   }
 
   async fn get_cell_in_row(&self, field_id: &str, row_id: &RowId) -> Arc<RowCell> {
