9f2847a188cafe00cba8d2934a39472354220c29
Mathias Mogensen <mathias@appflowy.io>
2024-12-02 01:22:13 +0100

    test: use nameOrDefault

--- frontend/appflowy_flutter/integration_test/shared/expectation.dart
+++ frontend/appflowy_flutter/integration_test/shared/expectation.dart
@@ -6,6 +6,7 @@ import 'package:appflowy/plugins/document/presentation/banner.dart';
 import 'package:appflowy/plugins/document/presentation/editor_plugins/header/document_cover_widget.dart';
 import 'package:appflowy/plugins/document/presentation/editor_plugins/header/emoji_icon_widget.dart';
 import 'package:appflowy/workspace/application/sidebar/folder/folder_bloc.dart';
+import 'package:appflowy/workspace/application/view/view_ext.dart';
 import 'package:appflowy/workspace/presentation/home/home_stack.dart';
 import 'package:appflowy/workspace/presentation/home/menu/view/view_item.dart';
 import 'package:appflowy/workspace/presentation/notifications/widgets/notification_item.dart';
@@ -196,7 +197,7 @@ extension Expectation on WidgetTester {
         return find.byWidgetPredicate(
           (widget) =>
               widget is SingleInnerViewItem &&
-              widget.view.name == name &&
+              widget.view.nameOrDefault == name &&
               widget.view.layout == layout,
           skipOffstage: false,
         );
@@ -206,7 +207,7 @@ extension Expectation on WidgetTester {
         of: find.byWidgetPredicate(
           (widget) =>
               widget is InnerViewItem &&
-              widget.view.name == parentName &&
+              widget.view.nameOrDefault == parentName &&
               widget.view.layout == parentLayout,
           skipOffstage: false,
         ),
