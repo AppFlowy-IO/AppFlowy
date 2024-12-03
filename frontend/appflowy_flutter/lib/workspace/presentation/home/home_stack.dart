f30aa092eb49e77b545f1be85ab71f5646d2ad82
Richard Shiue <71320345+richardshiue@users.noreply.github.com>
2024-12-03 16:55:32 +0800

    feat(desktop): implement ai chat side panel

--- frontend/appflowy_flutter/lib/workspace/presentation/home/home_stack.dart
+++ frontend/appflowy_flutter/lib/workspace/presentation/home/home_stack.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
@@ -59,10 +60,6 @@ class _HomeStackState extends State<HomeStack> {
     return BlocProvider<TabsBloc>.value(
       value: getIt<TabsBloc>(),
       child: BlocBuilder<TabsBloc, TabsState>(
-        buildWhen: (prev, curr) =>
-            prev.currentIndex != curr.currentIndex &&
-            prev.currentPageManager.plugin.id !=
-                curr.currentPageManager.plugin.id,
         builder: (context, state) => Column(
           children: [
             if (UniversalPlatform.isWindows)
@@ -93,17 +90,32 @@ class _HomeStackState extends State<HomeStack> {
                 index: selectedIndex,
                 children: state.pageManagers
                     .map(
-                      (pm) => Column(
-                        children: [
-                          pm.stackTopBar(layout: widget.layout),
-                          Expanded(
-                            child: PageStack(
-                              pageManager: pm,
-                              delegate: widget.delegate,
-                              userProfile: widget.userProfile,
-                            ),
-                          ),
-                        ],
+                      (pm) => LayoutBuilder(
+                        builder: (context, constraints) {
+                          return Row(
+                            children: [
+                              Expanded(
+                                child: Column(
+                                  children: [
+                                    pm.stackTopBar(layout: widget.layout),
+                                    Expanded(
+                                      child: PageStack(
+                                        pageManager: pm,
+                                        delegate: widget.delegate,
+                                        userProfile: widget.userProfile,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              SecondaryView(
+                                pageManager: pm,
+                                adaptedPercentageWidth:
+                                    constraints.maxWidth * 3 / 7,
+                              ),
+                            ],
+                          );
+                        },
                       ),
                     )
                     .toList(),
@@ -196,6 +208,241 @@ class _PageStackState extends State<PageStack>
   bool get wantKeepAlive => true;
 }
 
+class SecondaryView extends StatefulWidget {
+  const SecondaryView({
+    super.key,
+    required this.pageManager,
+    required this.adaptedPercentageWidth,
+  });
+
+  final PageManager pageManager;
+  final double adaptedPercentageWidth;
+
+  @override
+  State<SecondaryView> createState() => _SecondaryViewState();
+}
+
+class _SecondaryViewState extends State<SecondaryView>
+    with SingleTickerProviderStateMixin, AutomaticKeepAliveClientMixin {
+  late final ValueNotifier<double> widthNotifier;
+
+  late final AnimationController animationController;
+  late Animation<double> widthAnimation;
+  late final Animation<Offset> offsetAnimation;
+
+  CurvedAnimation get curveAnimation => CurvedAnimation(
+        parent: animationController,
+        curve: Curves.easeOut,
+      );
+
+  @override
+  void initState() {
+    super.initState();
+    widget.pageManager.showSecondaryPluginNotifier
+        .addListener(onShowSecondaryChanged);
+    final width = widget.pageManager.showSecondaryPluginNotifier.value
+        ? widget.adaptedPercentageWidth
+        : 0.0;
+    widthNotifier = ValueNotifier<double>(width)
+      ..addListener(updateWidthAnimation);
+
+    animationController = AnimationController(
+      duration: const Duration(milliseconds: 300),
+      vsync: this,
+    );
+
+    widthAnimation = Tween<double>(
+      begin: 0.0,
+      end: width,
+    ).animate(curveAnimation);
+    offsetAnimation = Tween<Offset>(
+      begin: const Offset(1.0, 0.0),
+      end: Offset.zero,
+    ).animate(curveAnimation);
+  }
+
+  @override
+  void dispose() {
+    widget.pageManager.showSecondaryPluginNotifier
+        .removeListener(onShowSecondaryChanged);
+    widthNotifier.dispose();
+    super.dispose();
+  }
+
+  void onShowSecondaryChanged() async {
+    if (widget.pageManager.showSecondaryPluginNotifier.value) {
+      widthNotifier.value = widget.adaptedPercentageWidth;
+      updateWidthAnimation();
+      await animationController.forward();
+    } else {
+      updateWidthAnimation();
+      await animationController.reverse();
+      setState(() => widthNotifier.value = 0.0);
+    }
+  }
+
+  void updateWidthAnimation() {
+    widthAnimation = Tween<double>(
+      begin: 0.0,
+      end: widthNotifier.value,
+    ).animate(curveAnimation);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    return Container(
+      color: Theme.of(context).colorScheme.surface,
+      child: FocusTraversalGroup(
+        child: ValueListenableBuilder(
+          valueListenable: widthNotifier,
+          builder: (context, value, child) {
+            return AnimatedBuilder(
+              animation: Listenable.merge([
+                widthAnimation,
+                offsetAnimation,
+              ]),
+              builder: (context, child) {
+                return Container(
+                  width: widthAnimation.value,
+                  alignment: Alignment(
+                    offsetAnimation.value.dx,
+                    offsetAnimation.value.dy,
+                  ),
+                  child: OverflowBox(
+                    alignment: AlignmentDirectional.centerStart,
+                    maxWidth: value,
+                    child: SecondaryViewResizer(
+                      pageManager: widget.pageManager,
+                      notifier: widthNotifier,
+                      child: Column(
+                        children: [
+                          widget.pageManager.stackSecondaryTopBar(value),
+                          Expanded(
+                            child:
+                                widget.pageManager.stackSecondaryWidget(value),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  @override
+  bool get wantKeepAlive => true;
+}
+
+class SecondaryViewResizer extends StatefulWidget {
+  const SecondaryViewResizer({
+    super.key,
+    required this.pageManager,
+    required this.notifier,
+    required this.child,
+  });
+
+  final PageManager pageManager;
+  final ValueNotifier<double> notifier;
+  final Widget child;
+
+  @override
+  State<SecondaryViewResizer> createState() => _SecondaryViewResizerState();
+}
+
+class _SecondaryViewResizerState extends State<SecondaryViewResizer> {
+  final overlayController = OverlayPortalController();
+  final layerLink = LayerLink();
+
+  bool isHover = false;
+  bool isDragging = false;
+  Timer? showHoverTimer;
+
+  @override
+  void initState() {
+    super.initState();
+    overlayController.show();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return OverlayPortal(
+      controller: overlayController,
+      overlayChildBuilder: (context) {
+        return CompositedTransformFollower(
+          showWhenUnlinked: false,
+          link: layerLink,
+          targetAnchor: Alignment.center,
+          followerAnchor: Alignment.center,
+          child: Center(
+            child: MouseRegion(
+              cursor: SystemMouseCursors.resizeLeftRight,
+              onEnter: (_) {
+                showHoverTimer = Timer(const Duration(milliseconds: 500), () {
+                  setState(() => isHover = true);
+                });
+              },
+              onExit: (_) {
+                showHoverTimer?.cancel();
+                setState(() => isHover = false);
+              },
+              child: GestureDetector(
+                behavior: HitTestBehavior.opaque,
+                onHorizontalDragStart: (_) => isDragging = true,
+                onHorizontalDragUpdate: (details) {
+                  final newWidth = MediaQuery.sizeOf(context).width -
+                      details.globalPosition.dx;
+                  if (newWidth > 0) {
+                    widget.notifier.value = newWidth;
+                  }
+                },
+                onHorizontalDragEnd: (_) => isDragging = false,
+                child: TweenAnimationBuilder(
+                  tween: ColorTween(
+                    end: isHover || isDragging
+                        ? Theme.of(context).colorScheme.primary
+                        : Colors.transparent,
+                  ),
+                  duration: const Duration(milliseconds: 100),
+                  builder: (context, color, child) {
+                    return SizedBox(
+                      width: 11,
+                      child: Center(
+                        child: Container(
+                          color: color,
+                          width: 2,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          CompositedTransformTarget(
+            link: layerLink,
+            child: Container(
+              width: 1,
+              color: Theme.of(context).dividerColor,
+            ),
+          ),
+          Flexible(child: widget.child),
+        ],
+      ),
+    );
+  }
+}
+
 class FadingIndexedStack extends StatefulWidget {
   const FadingIndexedStack({
     super.key,
@@ -268,8 +515,9 @@ class PageNotifier extends ChangeNotifier {
   /// This is the only place where the plugin is set.
   /// No need compare the old plugin with the new plugin. Just set it.
   void setPlugin(Plugin newPlugin, bool setLatest) {
-    _plugin.dispose();
-    newPlugin.init();
+    if (newPlugin.id != plugin.id) {
+      _plugin.dispose();
+    }
 
     // Set the plugin view as the latest view.
     if (setLatest) {
@@ -288,19 +536,32 @@ class PageManager {
   PageManager();
 
   final PageNotifier _notifier = PageNotifier();
+  final PageNotifier _secondaryNotifier = PageNotifier();
 
   PageNotifier get notifier => _notifier;
+  PageNotifier get secondaryNotifier => _secondaryNotifier;
 
   bool isPinned = false;
 
-  Widget title() {
-    return _notifier.plugin.widgetBuilder.leftBarItem;
-  }
+  final showSecondaryPluginNotifier = ValueNotifier(false);
 
   Plugin get plugin => _notifier.plugin;
 
-  void setPlugin(Plugin newPlugin, bool setLatest) {
+  void setPlugin(Plugin newPlugin, bool setLatest, [bool init = true]) {
+    if (init) {
+      newPlugin.init();
+    }
     _notifier.setPlugin(newPlugin, setLatest);
+  }
+
+  void setSecondaryPlugin(Plugin newPlugin) {
+    newPlugin.init();
+    _secondaryNotifier.setPlugin(newPlugin, false);
+    showSecondaryPluginNotifier.value = true;
+  }
+
+  void hideSecondaryPlugin() {
+    showSecondaryPluginNotifier.value = false;
   }
 
   Widget stackTopBar({required HomeLayout layout}) {
@@ -352,8 +613,59 @@ class PageManager {
     );
   }
 
+  Widget stackSecondaryWidget(double width) {
+    return ValueListenableBuilder(
+      valueListenable: showSecondaryPluginNotifier,
+      builder: (context, value, child) {
+        if (width == 0.0) {
+          return const SizedBox.shrink();
+        }
+
+        return child!;
+      },
+      child: ChangeNotifierProvider.value(
+        value: _secondaryNotifier,
+        child: Selector<PageNotifier, PluginWidgetBuilder>(
+          selector: (context, notifier) => notifier.plugin.widgetBuilder,
+          builder: (_, widgetBuilder, __) {
+            return widgetBuilder.buildWidget(
+              context: PluginContext(),
+              shrinkWrap: false,
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget stackSecondaryTopBar(double width) {
+    return ValueListenableBuilder(
+      valueListenable: showSecondaryPluginNotifier,
+      builder: (context, value, child) {
+        if (width == 0.0) {
+          return const SizedBox.shrink();
+        }
+
+        return child!;
+      },
+      child: ChangeNotifierProvider.value(
+        value: _secondaryNotifier,
+        child: Selector<PageNotifier, PluginWidgetBuilder>(
+          selector: (context, notifier) => notifier.plugin.widgetBuilder,
+          builder: (_, widgetBuilder, __) {
+            return const MoveWindowDetector(
+              child: HomeSecondaryTopBar(),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
   void dispose() {
     _notifier.dispose();
+    _secondaryNotifier.dispose();
+    showSecondaryPluginNotifier.dispose();
   }
 }
 
@@ -403,4 +715,64 @@ class _HomeTopBarState extends State<HomeTopBar>
 
   @override
   bool get wantKeepAlive => true;
+}
+
+class HomeSecondaryTopBar extends StatelessWidget {
+  const HomeSecondaryTopBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.surface,
+      ),
+      height: HomeSizes.topBarHeight + HomeInsets.topBarTitleVerticalPadding,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: HomeInsets.topBarTitleHorizontalPadding,
+          vertical: HomeInsets.topBarTitleVerticalPadding,
+        ),
+        child: Row(
+          children: [
+            FlowyIconButton(
+              width: 24,
+              radius: const BorderRadius.all(Radius.circular(8.0)),
+              icon: const FlowySvg(
+                FlowySvgs.show_menu_s,
+                size: Size.square(16),
+              ),
+              onPressed: () {
+                getIt<TabsBloc>().add(const TabsEvent.closeSecondaryPlugin());
+              },
+            ),
+            const HSpace(8.0),
+            FlowyIconButton(
+              width: 24,
+              radius: const BorderRadius.all(Radius.circular(8.0)),
+              icon: const FlowySvg(
+                FlowySvgs.full_view_s,
+                size: Size.square(16),
+              ),
+              onPressed: () {
+                getIt<TabsBloc>().add(const TabsEvent.expandSecondaryPlugin());
+              },
+            ),
+            Expanded(
+              child: Align(
+                alignment: AlignmentDirectional.centerEnd,
+                child: ChangeNotifierProvider.value(
+                  value: Provider.of<PageNotifier>(context, listen: false),
+                  child: Consumer(
+                    builder: (_, PageNotifier notifier, __) =>
+                        notifier.plugin.widgetBuilder.rightBarItem ??
+                        const SizedBox.shrink(),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
