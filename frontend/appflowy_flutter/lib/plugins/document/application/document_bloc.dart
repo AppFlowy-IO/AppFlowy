67eec6f41b61d0d011bec099694e07f034a6af9e
Lucas.Xu <lucas.xu@appflowy.io>
2024-12-10 14:14:55 +0800

    fix: integration test

--- frontend/appflowy_flutter/lib/plugins/document/application/document_bloc.dart
+++ frontend/appflowy_flutter/lib/plugins/document/application/document_bloc.dart
@@ -38,7 +38,7 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'document_bloc.freezed.dart';
 
-bool enableDocumentInternalLog = true;
+bool enableDocumentInternalLog = false;
 
 class DocumentBloc extends Bloc<DocumentEvent, DocumentState> {
   DocumentBloc({
