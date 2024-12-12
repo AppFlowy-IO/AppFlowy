e4385adfa9aad6d44698aa850b35254c38d9b79f
Mathias Mogensen <42929161+Xazin@users.noreply.github.com>
2024-12-12 02:21:23 +0100

    fix: rename untitled view (#6789)
    * fix: rename untitled view
    
    * test: add cloud test
    
    * chore: clean up code
    
    * test: use nameOrDefault
    
    * test: fix failing test
    
    * test: fix wrong assumption

--- frontend/appflowy_flutter/integration_test/desktop/cloud/cloud_runner.dart
+++ frontend/appflowy_flutter/integration_test/desktop/cloud/cloud_runner.dart
@@ -3,6 +3,8 @@ import 'data_migration/data_migration_test_runner.dart'
 import 'document/document_test_runner.dart' as document_test_runner;
 import 'set_env.dart' as preset_af_cloud_env_test;
 import 'sidebar/sidebar_move_page_test.dart' as sidebar_move_page_test;
+import 'sidebar/sidebar_rename_untitled_test.dart'
+    as sidebar_rename_untitled_test;
 import 'uncategorized/uncategorized_test_runner.dart'
     as uncategorized_test_runner;
 import 'workspace/workspace_test_runner.dart' as workspace_test_runner;
@@ -23,4 +25,5 @@ Future<void> main() async {
 
   // sidebar
   sidebar_move_page_test.main();
+  sidebar_rename_untitled_test.main();
 }
