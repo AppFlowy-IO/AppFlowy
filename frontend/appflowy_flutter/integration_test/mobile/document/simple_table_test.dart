a380684e0f4851511d45c4c988573fe695392e7e
Lucas.Xu <lucas.xu@appflowy.io>
2024-12-23 17:47:27 +0800

    test: delete column / delete row

--- frontend/appflowy_flutter/integration_test/mobile/document/simple_table_test.dart
+++ frontend/appflowy_flutter/integration_test/mobile/document/simple_table_test.dart
@@ -19,9 +19,11 @@ void main() {
 3. insert a row below the table
 4. insert a column left to the table
 5. insert a column right to the table
+6. delete the first row
+7. delete the first column
 ''', (tester) async {
       await tester.launchInAnonymousMode();
-      await tester.createNewDocumentOnMobile('toggle heading blocks');
+      await tester.createNewDocumentOnMobile('simple table');
 
       final editorState = tester.editor.getCurrentEditorState();
       // focus on the editor
@@ -33,78 +35,143 @@ void main() {
       );
       await tester.pumpAndSettle();
 
-      // open the plus menu and select the toggle heading block
-      await tester.openPlusMenuAndClickButton(
-        LocaleKeys.document_slashMenu_name_table.tr(),
-      );
-
-      // check the block is inserted
-      final table = editorState.getNodeAtPath([0])!;
-      expect(table.type, equals(SimpleTableBlockKeys.type));
-      expect(table.rowLength, equals(2));
-      expect(table.columnLength, equals(2));
-
-      // focus on the first cell
       final firstParagraphPath = [0, 0, 0, 0];
-      final selection = editorState.selection!;
-      expect(selection.isCollapsed, isTrue);
-      expect(selection.start.path, equals(firstParagraphPath));
 
-      // click the column menu button
-      await tester.clickColumnMenuButton(0);
+      // open the plus menu and select the table block
+      {
+        await tester.openPlusMenuAndClickButton(
+          LocaleKeys.document_slashMenu_name_table.tr(),
+        );
 
-      // insert left, insert right
-      await tester.tapButton(
-        find.findTextInFlowyText(
-          LocaleKeys.document_plugins_simpleTable_moreActions_insertLeft.tr(),
-        ),
-      );
-      await tester.tapButton(
-        find.findTextInFlowyText(
-          LocaleKeys.document_plugins_simpleTable_moreActions_insertRight.tr(),
-        ),
-      );
+        // check the block is inserted
+        final table = editorState.getNodeAtPath([0])!;
+        expect(table.type, equals(SimpleTableBlockKeys.type));
+        expect(table.rowLength, equals(2));
+        expect(table.columnLength, equals(2));
 
-      // click empty area to cancel the selection
-      await tester.tapAt(Offset.zero);
-      expect(editorState.selection, isNull);
-      await tester.pumpUntilNotFound(find.byType(SimpleTableCellBottomSheet));
+        // focus on the first cell
 
-      // check the table is updated
-      editorState.getNodeAtPath([0])!;
-      expect(table.rowLength, equals(2));
-      expect(table.columnLength, equals(4));
+        final selection = editorState.selection!;
+        expect(selection.isCollapsed, isTrue);
+        expect(selection.start.path, equals(firstParagraphPath));
+      }
 
-      // focus on the first cell
-      unawaited(
-        editorState.updateSelectionWithReason(
-          Selection.collapsed(Position(path: firstParagraphPath)),
-          reason: SelectionUpdateReason.uiEvent,
-        ),
-      );
-      await tester.pumpAndSettle();
+      // insert left and insert right
+      {
+        // click the column menu button
+        await tester.clickColumnMenuButton(0);
 
-      // click the row menu button
-      await tester.clickRowMenuButton(0);
+        // insert left, insert right
+        await tester.tapButton(
+          find.findTextInFlowyText(
+            LocaleKeys.document_plugins_simpleTable_moreActions_insertLeft.tr(),
+          ),
+        );
+        await tester.tapButton(
+          find.findTextInFlowyText(
+            LocaleKeys.document_plugins_simpleTable_moreActions_insertRight
+                .tr(),
+          ),
+        );
 
-      await tester.tapButton(
-        find.findTextInFlowyText(
-          LocaleKeys.document_plugins_simpleTable_moreActions_insertAbove.tr(),
-        ),
-      );
-      await tester.tapButton(
-        find.findTextInFlowyText(
-          LocaleKeys.document_plugins_simpleTable_moreActions_insertBelow.tr(),
-        ),
-      );
-      // click empty area to cancel the selection
-      await tester.tapAt(Offset.zero);
-      expect(editorState.selection, isNull);
-      await tester.pumpUntilNotFound(find.byType(SimpleTableCellBottomSheet));
+        // click empty area to cancel the selection
+        await tester.tapAt(Offset.zero);
+        expect(editorState.selection, isNull);
+        await tester.pumpUntilNotFound(find.byType(SimpleTableCellBottomSheet));
 
-      // check the table is updated
-      expect(table.rowLength, equals(4));
-      expect(table.columnLength, equals(4));
+        // check the table is updated
+        final table = editorState.getNodeAtPath([0])!;
+        expect(table.type, equals(SimpleTableBlockKeys.type));
+        expect(table.rowLength, equals(2));
+        expect(table.columnLength, equals(4));
+      }
+
+      // insert above and insert below
+      {
+        // focus on the first cell
+        unawaited(
+          editorState.updateSelectionWithReason(
+            Selection.collapsed(Position(path: firstParagraphPath)),
+            reason: SelectionUpdateReason.uiEvent,
+          ),
+        );
+        await tester.pumpAndSettle();
+
+        // click the row menu button
+        await tester.clickRowMenuButton(0);
+
+        await tester.tapButton(
+          find.findTextInFlowyText(
+            LocaleKeys.document_plugins_simpleTable_moreActions_insertAbove
+                .tr(),
+          ),
+        );
+        await tester.tapButton(
+          find.findTextInFlowyText(
+            LocaleKeys.document_plugins_simpleTable_moreActions_insertBelow
+                .tr(),
+          ),
+        );
+        // click empty area to cancel the selection
+        await tester.tapAt(Offset.zero);
+        expect(editorState.selection, isNull);
+        await tester.pumpUntilNotFound(find.byType(SimpleTableCellBottomSheet));
+
+        // check the table is updated
+        final table = editorState.getNodeAtPath([0])!;
+        expect(table.rowLength, equals(4));
+        expect(table.columnLength, equals(4));
+      }
+
+      // delete the first row
+      {
+        // focus on the first cell
+        unawaited(
+          editorState.updateSelectionWithReason(
+            Selection.collapsed(Position(path: firstParagraphPath)),
+            reason: SelectionUpdateReason.uiEvent,
+          ),
+        );
+        await tester.pumpAndSettle();
+
+        // delete the first row
+        await tester.clickRowMenuButton(0);
+        await tester.tapButton(
+          find.findTextInFlowyText(
+            LocaleKeys.document_plugins_simpleTable_moreActions_delete.tr(),
+          ),
+        );
+        await tester.pumpUntilNotFound(find.byType(SimpleTableCellBottomSheet));
+
+        // check the table is updated
+        final table = editorState.getNodeAtPath([0])!;
+        expect(table.rowLength, equals(3));
+        expect(table.columnLength, equals(4));
+      }
+
+      // delete the first column
+      {
+        unawaited(
+          editorState.updateSelectionWithReason(
+            Selection.collapsed(Position(path: firstParagraphPath)),
+            reason: SelectionUpdateReason.uiEvent,
+          ),
+        );
+        await tester.pumpAndSettle();
+
+        await tester.clickColumnMenuButton(0);
+        await tester.tapButton(
+          find.findTextInFlowyText(
+            LocaleKeys.document_plugins_simpleTable_moreActions_delete.tr(),
+          ),
+        );
+        await tester.pumpUntilNotFound(find.byType(SimpleTableCellBottomSheet));
+
+        // check the table is updated
+        final table = editorState.getNodeAtPath([0])!;
+        expect(table.rowLength, equals(3));
+        expect(table.columnLength, equals(3));
+      }
     });
   });
 }
