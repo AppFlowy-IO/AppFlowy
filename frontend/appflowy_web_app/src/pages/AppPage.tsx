256d0159672b53d12e5af08ef0fc98b5fc9f127b
Kilu.He <108015703+qinluhe@users.noreply.github.com>
2024-12-30 15:46:39 +0800

    fix: add tip for database page (#7090)

--- frontend/appflowy_web_app/src/pages/AppPage.tsx
+++ frontend/appflowy_web_app/src/pages/AppPage.tsx
@@ -1,25 +1,24 @@
 import { UIVariant, ViewComponentProps, ViewLayout, ViewMetaProps, YDoc } from '@/application/types';
 import Help from '@/components/_shared/help/Help';
+import { notify } from '@/components/_shared/notify';
 import { findView } from '@/components/_shared/outline/utils';
-
-import {
-  AppContext,
-  useAppHandlers,
-  useAppOutline,
-  useAppViewId,
-} from '@/components/app/app.hooks';
+import { ReactComponent as TipIcon } from '@/assets/warning.svg';
+import { AppContext, useAppHandlers, useAppOutline, useAppViewId } from '@/components/app/app.hooks';
 import DatabaseView from '@/components/app/DatabaseView';
 import { Document } from '@/components/document';
 import RecordNotFound from '@/components/error/RecordNotFound';
 import { getPlatform } from '@/utils/platform';
+import { desktopDownloadLink, openAppFlowySchema } from '@/utils/url';
+import { Button, Checkbox, FormControlLabel } from '@mui/material';
 import React, { lazy, memo, Suspense, useCallback, useContext, useEffect, useMemo } from 'react';
 
 const ViewHelmet = lazy(() => import('@/components/_shared/helmet/ViewHelmet'));
 
-function AppPage() {
+function AppPage () {
   const viewId = useAppViewId();
   const outline = useAppOutline();
   const ref = React.useRef<HTMLDivElement>(null);
+
   const {
     toView,
     loadViewMeta,
@@ -135,20 +134,69 @@ function AppPage() {
     localStorage.setItem('last_view_id', viewId);
   }, [View, viewId, doc]);
 
+  const layout = view?.layout;
+
+  useEffect(() => {
+    if (layout !== undefined && layout !== ViewLayout.Document && !localStorage.getItem('open_edit_tip')) {
+      notify.clear();
+      notify.info({
+        autoHideDuration: null,
+        type: 'info',
+        title: 'Edit in app',
+        message: <div className={'w-full gap-2 flex flex-col items-start'}>
+          <div>{`Editing databases is supported in AppFlowy's desktop and mobile apps`}
+          </div>
+          <div className={'text-sm flex items-center gap-2 text-text-caption'}>
+            <TipIcon className={'h-4 w-4 text-function-warning'} />
+            Don't have AppFlowy? <a
+            className={'text-fill-default hover:underline'}
+            href={desktopDownloadLink}
+          >Download</a></div>
+          <div className={'flex items-center max-sm:my-4 max-sm:flex-col mt-2 w-full justify-between'}>
+            <FormControlLabel
+              className={' max-sm:w-full'}
+              value="end"
+              onChange={(_e, value) => {
+                if (value) {
+                  localStorage.setItem('open_edit_tip', 'true');
+                } else {
+                  localStorage.removeItem('open_edit_tip');
+                }
+              }}
+              control={<Checkbox />}
+              label="Don't remind me again"
+            />
+            <Button
+              color={'primary'}
+              className={'max-sm:w-full max-sm:py-4 max-sm:text-base'}
+              onClick={() => window.open(openAppFlowySchema, '_current')}
+              variant={'contained'}
+            >
+              Open in AppFlowy
+            </Button>
+          </div>
+        </div>,
+        showActions: false,
+      });
+    }
+  }, [layout]);
+
   if (!viewId) return null;
   return (
-    <div ref={ref} className={'relative w-full h-full'}>
+    <div
+      ref={ref}
+      className={'relative w-full h-full'}
+    >
       {helmet}
 
       {notFound ? (
-        <RecordNotFound/>
+        <RecordNotFound />
       ) : (
         <div className={'w-full h-full'}>
           {viewDom}
         </div>
       )}
-      {view && doc && <Help/>}
-
+      {view && doc && <Help />}
     </div>
   );
 }
