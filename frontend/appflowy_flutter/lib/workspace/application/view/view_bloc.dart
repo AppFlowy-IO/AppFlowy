752b0e5f420b8097c219ad891351f39aad70719b
Lucas.Xu <lucas.xu@appflowy.io>
2024-12-31 09:50:44 +0800

    fix: flutter analyze

--- frontend/appflowy_flutter/lib/workspace/application/view/view_bloc.dart
+++ frontend/appflowy_flutter/lib/workspace/application/view/view_bloc.dart
@@ -16,6 +16,7 @@ import 'package:appflowy_backend/protobuf/flowy-folder/protobuf.dart';
 import 'package:appflowy_backend/protobuf/flowy-folder/view.pb.dart';
 import 'package:appflowy_result/appflowy_result.dart';
 import 'package:collection/collection.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:protobuf/protobuf.dart';
