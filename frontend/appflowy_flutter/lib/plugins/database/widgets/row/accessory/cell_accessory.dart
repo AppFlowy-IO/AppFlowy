25fa9e22803642b5860802bd39072aeef553443c
Richard Shiue <71320345+richardshiue@users.noreply.github.com>
2025-01-02 14:42:07 +0800

    fix: downgrade percent indicator

--- frontend/appflowy_flutter/lib/plugins/database/widgets/row/accessory/cell_accessory.dart
+++ frontend/appflowy_flutter/lib/plugins/database/widgets/row/accessory/cell_accessory.dart
@@ -1,3 +1,4 @@
+import 'package:flowy_infra_ui/flowy_infra_ui.dart';
 import 'package:flutter/material.dart';
 
 import 'package:appflowy/generated/flowy_svgs.g.dart';
@@ -188,6 +189,9 @@ class CellAccessoryContainer extends StatelessWidget {
       );
     }).toList();
 
-    return Wrap(spacing: 6, children: children);
+    return SeparatedRow(
+      separatorBuilder: () => const HSpace(6),
+      children: children,
+    );
   }
 }
