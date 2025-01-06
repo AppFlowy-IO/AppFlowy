2c503aa15e32f32154069c68f52ab73e3b78fd62
Lucas.Xu <lucas.xu@appflowy.io>
2025-01-06 11:41:35 +0800

    fix: context menu should not auto show when collaborating

--- frontend/appflowy_flutter/lib/plugins/document/application/document_bloc.dart
+++ frontend/appflowy_flutter/lib/plugins/document/application/document_bloc.dart
@@ -38,7 +38,7 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'document_bloc.freezed.dart';
 
-bool enableDocumentInternalLog = false;
+bool enableDocumentInternalLog = true;
 
 final Map<String, DocumentBloc> _documentBlocMap = {};
 
