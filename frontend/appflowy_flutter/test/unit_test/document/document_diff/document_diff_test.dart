fb7928f1b4616f39c1bfd0263ae351b388543fff
Lucas.Xu <lucas.xu@appflowy.io>
2025-01-15 20:32:44 +0800

    test: multiple delete and update diff

--- frontend/appflowy_flutter/test/unit_test/document/document_diff/document_diff_test.dart
+++ frontend/appflowy_flutter/test/unit_test/document/document_diff/document_diff_test.dart
@@ -133,5 +133,198 @@ void main() {
 
       await applyOperationAndVerifyDocument(previous, next, operations);
     });
+
+    test('insert multiple texts diff', () async {
+      final node11 = createNodeWithId(
+        id: '1',
+        text: 'Hello AppFlowy - First line',
+      );
+      final node15 = createNodeWithId(
+        id: '5',
+        text: 'Hello AppFlowy - Fifth line',
+      );
+      final node21 = createNodeWithId(
+        id: '1',
+        text: 'Hello AppFlowy - First line',
+      );
+      final node22 = createNodeWithId(
+        id: '2',
+        text: 'Hello AppFlowy - Second line',
+      );
+      final node23 = createNodeWithId(
+        id: '3',
+        text: 'Hello AppFlowy - Third line',
+      );
+      final node24 = createNodeWithId(
+        id: '4',
+        text: 'Hello AppFlowy - Fourth line',
+      );
+      final node25 = createNodeWithId(
+        id: '5',
+        text: 'Hello AppFlowy - Fifth line',
+      );
+
+      final previous = Document.blank()
+        ..insert(
+          [0],
+          [
+            node11,
+            node15,
+          ],
+        );
+      final next = Document.blank()
+        ..insert(
+          [0],
+          [
+            node21,
+            node22,
+            node23,
+            node24,
+            node25,
+          ],
+        );
+
+      final operations = diff.diffDocument(previous, next);
+
+      expect(operations.length, 1);
+
+      final op = operations[0] as InsertOperation;
+      expect(op.path, [1]);
+      expect(op.nodes, [node22, node23, node24]);
+
+      await applyOperationAndVerifyDocument(previous, next, operations);
+    });
+
+    test('delete multiple texts diff', () async {
+      final node11 = createNodeWithId(
+        id: '1',
+        text: 'Hello AppFlowy - First line',
+      );
+      final node12 = createNodeWithId(
+        id: '2',
+        text: 'Hello AppFlowy - Second line',
+      );
+      final node13 = createNodeWithId(
+        id: '3',
+        text: 'Hello AppFlowy - Third line',
+      );
+      final node14 = createNodeWithId(
+        id: '4',
+        text: 'Hello AppFlowy - Fourth line',
+      );
+      final node15 = createNodeWithId(
+        id: '5',
+        text: 'Hello AppFlowy - Fifth line',
+      );
+
+      final node21 = createNodeWithId(
+        id: '1',
+        text: 'Hello AppFlowy - First line',
+      );
+      final node25 = createNodeWithId(
+        id: '5',
+        text: 'Hello AppFlowy - Fifth line',
+      );
+
+      final previous = Document.blank()
+        ..insert(
+          [0],
+          [
+            node11,
+            node12,
+            node13,
+            node14,
+            node15,
+          ],
+        );
+      final next = Document.blank()
+        ..insert(
+          [0],
+          [
+            node21,
+            node25,
+          ],
+        );
+
+      final operations = diff.diffDocument(previous, next);
+
+      expect(operations.length, 1);
+
+      final op = operations[0] as DeleteOperation;
+      expect(op.path, [1]);
+      expect(op.nodes, [node12, node13, node14]);
+
+      await applyOperationAndVerifyDocument(previous, next, operations);
+    });
+
+    test('multiple delete and update diff', () async {
+      final node11 = createNodeWithId(
+        id: '1',
+        text: 'Hello AppFlowy - First line',
+      );
+      final node12 = createNodeWithId(
+        id: '2',
+        text: 'Hello AppFlowy - Second line',
+      );
+      final node13 = createNodeWithId(
+        id: '3',
+        text: 'Hello AppFlowy - Third line',
+      );
+      final node14 = createNodeWithId(
+        id: '4',
+        text: 'Hello AppFlowy - Fourth line',
+      );
+      final node15 = createNodeWithId(
+        id: '5',
+        text: 'Hello AppFlowy - Fifth line',
+      );
+
+      final node21 = createNodeWithId(
+        id: '1',
+        text: 'Hello AppFlowy - First line',
+      );
+      final node22 = createNodeWithId(
+        id: '2',
+        text: '',
+      );
+      final node25 = createNodeWithId(
+        id: '5',
+        text: 'Hello AppFlowy - Fifth line',
+      );
+
+      final previous = Document.blank()
+        ..insert(
+          [0],
+          [
+            node11,
+            node12,
+            node13,
+            node14,
+            node15,
+          ],
+        );
+      final next = Document.blank()
+        ..insert(
+          [0],
+          [
+            node21,
+            node22,
+            node25,
+          ],
+        );
+
+      final operations = diff.diffDocument(previous, next);
+
+      expect(operations.length, 2);
+      final op1 = operations[0] as UpdateOperation;
+      expect(op1.path, [1]);
+      expect(op1.attributes, node22.attributes);
+
+      final op2 = operations[1] as DeleteOperation;
+      expect(op2.path, [2]);
+      expect(op2.nodes, [node13, node14]);
+
+      await applyOperationAndVerifyDocument(previous, next, operations);
+    });
   });
 }
