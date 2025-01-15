163f0a1893e8f7bef115dd3c45931d6b85d5d445
Lucas.Xu <lucas.xu@appflowy.io>
2025-01-15 19:33:25 +0800

    test: delete and insert text diff with different id

--- frontend/appflowy_flutter/test/unit_test/document/document_diff/document_diff_test.dart
+++ frontend/appflowy_flutter/test/unit_test/document/document_diff/document_diff_test.dart
@@ -13,6 +13,8 @@ void main() {
       Log.shared.disableLog = false;
     });
 
+    final diff = DocumentDiff();
+
     Node createNodeWithId({required String id, required String text}) {
       return Node(
         id: id,
@@ -23,6 +25,21 @@ void main() {
       );
     }
 
+    Future<void> applyOperationAndVerifyDocument(
+      Document before,
+      Document after,
+      List<Operation> operations,
+    ) async {
+      final expected = after.toJson();
+      final editorState = EditorState(document: before);
+      final transaction = editorState.transaction;
+      for (final operation in operations) {
+        transaction.add(operation);
+      }
+      await editorState.apply(transaction);
+      expect(editorState.document.toJson(), expected);
+    }
+
     test('no diff when the document is the same', () async {
       // create two nodes with the same id and texts
       final node1 = createNodeWithId(id: '1', text: 'Hello AppFlowy');
@@ -30,28 +47,41 @@ void main() {
 
       final previous = Document.blank()..insert([0], [node1]);
       final next = Document.blank()..insert([0], [node2]);
-      final diff = DocumentDiff();
       final operations = diff.diffDocument(previous, next);
 
       expect(operations, isEmpty);
+
+      await applyOperationAndVerifyDocument(previous, next, operations);
     });
 
-    test('update text diff with the same id', () {
+    test('update text diff with the same id', () async {
       final node1 = createNodeWithId(id: '1', text: 'Hello AppFlowy');
       final node2 = createNodeWithId(id: '1', text: 'Hello AppFlowy 2');
 
       final previous = Document.blank()..insert([0], [node1]);
       final next = Document.blank()..insert([0], [node2]);
-      final diff = DocumentDiff();
       final operations = diff.diffDocument(previous, next);
 
       expect(operations.length, 1);
+      expect(operations[0], isA<UpdateOperation>());
 
-      final op = operations[0] as UpdateOperation;
+      await applyOperationAndVerifyDocument(previous, next, operations);
+    });
 
-      expect(op.path, [0]);
-      expect(op.attributes, node2.attributes);
-      expect(op.oldAttributes, node1.attributes);
+    test('delete and insert text diff with different id', () async {
+      final node1 = createNodeWithId(id: '1', text: 'Hello AppFlowy');
+      final node2 = createNodeWithId(id: '2', text: 'Hello AppFlowy 2');
+
+      final previous = Document.blank()..insert([0], [node1]);
+      final next = Document.blank()..insert([0], [node2]);
+
+      final operations = diff.diffDocument(previous, next);
+
+      expect(operations.length, 2);
+      expect(operations[0], isA<InsertOperation>());
+      expect(operations[1], isA<DeleteOperation>());
+
+      await applyOperationAndVerifyDocument(previous, next, operations);
     });
   });
 }
