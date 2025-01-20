953aa8ede68d4cde70d43cab8462f86c602d8744
LucasXu0 <lucas.xu@appflowy.io>
2025-01-20 17:49:41 +0800

    test: add google docs / googles sheets table test

--- frontend/appflowy_flutter/test/unit_test/document/html/paste_from_html_test.dart
+++ frontend/appflowy_flutter/test/unit_test/document/html/paste_from_html_test.dart
@@ -3,36 +3,11 @@ import 'package:appflowy/plugins/document/presentation/editor_plugins/simple_tab
 import 'package:appflowy_editor/appflowy_editor.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import '_html_samples.dart';
+
 void main() {
   group('paste from html:', () {
-    test('sample 1 - paste table from Notion', () {
-      // | Month | Savings |
-      // | -------- | ------- |
-      // | January | $250 |
-      // | February | $80 |
-      // | March | $420 |
-      const html = '''<meta charset="utf-8" />
-<table id="18196b61-6923-80d7-a184-fbc0352dabc3" class="simple-table">
-    <tbody>
-        <tr id="18196b61-6923-80a7-b70a-cbae038e1472">
-            <td id="Wi`b" class="">Month</td>
-            <td id="|EyR" class="">Savings</td>
-        </tr>
-        <tr id="18196b61-6923-804a-914e-e45f6086a714">
-            <td id="Wi`b" class="">January</td>
-            <td id="|EyR" class="">\$250</td>
-        </tr>
-        <tr id="18196b61-6923-80b1-bef5-e15e1d302dfd">
-            <td id="Wi`b" class="">February</td>
-            <td id="|EyR" class="">\$80</td>
-        </tr>
-        <tr id="18196b61-6923-8079-aefa-d96c17230695">
-            <td id="Wi`b" class="">March</td>
-            <td id="|EyR" class="">\$420</td>
-        </tr>
-    </tbody>
-</table>
-''';
+    void checkTable(String html) {
       final nodes = EditorState.blank().convertHtmlToNodes(html);
       expect(nodes.length, 1);
       final table = nodes.first;
@@ -45,6 +20,18 @@ void main() {
       expect(table.getCellText(2, 1), '\$80');
       expect(table.getCellText(3, 0), 'March');
       expect(table.getCellText(3, 1), '\$420');
+    }
+
+    test('sample 1 - paste table from Notion', () {
+      checkTable(tableFromNotion);
+    });
+
+    test('sample 2 - paste table from Google Docs', () {
+      checkTable(tableFromGoogleDocs);
+    });
+
+    test('sample 3 - paste table from Google Sheets', () {
+      checkTable(tableFromGoogleSheets);
     });
   });
 }
