2fcca75a7ab65db72be68a32815115ca40e0c0c9
Lucas.Xu <lucas.xu@appflowy.io>
2025-01-22 18:14:11 +0800

    fix: ignore the delete shortcut if the next node is a simple table node

--- frontend/appflowy_flutter/lib/plugins/document/presentation/editor_plugins/shortcuts/command_shortcuts.dart
+++ frontend/appflowy_flutter/lib/plugins/document/presentation/editor_plugins/shortcuts/command_shortcuts.dart
@@ -1,6 +1,7 @@
 import 'package:appflowy/generated/locale_keys.g.dart';
 import 'package:appflowy/plugins/document/presentation/editor_plugins/align_toolbar_item/custom_text_align_command.dart';
 import 'package:appflowy/plugins/document/presentation/editor_plugins/plugins.dart';
+import 'package:appflowy/plugins/document/presentation/editor_plugins/shortcuts/custom_delete_command.dart';
 import 'package:appflowy/plugins/document/presentation/editor_plugins/undo_redo/custom_undo_redo_commands.dart';
 import 'package:appflowy/workspace/presentation/settings/widgets/emoji_picker/emoji_picker.dart';
 import 'package:appflowy_editor/appflowy_editor.dart';
@@ -37,6 +38,8 @@ List<CommandShortcutEvent> commandShortcutEvents = [
 
   ...customTextAlignCommands,
 
+  customDeleteCommand,
+
   // remove standard shortcuts for copy, cut, paste, todo
   ...standardCommandShortcutEvents
     ..removeWhere(
@@ -50,6 +53,7 @@ List<CommandShortcutEvent> commandShortcutEvents = [
         redoCommand,
         exitEditingCommand,
         ...tableCommands,
+        deleteCommand,
       ].contains(shortcut),
     ),
 
