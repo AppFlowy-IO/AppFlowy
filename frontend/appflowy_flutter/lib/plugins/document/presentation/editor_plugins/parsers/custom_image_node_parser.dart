df6d6465305e6b0277c096d2010d671a11e55a5c
Morn <agedchen@gmail.com>
2025-02-06 19:59:47 +0800

    fix: support exporting to markdown with multiple images

--- frontend/appflowy_flutter/lib/plugins/document/presentation/editor_plugins/parsers/custom_image_node_parser.dart
+++ frontend/appflowy_flutter/lib/plugins/document/presentation/editor_plugins/parsers/custom_image_node_parser.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
+import 'package:appflowy/plugins/document/presentation/editor_plugins/image/multi_image_block_component/multi_image_block_component.dart';
 import 'package:appflowy_editor/appflowy_editor.dart';
+import 'package:path/path.dart' as p;
 
 import '../image/custom_image_block_component/custom_image_block_component.dart';
 
@@ -14,5 +18,57 @@ class CustomImageNodeParser extends NodeParser {
     final url = node.attributes[CustomImageBlockKeys.url];
     assert(url != null);
     return '![]($url)\n';
+  }
+}
+
+class CustomImageNodeFileParser extends NodeParser {
+  const CustomImageNodeFileParser(this.files, this.dirPath);
+
+  final List<Future<File>> files;
+  final String dirPath;
+
+  @override
+  String get id => ImageBlockKeys.type;
+
+  @override
+  String transform(Node node, DocumentMarkdownEncoder? encoder) {
+    assert(node.children.isEmpty);
+    final url = node.attributes[CustomImageBlockKeys.url];
+    final hasFile = File(url).existsSync();
+    if (hasFile) {
+      files.add(Future.value(File(url)));
+      return '![](${p.join(dirPath, p.basename(url))})\n';
+    }
+    assert(url != null);
+    return '![]($url)\n';
+  }
+}
+
+class CustomMultiImageNodeFileParser extends NodeParser {
+  const CustomMultiImageNodeFileParser(this.files, this.dirPath);
+
+  final List<Future<File>> files;
+  final String dirPath;
+
+  @override
+  String get id => MultiImageBlockKeys.type;
+
+  @override
+  String transform(Node node, DocumentMarkdownEncoder? encoder) {
+    assert(node.children.isEmpty);
+    final images = node.attributes[MultiImageBlockKeys.images] as List;
+    final List<String> markdownImages = [];
+    for (final image in images) {
+      final String url = image['url'] ?? '';
+      if (url.isEmpty) continue;
+      final hasFile = File(url).existsSync();
+      if (hasFile) {
+        files.add(Future.value(File(url)));
+        markdownImages.add('![](${p.join(dirPath, p.basename(url))})');
+      } else {
+        markdownImages.add('![]($url})');
+      }
+    }
+    return markdownImages.join('\n\n');
   }
 }
--- frontend/appflowy_flutter/lib/shared/markdown_to_document.dart
+++ frontend/appflowy_flutter/lib/shared/markdown_to_document.dart
@@ -1,5 +1,11 @@
+import 'dart:io';
+
 import 'package:appflowy/plugins/document/presentation/editor_plugins/plugins.dart';
+import 'package:appflowy_backend/log.dart';
 import 'package:appflowy_editor/appflowy_editor.dart';
+import 'package:archive/archive.dart';
+import 'package:path/path.dart' as p;
+import 'package:share_plus/share_plus.dart';
 
 Document customMarkdownToDocument(
   String markdown, {
@@ -27,4 +33,66 @@ String customDocumentToMarkdown(Document document) {
       const FileBlockNodeParser(),
     ],
   );
+}
+
+Future<String> documentToMarkdownFiles(Document document, String path) async {
+  final List<Future<File>> fileFutures = [];
+  final id = document.root.id;
+
+  /// create root Archive
+  final archive = Archive();
+  final fileName = p.basenameWithoutExtension(path);
+
+  /// create directory
+  final resourceDir = ArchiveFile('$id/', 0, null);
+  resourceDir.isFile = false;
+
+  final markdown = documentToMarkdown(
+    document,
+    customParsers: [
+      const MathEquationNodeParser(),
+      const CalloutNodeParser(),
+      const ToggleListNodeParser(),
+      CustomImageNodeFileParser(fileFutures, resourceDir.name),
+      CustomMultiImageNodeFileParser(fileFutures, resourceDir.name),
+      const SimpleTableNodeParser(),
+      const LinkPreviewNodeParser(),
+      const FileBlockNodeParser(),
+    ],
+  );
+
+  /// create resource directory
+  if (fileFutures.isNotEmpty) archive.addFile(resourceDir);
+
+  /// add markdown file to Archive
+  archive.addFile(ArchiveFile.string('$fileName-$id.md', markdown));
+
+  for (final fileFuture in fileFutures) {
+    final file = await fileFuture;
+    if (!file.existsSync()) continue;
+    final bytes = file.readAsBytesSync();
+    archive.addFile(
+      ArchiveFile(
+        p.join(resourceDir.name, p.basename(file.path)),
+        bytes.length,
+        bytes,
+      ),
+    );
+  }
+  if (archive.isNotEmpty) {
+    final zipEncoder = ZipEncoder();
+    final zip = zipEncoder.encode(archive);
+    if (zip != null) {
+      final zipFile = await File(path).writeAsBytes(zip);
+      if (Platform.isIOS) {
+        await Share.shareUri(zipFile.uri);
+        await zipFile.delete();
+      } else if (Platform.isAndroid) {
+        await Share.shareXFiles([XFile(zipFile.path)]);
+        await zipFile.delete();
+      }
+      Log.info('documentToMarkdownFiles to $path');
+    }
+  }
+  return markdown;
 }
