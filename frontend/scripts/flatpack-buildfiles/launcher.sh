6dc45c983071a1e6678e6aeda22a6323f73c5bda
Lucas <lucas.xu@appflowy.io>
2025-02-21 17:39:13 +0800

    feat: use flutter_distrubutor to build linux and macos packages (#7392)
    * feat: use flutter_distrubutor to build linux packages
    
    * feat: verify deb on Linux
    
    * chore: update rpm deps
    
    * chore: update codesign files
    
    * chore: update rpm make_config.yaml
    
    * chore: update release.yml
    
    * chore: update release.yml
    
    * chore: update feed url
    
    * chore: rename AppFlowy to appflowy
    
    * chore: update CHANGELOG.md (#7397)
    
    * chore: create release path if not exist
    
    * feat: support appimage
    
    * Revert "feat: support appimage"
    
    This reverts commit cb7dcf725c9443f5f9d0e3c3ceb4413a4f875e6a.
    
    * fix: cp deb/rpm error
    
    * feat: support appimage
    
    * chore: add linux build script
    
    * feat: add macos build script
    
    * feat: update linux scripts
    
    * chore: update linux scripts
    
    * chore: update relesae script
    
    * chore: update macos build scripts
    
    * chore: rename macOS package name
    
    * chore: add keychain in release.yaml
    
    * chore: update macos build steps in release.yaml
    
    * chore: update macos script desc
    
    * chore: remove sudo
    
    * feat: support tar.xz package type
    
    * feat: support tar.xz package type
    
    * chore: add fuse
    
    ---------
    
    Co-authored-by: Morn <agedchen@gmail.com>

--- frontend/scripts/flatpack-buildfiles/launcher.sh
+++ frontend/scripts/flatpack-buildfiles/launcher.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-gdbus call --session --dest io.appflowy.AppFlowy \
+gdbus call --session --dest io.appflowy.appflowy \
     --object-path /io/appflowy/AppFlowy/Object \
-    --method io.appflowy.AppFlowy.Open "['$1']" {}
+    --method io.appflowy.appflowy.Open "['$1']" {}
