7b32a92290be25c6ef5d2c1922056e8175ca2c91
Lucas <lucas.xu@appflowy.io>
2025-03-10 13:04:29 +0800

    fix: callout block build error (#7491)

--- frontend/appflowy_flutter/lib/plugins/document/presentation/editor_plugins/callout/callout_block_component.dart
+++ frontend/appflowy_flutter/lib/plugins/document/presentation/editor_plugins/callout/callout_block_component.dart
@@ -11,6 +11,7 @@ import 'package:easy_localization/easy_localization.dart' hide TextDirection;
 import 'package:flowy_infra/theme_extension.dart';
 import 'package:flowy_infra_ui/flowy_infra_ui.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:universal_platform/universal_platform.dart';
 
 import '../base/emoji_picker_button.dart';
