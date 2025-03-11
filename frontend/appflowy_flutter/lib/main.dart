2bc7eb4c8fd199d0a5a6b0ed4efc5176369b8307
LucasXu0 <lucas.xu@appflowy.io>
2025-03-11 15:53:13 +0800

    chore: revert change in main.dart

--- frontend/appflowy_flutter/lib/main.dart
+++ frontend/appflowy_flutter/lib/main.dart
@@ -1,41 +1,15 @@
-// The original content is temporarily commented out to allow generating a self-contained demo - feel free to uncomment later.
+import 'package:appflowy/generated/rust_bridge/frb_generated.dart';
+import 'package:scaled_app/scaled_app.dart';
 
-// import 'package:scaled_app/scaled_app.dart';
-//
-// import 'startup/startup.dart';
-//
-// Future<void> main() async {
-//   ScaledWidgetsFlutterBinding.ensureInitialized(
-//     scaleFactor: (_) => 1.0,
-//   );
-//
-//   await runAppFlowy();
-// }
-//
-
-import 'package:flutter/material.dart';
-import 'package:appflowy/src/rust/api/simple.dart';
-import 'package:appflowy/src/rust/frb_generated.dart';
+import 'startup/startup.dart';
 
 Future<void> main() async {
+  ScaledWidgetsFlutterBinding.ensureInitialized(
+    scaleFactor: (_) => 1.0,
+  );
+
+  // TODO(Lucas): Move it the task
   await RustLib.init();
-  runApp(const MyApp());
-}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('flutter_rust_bridge quickstart')),
-        body: Center(
-          child: Text(
-            'Action: Call Rust `greet("Tom")`\nResult: `${greet(name: "Tom")}`',
-          ),
-        ),
-      ),
-    );
-  }
+  await runAppFlowy();
 }
