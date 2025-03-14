be76d2459a8090c22d0fda16eefc314ef590c92b
Lucas.Xu <lucas.xu@appflowy.io>
2025-03-14 21:43:09 +0800

    fix: test

--- frontend/appflowy_flutter/lib/plugins/document/presentation/editor_plugins/undo_redo/custom_undo_redo_commands.dart
+++ frontend/appflowy_flutter/lib/plugins/document/presentation/editor_plugins/undo_redo/custom_undo_redo_commands.dart
@@ -1,6 +1,8 @@
 import 'package:appflowy/plugins/document/presentation/editor_notification.dart';
+import 'package:appflowy/plugins/document/presentation/editor_plugins/shared_context/shared_context.dart';
 import 'package:appflowy_editor/appflowy_editor.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 /// Undo
 ///
@@ -14,6 +16,15 @@ final CommandShortcutEvent customUndoCommand = CommandShortcutEvent(
   command: 'ctrl+z',
   macOSCommand: 'cmd+z',
   handler: (editorState) {
+    final context = editorState.document.root.context;
+    if (context == null) {
+      return KeyEventResult.ignored;
+    }
+    final editorContext = context.read<SharedEditorContext>();
+    if (editorContext.coverTitleFocusNode.hasFocus) {
+      return KeyEventResult.ignored;
+    }
+
     EditorNotification.undo().post();
     return KeyEventResult.handled;
   },
@@ -31,6 +42,15 @@ final CommandShortcutEvent customRedoCommand = CommandShortcutEvent(
   command: 'ctrl+y,ctrl+shift+z',
   macOSCommand: 'cmd+shift+z',
   handler: (editorState) {
+    final context = editorState.document.root.context;
+    if (context == null) {
+      return KeyEventResult.ignored;
+    }
+    final editorContext = context.read<SharedEditorContext>();
+    if (editorContext.coverTitleFocusNode.hasFocus) {
+      return KeyEventResult.ignored;
+    }
+
     EditorNotification.redo().post();
     return KeyEventResult.handled;
   },
