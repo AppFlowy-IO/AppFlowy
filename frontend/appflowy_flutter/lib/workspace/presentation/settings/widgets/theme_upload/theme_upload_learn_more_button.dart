86370a9e7fe8c2d6f4569153f0b666d1a5d3ef09
Lucas.Xu <lucas.xu@appflowy.io>
2025-04-07 15:08:46 +0800

    fix: use i18n for AppFlowy

--- frontend/appflowy_flutter/lib/workspace/presentation/settings/widgets/theme_upload/theme_upload_learn_more_button.dart
+++ frontend/appflowy_flutter/lib/workspace/presentation/settings/widgets/theme_upload/theme_upload_learn_more_button.dart
@@ -6,7 +6,6 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flowy_infra/theme_extension.dart';
 import 'package:flowy_infra_ui/flowy_infra_ui.dart';
 import 'package:flowy_infra_ui/widget/buttons/secondary_button.dart';
-import 'package:flowy_infra_ui/widget/error_page.dart';
 import 'package:flutter/material.dart';
 
 class ThemeUploadLearnMoreButton extends StatelessWidget {
