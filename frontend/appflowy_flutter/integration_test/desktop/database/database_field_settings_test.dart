21da9554e0b6fcbc3365541245e8c86bd1eacc1a
Richard Shiue <71320345+richardshiue@users.noreply.github.com>
2025-04-08 11:01:26 +0800

    test: grid width integration test

--- frontend/appflowy_flutter/integration_test/desktop/database/database_field_settings_test.dart
+++ frontend/appflowy_flutter/integration_test/desktop/database/database_field_settings_test.dart
@@ -66,5 +66,40 @@ void main() {
       await tester.tapDatabaseSortButton();
       await tester.tapCreateSortByFieldType(FieldType.RichText, "New field 1");
     });
+
+    testWidgets('field cell width', (tester) async {
+      await tester.initializeAppFlowy();
+      await tester.tapAnonymousSignInButton();
+
+      // create a database and add a linked database view
+      await tester.createNewPageWithNameUnderParent(layout: ViewLayoutPB.Grid);
+      await tester.tapCreateLinkedDatabaseViewButton(DatabaseLayoutPB.Grid);
+
+      // create a field
+      await tester.scrollToRight(find.byType(GridPage));
+      await tester.tapNewPropertyButton();
+      await tester.renameField('New field 1');
+      await tester.dismissFieldEditor();
+
+      // check the width of the field
+      expect(tester.getFieldWidth('New field 1'), 150);
+
+      // change the width of the field
+      await tester.changeFieldWidth('New field 1', 200);
+      expect(tester.getFieldWidth('New field 1'), 205);
+
+      // create another field, New field 1 to be same width
+      await tester.tapNewPropertyButton();
+      await tester.dismissFieldEditor();
+      expect(tester.getFieldWidth('New field 1'), 205);
+
+      // go back to inline database view, expect New field 1 to be 150px
+      await tester.tapTabBarLinkedViewByViewName('Untitled');
+      expect(tester.getFieldWidth('New field 1'), 150);
+
+      // go back to linked database view, expect New field 1 to be 205px
+      await tester.tapTabBarLinkedViewByViewName('Grid');
+      expect(tester.getFieldWidth('New field 1'), 205);
+    });
   });
 }
