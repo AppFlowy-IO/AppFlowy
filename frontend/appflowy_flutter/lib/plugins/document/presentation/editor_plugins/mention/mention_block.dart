7d0f6c9debd8dee526cf846e4ba276bd7361dc22
Richard Shiue <71320345+richardshiue@users.noreply.github.com>
2025-04-09 09:38:42 +0800

    fix(mobile): edit mention page (#7698)
    * fix(mobile): edit mention page
    
    * chore: type check
    
    * chore: replace usages
    
    * chore: use MentionType instead

--- frontend/appflowy_flutter/lib/plugins/document/presentation/editor_plugins/mention/mention_block.dart
+++ frontend/appflowy_flutter/lib/plugins/document/presentation/editor_plugins/mention/mention_block.dart
@@ -27,12 +27,12 @@ Node dateMentionNode() {
       operations: [
         TextInsert(
           MentionBlockKeys.mentionChar,
-          attributes: {
-            MentionBlockKeys.mention: {
-              MentionBlockKeys.type: MentionType.date.name,
-              MentionBlockKeys.date: DateTime.now().toIso8601String(),
-            },
-          },
+          attributes: MentionBlockKeys.buildMentionDateAttributes(
+            date: DateTime.now().toIso8601String(),
+            reminderId: null,
+            reminderOption: null,
+            includeTime: false,
+          ),
         ),
       ],
     ),
@@ -42,18 +42,51 @@ Node dateMentionNode() {
 class MentionBlockKeys {
   const MentionBlockKeys._();
 
-  static const reminderId = 'reminder_id'; // ReminderID
   static const mention = 'mention';
   static const type = 'type'; // MentionType, String
+
   static const pageId = 'page_id';
   static const blockId = 'block_id';
 
   // Related to Reminder and Date blocks
   static const date = 'date'; // Start Date
   static const includeTime = 'include_time';
+  static const reminderId = 'reminder_id'; // ReminderID
   static const reminderOption = 'reminder_option';
 
   static const mentionChar = '\$';
+
+  static Map<String, dynamic> buildMentionPageAttributes({
+    required MentionType mentionType,
+    required String pageId,
+    required String? blockId,
+  }) {
+    return {
+      MentionBlockKeys.mention: {
+        MentionBlockKeys.type: mentionType.name,
+        MentionBlockKeys.pageId: pageId,
+        if (blockId != null) MentionBlockKeys.blockId: blockId,
+      },
+    };
+  }
+
+  static Map<String, dynamic> buildMentionDateAttributes({
+    required String date,
+    required String? reminderId,
+    required String? reminderOption,
+    required bool includeTime,
+  }) {
+    return {
+      MentionBlockKeys.mention: {
+        MentionBlockKeys.type: MentionType.date.name,
+        MentionBlockKeys.date: date,
+        MentionBlockKeys.includeTime: includeTime,
+        if (reminderId != null) MentionBlockKeys.reminderId: reminderId,
+        if (reminderOption != null)
+          MentionBlockKeys.reminderOption: reminderOption,
+      },
+    };
+  }
 }
 
 class MentionBlock extends StatelessWidget {
