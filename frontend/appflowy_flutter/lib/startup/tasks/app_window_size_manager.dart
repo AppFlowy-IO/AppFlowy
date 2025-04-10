3c2d65a025d67a7a5d192ff0ee8e102b0b85eaec
Lucas.Xu <lucas.xu@appflowy.io>
2025-04-10 16:12:13 +0800

    feat: increase the default windows size

--- frontend/appflowy_flutter/lib/startup/tasks/app_window_size_manager.dart
+++ frontend/appflowy_flutter/lib/startup/tasks/app_window_size_manager.dart
@@ -8,9 +8,14 @@ import 'package:appflowy/startup/startup.dart';
 class WindowSizeManager {
   static const double minWindowHeight = 640.0;
   static const double minWindowWidth = 640.0;
+
   // Preventing failed assertion due to Texture Descriptor Validation
   static const double maxWindowHeight = 8192.0;
   static const double maxWindowWidth = 8192.0;
+
+  // Default windows size
+  static const double defaultWindowHeight = 960.0;
+  static const double defaultWindowWidth = 1280.0;
 
   static const double maxScaleFactor = 2.0;
   static const double minScaleFactor = 0.5;
@@ -36,8 +41,8 @@ class WindowSizeManager {
   Future<Size> getSize() async {
     final defaultWindowSize = jsonEncode(
       {
-        WindowSizeManager.height: minWindowHeight,
-        WindowSizeManager.width: minWindowWidth,
+        WindowSizeManager.height: defaultWindowHeight,
+        WindowSizeManager.width: defaultWindowWidth,
       },
     );
     final windowSize = await getIt<KeyValueStorage>().get(KVKeys.windowSize);
