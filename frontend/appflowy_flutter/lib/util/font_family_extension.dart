e7ecc072871c3699fb0a22380b75c07dd069a1c4
Lucas.Xu <lucas.xu@appflowy.io>
2025-05-07 11:32:18 +0800

    fix: google font family name issue

--- frontend/appflowy_flutter/lib/util/font_family_extension.dart
+++ frontend/appflowy_flutter/lib/util/font_family_extension.dart
@@ -1,4 +1,5 @@
 import 'package:appflowy/generated/locale_keys.g.dart';
+import 'package:appflowy/shared/google_fonts_extension.dart';
 import 'package:appflowy/shared/patterns/common_patterns.dart';
 import 'package:appflowy/workspace/application/settings/appearance/base_appearance.dart';
 import 'package:easy_localization/easy_localization.dart';
@@ -12,4 +13,11 @@ extension FontFamilyExtension on String {
   String get fontFamilyDisplayName => isEmpty || this == defaultFontFamily
       ? LocaleKeys.settings_appearance_fontFamily_defaultFont.tr()
       : parseFontFamilyName();
+
+  // the font display name is not the same as the font family name
+  // for example, the display name is "Noto Sans HK"
+  // the font family name is "NotoSansHK_Regular"
+  String get fontFamilyName => isEmpty || this == defaultFontFamily
+      ? defaultFontFamily
+      : getGoogleFontSafely(this).fontFamily ?? defaultFontFamily;
 }
