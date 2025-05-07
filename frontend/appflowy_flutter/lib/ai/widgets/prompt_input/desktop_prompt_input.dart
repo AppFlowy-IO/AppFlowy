4354227a0528947b8c87955947191df09b2c0083
Nathan <nathan@appflowy.io>
2025-05-07 14:31:08 +0800

    chore: enable select source when user is not anon

--- frontend/appflowy_flutter/lib/ai/widgets/prompt_input/desktop_prompt_input.dart
+++ frontend/appflowy_flutter/lib/ai/widgets/prompt_input/desktop_prompt_input.dart
@@ -1,5 +1,6 @@
 import 'package:appflowy/ai/ai.dart';
 import 'package:appflowy/plugins/ai_chat/application/chat_input_control_cubit.dart';
+import 'package:appflowy/plugins/ai_chat/application/chat_user_cubit.dart';
 import 'package:appflowy/plugins/ai_chat/presentation/layout_define.dart';
 import 'package:appflowy/startup/startup.dart';
 import 'package:appflowy/util/theme_extension.dart';
@@ -47,6 +48,7 @@ class _DesktopPromptInputState extends State<DesktopPromptInput> {
   final layerLink = LayerLink();
   final overlayController = OverlayPortalController();
   final inputControlCubit = ChatInputControlCubit();
+  final chatUserCubit = ChatUserCubit();
   final focusNode = FocusNode();
 
   late SendButtonState sendButtonState;
@@ -55,6 +57,7 @@ class _DesktopPromptInputState extends State<DesktopPromptInput> {
   @override
   void initState() {
     super.initState();
+    chatUserCubit.fetchUserProfile();
 
     widget.textController.addListener(handleTextControllerChanged);
     focusNode
@@ -88,13 +91,17 @@ class _DesktopPromptInputState extends State<DesktopPromptInput> {
     focusNode.dispose();
     widget.textController.removeListener(handleTextControllerChanged);
     inputControlCubit.close();
+    chatUserCubit.close();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider.value(
-      value: inputControlCubit,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider.value(value: inputControlCubit),
+        BlocProvider.value(value: chatUserCubit),
+      ],
       child: BlocListener<ChatInputControlCubit, ChatInputControlState>(
         listener: (context, state) {
           state.maybeWhen(
@@ -634,7 +641,9 @@ class _PromptBottomActions extends StatelessWidget {
 
               const Spacer(),
 
-              _selectSourcesButton(),
+              if (context.read<ChatUserCubit>().supportSelectSource())
+                _selectSourcesButton(),
+
               if (extraBottomActionButton != null) extraBottomActionButton!,
               // _mentionButton(context),
               if (state.supportChatWithFile) _attachmentButton(context),
