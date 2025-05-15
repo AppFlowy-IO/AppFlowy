b4e6ab36ab28c7bad7448986fc97c7372da98bb1
Lucas.Xu <lucas.xu@appflowy.io>
2025-05-15 15:08:27 +0800

    feat: integrate rust events

---
+++ frontend/appflowy_flutter/lib/features/share/data/rust_share_repository.dart
@@ -0,0 +1,133 @@
+import 'package:appflowy_backend/dispatch/dispatch.dart';
+import 'package:appflowy_backend/log.dart';
+import 'package:appflowy_backend/protobuf/flowy-error/errors.pb.dart';
+import 'package:appflowy_backend/protobuf/flowy-folder/protobuf.dart';
+import 'package:appflowy_result/appflowy_result.dart';
+
+import 'share_repository.dart';
+
+class RustShareRepository extends ShareRepository {
+  RustShareRepository();
+
+  @override
+  Future<FlowyResult<List<SharedUser>, FlowyError>> getUsersInSharedPage({
+    required String pageId,
+  }) async {
+    final request = GetSharedUsersPayloadPB(
+      viewId: pageId,
+    );
+    final result = await FolderEventGetSharedUsers(request).send();
+
+    return result.fold(
+      (success) {
+        Log.info('get shared users: $success');
+
+        return FlowySuccess(success.sharedUsers);
+      },
+      (failure) {
+        Log.error('getUsersInSharedPage: $failure');
+
+        return FlowyFailure(failure);
+      },
+    );
+  }
+
+  @override
+  Future<FlowyResult<void, FlowyError>> removeUserFromPage({
+    required String pageId,
+    required List<String> emails,
+  }) async {
+    final request = RemoveUserFromSharedPagePayloadPB(
+      viewId: pageId,
+      emails: emails,
+    );
+    final result = await FolderEventRemoveUserFromSharedPage(request).send();
+
+    return result.fold(
+      (success) {
+        Log.info('remove users($emails) from shared page($pageId)');
+
+        return FlowySuccess(success);
+      },
+      (failure) {
+        Log.error('removeUserFromPage: $failure');
+
+        return FlowyFailure(failure);
+      },
+    );
+  }
+
+  @override
+  Future<FlowyResult<void, FlowyError>> sharePageWithUser({
+    required String pageId,
+    required List<String> emails,
+    required ShareRole role,
+  }) async {
+    final request = SharePageWithUserPayloadPB(
+      viewId: pageId,
+      emails: emails,
+      accessLevel: role.accessLevel,
+    );
+    final result = await FolderEventSharePageWithUser(request).send();
+
+    return result.fold(
+      (success) {
+        Log.info('share page($pageId) with users($emails) with role($role)');
+
+        return FlowySuccess(success);
+      },
+      (failure) {
+        Log.error('sharePageWithUser: $failure');
+
+        return FlowyFailure(failure);
+      },
+    );
+  }
+}
+
+extension on RepeatedSharedUserPB {
+  List<SharedUser> get sharedUsers {
+    return items.map((e) => e.sharedUser).toList();
+  }
+}
+
+extension on SharedUserPB {
+  SharedUser get sharedUser {
+    return SharedUser(
+      email: email,
+      role: accessLevel.shareRole,
+    );
+  }
+}
+
+extension on AFAccessLevelPB {
+  ShareRole get shareRole {
+    switch (this) {
+      case AFAccessLevelPB.ReadOnly:
+        return ShareRole.readOnly;
+      case AFAccessLevelPB.ReadAndComment:
+        return ShareRole.readAndComment;
+      case AFAccessLevelPB.ReadAndWrite:
+        return ShareRole.readAndWrite;
+      case AFAccessLevelPB.FullAccess:
+        return ShareRole.fullAccess;
+      default:
+        throw Exception('Unknown share role: $this');
+    }
+  }
+}
+
+extension on ShareRole {
+  AFAccessLevelPB get accessLevel {
+    switch (this) {
+      case ShareRole.readOnly:
+        return AFAccessLevelPB.ReadOnly;
+      case ShareRole.readAndComment:
+        return AFAccessLevelPB.ReadAndComment;
+      case ShareRole.readAndWrite:
+        return AFAccessLevelPB.ReadAndWrite;
+      case ShareRole.fullAccess:
+        return AFAccessLevelPB.FullAccess;
+    }
+  }
+}
--- frontend/appflowy_flutter/lib/features/share/data/share_repository.dart
+++ frontend/appflowy_flutter/lib/features/share/data/share_repository.dart
@@ -1,3 +1,6 @@
+import 'package:appflowy_backend/protobuf/flowy-error/errors.pb.dart';
+import 'package:appflowy_result/appflowy_result.dart';
+
 /// Represents a user with a role on a shared page.
 class SharedUser {
   SharedUser({
@@ -12,34 +15,37 @@ class SharedUser {
 /// Abstract repository for sharing pages with users.
 abstract class ShareRepository {
   /// Gets the list of users and their roles for a shared page.
-  Future<List<SharedUser>> getUsersInSharedPage({
+  Future<FlowyResult<List<SharedUser>, FlowyError>> getUsersInSharedPage({
     required String pageId,
   });
 
   /// Removes a user from a shared page.
-  Future<void> removeUserFromPage({
+  Future<FlowyResult<void, FlowyError>> removeUserFromPage({
     required String pageId,
-    required String email,
+    required List<String> emails,
   });
 
   /// Shares a page with a user, assigning a role.
   ///
   /// If the user is already in the shared page, the role will be updated.
-  Future<void> sharePageWithUser({
+  Future<FlowyResult<void, FlowyError>> sharePageWithUser({
     required String pageId,
-    required String email,
     required ShareRole role,
+    required List<String> emails,
   });
 }
 
 /// The role a user can have on a shared page.
 enum ShareRole {
   /// Can view the page only.
-  canView,
+  readOnly,
 
-  /// Can edit the page.
-  canEdit,
+  /// Can read and comment on the page.
+  readAndComment,
 
-  /// Full access (edit, share, remove, etc.).
+  /// Can read and write to the page.
+  readAndWrite,
+
+  /// Full access (edit, share, remove, etc.) and can add new users.
   fullAccess,
 }
