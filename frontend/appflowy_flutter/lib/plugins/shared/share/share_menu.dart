b18fd5672a755636556333c57a882c2a9531b93b
Lucas.Xu <lucas.xu@appflowy.io>
2025-05-19 17:05:29 +0800

    fix: flutter compile

--- frontend/appflowy_flutter/lib/plugins/shared/share/share_menu.dart
+++ frontend/appflowy_flutter/lib/plugins/shared/share/share_menu.dart
@@ -1,9 +1,9 @@
+import 'package:appflowy/features/share_tab/presentation/share_tab.dart';
 import 'package:appflowy/generated/flowy_svgs.g.dart';
 import 'package:appflowy/generated/locale_keys.g.dart';
 import 'package:appflowy/mobile/presentation/home/tab/_round_underline_tab_indicator.dart';
 import 'package:appflowy/plugins/shared/share/export_tab.dart';
 import 'package:appflowy/plugins/shared/share/share_bloc.dart';
-import 'package:appflowy/plugins/shared/share/share_tab.dart';
 import 'package:appflowy_ui/appflowy_ui.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flowy_infra_ui/flowy_infra_ui.dart';
