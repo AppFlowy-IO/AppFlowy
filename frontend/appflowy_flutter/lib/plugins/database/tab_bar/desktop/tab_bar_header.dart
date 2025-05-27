7241ea02e3bd2275762ab4f3de41b1d1f19f6533
Richard Shiue <71320345+richardshiue@users.noreply.github.com>
2025-05-27 13:50:18 +0800

    fix: database view tooltip not displayed

--- frontend/appflowy_flutter/lib/plugins/database/tab_bar/desktop/tab_bar_header.dart
+++ frontend/appflowy_flutter/lib/plugins/database/tab_bar/desktop/tab_bar_header.dart
@@ -292,25 +292,29 @@ class _TabBarItemButtonState extends State<TabBarItemButton> {
         );
       },
       child: IntrinsicWidth(
-        child: FlowyButton(
-          radius: Corners.s6Border,
-          hoverColor: AFThemeExtension.of(context).greyHover,
-          onTap: () {
-            if (widget.isSelected) menuController.show();
-            widget.onTap.call();
-          },
-          margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 5),
-          onSecondaryTap: () {
-            menuController.show();
-          },
-          leftIcon: _buildViewIcon(),
-          text: FlowyText(
-            widget.view.nameOrDefault,
-            lineHeight: 1.0,
-            textAlign: TextAlign.center,
-            overflow: TextOverflow.ellipsis,
-            color: color,
-            fontWeight: widget.isSelected ? FontWeight.w500 : FontWeight.w400,
+        child: FlowyTooltip(
+          message: widget.view.nameOrDefault,
+          preferBelow: false,
+          child: FlowyButton(
+            radius: Corners.s6Border,
+            hoverColor: AFThemeExtension.of(context).greyHover,
+            onTap: () {
+              if (widget.isSelected) menuController.show();
+              widget.onTap.call();
+            },
+            margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 5),
+            onSecondaryTap: () {
+              menuController.show();
+            },
+            leftIcon: _buildViewIcon(),
+            text: FlowyText(
+              widget.view.nameOrDefault,
+              lineHeight: 1.0,
+              textAlign: TextAlign.center,
+              overflow: TextOverflow.ellipsis,
+              color: color,
+              fontWeight: widget.isSelected ? FontWeight.w500 : FontWeight.w400,
+            ),
           ),
         ),
       ),
