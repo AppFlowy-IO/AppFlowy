a8dda00fdac0c5a81b5ab64d02ece569d40d359e
Richard Shiue <71320345+richardshiue@users.noreply.github.com>
2025-05-30 15:59:24 +0800

    refactor: workspace icon

--- frontend/appflowy_flutter/lib/workspace/presentation/home/menu/sidebar/workspace/_sidebar_workspace_icon.dart
+++ frontend/appflowy_flutter/lib/workspace/presentation/home/menu/sidebar/workspace/_sidebar_workspace_icon.dart
@@ -1,39 +1,37 @@
 import 'package:appflowy/generated/locale_keys.g.dart';
 import 'package:appflowy/plugins/base/emoji/emoji_picker_screen.dart';
 import 'package:appflowy/shared/icon_emoji_picker/flowy_icon_emoji_picker.dart';
+import 'package:appflowy/shared/icon_emoji_picker/tab.dart';
 import 'package:appflowy/util/color_generator/color_generator.dart';
-import 'package:appflowy_backend/protobuf/flowy-user/user_profile.pb.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flowy_infra_ui/flowy_infra_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:universal_platform/universal_platform.dart';
 
-import '../../../../../../shared/icon_emoji_picker/tab.dart';
-
 class WorkspaceIcon extends StatefulWidget {
   const WorkspaceIcon({
     super.key,
-    required this.workspace,
-    required this.enableEdit,
+    required this.workspaceIcon,
+    required this.workspaceName,
     required this.iconSize,
+    required this.isEditable,
     required this.fontSize,
     required this.onSelected,
-    this.borderRadius = 4,
-    this.emojiSize,
-    this.alignment,
+    required this.borderRadius,
+    required this.emojiSize,
     required this.figmaLineHeight,
     this.showBorder = true,
   });
 
-  final UserWorkspacePB workspace;
+  final String workspaceIcon;
+  final String workspaceName;
   final double iconSize;
-  final bool enableEdit;
+  final bool isEditable;
   final double fontSize;
   final double? emojiSize;
   final void Function(EmojiIconData) onSelected;
   final double borderRadius;
-  final Alignment? alignment;
   final double figmaLineHeight;
   final bool showBorder;
 
@@ -46,20 +44,22 @@ class _WorkspaceIconState extends State<WorkspaceIcon> {
 
   @override
   Widget build(BuildContext context) {
-    final color = ColorGenerator(widget.workspace.name).randomColor();
-    Widget child = widget.workspace.icon.isNotEmpty
+    final (textColor, backgroundColor) =
+        ColorGenerator(widget.workspaceName).randomColor();
+
+    Widget child = widget.workspaceIcon.isNotEmpty
         ? FlowyText.emoji(
-            widget.workspace.icon,
+            widget.workspaceIcon,
             fontSize: widget.emojiSize,
             figmaLineHeight: widget.figmaLineHeight,
             optimizeEmojiAlign: true,
           )
         : FlowyText.semibold(
-            widget.workspace.name.isEmpty
+            widget.workspaceName.isEmpty
                 ? ''
-                : widget.workspace.name.substring(0, 1),
+                : widget.workspaceName.substring(0, 1),
             fontSize: widget.fontSize,
-            color: color.$1,
+            color: textColor,
           );
 
     child = Container(
@@ -67,18 +67,16 @@ class _WorkspaceIconState extends State<WorkspaceIcon> {
       width: widget.iconSize,
       height: widget.iconSize,
       decoration: BoxDecoration(
-        color: widget.workspace.icon.isNotEmpty ? null : color.$2,
+        color: widget.workspaceIcon.isEmpty ? backgroundColor : null,
         borderRadius: BorderRadius.circular(widget.borderRadius),
         border: widget.showBorder
-            ? Border.all(
-                color: const Color(0x1A717171),
-              )
+            ? Border.all(color: const Color(0x1A717171))
             : null,
       ),
       child: child,
     );
 
-    if (widget.enableEdit) {
+    if (widget.isEditable) {
       child = _buildEditableIcon(child);
     }
 
@@ -98,7 +96,9 @@ class _WorkspaceIconState extends State<WorkspaceIcon> {
           tabs: const [PickerTabType.emoji],
           onSelectedEmoji: (r) {
             widget.onSelected(r.data);
-            if (!r.keepOpen) controller.close();
+            if (!r.keepOpen) {
+              controller.close();
+            }
           },
         ),
         child: MouseRegion(
