4cd2aa364cf6310062d8d0c106104502276c4967
Madhuz02 <mahalakshmirl02@gmail.com>
2025-10-10 07:48:56 +0530

    New Update build.rs

--- frontend/rust-lib/flowy-error/build.rs
+++ frontend/rust-lib/flowy-error/build.rs
@@ -1,4 +1,12 @@
 fn main() {
-  #[cfg(feature = "dart")]
-  flowy_codegen::protobuf_file::dart_gen(env!("CARGO_PKG_NAME"));
+    #[cfg(feature = "dart")]
+    {
+        println!("Generating Dart protobuf code for package: {}", env!("CARGO_PKG_NAME"));
+        flowy_codegen::protobuf_file::dart_gen(env!("CARGO_PKG_NAME"));
+    }
+
+    #[cfg(not(feature = "dart"))]
+    {
+        println!("Dart feature not enabled; skipping code generation.");
+    }
 }
