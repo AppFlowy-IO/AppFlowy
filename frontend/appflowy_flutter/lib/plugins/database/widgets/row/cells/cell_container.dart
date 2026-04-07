07bfc82f836b9b883772060ab8d6852f5c693c84
Sayak Ghosh <ghoshsayakhelium@gmail.com>
2026-04-08 03:01:29 +0530

    keyboard arrow sorted

--- frontend/appflowy_flutter/lib/plugins/database/widgets/row/cells/cell_container.dart
+++ frontend/appflowy_flutter/lib/plugins/database/widgets/row/cells/cell_container.dart
@@ -1,5 +1,6 @@
 import 'package:flowy_infra/theme_extension.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:styled_widget/styled_widget.dart';
 
@@ -9,7 +10,7 @@ import '../../cell/editable_cell_builder.dart';
 import '../accessory/cell_accessory.dart';
 import '../accessory/cell_shortcuts.dart';
 
-class CellContainer extends StatelessWidget {
+class CellContainer extends StatefulWidget {
   const CellContainer({
     super.key,
     required this.child,
@@ -24,42 +25,92 @@ class CellContainer extends StatelessWidget {
   final bool isPrimary;
 
   @override
+  State<CellContainer> createState() => _CellContainerState();
+}
+
+class _CellContainerState extends State<CellContainer> {
+  late final FocusNode _focusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode = FocusNode();
+    _focusNode.addListener(_onFocusChange);
+  }
+
+  @override
+  void dispose() {
+    _focusNode.removeListener(_onFocusChange);
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  void _onFocusChange() {
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider.value(
-      value: child.cellContainerNotifier,
+      value: widget.child.cellContainerNotifier,
       child: Selector<CellContainerNotifier, bool>(
         selector: (context, notifier) => notifier.isFocus,
-        builder: (providerContext, isFocus, _) {
-          Widget container = Center(child: GridCellShortcuts(child: child));
+        builder: (providerContext, isChildFocus, _) {
+          Widget container = Center(child: GridCellShortcuts(child: widget.child));
 
-          if (accessoryBuilder != null) {
-            final accessories = accessoryBuilder!.call(
+          if (widget.accessoryBuilder != null) {
+            final accessories = widget.accessoryBuilder!.call(
               GridCellAccessoryBuildContext(
                 anchorContext: context,
-                isCellEditing: isFocus,
+                isCellEditing: isChildFocus,
               ),
             );
 
             if (accessories.isNotEmpty) {
               container = _GridCellEnterRegion(
                 accessories: accessories,
-                isPrimary: isPrimary,
+                isPrimary: widget.isPrimary,
                 child: container,
               );
             }
           }
 
-          return GestureDetector(
-            behavior: HitTestBehavior.opaque,
-            onTap: () {
-              if (!isFocus) {
-                child.requestFocus.notify();
+          final isSelectedOrEditing = isChildFocus || _focusNode.hasFocus;
+
+          return Focus(
+            focusNode: _focusNode,
+            onKeyEvent: (node, event) {
+              if (event is KeyDownEvent) {
+                if (event.logicalKey == LogicalKeyboardKey.enter) {
+                  widget.child.requestFocus.notify();
+                  return KeyEventResult.handled;
+                }
               }
+              return KeyEventResult.ignored;
             },
-            child: Container(
-              constraints: BoxConstraints(maxWidth: width, minHeight: 32),
-              decoration: _makeBoxDecoration(context, isFocus),
-              child: container,
+            child: GestureDetector(
+              behavior: HitTestBehavior.opaque,
+              onTap: () {
+                if (!isChildFocus) {
+                  if (!_focusNode.hasFocus) {
+                    _focusNode.requestFocus();
+                  } else {
+                    widget.child.requestFocus.notify();
+                  }
+                }
+              },
+              onDoubleTap: () {
+                if (!isChildFocus) {
+                  widget.child.requestFocus.notify();
+                }
+              },
+              child: Container(
+                constraints: BoxConstraints(maxWidth: widget.width, minHeight: 32),
+                decoration: _makeBoxDecoration(context, isSelectedOrEditing),
+                child: container,
+              ),
             ),
           );
         },
@@ -71,6 +122,7 @@ class CellContainer extends StatelessWidget {
     if (isFocus) {
       final borderSide = BorderSide(
         color: Theme.of(context).colorScheme.primary,
+        width: 1.5,
       );
 
       return BoxDecoration(border: Border.fromBorderSide(borderSide));
