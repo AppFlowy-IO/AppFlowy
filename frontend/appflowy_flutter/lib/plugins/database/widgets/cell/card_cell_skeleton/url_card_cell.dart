6bf78219aceccfff2736827c56ff388c068ad4d6
lovely90133 <xinlew666@gmail.com>
2026-04-14 00:34:27 +0800

    feat(数据库): 为URL卡片单元格添加点击打开链接功能
    在桌面端支持通过Ctrl/Cmd+点击打开URL链接，移动端直接点击即可打开。添加了键盘事件监听和鼠标指针状态变化，提升用户体验。

--- frontend/appflowy_flutter/lib/plugins/database/widgets/cell/card_cell_skeleton/url_card_cell.dart
+++ frontend/appflowy_flutter/lib/plugins/database/widgets/cell/card_cell_skeleton/url_card_cell.dart
@@ -4,7 +4,10 @@ import 'package:appflowy/plugins/database/application/database_controller.dart';
 import 'package:appflowy/plugins/database/application/cell/bloc/url_cell_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter/services.dart';
+import 'package:universal_platform/universal_platform.dart';
 
+import '../editable_cell_skeleton/url.dart';
 import 'card_cell.dart';
 
 class URLCardCellStyle extends CardCellStyle {
@@ -32,6 +35,34 @@ class URLCardCell extends CardCell<URLCardCellStyle> {
 }
 
 class _URLCellState extends State<URLCardCell> {
+  bool isLinkClickable = false;
+
+  @override
+  void initState() {
+    super.initState();
+    if (UniversalPlatform.isDesktop) {
+      HardwareKeyboard.instance.addHandler(_handleGlobalKeyEvent);
+    }
+  }
+
+  @override
+  void dispose() {
+    if (UniversalPlatform.isDesktop) {
+      HardwareKeyboard.instance.removeHandler(_handleGlobalKeyEvent);
+    }
+    super.dispose();
+  }
+
+  bool _handleGlobalKeyEvent(KeyEvent event) {
+    final keyboard = HardwareKeyboard.instance;
+    final canOpenLink = event is KeyDownEvent &&
+        (keyboard.isControlPressed || keyboard.isMetaPressed);
+    if (canOpenLink != isLinkClickable) {
+      setState(() => isLinkClickable = canOpenLink);
+    }
+    return false;
+  }
+
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
@@ -49,12 +80,28 @@ class _URLCellState extends State<URLCardCell> {
           if (state.content.isEmpty) {
             return const SizedBox.shrink();
           }
-          return Container(
-            alignment: AlignmentDirectional.centerStart,
-            padding: widget.style.padding,
-            child: Text(
-              state.content,
-              style: widget.style.textStyle,
+          return GestureDetector(
+            onTap: () {
+              if (UniversalPlatform.isDesktop) {
+                if (isLinkClickable) {
+                  openUrlCellLink(state.content);
+                }
+              } else {
+                openUrlCellLink(state.content);
+              }
+            },
+            child: MouseRegion(
+              cursor: isLinkClickable || UniversalPlatform.isMobile
+                  ? SystemMouseCursors.click
+                  : SystemMouseCursors.basic,
+              child: Container(
+                alignment: AlignmentDirectional.centerStart,
+                padding: widget.style.padding,
+                child: Text(
+                  state.content,
+                  style: widget.style.textStyle,
+                ),
+              ),
             ),
           );
         },
